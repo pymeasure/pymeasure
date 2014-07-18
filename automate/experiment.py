8f9091ab12085a6969f9273ba46237c29cf1bd3d
Colin Jermain <cjermain@gmail.com>
2014-07-17 23:46:26 -0400

    Moved Qt based Experiment/Parameter classes to extras and added more generic classes

--- automate/experiment.py
+++ automate/experiment.py
@@ -1,171 +1,277 @@
-from pyqtgraph.Qt import QtCore
-from PyQt4 import QtGui, uic
-from parameter import Parameter
-import numpy as np
+from threading import Event, Queue
 
-class Experiment(QtCore.QThread):
-	"""This is the base class for running actual experiments. We must define
-	init(), exit(), and run() below, making sure to retain the structure and 
-	signaling of the run() function. All other behaviors should be taken care
-	of automatically, including automatic shutdown in the case of thread 
-	termination or the window being closed, etc. Data storage here is 
-	ephemeral --- only the most recent values are stored, and it is left to
-	the plotting and logging classes to ensure persistance."""
-	
-	# Signals must be defined here
-	newData       = QtCore.pyqtSignal()
-	newTrace      = QtCore.pyqtSignal()
-	newLog        = QtCore.pyqtSignal(str)
-	aborted       = QtCore.pyqtSignal()
-	finishedTrace = QtCore.pyqtSignal()
+class Parameter(object):
+    """ Encapsulates the information for an experiment parameter
+    with information about the name, and unit if supplied.
+    """
 
-	def __init__(self, parent=None):
-		super(Experiment, self).__init__()
+    def __init__(self, name, unit=None, default=None):
+        self.name = name
+        self._value = default
+        self.unit = unit
+        self.default = default
+        
+    @property
+    def value(self):
+        if self.isSet():
+            return self._value
+        else:
+            raise ValueError("Parameter value is not set")
+            
+    @value.setter
+    def value(self, value):
+        self._value = value
+        
+    def isSet(self):
+        return self.value is not None
+        
+    def __repr__(self):
+        result = "<Parameter(name='%s'"
+        if self.isSet():
+            result += ",value='%s'" % repr(self.value)
+        if self.unit:
+            result += ",unit='%s'" % self.unit
+        return result + ")>"
 
-		# Thread control
-		self.exiting           = False 
-		self.shutdownCompleted = False
-		
-		self.data        = []
-		self.instruments = []
-	
-	def initParameters(self, params):
-		"""Establish the parameters for a measurement run"""
-		self.params = params
-	def initInstruments(self):
-		"""Bring instrumentation online"""
-		pass
+class IntegerParameter(Parameter):
+    
+    @property
+    def value(self):
+        if self.isSet():
+            return int(self._value)
+        else:
+            raise ValueError("Parameter value is not set")
+        
+    @value.setter
+    def value(self, value):
+        try:
+            self._value = int(value)
+        except ValueError:
+            raise ValueError("IntegerParameter given non-integer value of "
+                             "type '%s'" % type(value))
+                             
+    def __repr__(self):
+        result = super(IntegerParameter, self).__repr__()
+        return result.replace("<Parameter", "<IntegerParameter", 1)
 
-	def getData(self):
-		return self.data
-	
-	# Terminating the measurement thread
-	def __del__(self):
-		self.exiting = True
-		self.wait()
-	def stop(self):
-		self.__del__()
-	def exit(self):
-		# Should be called in the event of an intentional or inadvertant shutdown
-		self.exiting = True
-		if not self.shuttingDownInstruments:
-			# Prevent this from somehow being called twice...
-			self.shuttingDownInstruments = True
-			self.newLog.emit("<b>Shutting Down Measurement</b>")
-			for instr in self.instruments:
-				instr.shutdown()
-			self.newLog.emit("Done shutting down")
+class FloatParameter(Parameter):
+    
+    @property
+    def value(self):
+        if self.isSet():
+            return float(self._value)
+        else:
+            raise ValueError("Parameter value is not set")
+    
+    @value.setter
+    def value(self, value):
+        try:
+            self._value = float(value)
+        except ValueError:
+            raise ValueError("FloatParameter given non-float value of "
+                             "type '%s'" % type(value))
+    
+    def __repr__(self):
+        result = super(IntegerParameter, self).__repr__()
+        return result.replace("<Parameter", "<FloatParameter", 1)
 
-	# Running the actual measurement
-	def run(self):
-		"""Perform the measurement"""
-		try:
-			self.exiting = False
-			print "Running!"
-			self.init()
-			i = 0
-			while not self.exiting and i < 5:
-				num = np.random.random()
-				self.latestData = [num, num**2]
-				self.data.append([num, num**2])
-				self.newData.emit()
-				time.sleep(0.2)
-				i += 1
-		except Exception, ex:
-			logging.exception("Exception occured in measurement loop")
-		self.finished.emit()
-		self.exit()
-		return
 
-class ExperimentGUI(QtGui.QMainWindow):
-	"""This is the main window of the experiment. We pass it a subclass of Experiment
-	that will be run/stopped as desired from this window."""
-	def __init__(self, name, uiForm, parent=None):		
-		super(ExperimentGUI, self).__init__()
+class Procedure(object):
+    """Provides the base class of a procedure to organize the experiment
+    execution. Procedures should be run in ProcedureThreads to ensure that
+    concurrent control is properly managed. Example:
+    
+    procedure = Procedure()
+    thread = ProcedureThread() # or QProcedureThread()
+    thread.load(procedure)
+    thread.start()
+        
+    Inhereting classes should define the following methods:
+    - enter --> commands needed to startup instruments
+    - execute --> commands for procedure execution
+    - exit --> commands needed to stop instruments
+    
+    The exit method is called on sucessful completion, software error, or
+    abort.    
+    """
+    
+    def __init__(self):
+        self._updateParameters()
+    
+    def _updateParameters(self):
+        """ Collects all the Parameter objects for this procedure and stores
+        them in a meta dictionary so that the actual values can be set in their
+        stead
+        """
+        if not self._parameters:
+            self._parameters = {}
+        for item in dir(self):
+            parameter = getattr(self, item)
+            if isinstance(parameter, Parameter):
+                self._parameters[item] = parameter
+                if parameter.isSet():
+                    setattr(self, item, parameter.value)
+                else:
+                    delattr(self, item)
+    
+    def parametersAreSet(self):
+        """ Returns True if all parameters are set """
+        for name, parameter in self._parameters.iteritems():
+            if not hasattr(self, name):
+                return False
+        return True
+    
+    def checkParameters(self, function):
+        """ Raises an exception if any parameter is missing before calling
+        the associated function. Ensures that each value can be set and
+        got, which should cast it into the right format. Used as a decorator 
+        @checkParameters on the enter method
+        """
+        for name, parameter in self._parameters.iteritems():
+            if not hasattr(self, name):
+                raise NameError("Missing %s '%s' in %s" % (
+                    self.parameter.__class__, name, self.__class__))
+            else:
+                parameter.value = getattr(self, name)
+                setattr(self, name, parameter.value)
+    
+    def parameterValues(self):
+        """ Returns a dictionary of all the parameters and grabs any current
+        values that are not in the default definitions
+        """
+        result = {}
+        for name, parameter in self._parameters.iteritems():
+            if not hasattr(self, name):
+                result[name] = parameter
+            else:
+                parameter.value = getattr(self, name)
+                result[name] = parameter.value)
+        return result
+    
+    @checkParameters   
+    def enter(self):
+        pass
+        
+    def execute(self):
+        pass        
+        
+    def exit(self):
+        pass
 
-		self.setAttribute(QtCore.Qt.WA_DeleteOnClose)
 
-		# Load the UI file
-		self.ui = uic.loadUi(uiForm, self)
-		self.setWindowTitle(name)
-		# For logging
-		self.log = ''
+class ProcedureThread(Thread):
+    """Encapsulates the Procedure to be run within a thread."""
+    
+    def __init__(self):
+        Thread.__init__(self)
+        self.procedure = None
+        self.abortEvent = Event()
+        self.abortEvent.clear()
+        self.dataQueues = []
+        self.logQueue = Queue()
+        self.progressQueue = Queue()
+        self.finished = Event()
+        
+    def load(self, procedure):
+        if not isinstance(procedure, Procedure):
+            raise ValueError("Loading object must be inhereted from the"
+                             " Procedure class")
+        self.procedure = procedure
+        self.procedure.hasAborted = self.hasAborted
+        self.procedure.emitData = self.emitData
+        self.procedure.emitProgress = self.emitProgress
+        self.procedure.log = self.emitLog
+        
+    def run(self):
+        if self.procedure is None:
+            raise Exception("Attempting to run Procedure object before loading")
+        self.procedure.enter()
+        try:
+            self.procedure.execute()
+        except:
+            import sys, traceback
+            traceback.print_exc(file=sys.stdout)
+        finally:
+            self.procedure.exit()
+            self.finished.set()
+                  
+    def emitProgress(self, precent):
+        self.progressQueue.put(precent)
+    
+    def emitFinished(self):
+        self.finished.set()
+    
+    def addDataQueue(self, queue):
+        self.dataQueues.append(queue)
+            
+    def emitData(self, data):
+        for queue in self.dataQueues:
+            queue.put(data)
+            
+    def log(self, message):
+        self.logQueue.put(message)
+    
+    def hasAborted(self):
+        return self.abortEvent.isSet()
+        
+    def abort(self, timeout=0):
+        self.abortEvent.wait(timeout)
+        if not self.abortEvent.isSet():
+            self.abortEvent.set()
+        super(ProcedureThread, self).join()
 
-		# Load the previous user settings
-		self.settings = QtCore.QSettings("Cornell University", name)
-		self.runningExperiment = False
+try:
 
-	def closeEvent(self, ce):
-		self.fileQuit()
+    from PyQt4.QtCore import QThread, pyqtSignal
 
-	def fileQuit(self):
-		if self.runningExperiment:
-			self.expThread.stop()
-			self.runningExperiment = False 
-		self.close()
+    class QProcedureThread(QThread):
+        """Encapsulates the Procedure to be run within a QThread,
+        compatible with PyQt4.
+        """
+        
+        data = pyqtSignal(dict) 
+        progress = pyqtSignal(float)
+        log = pyqtSignal(str)
+        finished = pyqtSignal()
+        
+        def __init__(self, parent):
+            QThread.__init__(self, parent)
+            self.procedure = None
+            self.settings = settings
+            self.abortEvent = Event()
+            self.abortEvent.clear()
+        
+        def load(self, procedure):
+            if not isinstance(procedure, Procedure):
+                raise ValueError("Loading object must be inhereted from the"
+                                 " Procedure class")
+            self.procedure = procedure
+            self.procedure.hasAborted = self.hasAborted
+            self.procedure.emitData = self.data.emit
+            self.procedure.emitProgress = self.progress.emit
+            self.procedure.log = self.log.emit
+            
+        def run(self):
+            if self.procedure is None:
+                raise Exception("Attempting to run Procedure object before loading")
+            self.procedure.enter()
+            try:
+                self.procedure.execute()
+            except:
+                import sys, traceback
+                traceback.print_exc(file=sys.stdout)
+            finally:
+                self.procedure.exit()
+                self.finished.emit()
+        
+        def hasAborted(self):
+            return self.abortEvent.isSet()
+            
+        def abort(self, timeout=0):
+            self.abortEvent.wait(timeout)
+            if not self.abortEvent.isSet():
+                self.abortEvent.set()
+            super(QProcedureThread, self).wait()
 
-	def settingsDict(self, parent, unitMappings=None):
-		"""Packs current settings into a dictionary for objects that are children of
-		the specified object. UnitMappings is a dictionary containing multiplicative
-		factors to be multiplied by the values found in the actual input fields.
 
-		e.g. a QDoubleSpinBox called currentmA would warrent
-		unitMappings = {'currentmA': 1.0e-3}
-		"""
-		getters = {QtGui.QCheckBox:      "isChecked",
-				   QtGui.QComboBox:      "currentText",
-				   QtGui.QDoubleSpinBox: "value",
-				   QtGui.QSpinBox:       "value",
-				   QtGui.QLineEdit:      "text",
-				   QtGui.QRadioButton:   "isChecked",
-				   Parameter:            "value",}
 
-		d = {}
-		for fieldType, method in getters.iteritems():
-			for child in parent.findChildren(fieldType, QtCore.QRegExp("^(?!qt_spinbox_lineedit).*$")):
-				value = getattr(child, method)()
-				cname = str(child.objectName())
-				if isinstance(value, QtCore.QString):
-					value = str(value)
-				if unitMappings is not None and (cname in unitMappings):
-					d[cname] = value*unitMappings[cname]
-				if isinstance(child, Parameter):
-					# Also store the unit since it's available
-					d[cname+"Unit"] = child.getUnit()
-					d[cname] = value
-				else:
-					d[cname] = value
-		return d
-
-	def storeSettings(self):
-		"""Saves the values in the interface using qsettings"""
-		getters = {QtGui.QCheckBox:      ("QCheckBox", "isChecked", bool),
-				   QtGui.QComboBox:      ("QComboBox", "currentIndex", int),
-				   QtGui.QDoubleSpinBox: ("QDoubleSpinBox", "value", float),
-				   QtGui.QSpinBox:       ("QSpinBox", "value", int),
-				   QtGui.QLineEdit:      ("QLineEdit", "text", str),
-				   QtGui.QRadioButton:   ("QRadioButton", "isChecked", bool),
-				   Parameter:            ("Parameter", "value", float),}
-
-		for fieldType, info in getters.iteritems():
-			for child in self.ui.findChildren(fieldType, QtCore.QRegExp("^(?!qt_spinbox_lineedit).*$")):
-				self.settings.setValue(info[0]+"/"+child.objectName(), getattr(child, info[1])())
-
-	def loadSettings(self):
-		"""Restored saved settings using qsettings"""
-		setters = {QtGui.QCheckBox:      ("QCheckBox", "setChecked", "toBool"),
-				   QtGui.QComboBox:      ("QComboBox", "setCurrentIndex", "toInt"),
-				   QtGui.QDoubleSpinBox: ("QDoubleSpinbox", "setValue", "toFloat"),
-				   QtGui.QSpinBox:       ("QSpinBox", "setValue", "toInt"),
-				   QtGui.QLineEdit:      ("QLineEdit", "setText", "toString"),
-				   QtGui.QRadioButton:   ("QRadioButton", "setChecked", "toBool"),
-				   Parameter:            ("Parameter", "setValue", "toFloat"),}
-
-		for fieldType, info in setters.iteritems():
-			self.settings.beginGroup(info[0])
-			for key in self.settings.childKeys():
-				value = getattr(self.settings.value(key), info[2])()
-				if isinstance(value, tuple):
-					value = value[0]
-				getattr(self.ui.findChildren(fieldType, key)[0], info[1])(value)
-			self.settings.endGroup()
