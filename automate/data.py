0769d4897a030f35994d04a01502e2c55ded7a61
Colin Jermain <cjermain@gmail.com>
2014-07-18 13:46:47 -0400

    Fixed uniqueFilename method

--- automate/data.py
+++ automate/data.py
@@ -10,13 +10,13 @@ from os.path import exists, basename
 from datetime import datetime
 import re
 
-def uniqueFilename(directory, prefix='DATA', ext='csv'):
+def uniqueFilename(directory, prefix='DATA', suffix='', ext='csv'):
     """ Returns a unique filename based on the directory and prefix
     """
     date = datetime.now().strftime("%Y%m%d")
     i = 1
-    filename = "%s%s%s_%d.%s" % (directory, prefix, date, i, ext)
+    filename = "%s%s%s_%d%s.%s" % (directory, prefix, date, i, suffix, ext)
     while exists(filename):
         i += 1
-        filename = "%s%s%s_%d.%s" % (directory, prefix, date, i, ext)  
+        filename = "%s%s%s_%d%s.%s" % (directory, prefix, date, i, suffix, ext)  
     return filename
