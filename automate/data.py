ef5ac7ddb24be5a83e7ab8149e03c8571fa27916
Colin Jermain <cjermain@gmail.com>
2014-07-19 22:54:29 -0400

    Added Results object to data which uses the pandas package to handle data, tested for 100,000+ samples without breaking

--- automate/data.py
+++ automate/data.py
@@ -10,6 +10,9 @@ from automate.experiment import Procedure
 from os.path import exists, basename
 from datetime import datetime
 import re
+import numpy as np
+import pandas as pd
+from os.path import exists
 
 def uniqueFilename(directory, prefix='DATA', suffix='', ext='csv'):
     """ Returns a unique filename based on the directory and prefix
@@ -21,8 +24,8 @@ def uniqueFilename(directory, prefix='DATA', suffix='', ext='csv'):
         i += 1
         filename = "%s%s%s_%d%s.%s" % (directory, prefix, date, i, suffix, ext)  
     return filename
-    
 
+    
 class Results(object):
     """ Provides a base class for experiment results tracking, which should be
     extended for the specific data/logs collected for a Procedure
@@ -31,13 +34,24 @@ class Results(object):
     COMMENT = '#'
     DELIMITER = ','
     LINE_BREAK = "\n"
+    CHUNK_SIZE = 1000
 
-    def __init__(self, procedure):
+    def __init__(self, procedure, data_filename):
         if not isinstance(procedure, Procedure):
             raise ValueError("Results require a Procedure object")
         self.procedure = procedure
-        self.procedure_class = procedure.__class__        
+        self.procedure_class = procedure.__class__
         self.parameters = procedure.parameterObjects()
+        self._header_count = -1
+        
+        self.data_filename = data_filename
+        if exists(data_filename): # Assume header is already written
+            self._data = pd.read_csv(data_filename, comment=Results.COMMENT)
+        else:
+            with open(data_filename, 'w') as f:
+                f.write(self.header())
+            self._data = None
+        
         
     def header(self):
         """ Returns a text header to accompany a datafile so that the procedure
@@ -52,19 +66,20 @@ class Results(object):
             if parameter.unit:
                 h[-1] += " %s" % parameter.unit
         h.append("Data:")
+        self._header_count = len(h)
         h = [Results.COMMENT + l for l in h] # Comment each line
-        return Results.LINE_BREAK.join(h)
+        return Results.LINE_BREAK.join(h) + Results.LINE_BREAK
         
     @staticmethod
     def parseHeader(header):
         """ Returns a Procedure object with the parameters as defined in the
         header text.
         """
-        h = header.split(Results.LINE_BREAK)
+        header = header.split(Results.LINE_BREAK)
         procedure_module = None
         procedure_class = None
         parameters = {}
-        for line in h:
+        for line in header:
             if line.startswith(Results.COMMENT):
                 line = line[1:] # Uncomment
             else:
@@ -91,8 +106,50 @@ class Results(object):
             else:
                 raise Exception("Missing '%s' parameter when loading '%s' class" % (
                         parameter.name, procedure_class))
-        procedure.parameterObjects()
+        procedure.parameterObjects() # Enforce update of meta data
         return procedure
                 
-        
+    @staticmethod
+    def load(data_filename):
+        """ Returns a Results object with the associated Procedure object and
+        data
+        """
+        header = ""
+        header_read = False
+        header_count = 0
+        with open(data_filename, 'r') as f:
+            while not header_read:
+                line = f.readline()
+                if line.startswith(Results.COMMENT):
+                    header += line + Results.LINE_BREAK
+                    header_count += 1
+                else:
+                    header_read = True
+        procedure = Results.parseHeader(header)
+        results = Results(procedure, data_filename)
+        results._header_count = header_count
+        return results
     
+    @property
+    def data(self):
+        if self._header_count == -1: # Need to update header count for correct referencing
+            self._header_count = len(self.header()[-1].split(Results.LINE_BREAK))
+        if self._data is None or len(self._data) == 0: # Data has not been read
+            try:
+                self._data = pd.read_csv(self.data_filename, comment=Results.COMMENT)
+            except:
+                raise Exception("The data file is currently empty")
+        else: # Concatenate additional data
+            skiprows = len(self._data) + self._header_count
+            chunks = pd.read_csv(self.data_filename, comment=Results.COMMENT, header=0,
+                        names=self._data.columns,
+                        chunksize=Results.CHUNK_SIZE, skiprows=skiprows, iterator=True)
+            try:
+                tmp_frame = pd.concat(chunks, ignore_index=True)
+                self._data = pd.concat([self._data, tmp_frame], ignore_index=True)
+            except:
+                pass # All data is up to date
+        return self._data
+            
+
+
