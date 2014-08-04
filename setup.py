11fe5c1908404c43022a397c155348239765374a
Colin Jermain <cjermain@gmail.com>
2014-08-04 15:10:47 -0400

    Bump version of 0.1.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='Automate',
-    version='0.1.4',
+    version='0.1.5',
     author='Colin Jermain',
     author_email='clj72@cornell.edu',
     packages=['automate', 'automate.instruments', 'automate.composites'],
