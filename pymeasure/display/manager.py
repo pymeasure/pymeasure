266ed0d5af56982912d53eadcaeb957fe19ed772
Colin Jermain <cjermain@gmail.com>
2015-11-19 23:36:40 -0500

    Rewrite of Manager with new ZMQ communication

--- pymeasure/display/manager.py
+++ pymeasure/display/manager.py
@@ -23,9 +23,73 @@
 #
 
 from pymeasure.experiment import Procedure
-from pymeasure.display.procedure_process import QProcedureProcess
-from pymeasure.display.listeners import QResultsWriter
+from pymeasure.experiment.workers import ProcedureWorker
+from pymeasure.experiment.listeners import ResultsWriter
+from pymeasure.display.listeners import ProcedureMonitor
 from .qt_variant import QtCore
+
+
+class Experiment(QtCore.QObject):
+    """ The Experiment class helps group the :class:`.Procedure`,
+    :class:`.Results`, and their display functionality. Its function
+    is only a convenient container.
+
+    :param procedure: :class:`.Procedure` object
+    :param results: :class:`.Results` object
+    :param curve: :class:`.ResultsCurve` object
+    :param browser_item: :class:`.BrowserItem` object
+    """
+
+    def __init__(self, results, curve, browser_item, parent=None):
+        super(Experiment, self).__init__(self, parent=parent)
+        self.results = results
+        self.data_filename = self.results.data_filename
+        self.procedure = self.results.procedure
+        self.curve = curve
+        self.browser_item = browser_item
+
+
+class ExperimentQueue(QtCore.QObject):
+    """ Represents a Queue of Experiments and allows queries to
+    be easily preformed
+    """
+
+    def __init__(self):
+        self.queue = []
+
+    def append(self, experiment):
+        self.queue.append(experiment)
+
+    def remove(self, experiment):
+        if experiment not in self.queue:
+            raise Exception("Attempting to remove an Experiment that is "
+                            "not in the ExperimentQueue")
+        else:
+            if experiment.procedure.status == Procedure.RUNNING:
+                raise Exception("Attempting to remove a running experiment")
+            else:
+                self.queue.pop(self.queue.index(experiment))
+
+    def __contains__(self, value):
+        if isinstance(value, Experiment):
+            return value in self.queue
+
+    def next(self):
+        """ Returns the next experiment on the queue
+        """
+        for experiment in self.queue:
+            if experiment.procedure.status == Procedure.QUEUED:
+                return experiment
+        raise Exception("There are no queued experiments")
+
+    def has_next(self):
+        """ Returns True if another item is on the queue
+        """
+        try:
+            self.next()
+        except:
+            return False
+        return True
 
 
 class Manager(QtCore.QObject):
@@ -35,10 +99,8 @@ class Manager(QtCore.QObject):
     and a PyQtGraph `PlotItem` within the user interface, which are updated
     in accordance with the execution status of the Experiments.
     """
-    experiments = []
     _is_continuous = True
     _start_on_add = True
-    _running_process = None
     queued = QtCore.QSignal(object)
     running = QtCore.QSignal(object)
     finished = QtCore.QSignal(object)
@@ -46,43 +108,60 @@ class Manager(QtCore.QObject):
     aborted = QtCore.QSignal(object)
     abort_returned = QtCore.QSignal(object)
 
-    def __init__(self, plot, browser, parent=None):
+    def __init__(self, plot, browser, port=5558, parent=None):
         super(Manager, self).__init__(parent=parent)
+
+        self.experiments = ExperimentQueue()
+        self._worker = None
+        self._writer = None
+        self._running_experiment = None
+        
         self.plot = plot
         self.browser = browser
+
+        self.port = port
+        self.monitor = ProcedureMonitor('tcp://localhost:%d' % port)
+        # Route Monitor callbacks through signals and slots
+        self.monitor.running.connect(self._running)
+        self.monitor.finished.connect(self._finish)
+        self.monitor.progress.connect(self._update_progress)
+        self.monitor.status.connect(self._update_status)
+
+    def __del__(self):
+        """ Ensures that the processes and threads are properly
+        shutdown before deleting the Manager
+        """
+        self.monitor.stop()
+        self.monitor.join()
+        if self._worker is not None:
+            self._worker.stop()
+            self._worker.join()
+
+        if self._writer is not None:
+            self._writer.stop()
+            self._writer.join()
+
+        super(Manager, self).__del__()
 
     def is_running(self):
         """ Returns True if a procedure is currently running
         """
-        return self._running_process is not None
+        return self._worker is not None
 
-    def running_experiment(self):
-        """ Returns the results object of the running procedure
-        """
-        for experiment in self.experiments:
-            if experiment.procedure.status == Procedure.RUNNING:
-                return experiment
-        return None
+    def update_progress(self, progress):
+        if self.is_running():
+            self._running_experiment.browser_item.progressbar.setValue(progress)
 
-    def queued_experiments(self):
-        """ Returns the Experiments that are queued
-        """
-        queued = []
-        for experiment in self.experiments:
-            if experiment.procedure.status == Procedure.QUEUED:
-                queued.append(experiment)
-        return queued
+    def update_status(self, status):
+        if self.is_running():
+            self._running_experiment.procedure.status = status
 
-    def has_queued_experiments(self):
-        """ Returns True if there is at least one Experiment in the queue.
+    def load(self, experiment):
+        """ Load a previously executed Experiment
         """
-        return len(self.queued_experiments()) > 0
-
-    def file_paths(self):
-        """ Returns a list of file paths for the Experiments in the Manager
-        """
-        return [experiment.results.data_filename for
-                experiment in self.experiments]
+        self.plot.addItem(experiment.curve)
+        self.browser.add(experiment)
+        self.experiments.append(experiment)
 
     def queue(self, experiment):
         """ Adds an experiment to the queue.
@@ -92,13 +171,19 @@ class Manager(QtCore.QObject):
         if self._start_on_add and not self.is_running():
             self.next()
 
-    def load(self, experiment):
-        """ Load a previously executed experiment into the Browser.
+    def remove(self, experiment):
+        """ Removes an Experiment
         """
-        self.plot.addItem(experiment.curve)
-        self.browser.add(experiment)
+        self.experiments.remove(experiment)
+        self.browser.takeTopLevelItem(
+            self.browser.indexOfTopLevelItem(experiment.browser_item))
+        self.plot.removeItem(experiment.curve)
 
-        self.experiments.append(experiment)
+    def clear(self):
+        """ Remove all Experiments
+        """
+        for experiment in self.experiments[:]:
+            self.remove(experiment)
 
     def next(self):
         """ Initiates the start of the next experiment in the queue as long
@@ -108,87 +193,39 @@ class Manager(QtCore.QObject):
         if self.is_running():
             raise Exception("Another procedure is already running")
         else:
-            queued = self.queued_experiments()
-            if len(queued) == 0:
-                return
-                # raise Exception("No experiments are queued to be run")
-            else:
-                experiment = queued[0]
-                # index = self.experiments.index(experiment)
+            if self.experiments.has_next():
+                experiment = self.experiments.next()
+                self._running_experiment = experiment
 
-                self._running_process = QProcedureProcess()
-                self._running_process.load(experiment.procedure)
-                self._running_process.finished.connect(self._callback)
+                self._writer = ResultsWriter(
+                    experiment.data_filename,
+                    'tcp://localhost:%s' % self.port
+                )
+                self.writer.start()
+                sleep(0.01)
 
-                self._data_writer = QResultsWriter(experiment.results)
-                self._running_process.data.connect(self._data_writer.write)
+                self._worker = ProcedureWorker(
+                    experiment.data_filename,
+                    'tcp://*:%s' % self.port
+                )
+                self.worker.start()
 
-                self._running_process.progress.connect(
-                    experiment.browser_item.progressbar.setValue)
-                self._running_process.status_changed.connect(
-                    experiment.browser_item.setStatus)
+    def clean_up(self):
+        self._worker.stop()
+        self._writer.stop()
+        self._worker.join()
+        self._writer.join()
+        
+        del self._worker
+        del self._writer
 
-                self._data_writer.start(self._data_writer.run)
-                self._running_process.start(self._running_process.run)
-                self.running.emit(experiment)
+        self._worker = None
+        self._writer = None
+        self._running_experiment = None
 
-    def resume(self):
-        """ Resume processing of the queue.
-        """
-        self._start_on_add = True
-        self._continous = True
-        self.next()
-
-    def remove(self, experiment):
-        """ Removes the Experiment from the Manager, unless it is currently running.
-        """
-        if experiment not in self.experiments:
-            raise Exception("Attempting to remove an Experiemnt that is "
-                            "not in the Manager")
-        else:
-            if self.is_running() and experiment == self.running_experiment():
-                raise Exception("Attempting to remove the currently"
-                                " running experiment")
-            else:
-                self.browser.takeTopLevelItem(
-                    self.browser.indexOfTopLevelItem(experiment.browser_item))
-                self.plot.removeItem(experiment.curve)
-                self.experiments.pop(self.experiments.index(experiment))
-
-    def clear(self):
-        """ Remove all Experiments from the Manager.
-        """
-        for experiment in self.experiments[:]:
-            self.remove(experiment)
-
-    def abort(self):
-        """ Aborts the currently running Experiment, but raises an exception if
-        there is no running experiment
-        """
-        if not self.is_running():
-            raise Exception("Attempting to abort when no experiment is"
-                            " running")
-        else:
-            self._start_on_add = False
-            self._continous = False
-
-            self._running_process.abort()
-            self._data_writer.join()
-
-            self.aborted.emit(
-                self.experiment_from_procedure(self._running_process.procedure))
-
-    def _callback(self):
-        """ Handles the different cases upon which the running procedure thread
-        can call back
-        """
-        for experiment in self.experiments:
-            if experiment.procedure == self._running_process.procedure:
-                break
-        del self._running_process
-        del self._data_writer
-        self._running_process = None
-        self._data_writer = None
+    def finish(self):
+        experiment = self._running_experiment
+        self.clean_up()
         if experiment.procedure.status == Procedure.FAILED:
             self.failed.emit(experiment)
         elif experiment.procedure.status == Procedure.ABORTED:
@@ -198,16 +235,24 @@ class Manager(QtCore.QObject):
             if self._is_continuous:  # Continue running procedures
                 self.next()
 
-    def experiment_from_browser_item(self, browser_item):
-        for experiment in self.experiments:
-            if experiment.browser_item == browser_item:
-                return experiment
-        raise Exception("This BrowserItem did not match any Experiments "
-                        "in the Manager")
+    def resume(self):
+        """ Resume processing of the queue.
+        """
+        self._start_on_add = True
+        self._continous = True
+        self.next()
 
-    def experiment_from_procedure(self, procedure):
-        for experiment in self.experiments:
-            if experiment.procedure == procedure:
-                return experiment
-        raise Exception("This Procedure did not match any Experiments"
-                        " in the Manager")
+    def abort(self):
+        """ Aborts the currently running Experiment, but raises an exception if
+        there is no running experiment
+        """
+        if not self.is_running():
+            raise Exception("Attempting to abort when no experiment "
+                            "is running")
+        else:
+            self._start_on_add = False
+            self._continous = False
+
+            self._worker.stop()
+
+            self.aborted.emit(self._running_experiment)
