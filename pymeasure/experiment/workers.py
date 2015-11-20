17542e5d0191ed983aa1a281d4874807d69ea1e7
Colin Jermain <cjermain@gmail.com>
2015-11-20 14:48:41 -0500

    Fixed reference to Procedure in ProcedureWorker

--- pymeasure/experiment/workers.py
+++ pymeasure/experiment/workers.py
@@ -41,6 +41,7 @@ class ProcedureWorker(StoppableProcess):
         defined in the file at the filepath
         """
         self.results = Results.load(filepath)
+        self.procedure = results.procedure
         self.procedure.status = Procedure.QUEUED
 
         self.context = zmq.Context()
