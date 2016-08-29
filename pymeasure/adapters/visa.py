52028601f2dd4f82469579ea45b1864d8984f2ed
Minh-Hai <lotoddt@hotmail.com>
2016-08-29 12:18:44 -0400

    Remove support for old versions of PyVisa <1.7
    so that we don't have to spend much effort resolving the discrepancies
    between old- and new-stypled PyVisa.

--- pymeasure/adapters/visa.py
+++ pymeasure/adapters/visa.py
@@ -29,11 +29,9 @@ import numpy as np
 import copy
 import logging
 log = logging.getLogger(__name__)
-#log.addHandler(log.NullHandler())
+log.addHandler(logging.NullHandler())
 
 long = int # Python 3 fix
-
-
 
 class VISAAdapter(Adapter):
     """ Adapter class for the VISA library using PyVISA to communicate
@@ -42,24 +40,28 @@ class VISAAdapter(Adapter):
     :param resource: VISA resource name that identifies the address
     :param kwargs: Any valid key-word arguments for constructing a PyVISA instrument
     """
-    def __new__(cls, resourceName, **kwargs):
-        """ Choose a parent class based on PyVISA version"""
-
-        version = float(visa.__version__)
-        if version > 1.4:
-            mode = VISAAdapter17
-        else:
-            mode = VISAAdapter14
-
-        cls = type(cls.__name__ + '+' + mode.__name__, (cls, mode), {})
-        return super(VISAAdapter, cls).__new__(cls)
-
     def __init__(self, resourceName, **kwargs):
+        #Check PyVisa version
+        version = float(self.version)
+        if version < 1.7:
+            raise NotImplementedError("PyVisa {} is no longer supported. Please upgrade to version 1.8 or later.".format(version))
+
         if isinstance(resourceName, (int, long)):
             resourceName = "GPIB0::%d::INSTR" % resourceName
         super(VISAAdapter, self).__init__(resourceName, **kwargs)
         self.resource_name = resourceName
-
+        self.manager = visa.ResourceManager()
+        safeKeywords = ['resource_name', 'timeout', 'term_chars',
+                        'chunk_size', 'lock', 'delay', 'send_end',
+                        'values_format']
+        kwargsCopy = copy.deepcopy(kwargs)
+        for key in kwargsCopy:
+            if key not in safeKeywords:
+                kwargs.pop(key)
+        self.connection = self.manager.get_instrument(
+                                resourceName,
+                                **kwargs
+                          )
     @property
     def version(self):
         """ The string of the PyVISA version in use
@@ -86,10 +88,29 @@ class VISAAdapter(Adapter):
         :returns: String ASCII response of the instrument.
         """
         return self.connection.read()
-        
+
+    def ask(self, command):
+        """ Writes the command to the instrument and returns the resulting
+        ASCII response
+
+        :param command: SCPI command string to be sent to the instrument
+        :returns: String ASCII response of the instrument
+        """
+        return self.connection.query(command)
+
+    def ask_values(self, command):
+        """ Writes a command to the instrument and returns a list of formatted
+        values from the result. The format of the return is configurated by
+        self.config().
+
+        :param command: SCPI command to be sent to the instrument
+        :returns: Formatted response of the instrument.
+        """
+        return self.connection.query_values(command)
+
     def binary_values(self, command, header_bytes=0, dtype=np.float32):
-        """ Returns a numpy array from a query for binary data 
-    
+        """ Returns a numpy array from a query for binary data
+
         :param command: SCPI command to be sent to the instrument
         :param header_bytes: Integer number of bytes to ignore in header
         :param dtype: The NumPy data type to format the values with
@@ -99,71 +120,9 @@ class VISAAdapter(Adapter):
         binary = self.connection.read_raw()
         header, data = binary[:header_bytes], binary[header_bytes:]
         return np.fromstring(data, dtype=dtype)
-        
 
-class VISAAdapter14(Adapter):
-    """ Adapter class for VISA version 1.4. Be inherited by class VISAAdapter.
-
-    :param resource: VISA resource name that identifies the address.
-    :param kwargs: Any valid key-word arguments for constructing a PyVISA instrument.
-    """
-    def __init__(self, resourceName, **kwargs):
-        self.connection = visa.instrument(resourceName, **kwargs)
-
-    def config(self, **kwargs):
-        """ Reserve for future implementation."""
-        log.warning("Class method config() not yet implemented! Do nothing.")
-        pass
-
-    def ask(self, command):
-        """ Writes the command to the instrument and returns the resulting 
-        ASCII response
-
-        :param command: SCPI command string to be sent to the instrument
-        :returns: String ASCII response of the instrument
-        """
-        return self.connection.ask(command)
-
-    def ask_values(self, command):
-        """ Writes a command to the instrument and returns a list of formatted
-        values from the result 
-
-        :param command: SCPI command to be sent to the instrument
-        :returns: String ASCII response of the instrument
-        """
-        return self.connection.ask_for_values(command)
-
-    def wait_for_srq(self, timeout=25, delay=0.1):
-        """ Blocks until a SRQ, and leaves the bit high
-
-        :param timeout: Timeout duration in seconds
-        :param delay: Time delay between checking SRQ in seconds
-        """
-        self.connection.wait_for_srq(timeout)
-
-
-class VISAAdapter17(Adapter):
-    """ Adapter class for VISA version 1.5 or above. Be inherited by class VISAAdapter.
-
-    :param resource: VISA resource name that identifies the address.
-    :param kwargs: Any valid key-word arguments for constructing a PyVISA instrument.
-    """
-    def __init__(self, resourceName, **kwargs):
-        self.manager = visa.ResourceManager()
-        safeKeywords = ['resource_name', 'timeout', 'term_chars',
-                        'chunk_size', 'lock', 'delay', 'send_end',
-                        'values_format']
-        kwargsCopy = copy.deepcopy(kwargs)
-        for key in kwargsCopy:
-            if key not in safeKeywords:
-                kwargs.pop(key)
-        self.connection = self.manager.get_instrument(
-                                resourceName,
-                                **kwargs
-                          )
-        
     def config(self, is_binary = False, datatype = 'str',
-                    container = np.array, converter = 's', 
+                    container = np.array, converter = 's',
                     separator = ',', is_big_endian = False):
         """ Configurate the format of data transfer to and from the instrument.
 
@@ -181,24 +140,6 @@ class VISAAdapter17(Adapter):
         self.connection.values_format.separator = separator
         self.connection.values_format.is_big_endian = is_big_endian
 
-    def ask(self, command):
-        """ Writes the command to the instrument and returns the resulting 
-        ASCII response
-
-        :param command: SCPI command string to be sent to the instrument
-        :returns: String ASCII response of the instrument
-        """
-        return self.connection.query(command)
-
-    def ask_values(self, command):
-        """ Writes a command to the instrument and returns a list of formatted
-        values from the result. The format of the return is configurated by
-        self.config().
-
-        :param command: SCPI command to be sent to the instrument
-        :returns: Formatted response of the instrument.
-        """
-        return self.connection.query_values(command)
 
     def wait_for_srq(self, timeout=25, delay=0.1):
         """ Blocks until a SRQ, and leaves the bit high
