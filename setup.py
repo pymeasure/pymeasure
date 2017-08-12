eac21d5053ca6a64ce613d3053fee509639702bc
Colin Jermain <cjermain@gmail.com>
2017-08-12 11:05:21 -0400

    Bump to version 0.4.6

--- setup.py
+++ setup.py
@@ -27,12 +27,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='PyMeasure',
-    version='0.4.5',
+    version='0.4.6',
     author='PyMeasure Developers',
     packages=find_packages(),
     scripts=[],
     url='https://github.com/ralph-group/pymeasure',
-    download_url='https://github.com/ralph-group/pymeasure/tarball/v0.4.5',
+    download_url='https://github.com/ralph-group/pymeasure/tarball/v0.4.6',
     license='MIT License',
     description='Scientific measurement library for instruments, experiments, and live-plotting',
     long_description=open('README.rst').read() + "\n\n" + open('CHANGES.txt').read(),
