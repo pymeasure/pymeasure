2ed67a96f4ce93943526ac080c49a6261c6adda9
JuMaD <julian.dlugosch@gmail.com>
2017-09-25 18:08:21 +0200

    Basic Functions for the Keithley26XX Series added

--- pymeasure/instruments/keithley/Keithley2600AB.py
+++ pymeasure/instruments/keithley/Keithley2600AB.py
@@ -43,31 +43,21 @@ import time
 
 
 class Keithley2600AB(Instrument):
-    """Represents the Keithley 2600A and 2600B Series SourceMeters and provides a
+    """Represents the Keithley 26XX Series SourceMeters and provides a
     high-level interface for interacting with the instrument.
 
     Commands are either sent to the instrument when they are called (start_on_call = True) or appended to TSP_script (start_on_call = False).
-    The generated TSP script can then be sent to the instrument vie send_script()
+    The generated TSP script can then be sent to the instrument via execute_script()
 
     #Todo: Define and implement the required functions using TSP commands sent over the adapter
-    #Idea: Define TSP_string and append commands to the script. Start, i.e. send to the instrument, with start_measurement ??
+    #Todo: Give a minimal code example: Set up instrument, Set up Buffer, Perform measurement
 
     .. code-block:: python
 
         keithley = Keithley2600("GPIB::1")
 
         keithley.apply_current()                # Sets up to source current
-        keithley.source_current_range = 10e-3   # Sets the source current range to 10 mA
-        keithley.compliance_voltage = 10        # Sets the compliance voltage to 10 V
-        keithley.source_current = 0             # Sets the source current to 0 mA
-        keithley.enable_source()                # Enables the source output
 
-        keithley.measure_voltage()              # Sets up to measure voltage
-
-        keithley.ramp_to_current(5e-3)          # Ramps the current to 5 mA
-        print(keithley.voltage)                 # Prints the voltage in Volts
-
-        keithley.shutdown()                     # Ramps the current to 0 mA and disables output
         """
 
     def __init__(self, adapter, **kwargs):
@@ -83,14 +73,15 @@ class Keithley2600AB(Instrument):
         self.start_on_call = True
         self.TSP_script = ""
 
-
-
+################################
+# TSP script related functions #
+################################
     def write_command(self, new_command):
         """Writes the TSP command either directly to the instrument or to the TSP_script, depending on the choice of start_on_call"""
         if self.start_on_call:
             self.write(new_command)
         else if not start_on_call:
-            self.TSP_script = self.TSP_script + " " + new_command  # TODO: Check whether the command is a valid TSP command?!
+            self.TSP_script = self.TSP_script + "\n" + new_command  # TODO: Check whether the command is a valid TSP command?!
 
     def execute_script(self):
         """Executes the TSP_script, i.e. sends the string with the commands to the instrument."""
@@ -99,15 +90,14 @@ class Keithley2600AB(Instrument):
     def clear_script(self):
         """clears the script so it can be redefined"""
         self.TSP_script = ""
-
-
+#todo: add load_TSP function that allows to load external TSP scripts (load --> execute makes them usable)
 #####################
 # general functions #
 #####################
-
-    def reset(self):
-            """Resets the SMU"""
-            self.write_command('reset()')
+    def reset(self, smux = 'smua'):
+            """Resets the SMU
+            :param smux: SMU to be reset"""
+            self.write_command(f'{smux}.reset()')
 
     def set_output(self, smux = 'smua', state):
         """Turn the output on or off. State of the output can also be changed with :code enable_source or :code disable_source"""
@@ -127,8 +117,8 @@ class Keithley2600AB(Instrument):
         :param smux:    SMU channel to be used
         :param state:   auto-range channel to be set. can be OFF: Disabled, ON: Enabled or FOLLOW_LIMIT: Measure range automatically set to the limit range
         :param measurefunction: The measurement function for which the autorange property is changed."""
-        voltages = {'voltage','voltages'}
-        currents = {'current''currents'}
+        voltages = {'voltage','voltages','v','V'}
+        currents = {'current''currents','i','I'}
         measurefunction.lower()
 
         if measurefunction in voltages:
@@ -155,12 +145,63 @@ class Keithley2600AB(Instrument):
         else:
                 if keyword == 'current':
                     self.write_command(f'{smux}.source.func = {smux}.OUTPUT_DCAMPS')
+###########
+# buffer  #
+###########
 
+    def clear_buffer(self, smux = 'smua', id = '1'):
+        """Clears the dedicaded reading buffer
+        :param smux:    The SMU which buffer should be cleared.
+        :param id:      The id of the buffer to be cleared. Each smu has two dedicaded reading buffers. use id ='all' to clear all reading buffers
+        """
+        if id <> 'all':
+            self.write_command(f'{smux}.nvbuffer{id}.clear()')
+        else:
+            for i = 1 to 2:
+                self.write_command(f'{smux}.nvbuffer{i}.clear()')
+
+    def set_buffer_ascii(self, precision = 6):
+        """This function set the data format to ASCII with the given precision
+        :param precision:   Precision used for all data received fromt he buffer"""
+
+        self.write_command(f'format.data = format.ASCII\n format.asciiprecision = {precision}')
+
+    def get_buffer_data(self, buffer='smua.nvbuffer1'):
+        """Returns the Data from a buffer as numpy array
+        :param buffer:  The instrument buffer to be read."""
+        #todo: Check whether it is that simple to get data into np array
+        return np.array(self.write(f'printbuffer(1, {buffer}.n,{buffer})'))
+        return np.array(self.write(f'printbuffer(1, {buffer}.n,{buffer})'))
 ###########
 # voltage #
 ###########
 
+    def sweep(self, start, stop, stime, points, smux = 'smua', keyword='lin',source='V'):
+        """ This method uses the Keithley built-in TSP scripts to execute linear or logarithmic sweeps
+        :param start:   Start value of the sweep parameter
+        :param stop:    End value of the sweep parameter
+        :param stime:   Settling time - i.e. time after sourcing before first measurement
+        :param smux.:   SMU to be used
+        :param keyword: Sweeping method. Can be either 'lin' for linear or 'log' for logarithmic
+        :param source:  Sourced quantity (i.e. 'V' or 'I')
+        """
 
+        if source == 'V':
+            if keyword == 'lin':
+                self.write_command(f'SweepVLinMeasureI({smux}, {start}, {stop}, {stime}, {points}')
+            elif keyword == 'log':
+                self.write_command(f'SweepVLogMeasureI({smux}, {start}, {stop}, {stime}, {points})')
+            else:
+                print("Invalid choice of keyword in sweep(source='V')")
+        elif source == 'I':
+            if keyword == 'lin':
+                self.write_command(f'SweepILinMeasureV({smux}, {start}, {stop}, {stime}, {points}')
+            elif keyword == 'log':
+                self.write_command(f'SweepILogMeasureV({smux}, {start}, {stop}, {stime}, {points})')
+            else:
+                print("Invalid choice of keyword in sweep(source='I')")
+        else:
+            print("Invalid choice of method in sweep()")
 
 
 
@@ -182,21 +223,20 @@ class Keithley2600AB(Instrument):
        """Sounds a system beep.
        :param frequency: the frequency of the beep
        :param duration: the duration of the beep"""
-       self.write(f'beeper.beep({duration}, {frequency})')
-
+       self.write_command(f'beeper.beep({duration}, {frequency})')
 
     def triad(self, base_frequency, duration):
-        """ Sounds a musical triad using the system beep.
+        """ Sounds a musical triad using the system beep. Can for instance be used to indicate a finished measurement.
 
-        :param base_frequency: A frequency in Hz between 65 Hz and 1.3 MHz
-        :param duration: A time in seconds between 0 and 7.9 seconds
+        :param base_frequency: Frequency of the base tone in Hz (between 65 Hz and 1.3 MHz)
+        :param duration: A time in seconds per beep (between 0 and 7.9 seconds)
         """
         self.beep(base_frequency, duration)
         time.sleep(duration)
         self.beep(base_frequency*5.0/4.0, duration)
         time.sleep(duration)
         self.beep(base_frequency*6.0/4.0, duration)
-
+        #todo: change function so sleep gets executed within the instrument and not on PC
 ###############
 # Current (A) #
 ###############
@@ -205,7 +245,6 @@ class Keithley2600AB(Instrument):
         :param smux:    SMU to be used. Commonly smua
         :param current: current that is to be set"""
         self.write_command(f'{smux}.source.levelv = {current}') #todo: Use validator for current
-
 
 ###############
 # Voltage (V) #
@@ -230,75 +269,6 @@ class Keithley2600AB(Instrument):
             if self.start_on_call:
             # read and return buffer
                 else:
-            #todo: write measure_voltage method
-
-            ###########
-            # current #
-            ###########
-
-    ###############
-    #   Buffer    #
-    ###############
+            #todo: write measure_voltage method that reads the buffer
 
 
-    ###############
-    # Time/Trigger#
-    ###############
-
-
-
-
-
-
-#############FROM MOCK INSTRUMENT #############################
-
-    def get_time(self):
-        """Get elapsed time"""
-        if self._tstart == 0:
-            self._tstart = time.time()
-        self._time = time.time() - self._tstart
-        return self._time
-
-    def set_time(self, value):
-        """
-        Wait for the timer to reach the specified time.
-        If value = 0, reset.
-        """
-        if value == 0:
-            self._tstart = 0
-        else:
-            while self.time < value:
-                time.sleep(0.001)
-
-    def reset_time(self):
-        """Reset the timer to 0 s."""
-        self.time = 0
-        self.get_time()
-
-    time = property(fget=get_time, fset=set_time)
-
-    def get_wave(self):
-        """Get wave."""
-        return float(numpy.sin(self.time))
-
-    wave = property(fget=get_wave)
-
-    def get_voltage(self):
-        """Get the voltage."""
-        time.sleep(self._wait)
-        return self._voltage
-
-    def __getitem__(self, keys):
-        return keys
-
-    voltage = property(fget=get_voltage)
-
-    def get_output_voltage(self):
-        return self._output_voltage
-
-    def set_output_voltage(self, value):
-        """Set the voltage."""
-        time.sleep(self._wait)
-        self._output_voltage = value
-
-    output_voltage = property(fget=get_output_voltage, fset=set_output_voltage)
