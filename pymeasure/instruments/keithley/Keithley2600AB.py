3e0e1346b2084783e1bfb5edfdfcafddf8e7d901
JuMaD <julian.dlugosch@gmail.com>
2017-09-26 12:48:28 +0200

    Some corrections and additions to Keithley2600AB

--- pymeasure/instruments/keithley/Keithley2600AB.py
+++ pymeasure/instruments/keithley/Keithley2600AB.py
@@ -21,11 +21,6 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 #
-
-import numpy
-import time
-
-from pymeasure.adapters import FakeAdapter
 from pymeasure.instruments import Instrument
 
 import logging
@@ -46,8 +41,10 @@ class Keithley2600AB(Instrument):
     """Represents the Keithley 26XX Series SourceMeters and provides a
     high-level interface for interacting with the instrument.
 
-    Commands are either sent to the instrument when they are called (start_on_call = True) or appended to TSP_script (start_on_call = False).
-    The generated TSP script can then be sent to the instrument via execute_script()
+    Commands are sent directly to the instrument. __start_on_call decides whether the received commands are executed
+    by the instrument immediately or after calling execute_script().
+
+    External TSP scripts (in Lua) can be loaded onto the instrument via load_TSP(filename)
 
     #Todo: Define and implement the required functions using TSP commands sent over the adapter
     #Todo: Give a minimal code example: Set up instrument, Set up Buffer, Perform measurement
@@ -72,21 +69,24 @@ class Keithley2600AB(Instrument):
         #TSP script commands can be directly sent and executed or appended to a string and send as one. Having self.TSP_script also allows to use already implemented TSP scripts (has to be tested!)
         self.__start_on_call = True
         self.__start_on_call_change = False
-        self.TSP_script = ""
+
 
 ################################
 # TSP script related functions #
 ################################
     def start_on_call(self, value=True):
-        self.__start_on_call = value
-        self.__start_on_call_change = True
-
-
+        """Set function for the private parameter __start_on_call that also acticates the flag __start_on_call_change.
+        This way, write_command can fetch the first sent command after change of __start_on_call from True to False.
+        :param value:   Boolean value that decides whether the Keithley should execute the commands on receival or wait until a script is loaded command by command."""
+        # only change state, if necessary
+        if value != __start_on_call:
+            self.__start_on_call = value
+            self.__start_on_call_change = True
 
     def write_command(self, new_command):
         """Writes the TSP command to the instrument. if start_on_call is False, the script is not executed upon receiving the command"""
 
-        if not start_on_call and self.__start_on_call_change: # catch only first write after change of property __start_on_call
+        if not start_on_call and self.__start_on_call_change:  # catch only first write after change of property __start_on_call from True to False
             self.write('loadscript')
             self.__start_on_call_change = False
 
@@ -94,17 +94,28 @@ class Keithley2600AB(Instrument):
 
     def execute_script(self):
         """Executes the TSP_script """
-        self.write('endscript')
-        self.write('run()')
+        if not start_on_call:
+            self.write('endscript')
+            self.write('run()')
 
-    def clear_script(self):
-        """clears the script so it can be redefined"""
-        self.TSP_script = ""
-#todo: add load_TSP function that allows to load external TSP scripts (load --> execute makes them usable)
+
+
     def load_TSP(self, filename):
-        self.write('loadscript')
-        #load file here
-        self.write('endscript')
+        """Loads a TSP script (external file) as anonymous script onto the connected instrument.The script can be executed using self.execute_script
+        :param filename:    The filename, including directory, of the TSP script file"""
+        if self.__start_on_call == True:
+            self.start_on_call(False)
+        self.write_command('loadscript')
+        for line in open(str(filename), mode='r'):
+            self.write_command(line)
+
+        # todo: include a way to use the loaded script as named and not anonymous script. I.e. save it on the instrument for later execution. This also requires a change in execute_script()
+
+        log.info('Uploaded TSP script:' + filename)
+
+
+
+
 
 
 #####################
@@ -115,12 +126,12 @@ class Keithley2600AB(Instrument):
             :param smux: SMU to be reset"""
             self.write_command(f'{smux}.reset()')
 
-    def set_output(self, smux = 'smua', state):
+    def set_output(self, smux='smua', state='ON'):
         """Turn the output on or off. State of the output can also be changed with :code enable_source or :code disable_source"""
         #Turn it into  allcaps
         state.upper()
-        if state == "ON" or state == "OFF"
-            self.write_command(f'{smua}.source.output = OUTPUT_{state}')
+        if state == "ON" or state == "OFF":
+            self.write_command(f'{smux}.source.output = OUTPUT_{state}')
 
     def enable_source(self, smux='smua'):
         self.write_command(f'{smua}.source.output = OUTPUT_ON')
@@ -128,7 +139,7 @@ class Keithley2600AB(Instrument):
     def disable_source(self, smux='smua'):
         self.write_command(f'{smua}.source.output = OUTPUT_OFF')
 
-    def set_autorange(self, smux = 'smua', state='ON', measurefunction):
+    def set_autorange(self, measurefunction, smux='smua', state='ON'):
         """Sets the auto-range value of the smu channel
         :param smux:    SMU channel to be used
         :param state:   auto-range channel to be set. can be OFF: Disabled, ON: Enabled or FOLLOW_LIMIT: Measure range automatically set to the limit range
@@ -152,7 +163,7 @@ class Keithley2600AB(Instrument):
         :param smux: The channel of the SMU that is used"""
         self.write_command(f'{smux}.measure.delay = {mdelay}')
 
-    def set_output_fcn (self, smux = 'smua', keyword):
+    def set_output_fcn(self, keyword, smux='smua'):
         """Set the source function to be used .
         :param smux:    SMU to be used. Commonly smua
         :param keyword: Defines whether the source function is set to voltage or current"""""
@@ -161,6 +172,31 @@ class Keithley2600AB(Instrument):
         else:
                 if keyword == 'current':
                     self.write_command(f'{smux}.source.func = {smux}.OUTPUT_DCAMPS')
+
+
+
+                ##########
+                # screen #
+                ##########
+
+    def clear_screen(self):
+        """Clears the screen of the connected device"""
+        self.write_command('display.clear()')
+
+    def set_screentext(self, textstring):
+        """Sets the text on the screen of the device.
+        :param textstring:  Text that is displayed on screen. The following formating characters can be used:
+                            $N Newline, starts text on the next line; if the cursor is already on line 2, text will be ignored
+                            after the $N is received
+                            $R Sets text to normal intensity, nonblinking
+                            $B Sets text to blink
+                            $D Sets text to dim intensity
+                            $F Sets the text to background blink
+                            $$ Escape sequence to display a single dollar symbol ($)
+                            """
+        self.clear_screen()
+        self.write_command(f'display.settext{textstring}')
+
 ###########
 # buffer  #
 ###########
@@ -170,10 +206,10 @@ class Keithley2600AB(Instrument):
         :param smux:    The SMU which buffer should be cleared.
         :param id:      The id of the buffer to be cleared. Each smu has two dedicaded reading buffers. use id ='all' to clear all reading buffers
         """
-        if id <> 'all':
+        if id != 'all':
             self.write_command(f'{smux}.nvbuffer{id}.clear()')
         else:
-            for i = 1 to 2:
+            for i in range(1, 2):
                 self.write_command(f'{smux}.nvbuffer{i}.clear()')
 
     def set_buffer_ascii(self, precision = 6):
@@ -187,16 +223,19 @@ class Keithley2600AB(Instrument):
         :param buffer:  The instrument buffer to be read."""
         #todo: Check whether it is that simple to get data into np array
         return np.array(self.write(f'printbuffer(1, {buffer}.n,{buffer})'))
-        return np.array(self.write(f'printbuffer(1, {buffer}.n,{buffer})'))
+
 ###########
 # voltage #
 ###########
 
-    def sweep(self, start, stop, stime, points, smux = 'smua', keyword='lin',source='V'):
-        """ This method uses the Keithley built-in TSP scripts to execute linear or logarithmic sweeps
+    def autosweep(self, start, stop, stime, points, smux='smua', keyword='lin', source='V'):
+        """ This method uses the Keithley built-in TSP scripts to execute linear or logarithmic sweeps.
+        Autozero (the internal function to avoid zero drift) is disabled for the sweep to ensure it does not cause timing issues.
+        All measured values are written to the internal buffer and read out after the measurement - i.e. data is shown only after a complete cycle.
         :param start:   Start value of the sweep parameter
         :param stop:    End value of the sweep parameter
         :param stime:   Settling time - i.e. time after sourcing before first measurement
+        :param points:  Number of measurement points. Should be (max-min)/stepwidth
         :param smux.:   SMU to be used
         :param keyword: Sweeping method. Can be either 'lin' for linear or 'log' for logarithmic
         :param source:  Sourced quantity (i.e. 'V' or 'I')
@@ -218,12 +257,6 @@ class Keithley2600AB(Instrument):
                 print("Invalid choice of keyword in sweep(source='I')")
         else:
             print("Invalid choice of method in sweep()")
-
-
-
-
-
-
 
 
 ###########
@@ -256,7 +289,7 @@ class Keithley2600AB(Instrument):
 ###############
 # Current (A) #
 ###############
-    def set_current (self, smux = 'smua', current):
+    def set_current(self, current, smux='smua'):
         """Sets the current
         :param smux:    SMU to be used. Commonly smua
         :param current: current that is to be set"""
@@ -265,13 +298,13 @@ class Keithley2600AB(Instrument):
 ###############
 # Voltage (V) #
 ###############
-    def set_voltage(self, smux='smua', voltage):
+    def set_voltage(self, voltage, smux='smua'):
             """Sets the voltage
             :param smux:    SMU to be used. Commonly smua
             :param voltage: voltage that is to be set"""
             self.write_command(f'{smux}.source.levelv = {voltage}')  # todo: Use validator for voltage
 
-    def read_voltage(self, smux='smua', buffer='nvbuffer1', datapoints=10):
+    def read_voltage(self, datapoints, smux='smua', buffer='nvbuffer1'):
             """Measures the voltage and saves data to internal buffer, then returns ....
             :param smux:        SMU to be used. Commonly smua
             :param buffer:      The Keithley 26XX series offers two dedicated buffers per SMU channel. This parameter defines which one is used to buffer measurement data
@@ -279,12 +312,8 @@ class Keithley2600AB(Instrument):
             # clear buffer
             self.write_command(f'{smux}.{buffer}.clear() smua.nvbuffer1.appendmode = 1')
             # set number of points and fill buffer
-            self.write_command(f'{smux}.measure.count = {datapoints})
-            self.write_command(f'{smux}.measure.v({smux}.{buffer})
+            self.write_command(f'{smux}.measure.count = {datapoints}')
+            self.write_command(f'{smux}.measure.v({smux}.{buffer}')
 
-            if self.start_on_call:
-            # read and return buffer
-                else:
+
             #todo: write measure_voltage method that reads the buffer
-
-
