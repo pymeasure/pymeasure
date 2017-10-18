71dae7c59172063fdff80db1d77c3cb84d200e0d
Colin Jermain <cjermain@gmail.com>
2017-10-18 18:57:15 -0400

    Bumping to version 0.5

--- setup.py
+++ setup.py
@@ -27,12 +27,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='PyMeasure',
-    version='0.4.6',
+    version='0.5',
     author='PyMeasure Developers',
     packages=find_packages(),
     scripts=[],
     url='https://github.com/ralph-group/pymeasure',
-    download_url='https://github.com/ralph-group/pymeasure/tarball/v0.4.6',
+    download_url='https://github.com/ralph-group/pymeasure/tarball/v0.5',
     license='MIT License',
     description='Scientific measurement library for instruments, experiments, and live-plotting',
     long_description=open('README.rst').read() + "\n\n" + open('CHANGES.txt').read(),
