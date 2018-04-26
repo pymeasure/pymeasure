bd8c2532e04b0853fb36cb48ef4fe7d435f97794
Sekar <Vikram.Sekar@idt.com>
2018-04-25 17:30:03 -0700

    added sweep variable methods

--- pymeasure/instruments/agilent/agilent4156.py
+++ pymeasure/instruments/agilent/agilent4156.py
@@ -28,7 +28,7 @@ log.addHandler(logging.NullHandler())
 
 from pymeasure.instruments import Instrument, RangeException
 from pymeasure.instruments.agilent.agilent4156_channels import smu1
-from pymeasure.instruments.validators import strict_discrete_set, truncated_discrete_set
+from pymeasure.instruments.validators import *
 import numpy as np
 
 class Agilent4156(Instrument):
@@ -43,6 +43,9 @@ class Agilent4156(Instrument):
             **kwargs
         )
         self.smu1 = smu1(self.adapter)
+        self.var1 = var1(self.adapter)
+        self.var2 = var2(self.adapter)
+        self.vard = vard(self.adapter)
 
     analyzer_mode = Instrument.control(
         ":PAGE:CHAN:MODE?", ":PAGE:CHAN:MODE %s",
@@ -90,20 +93,230 @@ class Agilent4156(Instrument):
         check_get_errors=True
     )
 
+class var1(Instrument):
+    """ Class to handle all the definitions needed for VAR1
+    """
+    def __init__(self, resourceName, **kwargs):
+        super().__init__(
+            resourceName,
+            "Definitions for VAR1 sweep variable.",
+            **kwargs
+        )
 
-    def save_in_display_list(self, name):
-        """ Save the voltage or current in the instrument display list """
-        self.write(':PAGE:DISP:MODE LIST')
-        self.write(':PAGE:DISP:LIST ' + name)
+    start = Instrument.control(
+        ":PAGE:MEAS:VAR1:START?",
+        ":PAGE:MEAS:VAR1:START %g",
+        """
+        This command sets the sweep START value of VAR1.
+        At *RST, this value is 0 V.
+        -200 to 200 V or -1 to 1 A. The range of this value
+        depends on the unit type of VAR1.
+        Input is only validated for voltages.
+        """,
+        validator=strict_range,
+        values=[-200, 200],
+        check_set_errors=True,
+        check_get_errors=True
+    )
 
-    def save_in_variable_list(self, name):
-        """ Save the voltage or current in the instrument variable list """
-        self.write(':PAGE:DISP:MODE LIST')
-        self.write(':PAGE:DISP:DVAR ' + name)
-        if channel_in_group(ch, SMU_CHANNELS) is True:
-            name = check_current_voltage_name(name)
-            self.write(send_to_channel(ch) + "INAM %s" % name)
-        else:
-            raise ValueError("Cannot set current name for non-SMU units")
+    stop = Instrument.control(
+        ":PAGE:MEAS:VAR1:STOP?",
+        ":PAGE:MEAS:VAR1:STOP %g",
+        """
+        This command sets the sweep STOP value of VAR1.
+        At *RST, this value is 1 V.
+        -200 to 200 V or -1 to 1 A. The range of this value
+        depends on the unit type of VAR1.
+        Input is only validated for voltages.
+        """,
+        validator=strict_range,
+        values=[-200, 200],
+        check_set_errors=True,
+        check_get_errors=True
+    )
 
-    
+    step = Instrument.control(
+        ":PAGE:MEAS:VAR1:STEP?",
+        ":PAGE:MEAS:VAR1:STEP %g",
+        """
+        This command sets the sweep STEP value of VAR1 for the linear sweep.
+        This parameter is not used for logarithmic sweep.
+        -400 to 400 V or -2 to 2 A. Input is only validated for voltages.
+        The range of this value depends on the unit type of VAR1.
+        The polarity of step value is automatically determined by the relation between start
+        and stop values. So, for the step value you specify, only absolute value has meaning.
+        The polarity has no meaning.
+        """,
+        validator=strict_range,
+        values=[-400, 400],
+        check_set_errors=True,
+        check_get_errors=True
+    )
+
+    compliance = Instrument.control(
+        ":PAGE:MEAS:VAR1:COMP?",
+        ":PAGE:MEAS:VAR1:COMP %g",
+        """
+        This command sets the COMPLIANCE value of VAR1 in Volts/Ampsself. At *RST, this value is 100 mA.
+        Only current compliance is validated in function input.
+        """,
+        validator=strict_range,
+        values=[-1, 1],
+        check_set_errors=True,
+        check_get_errors=True
+    )
+
+    spacing = Instrument.control(
+        ":PAGE:MEAS:VAR1:SPAC?",
+        ":PAGE:MEAS:VAR1:SPAC %s",
+        """
+        This command selects the sweep type of VAR1: linear staircase or logarithmic
+        staircase. Valid inputs are 'LINEAR', 'LOG10', 'LOG25', 'LOG50'.
+        """,
+        validator=strict_discrete_set,
+        values={'LINEAR':'LIN', 'LOG10':'L10', 'LOG25':'L25', 'LOG50':'L50'},
+        map_values=True,
+        check_set_errors=True,
+        check_get_errors=True
+    )
+
+class var2(Instrument):
+    """ Class to handle all the definitions needed for VAR2
+    """
+    def __init__(self, resourceName, **kwargs):
+        super().__init__(
+            resourceName,
+            "Definitions for VAR2 sweep variable.",
+            **kwargs
+        )
+
+    start = Instrument.control(
+        ":PAGE:MEAS:VAR2:START?",
+        ":PAGE:MEAS:VAR2:START %g",
+        """
+        This command sets the sweep START value of VAR2.
+        At *RST, this value is 0 V.
+        -200 to 200 V or -1 to 1 A. The range of this value
+        depends on the unit type of VAR2.
+        Input is only validated for voltages.
+        """,
+        validator=strict_range,
+        values=[-200, 200],
+        check_set_errors=True,
+        check_get_errors=True
+    )
+
+    step = Instrument.control(
+        ":PAGE:MEAS:VAR2:STEP?",
+        ":PAGE:MEAS:VAR2:STEP %g",
+        """
+        This command sets the sweep STEP value of VAR2 for the linear sweep.
+        This parameter is not used for logarithmic sweep.
+        -400 to 400 V or -2 to 2 A. Input is only validated for voltages.
+        The range of this value depends on the unit type of VAR2.
+        The polarity of step value is automatically determined by the relation between start
+        and stop values. So, for the step value you specify, only absolute value has meaning.
+        The polarity has no meaning.
+        """,
+        validator=strict_range,
+        values=[-400, 400],
+        check_set_errors=True,
+        check_get_errors=True
+    )
+
+    points = Instrument.control(
+        ":PAGE:MEAS:VAR2:POINTS?",
+        ":PAGE:MEAS:VAR2:POINTS %g",
+        """
+        This command sets the number of sweep steps of VAR2.
+        You use this command only if there is an SMU or VSU
+        whose function (FCTN) is VAR2.
+        At *RST, this value is 5.
+        """,
+        validator=strict_discrete_set,
+        values=range(1, 128),
+        check_set_errors=True,
+        check_get_errors=True
+    )
+
+    compliance = Instrument.control(
+        ":PAGE:MEAS:VAR2:COMP?",
+        ":PAGE:MEAS:VAR2:COMP %g",
+        """
+        This command sets the COMPLIANCE value of VAR2 in Volts/Ampsself. At *RST, this value is 100 mA.
+        Only current compliance is validated in function input.
+        """,
+        validator=strict_range,
+        values=[-1, 1],
+        check_set_errors=True,
+        check_get_errors=True
+    )
+
+class vard(Instrument):
+    """ Class to handle all the definitions needed for VARD.
+    VARD is always defined in relation to VAR1.
+    """
+
+    def __init__(self, resourceName, **kwargs):
+        super().__init__(
+            resourceName,
+            "Definitions for VARD sweep variable.",
+            **kwargs
+        )
+
+    offset = Instrument.control(
+        ":PAGE:MEAS:VARD:OFFSET?",
+        ":PAGE:MEAS:VARD:OFFSET %g",
+        """
+        This command sets the OFFSET value of VAR1'.
+        For each step of sweep, the output values of VAR1' are determined by the following
+        equation: VAR1’ = VAR1 X RATio + OFFSet
+        You use this command only if there is an SMU or VSU whose function (FCTN) is
+        VAR1'. Only voltage input is validated.
+        """,
+        validator=strict_range,
+        values=[-400, 400],
+        check_set_errors=True,
+        check_get_errors=True
+    )
+
+    ratio = Instrument.control(
+        ":PAGE:MEAS:VARD:RATIO?",
+        ":PAGE:MEAS:VARD:RATIO %g",
+        """
+        This command sets the RATIO of VAR1'.
+        For each step of sweep, the output values of VAR1' are determined by the following
+        equation: VAR1’ = VAR1  RATio + OFFSet
+        You use this command only if there is an SMU or VSU whose function (FCTN) is
+        VAR1'. At *RST, this value is not defined.
+        """,
+    )
+
+    compliance = Instrument.control(
+        ":PAGE:MEAS:VARD:COMP?",
+        ":PAGE:MEAS:VARD:COMP %g",
+        """
+        This command sets the COMPLIANCE value of VAR1'.
+        You use this command only if there is an SMU whose function (FCTN) is VAR1'.
+        Only current compliance setting is validated.
+        """,
+        validator=strict_range,
+        values=[-1, 1],
+        check_set_errors=True,
+        check_get_errors=True
+    )
+
+def save_in_display_list(self, name):
+    """ Save the voltage or current in the instrument display list """
+    self.write(':PAGE:DISP:MODE LIST')
+    self.write(':PAGE:DISP:LIST ' + name)
+
+def save_in_variable_list(self, name):
+    """ Save the voltage or current in the instrument variable list """
+    self.write(':PAGE:DISP:MODE LIST')
+    self.write(':PAGE:DISP:DVAR ' + name)
+    if channel_in_group(ch, SMU_CHANNELS) is True:
+        name = check_current_voltage_name(name)
+        self.write(send_to_channel(ch) + "INAM %s" % name)
+    else:
+        raise ValueError("Cannot set current name for non-SMU units")
