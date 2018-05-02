28c522dc3e13e50909a2f3822c1055554359cae2
Sylvain Karlen <sylvainkarlen@msn.com>
2018-05-02 13:52:49 +0200

    Minor update on newly added instrument

--- pymeasure/instruments/srs/sg380.py
+++ pymeasure/instruments/srs/sg380.py
@@ -20,39 +20,67 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
+
 #
-
-from pymeasure.instruments import Instrument, discreteTruncate
-from pymeasure.instruments.validators import strict_discrete_set, truncated_discrete_set
-
-import numpy as np
-
+from pymeasure.instruments import Instrument
+from pymeasure.instruments.validators import truncated_range
 
 
 class SG380(Instrument):
 
-    MOD_TYPES_VALUES = ['AM','FM','PM','SWEEP','PULSE','BLANK','IQ']
+    MOD_TYPES_VALUES = ['AM', 'FM', 'PM', 'SWEEP', 'PULSE', 'BLANK', 'IQ']
 
-    MOD_FUNCTIONS = ['SINE','RAMP','TRIANGLE','SQUARE','NOISE','EXTERNAL']
+    MOD_FUNCTIONS = ['SINE', 'RAMP', 'TRIANGLE', 'SQUARE', 'NOISE', 'EXTERNAL']
 
-    frequency = Instrument.control(
-        "FREQ?", "FREQ%.6f",
-        """ A floating point property that represents the frequency
-        in Hz. This property can be set. """
-    )
-    #TODO: restrict modulation depth to allowed values (depending on frequency)
+    MIN_RF = 0.0
+
+    MAX_RF = 4E9
+
+    # TODO: restrict modulation depth to allowed values (depending on
+    # frequency)
     fm_dev = Instrument.control(
         "FDEV?", "FDEV%.6f",
         """ A floating point property that represents the modulation frequency
         deviation in Hz. This property can be set. """
     )
-    
+
     rate = Instrument.control(
         "RATE?", "RATE%.6f",
         """ A floating point property that represents the modulation rate
         in Hz. This property can be set. """
-    )    
-    
+    )
+
+    def __init__(self, resourceName, **kwargs):
+        super(SG380, self).__init__(
+            resourceName,
+            "Stanford Research Systems SG380 RF Signal Generator",
+            **kwargs
+        )
+
+    @property
+    def has_doubler(self):
+        """Gets the modulation type"""
+        return bool(self.ask("OPTN? 2"))
+
+    @property
+    def has_IQ(self):
+        """Gets the modulation type"""
+        return bool(self.ask("OPTN? 3"))
+
+    @property
+    def frequency(self):
+        """Gets RF frequency"""
+        return float(self.ask("FREQ?"))
+
+    @frequency.setter
+    def frequency(self, frequency):
+        """Defines RF frequency"""
+        if self.has_doubler:
+            truncated_range(frequency, (SG380.MIN_RF, 2*SG380.MAX_RF))
+        else:
+            truncated_range(frequency, (SG380.MIN_RF, SG380.MAX_RF))
+        self.write("FREQ%.6f" % frequency)
+
     @property
     def mod_type(self):
         """Gets the modulation type"""
@@ -62,11 +90,13 @@ class SG380(Instrument):
     def mod_type(self, type_):
         """Defines the modulation type"""
         if type_ not in SG380.MOD_TYPES_VALUES:
-            index = 1
+            raise RuntimeError('Undefined modulation type')
+        elif (type_ == 'IQ') and not self.has_IQ:
+            raise RuntimeError('IQ option not installed')
         else:
             index = SG380.MOD_TYPES_VALUES.index(type_)
         self.write("TYPE%d" % index)
-        
+
     @property
     def mod_function(self):
         """Gets the modulation function"""
