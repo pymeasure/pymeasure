7a75a0c2d419df93f6f0c595a1936427af09aae7
Casper Schippers <c.f.schippers@tue.nl>
2019-01-17 11:46:38 +0100

    Implemented number of properties & error checking
    Implemented function, frequency & voltage (amplitude, offset, high & low) properties, and implemented error checking

--- pymeasure/instruments/agilent/agilent33220A.py
+++ pymeasure/instruments/agilent/agilent33220A.py
@@ -22,8 +22,89 @@
 # THE SOFTWARE.
 #
 
+import logging
+log = logging.getLogger(__name__)
+log.addHandler(logging.NullHandler())
+
 from pymeasure.instruments import Instrument
+from pymeasure.instruments.validators import strict_discrete_set, strict_range
 
 
 class Agilent33220A(Instrument):
-    pass
+    function = Instrument.control(
+        "FUNC?", "FUNC %s",
+        """ A string property that controls the output waveform. Can be set to:
+        SIN<USOID>, SQU<ARE>, RAMP, PULS<E>, NOIS<E>, DC, USER. """,
+        validator=strict_discrete_set,
+        values=["SINUSOID", "SIN", "SQUARE", "SQU", "RAMP",
+                "PULSE", "PULS", "NOISE", "NOIS", "DC", "USER"],
+    )
+
+    frequency = Instrument.control(
+        "FREQ?", "FREQ %s",
+        """ A floating point property that controls the frequency of the output
+        waveform in Hz, from ... to .... Can be set. """,
+        # validator=strict_range,
+        # values=[..., ...]
+    )
+
+    voltage = Instrument.control(
+        "VOLT?", "VOLT %f",
+        """ A floating point property that controls the voltage amplitude of the output
+        waveform in V, from ... to .... Can be set. """,
+        # validator=strict_range,
+        # values=[..., ...]
+    )
+
+    voltage_offset = Instrument.control(
+        "VOLT:OFFS?", "VOLT:OFFS %f",
+        """ A floating point property that controls the voltage offset of the output
+        waveform in V, from ... to .... Can be set. """,
+        # validator=strict_range,
+        # values=[..., ...]
+    )
+
+    voltage_high = Instrument.control(
+        "VOLT:HIGH?", "VOLT:HIGH %f",
+        """ A floating point property that controls the upper voltage of the output
+        waveform in V, from ... to .... Can be set. """,
+        # validator=strict_range,
+        # values=[..., ...]
+    )
+
+    voltage_low = Instrument.control(
+        "VOLT:LOW?", "VOLT:LOW %f",
+        """ A floating point property that controls the lower voltage of the output
+        waveform in V, from ... to .... Can be set. """,
+        # validator=strict_range,
+        # values=[..., ...]
+    )
+
+# FUCNtion:SQUare:DCYCLe
+# FUNCtion:RAMP:SYMMetry
+# FUNCtion:PULSe:WIDTh <seconds>
+# FUNCtion:PULSe:DCYCLe <percent>
+# FUNCtion:PULSe:TRANsition <seconds>
+# FUNCtion:PULSe:
+# OUTPut
+# OUTPut:LOAD ???
+# OUTPut:POLarity {NORMal / INVerted}
+
+# BURSt:MODE {TRIGgered / GATed}
+# BURSt:NCYCLes
+
+# TRIG
+# OUTput:TRIGger {OFF / ON}
+
+# SYSTem:LOCal
+# SYSTem:REMote
+
+    def check_errors(self):
+        """ Read all errors from the instrument."""
+        while True:
+            err = self.values("SYST:ERR?")
+            if int(err[0]) != 0:
+                errmsg = "Agilent 33220A: %s: %s" % (err[0], err[1])
+                log.error(errmsg + '\n')
+            else:
+                break
