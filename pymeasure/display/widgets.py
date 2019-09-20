0b3d075531dd98b171a740187f31bccbe6476491
Casper Schippers <c.f.schippers@tue.nl>
2019-09-20 09:23:42 +0200

    Implemented sequencer
    Moved from private package to pymeasure repo

--- pymeasure/display/widgets.py
+++ pymeasure/display/widgets.py
@@ -27,6 +27,10 @@ import logging
 import os
 import re
 import pyqtgraph as pg
+from functools import partial
+import numpy
+from collections import ChainMap
+from itertools import product
 
 from .browser import Browser
 from .curves import ResultsCurve, Crosshairs
@@ -418,3 +422,352 @@ class ResultsDialog(QtGui.QFileDialog):
                 new_item = QtGui.QTreeWidgetItem([param.name, str(param)])
                 self.preview_param.addTopLevelItem(new_item)
             self.preview_param.sortItems(0, QtCore.Qt.AscendingOrder)
+
+
+SAFE_FUNCTIONS = {
+    'range': range,
+    'sorted': sorted,
+    'list': list,
+    'arange': numpy.arange,
+    'linspace': numpy.linspace,
+    'arccos': numpy.arccos,
+    'arcsin': numpy.arcsin,
+    'arctan': numpy.arctan,
+    'arctan2': numpy.arctan2,
+    'ceil': numpy.ceil,
+    'cos': numpy.cos,
+    'cosh': numpy.cosh,
+    'degrees': numpy.degrees,
+    'e': numpy.e,
+    'exp': numpy.exp,
+    'fabs': numpy.fabs,
+    'floor': numpy.floor,
+    'fmod': numpy.fmod,
+    'frexp': numpy.frexp,
+    'hypot': numpy.hypot,
+    'ldexp': numpy.ldexp,
+    'log': numpy.log,
+    'log10': numpy.log10,
+    'modf': numpy.modf,
+    'pi': numpy.pi,
+    'power': numpy.power,
+    'radians': numpy.radians,
+    'sin': numpy.sin,
+    'sinh': numpy.sinh,
+    'sqrt': numpy.sqrt,
+    'tan': numpy.tan,
+    'tanh': numpy.tanh,
+}
+
+
+class SequencerWidget(QtGui.QWidget):
+    """
+    Widget that allows to generate a sequence of measurements with varying parameters.
+    Moreover, one can write a simple text file to easily load a sequence.
+
+    Currently requires a queue function of the ManagedWindow to have a "procedure" argument.
+    """
+
+    MAXDEPTH = 10
+
+    def __init__(self, parent, inputs=None):
+        super().__init__(parent)
+        self._parent = parent
+
+        if inputs is not None:
+            self._inputs = inputs
+        else:
+            self._inputs = self._parent.displays
+
+        self._get_properties()
+        self._layout()
+        self._add_to_interface()
+
+    def _get_properties(self):
+        parameter_objects = self._parent.procedure_class().parameter_objects()
+        self.parameter_objects = {key: parameter
+                                  for key, parameter
+                                  in parameter_objects.items()
+                                  if key in self._inputs}
+
+        self.names = {key: obj.name for key, obj
+                      in self.parameter_objects.items()}
+        self.names_inv = {name: key for key, name in self.names.items()}
+
+    def _layout(self):
+        self.tree = QtGui.QTreeWidget(self)
+        self.tree.setHeaderLabels(["Level", "Parameter", "Sequence"])
+        width = self.tree.viewport().size().width()
+        self.tree.setColumnWidth(0, int(0.7 * width))
+        self.tree.setColumnWidth(1, int(0.9 * width))
+        self.tree.setColumnWidth(2, int(0.9 * width))
+
+        add_root_item_btn = QtGui.QPushButton("Add root item")
+        add_root_item_btn.clicked.connect(
+            partial(self._add_tree_item, at_root=True)
+        )
+
+        add_tree_item_btn = QtGui.QPushButton("Add item")
+        add_tree_item_btn.clicked.connect(
+            partial(self._add_tree_item, at_root=False)
+        )
+
+        remove_tree_item_btn = QtGui.QPushButton("Remove item")
+        remove_tree_item_btn.clicked.connect(self._remove_selected_tree_item)
+
+        btn_box = QtGui.QHBoxLayout()
+        btn_box.addWidget(add_root_item_btn)
+        btn_box.addWidget(add_tree_item_btn)
+        btn_box.addWidget(remove_tree_item_btn)
+
+        load_seq_button = QtGui.QPushButton("Load sequence")
+        load_seq_button.clicked.connect(self.load_sequence)
+        load_seq_button.setToolTip("Load a sequence from a file.")
+
+        queue_button = QtGui.QPushButton("Queue sequence")
+        queue_button.clicked.connect(self.queue_sequence)
+
+        btn_box_2 = QtGui.QHBoxLayout()
+        btn_box_2.addWidget(load_seq_button)
+        btn_box_2.addWidget(queue_button)
+
+        vbox = QtGui.QVBoxLayout(self)
+        vbox.setSpacing(6)
+        vbox.addWidget(self.tree)
+        vbox.addLayout(btn_box)
+        vbox.addLayout(btn_box_2)
+        self.setLayout(vbox)
+
+    def _add_tree_item(self, at_root=False,
+                       level=None, parameter=None, sequence=None):
+        selected = self.tree.selectedItems()
+
+        if level == 0:
+            at_root = True
+
+        if len(selected) >= 1 and not at_root:
+            parent = selected[0]
+        else:
+            parent = self.tree.invisibleRootItem()
+
+        if level is not None and level > 0:
+            p_depth = self._depth_of_child(parent)
+
+            while p_depth > level - 1:
+                parent = parent.parent()
+                p_depth = self._depth_of_child(parent)
+
+        comboBox = QtGui.QComboBox()
+        lineEdit = QtGui.QLineEdit()
+
+        comboBox.addItems(list(sorted(self.names_inv.keys())))
+
+        item = QtGui.QTreeWidgetItem(parent, [""])
+        depth = self._depth_of_child(item)
+        item.setText(0, "{:d}".format(depth))
+
+        self.tree.setItemWidget(item, 1, comboBox)
+        self.tree.setItemWidget(item, 2, lineEdit)
+
+        self.tree.expandAll()
+
+        for selected_item in selected:
+            selected_item.setSelected(False)
+
+        if parameter is not None:
+            idx = self.tree.itemWidget(item, 1).findText(parameter)
+            self.tree.itemWidget(item, 1).setCurrentIndex(idx)
+            if idx == -1:
+                log.error(
+                    "Parameter '{}' not found while loading sequence".format(
+                        parameter) + ", probably mistyped."
+                )
+
+        if sequence is not None:
+            self.tree.itemWidget(item, 2).setText(sequence)
+
+        item.setSelected(True)
+
+    def _remove_selected_tree_item(self):
+        selected = self.tree.selectedItems()
+        if len(selected) == 0:
+            return
+
+        item = selected[0]
+        parent = item.parent()
+
+        if parent is None:
+            parent = self.tree.invisibleRootItem()
+
+        parent.removeChild(item)
+
+        for selected_item in self.tree.selectedItems():
+            selected_item.setSelected(False)
+
+        parent.setSelected(True)
+
+    def _add_to_interface(self):
+        sequencer_dock = QtGui.QWidget()
+        sequencer_vbox = QtGui.QVBoxLayout()
+
+        hbox = QtGui.QHBoxLayout()
+        hbox.setSpacing(10)
+        hbox.setContentsMargins(-1, 6, -1, 6)
+        hbox.addStretch()
+
+        sequencer_vbox.addWidget(self)
+        sequencer_vbox.addLayout(hbox)
+        sequencer_vbox.addStretch()
+        sequencer_dock.setLayout(sequencer_vbox)
+
+        dock = QtGui.QDockWidget('Sequencer')
+        dock.setWidget(sequencer_dock)
+        dock.setFeatures(QtGui.QDockWidget.NoDockWidgetFeatures)
+        self._parent.addDockWidget(QtCore.Qt.LeftDockWidgetArea, dock)
+
+    def queue_sequence(self):
+        sequence = self._generate_sequence_from_tree()
+        n = 0
+
+        for entry in sequence:
+            parameters = dict(ChainMap(*entry[::-1]))
+
+            procedure = self._parent.make_procedure()
+            procedure.set_parameters(parameters)
+            self._parent.queue(procedure=procedure)
+            n += 1
+
+        log.info(
+            "Queued {:d} measurements based on the entered sequences.".format(
+                n)
+        )
+
+    def load_sequence(self, *, fileName=None):
+        if fileName is None:
+            fileName, _ = QtGui.QFileDialog.getOpenFileName(self, 'OpenFile')
+
+        if len(fileName) == 0:
+            return
+
+        content = []
+
+        with open(fileName, "r") as file:
+            content = file.readlines()
+
+        pattern = re.compile("([-]+) \"(.*?)\", \"(.*?)\"")
+        for line in content:
+            line = line.strip()
+            match = pattern.search(line)
+
+            if not match:
+                continue
+
+            level = len(match.group(1)) - 1
+
+            if level < 0:
+                continue
+
+            parameter = match.group(2)
+            sequence = match.group(3)
+
+            at_root = False
+
+            self._add_tree_item(at_root, level, parameter, sequence)
+
+    def _generate_sequence_from_tree(self):
+        iterator = QtGui.QTreeWidgetItemIterator(self.tree)
+        sequences = []
+        current_sequence = [[] for i in range(self.MAXDEPTH)]
+        temp_sequence = [[] for i in range(self.MAXDEPTH)]
+
+        while iterator.value():
+            item = iterator.value()
+            depth = self._depth_of_child(item)
+
+            name = self.tree.itemWidget(item, 1).currentText()
+            parameter = self.names_inv[name]
+            values = self.eval_string(
+                self.tree.itemWidget(item, 2).text(),
+                name, depth,
+            )
+
+            try:
+                sequence_entry = [{parameter: value} for value in values]
+            except TypeError:
+                log.error(
+                    "TypeError, likely no sequence for one of the parameters"
+                )
+            else:
+                current_sequence[depth].extend(sequence_entry)
+
+            iterator += 1
+            next_depth = self._depth_of_child(iterator.value())
+
+            for depth_idx in range(depth, next_depth, -1):
+                temp_sequence[depth_idx].extend(current_sequence[depth_idx])
+
+                if depth_idx != 0:
+                    sequence_products = list(product(
+                        current_sequence[depth_idx - 1],
+                        temp_sequence[depth_idx]
+                    ))
+
+                    try:
+                        element = sequence_products[0][1]
+                    except IndexError:
+                        log.error(
+                            "IndexError, likely empty nested parameter"
+                        )
+                    else:
+                        if isinstance(element, tuple):
+                            sequence_products = [
+                                (v1, *v2) for (v1, v2) in sequence_products
+                            ]
+
+                    temp_sequence[depth_idx - 1].extend(sequence_products)
+                    temp_sequence[depth_idx] = []
+
+                current_sequence[depth_idx] = []
+                current_sequence[depth_idx - 1] = []
+
+        sequences = temp_sequence[0]
+
+        for idx in range(len(sequences)):
+            if not isinstance(sequences[idx], tuple):
+                sequences[idx] = (sequences[idx],)
+
+        return sequences
+
+    @staticmethod
+    def _depth_of_child(item):
+        depth = -1
+        while item:
+            item = item.parent()
+            depth += 1
+        return depth
+
+    @staticmethod
+    def eval_string(string, name=None, depth=None):
+        evaluated_string = None
+        if len(string) > 0:
+            try:
+                evaluated_string = eval(
+                    string, {"__builtins__": None}, SAFE_FUNCTIONS
+                )
+            except TypeError:
+                log.error("TypeError, likely a typo in one of the " +
+                          "functions for parameter {}, depth {}".format(
+                              name, depth
+                          ))
+            except SyntaxError:
+                log.error("SyntaxError, likely unbalanced brackets " +
+                          "for parameter {}, depth {}".format(name, depth))
+            except ValueError:
+                log.error("ValueError, likely wrong function argument " +
+                          "for parameter {}, depth {}".format(name, depth))
+        else:
+            log.error("No sequence entered for " +
+                      "for parameter {}, depth {}".format(name, depth))
+
+        evaluated_string = numpy.array(evaluated_string)
+        return evaluated_string
