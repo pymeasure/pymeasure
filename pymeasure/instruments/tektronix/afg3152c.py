a3d38009d693d6350526f2d839515e4c8be5d639
Stefano Pirotta <reblochon@riseup.net>
2019-10-14 19:53:42 +0200

    Upgrade with basic fuctions read and write

--- pymeasure/instruments/tektronix/afg3152c.py
+++ pymeasure/instruments/tektronix/afg3152c.py
@@ -23,40 +23,63 @@
 #
 from pymeasure.instruments import Instrument, RangeException
 from pymeasure.instruments.validators import strict_range, strict_discrete_set
-from math import sqrt,log10
 
 class Channel(object):
-
-        SOURCE_VALUES= [1,2]
+        SHAPES={
+            'sinusoidal':'SIN',
+            'square':'SQU',
+            'pulse':'PULS',
+            'ramp':'RAMP',
+            'prnoise':'PRN',
+            'dc':'DC',
+            'sinc':'SINC',
+            'gaussian':'GAUS',
+            'lorentz':'LOR',
+            'erise':'ERIS',
+            'edecay':'EDEC',
+            'haversine':'HAV'
+        }
         FREQ_LIMIT=[1e-6,150e6] #Frequeny limit for sinusoidal function
 
+        shape=Instrument.control(
+            "function:shape?","function:shape %s",
+            """ A string property that controls the shape of the output.
+            This property can be set.""",
+            validator=strict_discrete_set,
+            values=SHAPES,
+            map_values=True
+        )
 
         amplitude=Instrument.control(
-            "voltage:amplitude?","voltage:amplitude %e"
-            """ A floating point property that read the output power
-            in Vpp, Vrms or dBm for ch1."""
+            "voltage:amplitude?","voltage:amplitude %e",
+            """ A floating point property tha t read the output power
+            in Vpp, Vrms or dBm. This property can be set."""
         )
 
         offset=Instrument.control(
-            "source1:voltage:offset?","source1:voltage:offset %e",
+            "voltage:offset?","voltage:offset %e",
             """ A floating point property that represents the output power
-            in V for ch1. This property can be set."""
+            in V. This property can be set."""
         )
 
         frequency=Instrument.control(
-            "source1:frequency:fixed?","source1:frequency:fixed %e",
-            """ A floating point property that represents the frequency of ch1.
+            "frequency:fixed?","frequency:fixed %e",
+            """ A floating point property that represents the frequency.
             This property can be set.""",
             validator=strict_range,
             values=FREQ_LIMIT
         )
+        duty=Instrument.control(
+            "pulse:dcycle?","pulse:dcycle %d",
+            """ A floating point property that represents the duty cycle of pulse.
+            This property can be set.""",
+            validator=strict_range,
+            values=[0.001,99.999]
+        )
 
         def __init__(self, instrument, number):
             self.instrument=instrument
-            if number in self.SOURCE_VALUES:
-                self.number=number
-            else:
-                raise ValueError("%d: invalid input source provided" %number)
+            self.number=number
 
         def write(self, command):
             self.instrument.write("source%d:%s" %(self.number, command))
@@ -64,13 +87,28 @@ class Channel(object):
         def ask(self, command):
             self.instrument.ask("source%d:%s" %(self.number, command))
 
-        """def read(self, command):
-            self.instrument.read("source%d:%s?" %(self.number,command))"""
+        def read(self):
+            self.instrument.read()
+
         def enable(self):
             self.instrument.write("output%d:state on" %self.number)
 
         def disable(self):
             self.instrument.write("output%d:state off" %self.number)
+
+        def waveform(self,shape='SIN',frequency=1e6,units='VPP',amplitude=1,offset=0):
+            """General setting method for a complete wavefunction"""
+            self.instrument.write("source%d:function:shape %s" %(self.number,shape))
+            self.instrument.write("source%d:frequency:fixed %e" %(self.number,frequency))
+            self.instrument.write("source%d:voltage:unit %s" %(self.number,units))
+            self.instrument.write("source%d:voltage:amplitude %e%s" %(self.number,amplitude,units))
+            self.instrument.write("source%d:voltage:offset %eV" %(self.number,offset))
+
+        def values(self, command, **kwargs):
+            """ Reads a set of values from the instrument through the adapter,
+            passing on any key-word arguments.
+            """
+            return self.instrument.values("source%d:%s" %(self.number, command), **kwargs)
 
 class AFG3152C(Instrument):
     """Represents the Tektronix AFG 3000 series arbitrary function generator
@@ -78,11 +116,10 @@ class AFG3152C(Instrument):
     .. code-block:: python
 
         afg = AFG3152C("GPIB::1")
-
-        afg.unint_ch1='VPP'            # Sets units for CH1
-        afg.amplitude_ch1 = 1          # Sets the CH1 level to 1 VPP
-        afg.frequency_ch1=1e3          # Sets the CH1 frequency to 1KHz
-        afg.enable(1)                  # Enables the output from CH1
+        afg.ch1.shapes='sin'
+        afg.ch1.amplitude=1            # Sets the CH1 level to 1 VPP
+        afg.ch1.frequency=1e3          # Sets the CH1 frequency to 1KHz
+        afg.ch1.enable                  # Enables the output from CH1
 
     """
 
@@ -92,6 +129,5 @@ class AFG3152C(Instrument):
             "Tektronix AFG3152C arbitrary function generator",
             **kwargs
         )
-
-    self.ch1=Channel(self,1)
-    self.ch2=Channel(self,2)
+        self.ch1=Channel(self,1)
+        self.ch2=Channel(self,2)
