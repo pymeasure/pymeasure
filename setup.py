8aee08988adbe49ec2f2deca708d0e0af4fe2d9d
KOLANICH <kolan_n@mail.ru>
2020-03-03 14:07:20 +0300

    Moved the metadata into setup.cfg

--- setup.py
+++ setup.py
@@ -25,53 +25,5 @@
 
 from setuptools import setup, find_packages
 
-setup(
-    name='PyMeasure',
-    version='0.7.0',
-    author='PyMeasure Developers',
-    packages=find_packages(),
-    scripts=[],
-    url='https://github.com/ralph-group/pymeasure',
-    download_url='https://github.com/ralph-group/pymeasure/tarball/v0.7.0',
-    license='MIT License',
-    description='Scientific measurement library for instruments, experiments, and live-plotting',
-    long_description=open('README.rst').read() + "\n\n" + open('CHANGES.txt').read(),
-    install_requires=[
-        "numpy >= 1.6.1",
-        "pandas >= 0.14",
-        "pyvisa >= 1.8",
-        "pyserial >= 2.7",
-        "pyqtgraph >= 0.9.10"
-    ],
-    extras_require={
-        'matplotlib': ['matplotlib >= 2.0.2'],
-        'tcp': [
-            'zmq >= 16.0.2',
-            'cloudpickle >= 0.3.1'
-        ],
-        'python-vxi11': ['python-vxi11 >= 0.9']
-    },
-    setup_requires=[
-        'pytest-runner'
-    ],
-    tests_require=[
-        'pytest >= 2.9.1',
-        'pytest-qt >= 2.4.0'
-    ],
-    classifiers=[
-        "Development Status :: 4 - Beta",
-        "Intended Audience :: Science/Research",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: MacOS",
-        "Operating System :: Microsoft :: Windows",
-        "Operating System :: POSIX",
-        "Operating System :: Unix",
-        "Programming Language :: Python :: 3 :: Only",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Topic :: Scientific/Engineering",
-    ],
-    keywords="measure instrument experiment control automate graph plot"
-)
+if __name__ == "__main__":
+    setup()
