e97455eb9d700b57dc15e79cce8eaaba57115ec2
Harvey <hw-li@foxmail.com>
2020-06-12 21:59:09 +0800

    Add conductance test setting

--- pymeasure/instruments/microtest/microtest8761.py
+++ pymeasure/instruments/microtest/microtest8761.py
@@ -29,8 +29,8 @@ log = logging.getLogger(__name__)
 log.addHandler(logging.NullHandler())
 
 from pymeasure.instruments import Instrument
-from pymeasure.instruments.validators import truncated_range
-
+from pymeasure.instruments.validators import strict_discrete_set,\
+    strict_range
 
 from pymeasure.adapters import VISAAdapter
 
@@ -42,15 +42,51 @@ class Microtest8761(Instrument):
     It can also be used for microtest 8740 8751 and 8700
     
     .. code-block:: python
+        generator = Microtest8761("ASRLCOM8::INSTR")    #connect to instrument
 
+        generator.testitem_conductance = 'ON'           #set test item: conductance
+        generator.conductance_high = 10                 #set upper limit of conductance as 10 ohm
+        generator.conductance_low = 1                   #set lower limit of conductance as 1 ohm
+
+        data = generator.test()                         #begin test and return test data
     """
+
+    testitem_conductance = Instrument.control(
+        ":CONF:TESTITM:COND?", ":CONF:TESTITM:COND %f",
+        """ A boolean property that set test item: conductance test.
+        """,
+        validator=strict_discrete_set,
+        map_values=True,
+        values={True: 1, 'on': 1, 'ON': 1, False: 0, 'off': 0, 'OFF': 0},
+    )
+
     ##############################
-    # Conduction Resistance Test #
+    # Conductance Test #
     ##############################
+    conductance_high = Instrument.control(
+        ":CONF:COND:HILMT?", ":CONF:COND:HILMT %f",
+        """ A floating property that controls the upper limit conductance 
+        of the cable in ohm, from 0.001 ohm to 52 ohm (for microter 8761, 
+        other version maybe different). Can be set. """,
+        validator=strict_range,
+        values=[0.001, 52],
+    )
+
+    conductance_low = Instrument.control(
+        ":CONF:COND:HILMT?", ":CONF:COND:HILMT %f",
+        """ A floating property that controls the upper limit conductance 
+        of the cable in ohm, from 0.001 ohm to 52 ohm (for microter 8761,
+        oter version maybe different). Can be set. """,
+        validator=strict_range,
+        values=[0.001, 52],
+    )
+
+
     def test(self):
-        """ Read conduction resistance data.
+        """ Read Conductance data.
         """
         self.write(":KEY TEST")
+        self.read()
 
     def info(self):
         """ Dsiplay system information.
