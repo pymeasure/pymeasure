5081964dbbe6743a41f823aa7b4350f5e09cdd36
Harvey1024 <hw-li@foxmail.com>
2020-07-17 13:27:17 +0800

    Add test function

--- pymeasure/instruments/microtest/microtest8761.py
+++ pymeasure/instruments/microtest/microtest8761.py
@@ -23,77 +23,64 @@
 #
 
 
-import logging
+from pymeasure.adapters import VISAAdapter
 
+from pymeasure.instruments import Instrument
+import logging
+import re
+import time
 log = logging.getLogger(__name__)
 log.addHandler(logging.NullHandler())
 
-from pymeasure.instruments import Instrument
-from pymeasure.instruments.validators import strict_discrete_set,\
-    strict_range
-
-from pymeasure.adapters import VISAAdapter
-
 
 class Microtest8761(Instrument):
-    """ Represents the microtest 8761 cable harness tester
+    """ Represents the microtest 8761 cable harness tester,
     interface for interacting with the instrument.
 
     It can also be used for microtest 8740 8751 and 8700
-    
-    .. code-block:: python
-        generator = Microtest8761("ASRLCOM8::INSTR")    #connect to instrument
-
-        generator.testitem_conductance = 'ON'           #set test item: conductance
-        generator.conductance_high = 10                 #set upper limit of conductance as 10 ohm
-        generator.conductance_low = 1                   #set lower limit of conductance as 1 ohm
-
-        data = generator.test()                         #begin test and return test data
     """
 
-    testitem_conductance = Instrument.control(
-        ":CONF:TESTITM:COND?", ":CONF:TESTITM:COND %f",
-        """ A boolean property that set test item: conductance test.
-        """,
-        validator=strict_discrete_set,
-        map_values=True,
-        values={True: 1, 'on': 1, 'ON': 1, False: 0, 'off': 0, 'OFF': 0},
-    )
+    def __init__(self, adapter=VISAAdapter, **kwargs):
+        super().__init__(adapter, "microtest 8761", **kwargs)
 
-    ##############################
-    # Conductance Test #
-    ##############################
-    conductance_high = Instrument.control(
-        ":CONF:COND:HILMT?", ":CONF:COND:HILMT %f",
-        """ A floating property that controls the upper limit conductance 
-        of the cable in ohm, from 0.001 ohm to 52 ohm (for microter 8761, 
-        other version maybe different). Can be set. """,
-        validator=strict_range,
-        values=[0.001, 52],
-    )
-
-    conductance_low = Instrument.control(
-        ":CONF:COND:HILMT?", ":CONF:COND:HILMT %f",
-        """ A floating property that controls the upper limit conductance 
-        of the cable in ohm, from 0.001 ohm to 52 ohm (for microter 8761,
-        oter version maybe different). Can be set. """,
-        validator=strict_range,
-        values=[0.001, 52],
-    )
-
-
-    def test(self):
+    def test(self, timeout=2000):
         """ Read Conductance data.
+            timeout unit: ms
         """
-        self.write(":KEY TEST")
-        self.read()
+        self.adapter.write(":KEY TEST")
+        self.adapter.connection.timeout = timeout
+        data = self.adapter.connection.read_raw()
+        self.adapter.connection.read_raw()
+
+        return data
+
+        # self.data = self.query_binary_values(":KEY TEST")
 
     def info(self):
         """ Dsiplay system information.
         """
         self.write(":SYSTEM :INFO X1")
 
-    def __init__(self, adapter, **kwargs):
-        super(Microtest8761, self).__init__(
-            adapter, "Microtest 8761 cable harness tester", **kwargs
-        )
+    def dataFormat(self, data):
+        """Convert binary string to list.
+            returm list of name, resistance and unit.
+        """
+        # replace useless characters.
+        data = data.replace(b'\xea', b' ohm ')
+        data = data.replace(b'X ', b'')
+        data = data.replace(b'>', b'')
+        data = data.replace(b'\00', b'').decode('ascii')
+
+        # split string to list by \s
+        datalist = re.split(r'[\s]+', data)
+
+        namelist = []
+        resistance = []
+        unit = []
+        for i in range(len(datalist)):
+            if(datalist[i] == 'COND'):
+                namelist.append(datalist[i+1])
+                resistance.append(float(datalist[i+2]))
+                unit.append(datalist[i+3])
+
+        return namelist, resistance, unit
