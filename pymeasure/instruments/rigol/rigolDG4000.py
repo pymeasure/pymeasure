1a7070432ded6a2bbb50d04e4b641ab74f912373
Tabraze Malik <tabraze.malik@gmail.com>
2020-08-02 01:52:49 +0100

    Implemented basic function waveform support

--- pymeasure/instruments/rigol/rigolDG4000.py
+++ pymeasure/instruments/rigol/rigolDG4000.py
@@ -28,8 +28,9 @@ log.addHandler(logging.NullHandler())
 
 from pymeasure.instruments import Instrument
 from pymeasure.instruments.validators import (
-    truncated_range, truncated_discrete_set,
-    strict_discrete_set, strict_range, joined_validators
+    strict_discrete_set,
+    strict_range,
+    joined_validators
 )
 from pymeasure.adapters import VISAAdapter
 
@@ -37,6 +38,13 @@ from pymeasure.adapters import VISAAdapter
 class RigolDG4000(Instrument):
     """Represents any Rigol DG4000 series function generator
     """
+    current_settings = ({
+        'frequency':1000,
+        'amplitude':5,
+        'offset':0,
+        'phase':0,
+        'delay':0
+        })
 
     NVM_LOCATIONS = ['USER'+str(x) for x in range(1,11)]
     list_or_floats = joined_validators(strict_discrete_set,strict_range)
@@ -90,3 +98,42 @@ class RigolDG4000(Instrument):
 
     def display_sreen_saver_now(self):
         self.write(":DISPlay:SAVer:IMMediate")
+
+    def source_apply(self,channel,**kwargs):
+        std_parameters = ['frequency','amplitude','offset','phase']
+
+        # Need to accomodate different limits vs function and instrument.
+        allowed_parameter_values =({
+            'frequency':[1e-6,60e6],
+            'amplitude':[1e-3,10],
+            'offset':[-5,5],
+            'phase':[0,360],
+            'delay':[0,360]
+        })
+
+        allowed_waveform_parameters = ({
+            'CUSTom':std_parameters,
+            'HARMonic':std_parameters,
+            'NOISe':['amplitude','offset'],
+            'PULSe':['frequency','amplitude','offset','delay'],
+            'RAMP':std_parameters,
+            'SINusoid':std_parameters,
+            'SQUare':std_parameters,
+            'USER':std_parameters
+        })
+        list_or_floats = joined_validators(strict_discrete_set, strict_range)
+        allowed_waveforms = ['CUSTom', 'HARMonic', 'NOISe', 'PULSe', 'RAMP', 'SINusoid', 'SQUare', 'USER','CUST', 'HARM', 'NOIS', 'PULS', 'SIN', 'SQU']
+        if 'waveform' in kwargs:
+            waveform = strict_discrete_set(kwargs['waveform'],allowed_waveforms)
+            command = f":SOURce{channel}:APPLy:{kwargs['waveform']} "
+            for parameter in allowed_waveform_parameters[waveform]:
+                if parameter in kwargs:
+                    parameter_value = list_or_floats(kwargs[parameter],[["MIN","MAX"] ,allowed_parameter_values[parameter]])
+                    command = command +f"{parameter_value},"
+                else:
+                    command = command + f"{self.current_settings[parameter]},"
+        command = command[0:-1]
+        self.write(command)
+
+        for parameter in allowed_waveform_parameters[waveform]:
+            self.current_settings[parameter] = kwargs[parameter]
