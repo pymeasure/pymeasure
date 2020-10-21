445075b8a331c97db1c06abdd400c6a2b44865be
Christoph Buchner <bilderbuchi@phononoia.at>
2020-10-21 20:43:54 +0200

    Remove Python 3.5 support as it is EOL.

--- setup.py
+++ setup.py
@@ -67,7 +67,6 @@ setup(
         "Operating System :: POSIX",
         "Operating System :: Unix",
         "Programming Language :: Python :: 3 :: Only",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
