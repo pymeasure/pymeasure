2c2bc376cb8f723943c69be56f135990967661ee
Ian Sequeira <isequeir@uci.edu>
2020-10-26 22:23:47 -0700

    update to jdsypymeasure
    create scekoton class ready to add more plot widgets too

--- pymeasure/display/windows.py
+++ pymeasure/display/windows.py
@@ -892,3 +892,389 @@ class ManagedImageWindow(QtGui.QMainWindow):
         if not self.manager.experiments.has_next():
             self.abort_button.setEnabled(False)
             self.browser_widget.clear_button.setEnabled(True)
+
+
+class ManagedWindowMultiWidgets(QtGui.QMainWindow):
+    """
+    Abstract base class.
+
+    The ManagedWindow provides an interface for inputting experiment
+    parameters, running several experiments
+    (:class:`~pymeasure.experiment.procedure.Procedure`), plotting
+    result curves, and listing the experiments conducted during a session.
+
+    The ManagedWindow uses a Manager to control Workers in a Queue,
+    and provides a simple interface. The :meth:`~.queue` method must be
+    overridden by the child class.
+
+    .. seealso::
+
+        Tutorial :ref:`tutorial-managedwindow`
+            A tutorial and example on the basic configuration and usage of ManagedWindow.
+
+    .. attribute:: plot
+
+        The `pyqtgraph.PlotItem`_ object for this window. Can be
+        accessed to further customise the plot view programmatically, e.g.,
+        display log-log or semi-log axes by default, change axis range, etc.
+
+    .. _pyqtgraph.PlotItem: http://www.pyqtgraph.org/documentation/graphicsItems/plotitem.html
+
+
+    """
+    EDITOR = 'gedit'
+
+    def __init__(self, procedure_class, inputs=(), displays=(), x_axis=None, y_axis=None,
+                 log_channel='', log_level=logging.INFO, parent=None, sequencer=False,
+                 sequencer_inputs=None, sequence_file=None, inputs_in_scrollarea=False):
+        super().__init__(parent)
+        app = QtCore.QCoreApplication.instance()
+        app.aboutToQuit.connect(self.quit)
+        self.procedure_class = procedure_class
+        self.inputs = inputs
+        self.displays = displays
+        self.use_sequencer = sequencer
+        self.sequencer_inputs = sequencer_inputs
+        self.sequence_file = sequence_file
+        self.inputs_in_scrollarea = inputs_in_scrollarea
+        self.log = logging.getLogger(log_channel)
+        self.log_level = log_level
+        log.setLevel(log_level)
+        self.log.setLevel(log_level)
+        self.x_axis, self.y_axis = x_axis, y_axis
+        self._setup_ui()
+        self._layout()
+        self.setup_plot(self.plot)
+
+    def _setup_ui(self):
+        self.log_widget = LogWidget()
+        self.log.addHandler(self.log_widget.handler)  # needs to be in Qt context?
+        log.info("ManagedWindow connected to logging")
+
+        self.queue_button = QtGui.QPushButton('Queue', self)
+        self.queue_button.clicked.connect(self.queue)
+
+        self.abort_button = QtGui.QPushButton('Abort', self)
+        self.abort_button.setEnabled(False)
+        self.abort_button.clicked.connect(self.abort)
+
+        self.plot_widget = PlotWidget(self.procedure_class.DATA_COLUMNS, self.x_axis, self.y_axis)
+        self.plot = self.plot_widget.plot
+
+        self.browser_widget = BrowserWidget(
+            self.procedure_class,
+            self.displays,
+            [self.x_axis, self.y_axis],
+            parent=self
+        )
+        self.browser_widget.show_button.clicked.connect(self.show_experiments)
+        self.browser_widget.hide_button.clicked.connect(self.hide_experiments)
+        self.browser_widget.clear_button.clicked.connect(self.clear_experiments)
+        self.browser_widget.open_button.clicked.connect(self.open_experiment)
+        self.browser = self.browser_widget.browser
+
+        self.browser.setContextMenuPolicy(QtCore.Qt.CustomContextMenu)
+        self.browser.customContextMenuRequested.connect(self.browser_item_menu)
+        self.browser.itemChanged.connect(self.browser_item_changed)
+
+        self.inputs = InputsWidget(
+            self.procedure_class,
+            self.inputs,
+            parent=self
+        )
+
+        self.manager = Manager(self.plot, self.browser, log_level=self.log_level, parent=self)
+        self.manager.abort_returned.connect(self.abort_returned)
+        self.manager.queued.connect(self.queued)
+        self.manager.running.connect(self.running)
+        self.manager.finished.connect(self.finished)
+        self.manager.log.connect(self.log.handle)
+
+        if self.use_sequencer:
+            self.sequencer = SequencerWidget(
+                self.sequencer_inputs,
+                self.sequence_file,
+                parent=self
+            )
+
+    def _layout(self):
+        self.main = QtGui.QWidget(self)
+
+        inputs_dock = QtGui.QWidget(self)
+        inputs_vbox = QtGui.QVBoxLayout(self.main)
+
+        hbox = QtGui.QHBoxLayout()
+        hbox.setSpacing(10)
+        hbox.setContentsMargins(-1, 6, -1, 6)
+        hbox.addWidget(self.queue_button)
+        hbox.addWidget(self.abort_button)
+        hbox.addStretch()
+
+        if self.inputs_in_scrollarea:
+            inputs_scroll = QtGui.QScrollArea()
+            inputs_scroll.setWidgetResizable(True)
+            inputs_scroll.setFrameStyle(QtGui.QScrollArea.NoFrame)
+
+            self.inputs.setSizePolicy(1, 0)
+            inputs_scroll.setWidget(self.inputs)
+            inputs_vbox.addWidget(inputs_scroll, 1)
+
+        else:
+            inputs_vbox.addWidget(self.inputs)
+
+        inputs_vbox.addLayout(hbox)
+        inputs_vbox.addStretch(0)
+        inputs_dock.setLayout(inputs_vbox)
+
+        dock = QtGui.QDockWidget('Input Parameters')
+        dock.setWidget(inputs_dock)
+        dock.setFeatures(QtGui.QDockWidget.NoDockWidgetFeatures)
+        self.addDockWidget(QtCore.Qt.LeftDockWidgetArea, dock)
+
+        if self.use_sequencer:
+            sequencer_dock = QtGui.QDockWidget('Sequencer')
+            sequencer_dock.setWidget(self.sequencer)
+            sequencer_dock.setFeatures(QtGui.QDockWidget.NoDockWidgetFeatures)
+            self.addDockWidget(QtCore.Qt.LeftDockWidgetArea, sequencer_dock)
+
+        tabs = QtGui.QTabWidget(self.main)
+        tabs.addTab(self.plot_widget, "Results Graph")
+        tabs.addTab(self.log_widget, "Experiment Log")
+
+        splitter = QtGui.QSplitter(QtCore.Qt.Vertical)
+        splitter.addWidget(tabs)
+        splitter.addWidget(self.browser_widget)
+        self.plot_widget.setMinimumSize(100, 200)
+
+        vbox = QtGui.QVBoxLayout(self.main)
+        vbox.setSpacing(0)
+        vbox.addWidget(splitter)
+
+        self.main.setLayout(vbox)
+        self.setCentralWidget(self.main)
+        self.main.show()
+        self.resize(1000, 800)
+
+    def quit(self, evt=None):
+        self.close()
+
+    def browser_item_changed(self, item, column):
+        if column == 0:
+            state = item.checkState(0)
+            experiment = self.manager.experiments.with_browser_item(item)
+            if state == 0:
+                self.plot.removeItem(experiment.curve)
+            else:
+                experiment.curve.x = self.plot_widget.plot_frame.x_axis
+                experiment.curve.y = self.plot_widget.plot_frame.y_axis
+                experiment.curve.update()
+                self.plot.addItem(experiment.curve)
+
+    def browser_item_menu(self, position):
+        item = self.browser.itemAt(position)
+
+        if item is not None:
+            experiment = self.manager.experiments.with_browser_item(item)
+
+            menu = QtGui.QMenu(self)
+
+            # Open
+            action_open = QtGui.QAction(menu)
+            action_open.setText("Open Data Externally")
+            action_open.triggered.connect(
+                lambda: self.open_file_externally(experiment.results.data_filename))
+            menu.addAction(action_open)
+
+            # Change Color
+            action_change_color = QtGui.QAction(menu)
+            action_change_color.setText("Change Color")
+            action_change_color.triggered.connect(
+                lambda: self.change_color(experiment))
+            menu.addAction(action_change_color)
+
+            # Remove
+            action_remove = QtGui.QAction(menu)
+            action_remove.setText("Remove Graph")
+            if self.manager.is_running():
+                if self.manager.running_experiment() == experiment:  # Experiment running
+                    action_remove.setEnabled(False)
+            action_remove.triggered.connect(lambda: self.remove_experiment(experiment))
+            menu.addAction(action_remove)
+
+            # Use parameters
+            action_use = QtGui.QAction(menu)
+            action_use.setText("Use These Parameters")
+            action_use.triggered.connect(
+                lambda: self.set_parameters(experiment.procedure.parameter_objects()))
+            menu.addAction(action_use)
+            menu.exec_(self.browser.viewport().mapToGlobal(position))
+
+    def remove_experiment(self, experiment):
+        reply = QtGui.QMessageBox.question(self, 'Remove Graph',
+                                           "Are you sure you want to remove the graph?",
+                                           QtGui.QMessageBox.Yes |
+                                           QtGui.QMessageBox.No, QtGui.QMessageBox.No)
+        if reply == QtGui.QMessageBox.Yes:
+            self.manager.remove(experiment)
+
+    def show_experiments(self):
+        root = self.browser.invisibleRootItem()
+        for i in range(root.childCount()):
+            item = root.child(i)
+            item.setCheckState(0, QtCore.Qt.Checked)
+
+    def hide_experiments(self):
+        root = self.browser.invisibleRootItem()
+        for i in range(root.childCount()):
+            item = root.child(i)
+            item.setCheckState(0, QtCore.Qt.Unchecked)
+
+    def clear_experiments(self):
+        self.manager.clear()
+
+    def open_experiment(self):
+        dialog = ResultsDialog(self.procedure_class.DATA_COLUMNS, self.x_axis, self.y_axis)
+        if dialog.exec_():
+            filenames = dialog.selectedFiles()
+            for filename in map(str, filenames):
+                if filename in self.manager.experiments:
+                    QtGui.QMessageBox.warning(self, "Load Error",
+                                              "The file %s cannot be opened twice." % os.path.basename(
+                                                  filename))
+                elif filename == '':
+                    return
+                else:
+                    results = Results.load(filename)
+                    experiment = self.new_experiment(results)
+                    experiment.curve.update()
+                    experiment.browser_item.progressbar.setValue(100.)
+                    self.manager.load(experiment)
+                    log.info('Opened data file %s' % filename)
+
+    def change_color(self, experiment):
+        color = QtGui.QColorDialog.getColor(
+            initial=experiment.curve.opts['pen'].color(), parent=self)
+        if color.isValid():
+            pixelmap = QtGui.QPixmap(24, 24)
+            pixelmap.fill(color)
+            experiment.browser_item.setIcon(0, QtGui.QIcon(pixelmap))
+            experiment.curve.setPen(pg.mkPen(color=color, width=2))
+
+    def open_file_externally(self, filename):
+        # TODO: Make this function OS-agnostic
+        import subprocess
+        proc = subprocess.Popen([self.EDITOR, filename])
+
+    def make_procedure(self):
+        if not isinstance(self.inputs, InputsWidget):
+            raise Exception("ManagedWindow can not make a Procedure"
+                            " without a InputsWidget type")
+        return self.inputs.get_procedure()
+
+    def new_curve(self, results, color=None, **kwargs):
+        if color is None:
+            color = pg.intColor(self.browser.topLevelItemCount() % 8)
+        return self.plot_widget.new_curve(results, color=color, **kwargs)
+
+    def new_experiment(self, results, curve=None):
+        if curve is None:
+            curve = self.new_curve(results)
+        browser_item = BrowserItem(results, curve)
+        return Experiment(results, curve, browser_item)
+
+    def set_parameters(self, parameters):
+        """ This method should be overwritten by the child class. The
+        parameters argument is a dictionary of Parameter objects.
+        The Parameters should overwrite the GUI values so that a user
+        can click "Queue" to capture the same parameters.
+        """
+        if not isinstance(self.inputs, InputsWidget):
+            raise Exception("ManagedWindow can not set parameters"
+                            " without a InputsWidget")
+        self.inputs.set_parameters(parameters)
+
+    def queue(self):
+        """
+
+        Abstract method, which must be overridden by the child class.
+
+        Implementations must call ``self.manager.queue(experiment)`` and pass
+        an ``experiment``
+        (:class:`~pymeasure.experiment.experiment.Experiment`) object which
+        contains the
+        :class:`~pymeasure.experiment.results.Results` and
+        :class:`~pymeasure.experiment.procedure.Procedure` to be run.
+
+        For example:
+
+        .. code-block:: python
+
+            def queue(self):
+                filename = unique_filename('results', prefix="data") # from pymeasure.experiment
+
+                procedure = self.make_procedure() # Procedure class was passed at construction
+                results = Results(procedure, filename)
+                experiment = self.new_experiment(results)
+
+                self.manager.queue(experiment)
+
+        """
+        raise NotImplementedError(
+            "Abstract method ManagedWindow.queue not implemented")
+
+    def setup_plot(self, plot):
+        """
+        This method does nothing by default, but can be overridden by the child
+        class in order to set up custom options for the plot
+
+        This method is called during the constructor, after all other set up has
+        been completed, and is provided as a convenience method to parallel Plotter.
+
+        :param plot: This window's PlotItem instance.
+
+        .. _PlotItem: http://www.pyqtgraph.org/documentation/graphicsItems/plotitem.html
+        """
+        pass
+
+    def abort(self):
+        self.abort_button.setEnabled(False)
+        self.abort_button.setText("Resume")
+        self.abort_button.clicked.disconnect()
+        self.abort_button.clicked.connect(self.resume)
+        try:
+            self.manager.abort()
+        except:
+            log.error('Failed to abort experiment', exc_info=True)
+            self.abort_button.setText("Abort")
+            self.abort_button.clicked.disconnect()
+            self.abort_button.clicked.connect(self.abort)
+
+    def resume(self):
+        self.abort_button.setText("Abort")
+        self.abort_button.clicked.disconnect()
+        self.abort_button.clicked.connect(self.abort)
+        if self.manager.experiments.has_next():
+            self.manager.resume()
+        else:
+            self.abort_button.setEnabled(False)
+
+    def queued(self, experiment):
+        self.abort_button.setEnabled(True)
+        self.browser_widget.show_button.setEnabled(True)
+        self.browser_widget.hide_button.setEnabled(True)
+        self.browser_widget.clear_button.setEnabled(True)
+
+    def running(self, experiment):
+        self.browser_widget.clear_button.setEnabled(False)
+
+    def abort_returned(self, experiment):
+        if self.manager.experiments.has_next():
+            self.abort_button.setText("Resume")
+            self.abort_button.setEnabled(True)
+        else:
+            self.browser_widget.clear_button.setEnabled(True)
+
+    def finished(self, experiment):
+        if not self.manager.experiments.has_next():
+            self.abort_button.setEnabled(False)
+            self.browser_widget.clear_button.setEnabled(True)
