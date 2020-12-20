fa8987ad789c3d068bef8b95c4202c4efcd2bc6b
Michele Sardo <michele.sardo@st.com>
2020-12-20 18:30:07 +0100

    update method has been renamed to update_data to avoid clash with ImageItem update method.

--- pymeasure/display/curves.py
+++ pymeasure/display/curves.py
@@ -59,7 +59,7 @@ class ResultsCurve(pg.PlotDataItem):
             self._errorBars = pg.ErrorBarItem(pen=kwargs.get('pen', None))
             self.xerr, self.yerr = xerr, yerr
 
-    def update(self):
+    def update_data(self):
         """Updates the data by polling the results"""
         if self.force_reload:
             self.results.reload()
@@ -114,7 +114,7 @@ class ResultsImage(pg.ImageItem):
         self.translate(int(self.xstart/self.xstep)-0.5,
                        int(self.ystart/self.ystep)-0.5) # 0.5 so pixels centered
 
-    def update_img(self):
+    def update_data(self):
         if self.force_reload:
             self.results.reload()
 
