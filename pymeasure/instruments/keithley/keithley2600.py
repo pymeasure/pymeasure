7f0a74aab7daae848c754673a36fd78da07949ab
Daivesd <Daivesd@users.noreply.github.com>
2020-12-22 12:18:55 +0100

    Update keithley2600.py
    Introduced properties definition via Instrument.control

--- pymeasure/instruments/keithley/keithley2600.py
+++ pymeasure/instruments/keithley/keithley2600.py
@@ -25,6 +25,7 @@ import logging
 import time
 import numpy as np
 from pymeasure.instruments import Instrument
+from pymeasure.instruments.validators import truncated_range, strict_discrete_set
 
 # Setup logging
 log = logging.getLogger(__name__)
@@ -44,150 +45,118 @@ class Keithley2600(Instrument):
 
 class Channel(object):
 
-    @property
-    def source_output(self):
-        if self.instrument.ask('print(smu%s.source.output)' % self.channel)==1:
-            return 'output is ON'
-        elif self.instrument.ask('print(smu%s.source.output)' % self.channel)==0:
-            return 'output is OFF'
-    @source_output.setter
-    def source_output(self, state):
-        if state in ['on', 'off']:
-            statedict={
-                'off': 0,
-                'on': 1
-            }
-            self.instrument.write('smu%s.source.output=%d' % (self.channel,statedict[state]))
-        else:
-            raise ValueError('State has to be either on or off!')
+    source_output=Instrument.control(
+        'source_output', 'source_output=%d',
+        """Property controlling the channel output state (ON of OFF)
+        """, 
+        validator=strict_discrete_set,
+        values={'OFF': 0, 'ON': 1},
+        map_values=True
+    )
+    
+    source_mode=Instrument.control(
+        'source_func', 'source_func=%d',
+        """Property controlling the channel soource function (Voltage or Current)
+        """, 
+        validator=strict_discrete_set,
+        values={'Voltage': 1, 'Current': 0},
+        map_values=True
+    )
 
-    @property
-    def source_mode(self):
-        return self.instrument.ask('print(smu%s.source.func)' % self.channel)
-    @source_mode.setter
-    def source_mode(self, mode):
-        if mode in ['current', 'voltage']:
-            modedict={
-                "voltage": 1,
-                "current": 0
-            }
-            self.instrument.write('smu%s.source.func=%d' % (self.channel, modedict[mode]))
-        else:
-            raise ValueError('Mode has to be either current or voltage!')
+    measure_nplc=Instrument.control(
+        'measure.nplc', 'measure.nplc=%f',
+        """ Property controlling the nplc value """,
+        validator=truncated_range,
+        values=[0.001,25],
+        map_values=True
+    )
 
-    @property
-    def measure_nplc(self):
-        return self.instrument.ask('print(smu%s.measure.nplc)' % self.channel)
-    @measure_nplc.setter
-    def measure_nplc(self, nplc):
-        if nplc in range(0.001,25):
-            self.instrument.write('smu%s.measure.nplc=%f' % (self.channel, nplc))
-        else:
-            raise ValueError('NPLC has to be in the interval [0.001,25]!')
     ###############
     # Current (A) #
     ###############
-    @property
-    def current(self):
-        return float(self.instrument.ask('print(smu%s.measure.i())' % self.channel))
+    current=Instrument.measurement(
+        'measure.i()',
+        """ Reads the current in Amps """
+    )
 
-    @property
-    def source_current(self):
-        return float(self.instrument.ask('print(smu%s.source.leveli)' % self.channel))
-    @source_current.setter
-    def source_current(self, current):
-        self.instrument.write('smu%s.source.leveli=%f' % (self.channel, current))
+    source_current=Instrument.control(
+        'source.leveli', 'source.leveli=%f',
+        """ Property controlling the applied source current """,
+        validator=truncated_range,
+        values=[-1.5,1.5]
+    )
 
-    @property
-    def compliance_current(self):
-        return float(self.instrument.ask('print(smu%s.source.limiti)' % self.channel))
-    @compliance_current.setter
-    def compliance_current(self, complvalue):
-        self.instrument.write('smu%s.source.limiti=%f' % (self.channel, complvalue))
+    compliance_current=Instrument.control(
+        'source.limiti', 'source.limit=%f',
+        """ Property controlling the source compliance current """,
+        validator=truncated_range,
+        values=[-1.5,1.5]
+    )
 
-    @property
-    def source_current_range(self):
-        return float(self.instrument.ask('print(smu%s.source.rangei' % self.channel))
-    @source_current_range.setter
-    def source_current_range(self, rng):
-        if rng in range(-1.5,1.5):
-            self.instrument.write('smu%s.source.rangei=%f' % (self.channel, range))
-        else:
-            raise ValueError('Source current range has to be in the interval [-1.5,1.5]!')
+    source_current_range=Instrument.control(
+        'source.rangei', 'source.rangei=%f',
+        """Property controlling the source current range """,
+        validator=truncated_range,
+        values=[-1.5,1.5]
+    )
 
-    @property
-    def current_range(self):
-        return float(self.instrument.ask('print(smu%s.measure.rangei' % self.channel))
-    @current_range.setter
-    def current_range(self, rng):
-        if rng in range(-1.5,1.5):
-            self.instrument.write('smu%s.measure.rangei=%f' % (self.channel, rng))
-        else:
-            raise ValueError('Source current range has to be in the interval [-1.5,1.5]!')
+    current_range=Instrument.control(
+        'measure.rangei', 'measure.rangei=%f',
+        """Property controlling the measurement current range """,
+        validator=truncated_range,
+        values=[-1.5,1.5]
+    )
+    
     ###############
     # Voltage (V) #
     ###############
+    voltage=Instrument.measurement(
+        'measure.v()',
+        """ Reads the voltage in Volts """
+    )
+    
+    source_voltage=Instrument.control(
+        'source.levelv', 'source.levelv=%f',
+        """ Property controlling the applied source voltage """,
+        validator=truncated_range,
+        values=[-200,200]
+    )
 
-    @property
-    def voltage(self):
-        return float(self.instrument.ask('print(smu%s.measure.v())' % self.channel))
-    @property
-    def source_voltage(self):
-        return float(self.instrument.ask('print(smu%s.source.levelv)' % self.channel))
-    @source_voltage.setter
-    def source_voltage(self, voltage):
-        self.instrument.write('smu%s.source.levelv=%f' % (self.channel,voltage))
+    compliance_voltage=Instrument.control(
+        'source.limitv', 'source.limiv=%f',
+        """ Property controlling the source compliance voltage """,
+        validator=truncated_range,
+        values=[-200,200]
+    )
 
-    @property
-    def compliance_voltage(self):
-        return float(self.instrument.ask('print(smu%s.source.limitv)' % self.channel))
-    @compliance_voltage.setter
-    def compliance_voltage(self, complvalue):
-        if complvalue in range(-200,200):
-            self.instrument.write('smu%s.source.limitv=%f' % (self.channel, complvalue))
-        else:
-            raise ValueError('Compliance voltage must be in the range [-200,200]!')
+    source_voltage_range=Instrument.control(
+        'source.rangev', 'source.rangev=%f',
+        """Property controlling the source current range """,
+        validator=truncated_range,
+        values=[-200,200]
+    )
 
-    @property
-    def source_voltage_range(self):
-        return float(self.instrument.ask('print(smu%s.source.rangev' % self.channel))
-    @source_voltage_range.setter
-    def source_voltage_range(self, rng):
-        if rng in range(-200,200):
-            self.instrument.write('smu%s.source.rangev=%f' % (self.channel, rng))
-        else:
-            raise ValueError('Source voltage range has to be in the interval [-200,200]!')
+    voltage_range=Instrument.control(
+        'measure.rangev', 'measure.rangev=%f',
+        """Property controlling the measurement voltage range """,
+        validator=truncated_range,
+        values=[-200,200]
+    )
 
-    @property
-    def voltage_range(self):
-        return float(self.instrument.ask('print(smu%s.measure.rangev' % self.channel))
-    @voltage_range.setter
-    def voltage_range(self, rng):
-        if rng in range(-200,200):
-            self.instrument.write('smu%s.measure.rangev=%f' % (self.channel, rng))
-        else:
-            raise ValueError('Source current range has to be in the interval [-200,200]!')
     ####################
     # Resistance (Ohm) #
     ####################
+    resistance=Instrument.measurement(
+        'measure.r()',
+        """ Reads the resistance in Ohms """
+    )
 
-    @property
-    def resistance(self):
-        return float(self.instrument.ask('print(smu%s.measure.r())' % self.channel))
-
-    @property
-    def wires_mode(self):
-        return self.instrument.ask('print(smu%s.sense)' % self.channel)
-    @wires_mode.setter
-    def wires_mode(self, mode):
-        if mode in ['4', '2']:
-            modedict = {
-                "4": 1,
-                "2": 0
-            }
-            self.instrument.write('smu%s.sense=%d' % (self.channel, modedict[mode]))
-        else:
-            raise ValueError('Wire mode has to be either 4 (wires) or 2 (wires)!')
+    wires_mode=Instrument.control(
+        'sense', 'sense=%d',
+        """Property controlling the resistance measurement mode: 4 wires or 2 wires""",
+        validator=strict_discrete_set,
+        values={'4': 1, '2': 0}
+    )
     ###########
     # Methods #
     ###########
@@ -197,10 +166,10 @@ class Channel(object):
         self.channel = channel
 
     def ask(self, cmd):
-        return self.instrument.ask(cmd)
+        return float(self.instrument.ask('print(smu%s.%s)' %(self.channel,cmd)))
 
     def write(self, cmd):
-        self.instrument.write(cmd)
+        self.instrument.write('smu%s.%s' %(self.channel,cmd))
 
     def measure_voltage(self, nplc=1, voltage=21.0, auto_range=True):
         """ Configures the measurement of voltage.
@@ -209,10 +178,10 @@ class Channel(object):
         :param auto_range: Enables auto_range if True, else uses the set voltage
         """
         log.info("%s is measuring voltage." % self.channel)
-        self.write('smu%s.measure.v()' % self.channel)
-        self.write('smu%s.measure.nplc=%f' % (self.channel, nplc))
+        self.instrument.write('smu%s.measure.v()' % self.channel)
+        self.instrument.write('smu%s.measure.nplc=%f' % (self.channel, nplc))
         if auto_range:
-            self.write('smu%s.measure.autorangev=1' % self.channel)
+            self.instrument.write('smu%s.measure.autorangev=1' % self.channel)
         else:
             self.voltage_range = voltage
         self.check_errors()
@@ -224,10 +193,10 @@ class Channel(object):
         :param auto_range: Enables auto_range if True, else uses the set current
         """
         log.info("%s is measuring current." % self.channel)
-        self.write('smu%s.measure.i()' % self.channel)
-        self.write('smu%s.measure.nplc=%f' % (self.channel, nplc))
+        self.instrument.write('smu%s.measure.i()' % self.channel)
+        self.instrument.write('smu%s.measure.nplc=%f' % (self.channel, nplc))
         if auto_range:
-            self.write('smu%s.measure.autorangei=1' % self.channel)
+            self.instrument.write('smu%s.measure.autorangei=1' % self.channel)
         else:
             self.current_range = current
         self.check_errors()
@@ -236,17 +205,17 @@ class Channel(object):
         """ Configures the source to use an automatic range.
         """
         if self.source_mode == 'current':
-            self.write('smu%s.source.autorangei=1' % self.channel)
+            self.instrument.write('smu%s.source.autorangei=1' % self.channel)
         else:
-            self.write('smu%s.source.autorangev=1' % self.channel)
+            self.instrument.write('smu%s.source.autorangev=1' % self.channel)
 
     def apply_current(self, current_range=None, compliance_voltage=0.1):
         """ Configures the instrument to apply a source current, and
                 uses an auto range unless a current range is specified.
                 The compliance voltage is also set.
                 :param compliance_voltage: A float in the correct range for a
-                                           :attr:`~.Keithley2400.compliance_voltage`
-                :param current_range: A :attr:`~.Keithley2400.current_range` value or None
+                                           :attr:`~.Keithley2600.compliance_voltage`
+                :param current_range: A :attr:`~.Keithley2600.current_range` value or None
                 """
         log.info("%s is sourcing current." % self.channel)
         self.source_mode = 'current'
@@ -263,8 +232,8 @@ class Channel(object):
         uses an auto range unless a voltage range is specified.
         The compliance current is also set.
         :param compliance_current: A float in the correct range for a
-                                   :attr:`~.Keithley2400.compliance_current`
-        :param voltage_range: A :attr:`~.Keithley2400.voltage_range` value or None
+                                   :attr:`~.Keithley2600.compliance_current`
+        :param voltage_range: A :attr:`~.Keithley2600.voltage_range` value or None
         """
         log.info("%s is sourcing voltage." % self.name)
         self.source_mode = 'voltage'
