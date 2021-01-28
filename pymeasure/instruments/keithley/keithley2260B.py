9b2691cec56d2fb7565c01db827d9c32295a0930
Benjamin Klebel <32774645+bklebel@users.noreply.github.com>
2021-01-28 16:06:53 +0100

    final (pre-reviewed) commit for minimal implementation of DC power supply

--- pymeasure/instruments/keithley/keithley2260B.py
+++ pymeasure/instruments/keithley/keithley2260B.py
@@ -1,10 +1,10 @@
 from pymeasure.instruments import Instrument
 from pymeasure.instruments.validators import (
-    truncated_range,
-    truncated_discrete_set,
+    # truncated_range,
+    # truncated_discrete_set,
     strict_discrete_set,
 )
-from pymeasure.adapters import VISAAdapter
+# from pymeasure.adapters import VISAAdapter
 from .buffer import KeithleyBuffer
 
 import logging
@@ -17,21 +17,25 @@ class Keithley2260B(Instrument, KeithleyBuffer):
     """ Represents the Keithley 2260B Power Supply (minimal implementation)
     and provides a high-level interface for interacting with the instrument.
 
-    # .. code-block:: python
+    For a connection through tcpip, the socket is hardcoded to 2268,
+        example connection string: 'TCPIP::xxx.xxx.xxx.xxx::2268::SOCKET'
+        the read termination is \n
 
-    #     source = Keithley2260B("GPIB::1")
-    #     source.voltage = 1
-    #     print(source.voltage)
-    #     print(source.current)
-    #     print(source.voltage)
+    .. code-block:: python
+
+        source = Keithley2260B("GPIB::1")
+        source.voltage = 1
+        print(source.voltage)
+        print(source.current)
+        print(source.power)
+        print(source.applied)
 
     """
 
     enabled = Instrument.control(
         "OUTPut?", "OUTPut %d",
         """A boolean property that controls whether the source is enabled, takes
-        values True or False. The convenience methods :meth:`~.Keithley6221.enable_source` and
-        :meth:`~.Keithley6221.disable_source` can also be used.""",
+        values True or False (1 or 0). """,
         validator=strict_discrete_set,
         values={True: 1, False: 0},
         map_values=True
@@ -40,38 +44,30 @@ class Keithley2260B(Instrument, KeithleyBuffer):
     current = Instrument.control(
         ":SOUR:CURR?", ":SOUR:CURR %g",
         """ A floating point property that controls the source current
-        in amps. """,
-        # validator=truncated_range,
-        # values=[-0.105, 0.105]
+        in amps. The allowed range is not fixed. """,
     )
 
     voltage = Instrument.control(
         ":SOUR:VOLT?", ":SOUR:VOLT %g",
         """ A floating point property that controls the source voltage
-        in volts. """,
-        # validator=truncated_range,
-        # values=[-0.105, 0.105]
+        in volts. The allowed range is not fixed. """,
     )
 
     power = Instrument.measurement(
         ":MEAS:POW?",
-        """ Reads the power the dc power supply is putting out. """
+        """ Reads the power (in watts) the dc power supply is putting out.  """
+    )
+
+    applied = Instrument.measurement(
+        ":APPly?",
+        """ Reads the applied voltage (volts) and current (amps), returns as list."""
     )
 
     def __init__(self, adapter, **kwargs):
-        # 'TCPIP::xxx.xxx.xxx.xxx::2268::SOCKET'
         super().__init__(adapter, "Keithley 2260B DC Power Supply", **kwargs)
-        # Set up data transfer format
-        if isinstance(self.adapter, VISAAdapter):
-            self.adapter.config(
-                is_binary=False, datatype="float32", converter="f", separator=","
-            )
-            self.adapter.connection.read_termination = "\r"
+        # TODO: implement the read termination in the proper way, if there is a proper way
+        self.adapter.connection.read_termination = "\n"
 
-    @property
-    def apply(self):
-        return self.ask("apply?")
-
-    @apply.setter
     def apply(self, voltage, current):
-        self.write("apply %g,%g".format(voltage, current))
+        """convenience function to set voltage (volts) and current (amps) at once."""
+        self.write("apply {:g},{:g}".format(voltage, current))
