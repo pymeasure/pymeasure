f513a0f60e616259a1b721afa579bc976cdb0403
Benjamin Klebel <32774645+bklebel@users.noreply.github.com>
2021-01-28 14:44:22 +0100

    connection trial 1

--- pymeasure/instruments/keithley/keithley2260B.py
+++ pymeasure/instruments/keithley/keithley2260B.py
@@ -1,14 +1,14 @@
-
-
 from pymeasure.instruments import Instrument
 from pymeasure.instruments.validators import (
-    truncated_range, truncated_discrete_set,
-    strict_discrete_set
+    truncated_range,
+    truncated_discrete_set,
+    strict_discrete_set,
 )
 from pymeasure.adapters import VISAAdapter
 from .buffer import KeithleyBuffer
 
 import logging
+
 log = logging.getLogger(__name__)
 log.addHandler(logging.NullHandler())
 
@@ -26,15 +26,12 @@ class Keithley2260B(Instrument, KeithleyBuffer):
     """
 
     def __init__(self, adapter, **kwargs):
-        super().__init__(
-            adapter, "Keithley 2260B DC Power Supply", **kwargs
-        )
+        # 'TCPIP::192.168.2.104::1865::SOCKET'
+        # 'TCPIP::192.168.3.102::2268::SOCKET'
+        # 'TCPIP::192.168.3.5::2268::SOCKET'
+        super().__init__(adapter, "Keithley 2260B DC Power Supply", **kwargs)
         # Set up data transfer format
-        # if isinstance(self.adapter, VISAAdapter):
-        #     self.adapter.config(
-        #         is_binary=False,
-        #         datatype='float32',
-        #         converter='f',
-        #         separator=','
-        #     )
-
+        if isinstance(self.adapter, VISAAdapter):
+            self.adapter.config(
+                is_binary=False, datatype="float32", converter="f", separator=","
+            )
