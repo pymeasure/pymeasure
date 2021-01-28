58fe440a91ac9a606e8ff3c4d5acca67dc25104d
Benjamin Klebel <32774645+bklebel@users.noreply.github.com>
2021-01-28 15:11:36 +0100

    minimal current and voltage control

--- pymeasure/instruments/keithley/keithley2260B.py
+++ pymeasure/instruments/keithley/keithley2260B.py
@@ -19,19 +19,59 @@ class Keithley2260B(Instrument, KeithleyBuffer):
 
     # .. code-block:: python
 
-    #     meter = Keithley2000("GPIB::1")
-    #     meter.measure_voltage()
-    #     print(meter.voltage)
+    #     source = Keithley2260B("GPIB::1")
+    #     source.voltage = 1
+    #     print(source.voltage)
+    #     print(source.current)
+    #     print(source.voltage)
 
     """
 
+    enabled = Instrument.control(
+        "OUTPut?", "OUTPut %d",
+        """A boolean property that controls whether the source is enabled, takes
+        values True or False. The convenience methods :meth:`~.Keithley6221.enable_source` and
+        :meth:`~.Keithley6221.disable_source` can also be used.""",
+        validator=strict_discrete_set,
+        values={True: 1, False: 0},
+        map_values=True
+    )
+
+    current = Instrument.control(
+        ":SOUR:CURR?", ":SOUR:CURR %g",
+        """ A floating point property that controls the source current
+        in amps. """,
+        # validator=truncated_range,
+        # values=[-0.105, 0.105]
+    )
+
+    voltage = Instrument.control(
+        ":SOUR:VOLT?", ":SOUR:VOLT %g",
+        """ A floating point property that controls the source voltage
+        in volts. """,
+        # validator=truncated_range,
+        # values=[-0.105, 0.105]
+    )
+
+    power = Instrument.measurement(
+        ":MEAS:POW?",
+        """ Reads the power the dc power supply is putting out. """
+    )
+
     def __init__(self, adapter, **kwargs):
-        # 'TCPIP::192.168.2.104::1865::SOCKET'
-        # 'TCPIP::192.168.3.102::2268::SOCKET'
-        # 'TCPIP::192.168.3.5::2268::SOCKET'
+        # 'TCPIP::xxx.xxx.xxx.xxx::2268::SOCKET'
         super().__init__(adapter, "Keithley 2260B DC Power Supply", **kwargs)
         # Set up data transfer format
         if isinstance(self.adapter, VISAAdapter):
             self.adapter.config(
                 is_binary=False, datatype="float32", converter="f", separator=","
             )
+            self.adapter.connection.read_termination = "\r"
+
+    @property
+    def apply(self):
+        return self.ask("apply?")
+
+    @apply.setter
+    def apply(self, voltage, current):
+        self.write("apply %g,%g".format(voltage, current))
