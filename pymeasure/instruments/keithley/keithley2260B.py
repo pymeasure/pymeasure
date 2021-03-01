042800ea3f96bd02e9bc2148576098aaaf563447
Benjamin Klebel <32774645+bklebel@users.noreply.github.com>
2021-03-01 10:10:29 +0100

    fixed imports

--- pymeasure/instruments/keithley/keithley2260B.py
+++ pymeasure/instruments/keithley/keithley2260B.py
@@ -1,11 +1,5 @@
 from pymeasure.instruments import Instrument
-from pymeasure.instruments.validators import (
-    # truncated_range,
-    # truncated_discrete_set,
-    strict_discrete_set,
-)
-# from pymeasure.adapters import VISAAdapter
-from .buffer import KeithleyBuffer
+from pymeasure.instruments.validators import strict_discrete_set
 
 import logging
 
