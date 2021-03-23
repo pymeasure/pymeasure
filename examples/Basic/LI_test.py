3b36a6014bc5a470687ec093e388873318b7fd07
Casper Schippers <Casperschippers@hotmail.com>
2021-03-24 00:27:02 +0100

    Removed setup-specific testing/debugging file

--- examples/Basic/LI_test.py
+++
@@ -1,5 +0,0 @@
-from pymeasure.instruments.signalrecovery import DSP7265
-
-if __name__ == '__main__':
-    li = DSP7265("visa://phys8173.campus.tue.nl/GPIB0::11::INSTR")
-    print(li.curve_buffer_length)
