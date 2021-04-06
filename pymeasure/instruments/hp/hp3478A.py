9b6e69556360d4cc675c929f7665fba0582ab928
LongnoseRob/JI1MNC <longnoserob@gmail.com>
2021-04-06 23:50:47 +0900

    reowkring some  lists to dicts adding closing and reset features

--- pymeasure/instruments/hp/hp3478A.py
+++ pymeasure/instruments/hp/hp3478A.py
@@ -42,22 +42,42 @@ class HP3478A(Instrument):
 
     As this unit predates SCPI some tricks are required to get this working
     """
-    DIGITS=['n/a','5 digits','4 digits','3 digits']
 
-    MODES=['n/a','DC Volts','AC Volts','Ohms (2-wire)','Ohms (4-wire)',
-           'DC current','AC current','Extended Ohms']
+    DIGITS={3: "N3",
+            4: "N4",
+            5: "N5"
+            }
 
-    RANGES=[['n/a'],
-                ['n/a','30 mV DC','300 mV DC','3 V DC','30 V DC','300 V DC'],
-                ['n/a','300 mV AC','3 V AC','30 V AC','300 V AC'],
-                ['n/a', '30 Ohm', '300 Ohm', '3 kOhm', '30 kOhm', '300 kOhm',
-                 '3 MOhm','30 MOhm'],
-                ['n/a', '30 Ohm', '300 Ohm', '3 kOhm', '30 kOhm', '300 kOhm',
-                 '3 MOhm','30 MOhm'],
-                ['n/a','300 mA DC','3 A DC'],
-                ['n/a','300 mA AC','3 A AC'],
-                ['n/a','extended Ohms']]
+    AZ={"off": "Z0",
+        "on": "Z1"
+        }
 
+    MODES={"DCV": "F1",
+           "ACV": "F2",
+           "R2W": "F3",
+           "R4W": "F4",
+           "DCI": "F5",
+           "ACI": "F6",
+           "Rext": "F7"
+           }
+
+    RANGES={"DCV": {3E-2: "R-2",3E-1: "R-1",3: "R0",30: "R1",300:"R2","auto": "RA"},
+            "ACV": {3E-1: "R-1",3: "R0",30: "R1" ,300: "R2","auto": "RA"},
+            "R2W": {30: "R1",300: "R2", 3E3: "R3",3E4: "R4",3E5: "R5", 3E6: "R6", 3E7: "R7","auto": "RA"},
+            "R4W": {30: "R1",300: "R2", 3E3: "R3",3E4: "R4",3E5: "R5", 3E6: "R6", 3E7: "R7","auto": "RA"},
+            "DCI": {3E-1: "R-1",3: "R0","auto": "RA"},
+            "ACI": {3E-1: "R-1",3: "R0","auto": "RA"},
+            "Rext": {3E6: "R7","auto": "RA"}
+            }
+
+    TRIGGERS={
+        "auto": "T1",
+        "internal": "T1",
+        "external": "T2",
+        "single": "T3",
+        "hold": "T4",
+        "fast": "T5"
+        }
 
     def __init__(self, resourceName, **kwargs):
         super(HP3478A, self).__init__(
@@ -69,7 +89,7 @@ class HP3478A(Instrument):
             **kwargs
         )
 
-    @property
+
     def error_status(self):
         """
         get the Error-status register value
@@ -83,7 +103,6 @@ class HP3478A(Instrument):
         estatus=self.ask('E')
         return estatus
 
-    @property
     def status(self):
         """
         Reads the HP3478A status register (5-bytes)
@@ -94,35 +113,34 @@ class HP3478A(Instrument):
             DESCRIPTION.
 
         """
-        status_read=self.values('B')
+        status_read=self.adapter.connection.query_binary_values("B","b",data_points=5)
         status=[]
         for i in range(0,len(status_read)):
             # print(bin(status_read[i]))
             status.append(status_read[i])
         return status
 
-    @property
-    def status_readable(self):
-        """
-        Delivers a human-readble status based on the status() command
+    # def status_readable(self):
+    #     """
+    #     Delivers a human-readble status based on the status() command
 
-        Returns
-        -------
-        None.
+    #     Returns
+    #     -------
+    #     None.
 
-        """
-        status_read=self.status()
-        d_select=status_read[0]&3
-        r_select=(status_read[0]>>2)&7
-        m_select=(status_read[0]>>5)&7
-        print(RANGES[m_select][r_select])
-        print(DIGITS[d_select])
-        print(MODES[m_select])
-        #TODO: add decodung on status byte, e.g. AZ on/off, Front/rear selection
-        print('General status: ',status_read[1])
-        print('srq mask: ',status_read[2])
-        print('Error status: ',status_read[3])
-        print('DAC value: ',status_read[4])
+    #     """
+    #     status_read=self.status()
+    #     d_select=status_read[0]&3
+    #     r_select=(status_read[0]>>2)&7
+    #     m_select=(status_read[0]>>5)&7
+    #     print(RANGES[m_select][r_select])
+    #     print(DIGITS[d_select])
+    #     print(MODES[m_select])
+    #     #TODO: add decodung on status byte, e.g. AZ on/off, Front/rear selection
+    #     print('General status: ',status_read[1])
+    #     print('srq mask: ',status_read[2])
+    #     print('Error status: ',status_read[3])
+    #     print('DAC value: ',status_read[4])
 
     @property
     def auto_zero(self):
@@ -163,7 +181,6 @@ class HP3478A(Instrument):
             if on_off==1:
                 self.write('Z1')
 
-    @property
     def active_connectors(self):
         """
         checks the status of the front/back switch, selecting either the front
@@ -181,8 +198,8 @@ class HP3478A(Instrument):
         front_back=status_read[1]>>4&1
         return front_back
 
-    
-    def measure(self,mode='DC Volts',measurement_range='auto',auto_zero=1,digits=5, trigger='auto'):
+
+    def measure(self,mode='DCV',measurement_range='auto',auto_zero="on",digits=5, trigger='auto'):
         """
         returens a measurement result, depeding on the parameters
         see also the other measure_ functions for the HP3478A.
@@ -208,38 +225,32 @@ class HP3478A(Instrument):
             DESCRIPTION.
 
         """
-        measurement_string='F'
+        measurement_string=''
         if mode in self.MODES:
-            measurement_string=measurement_string+str(self.MODES.index(mode))
+            measurement_string=self.MODES[mode]
         else:
             raise Exception('Mode string not supported')
-        
-        if measurement_range=='auto':
-            measurement_string=measurement_string+('RA')
-            
-        if measurement_range in self.RANGES[self.MODES.index(mode)]:
-            measurement_string=measurement_string+'R'
-            if self.MODES.index(mode)==1:
-                measurement_string=measurement_string+str(self.RANGES[self.MODES.index(mode)].index(measurement_range)-3)
-            if self.MODES.index(mode)==2:
-                measurement_string=measurement_string+str(self.RANGES[self.MODES.index(mode)].index(measurement_range)-2)
-            if self.MODES.index(mode)==3:
-                measurement_string=measurement_string+str(self.RANGES[self.MODES.index(mode)].index(measurement_range)-1)
-            if self.MODES.index(mode)==4:
-                measurement_string=measurement_string+str(self.RANGES[self.MODES.index(mode)].index(measurement_range)-1)
-            if self.MODES.index(mode)==5:
-                measurement_string=measurement_string+str(self.RANGES[self.MODES.index(mode)].index(measurement_range)-2)
-            if self.MODES.index(mode)==6:
-                measurement_string=measurement_string+str(self.RANGES[self.MODES.index(mode)].index(measurement_range)-2)                
-            if self.MODES.index(mode)==7:
-                raise Exception("nbot yet implemented")
-                
-        # measurement_string='F1RAN5T1Z1'
-        measured_value=self.ask(measurement_string)
+
+        if measurement_range in self.RANGES[mode]:
+            measurement_string=measurement_string+self.RANGES[mode][measurement_range]
+
+        if auto_zero in self.AZ:
+            measurement_string=measurement_string+self.AZ[auto_zero]
+
+        if digits in self.DIGITS:
+            measurement_string=measurement_string+self.DIGITS[digits]
+
+        if trigger in self.TRIGGERS:
+            measurement_string=measurement_string+self.TRIGGERS[trigger]
+
+        # print(measurement_string)
+        measured_value=float(self.ask(measurement_string))
         return measured_value
-    
-    
+
+
     def display_reset(self):
+
+
         """
         Resets the display of the HP3478A to the normal measurement dusplay.
         Especially helpful after using D2 or D3 command
@@ -250,5 +261,14 @@ class HP3478A(Instrument):
 
         """
         self.write('D1')
-        
-        
+
+    def reset(self):
+        self.adapter.connection.clear()
+
+    def close(self):
+        self.adapter.connection.close()
+
+    def shutdown(self):
+        self.adapter.connection.clear()
+        self.adapter.connection.close()
+
