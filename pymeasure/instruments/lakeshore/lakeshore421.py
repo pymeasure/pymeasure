d0932e336ee6f5ca62f3e95a8a5a58d01f6b29d0
Casper Schippers <casperschippers@hotmail.com>
2021-04-22 10:38:01 +0200

    Change units of Range from gauss to tesla (SI)

--- pymeasure/instruments/lakeshore/lakeshore421.py
+++ pymeasure/instruments/lakeshore/lakeshore421.py
@@ -45,7 +45,7 @@ class LakeShore421(Instrument):
     """
 
     MULTIPLIERS = {1e3: 'k', 1: '', 1e-3: 'm', 1e-6: 'n'}
-    RANGES = {30: 0, 300: 1, 3000: 2, 30000: 3}
+    RANGES = {0.003: 0, 0.03: 1, 0.3: 2, 3: 3}  # in Tesla
     PROBETYPES = {"High Sensitivity": 0,
                   "High Stability": 1,
                   "Ultra-High Sensitivity": 2}
@@ -113,7 +113,7 @@ class LakeShore421(Instrument):
     field_range = Instrument.control(
         "RANGE?", "RANGE %d",
         """ A floating point property that controls the field range of the
-        meter. Valid values are 30, 300, 3000, and 3000 (in Gauss). """,
+        meter. Valid values are 0.003, 0.03, 0.3, and 3 (in Tesla). """,
         validator=truncated_discrete_set,
         values=RANGES,
         map_values=True
