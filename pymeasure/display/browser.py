a590aa4e266002f628fca9eb6851f87ae5e5e948
Andreas Maeder <maederan@phys.ethz.ch>
2021-10-14 12:48:01 +0200

    Revert Changes to only the pytqgraph lw change

--- pymeasure/display/browser.py
+++ pymeasure/display/browser.py
@@ -43,7 +43,7 @@ class BrowserItem(QtGui.QTreeWidgetItem):
         pixelmap.fill(color)
         self.setIcon(0, QtGui.QIcon(pixelmap))
         self.setFlags(self.flags() | QtCore.Qt.ItemIsUserCheckable)
-        self.setCheckState(0, QtCore.Qt.Unchecked)
+        self.setCheckState(0, QtCore.Qt.Checked)
         self.setText(1, basename(results.data_filename))
 
         self.setStatus(results.procedure.status)
