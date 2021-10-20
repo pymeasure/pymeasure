68074c5a539e3a61cbe08231c31574b26abc3e8e
Michele Sardo <msmttchr@gmail.com>
2021-10-20 11:00:38 +0200

    Fix in tests following code changes

--- tests/experiment/test_parameters.py
+++ tests/experiment/test_parameters.py
@@ -35,14 +35,14 @@ from pymeasure.experiment.parameters import VectorParameter
 def test_parameter_default():
     p = Parameter('Test', default=5)
     assert p.value == 5
-    assert p.cli_args[0] == {"default" : 5}
+    assert p.cli_args[0] == 5
     assert p.cli_args[1] == [('units are', 'units'), 'default']
 
 
 def test_integer_units():
     p = IntegerParameter('Test', units='V')
     assert p.units == 'V'
-    assert p.cli_args[0] == {"default" : None}
+    assert p.cli_args[0] == None
     assert p.cli_args[1] == [('units are', 'units'), 'default', 'minimum', 'maximum']
 
 def test_integer_value():
@@ -98,7 +98,7 @@ def test_boolean_value():
     assert p.value == False
     p.value = True
     assert p.value == True
-    assert p.cli_args[0] == {"default" : None}
+    assert p.cli_args[0] == None
     assert p.cli_args[1] == [('units are', 'units'), 'default']
 
 
@@ -119,7 +119,7 @@ def test_float_value():
     assert p.units == 'tests'
     with pytest.raises(ValueError):
         p.value = '31.3 incorrect units'  # not the correct units
-    assert p.cli_args[0] == {"default" : None}
+    assert p.cli_args[0] == None
     assert p.cli_args[1] == [('units are', 'units'), 'default', 'decimals']
 
 
@@ -155,8 +155,8 @@ def test_list_value():
     assert p.value == 'and four'
     with pytest.raises(ValueError):
         p.value = 5
-    assert p.cli_args[0] == {"default" : None}
-    assert p.cli_args[1] == [('units are', 'units'), 'default', 'choices']
+    assert p.cli_args[0] == None
+    assert p.cli_args[1] == [('units are', 'units'), 'default', ('choices are', 'choices')]
 
 
 def test_list_value_with_units():
@@ -171,16 +171,16 @@ def test_list_value_with_units():
     assert p.value == 'three'
     p.value = 'and four tests'
     assert p.value == 'and four'
-    assert p.cli_args[0] == {"default" : None}
-    assert p.cli_args[1] == [('units are', 'units'), 'default', 'choices']
+    assert p.cli_args[0] == None
+    assert p.cli_args[1] == [('units are', 'units'), 'default', ('choices are', 'choices')]
 
 
 def test_list_order():
     p = ListParameter('Test', choices=[1, 2.2, 'three', 'and four'])
     # check if order is preserved, choices are internally stored as dict
     assert p.choices == (1, 2.2, 'three', 'and four')
-    assert p.cli_args[0] == {"default" : None}
-    assert p.cli_args[1] == [('units are', 'units'), 'default', 'choices']
+    assert p.cli_args[0] == None
+    assert p.cli_args[1] == [('units are', 'units'), 'default', ('choices are', 'choices')]
 
 
 def test_vector():
@@ -200,7 +200,7 @@ def test_vector():
     with pytest.raises(ValueError):
         p.value = '0, 1, 2'
 
-    assert p.cli_args[0] == {"default" : None}
+    assert p.cli_args[0] == None
     assert p.cli_args[1] == [('units are', 'units'), 'default', '_length']
 
 # TODO: Add tests for Measurable
