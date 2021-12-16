0bbadaddc08ea615cd878317bb8782e565cd88b8
Benjamin Klebel <32774645+bklebel@users.noreply.github.com>
2021-12-16 17:13:38 +0100

    restructuring retrying adapter for one single class

--- pymeasure/instruments/oxfordinstruments/adapters.py
+++ pymeasure/instruments/oxfordinstruments/adapters.py
@@ -13,19 +13,16 @@ class RetryVISAError(Exception):
     pass
 
 
-class RetryingVISAAdapter(VISAAdapter):
+class RetryingVISAAdapter_oxfordinstruments(VISAAdapter):
     """Adapter class for the VISA library using PyVISA to communicate
     with instruments.
     Checks replies from instruments for sanity
 
     :param sanity: regex string of how a reply from the device must look like
                         defaults to accepting all replies
+                        default is for devices from Oxford Instruments
     :param resource: VISA resource name that identifies the address
-    :param visa_library: VisaLibrary Instance, path of the VISA library or VisaLibrary spec string (@py or @ni).
-                         if not given, the default for the platform will be used.
-    :param preprocess_reply: optional callable used to preprocess strings
-        received from the instrument. The callable returns the processed string.
-    :param kwargs: Any valid key-word arguments for constructing a PyVISA instrument
+    :param kwargs: key-word arguments for constructing a PyVISA Adapter
     """
 
     connError = VisaIOError(-1073807194)
@@ -33,7 +30,7 @@ class RetryingVISAAdapter(VISAAdapter):
     visaIOError = VisaIOError(-1073807298)
     visaNotFoundError = VisaIOError(-1073807343)
 
-    def __init__(self, resource_name, sanity=".*", **kwargs):
+    def __init__(self, resource_name, sanity="{}.*", **kwargs):
         super().__init__(resource_name, **kwargs)
         self.sanity_regex = sanity
 
@@ -43,7 +40,7 @@ class RetryingVISAAdapter(VISAAdapter):
         Do a sanity check for the returned value, if this fails recursively
         repeat
 
-        :param command: SCPI command string to be sent to the instrument
+        :param command: ASCII command string to be sent to the instrument
         :returns: String ASCII response of the instrument
         """
         if count > 5:
@@ -54,72 +51,24 @@ class RetryingVISAAdapter(VISAAdapter):
             count += 1
         # log.debug(f"query for command {command} ")
         device_output = self.connection.query(command)
-        # log.debug(f"device_output: {device_output}")
+        log.debug("query for command %s; device_output: %s", command, device_output)
         reply_sane = self.sanity_handling(device_output, command, count=count)
         # log.debug(f"reply_sane: {reply_sane}")
         return reply_sane
 
-    def sanity_handling(self, device_output, command, count=0, *args, **kwargs):
-        """match the reply from a device with the specifying regex
-        retry the request in case of mismatch
-
-
-        :param device_output: String ASCII response of the device
-        :param command: command used in the initial query
-        :returns:   in case it matches: device_output,
-                    else: recursively retry self.ask(command)
-        """
-        # log.debug("now handling sanity in general RetryingVISAAdapter")
-        # log.debug(f"sanity regex = {self.sanity_regex}")
-        # log.debug(f"device_output = {device_output}")
-        try:
-            if not re.match(self.sanity_regex, device_output):
-                log.debug(
-                    f"reply '{device_output}' is not sane, trying again, this is trial nr. {count}"
-                )
-                try:
-                    self.read()
-                except VisaIOError as e_visa:
-                    if (
-                        isinstance(e_visa, type(self.timeouterror))
-                        and e_visa.args == self.timeouterror.args
-                    ):
-                        pass
-                    else:
-                        raise e_visa
-                return self.ask(command, count=count)
-        except TypeError:
-            log.debug(f"got a TypeError, trying again")
-            try:
-                self.read()
-            except VisaIOError as e_visa:
-                if (
-                    isinstance(e_visa, type(self.timeouterror))
-                    and e_visa.args == self.timeouterror.args
-                ):
-                    pass
-                else:
-                    raise e_visa
-            return self.ask(command, count=count)
-        log.debug("finally, a sane reply!")
-        return device_output
-
-    def __repr__(self):
-        return "<RetryingVISAAdapter(resource='%s')>" % self.connection.resource_name
-
-
-class RetryingVISAAdapter_oinst(RetryingVISAAdapter):
-    """docstring for RetryingVisaAdapter"""
-
-    def __init__(self, resource_name, **kwargs):
-        super().__init__(resource_name, sanity="{}.*", **kwargs)
-        # self.sanity_regex = sanity    
-
     def write(self, command):
-        """write command to instrument, check whether reply is sane"""
+        """write command to instrument
+        check whether the reply indicates that the given command was not understood
+        The devices from oxford instruments reply with '?xxx' to
+            a command 'xxx' if this command is not known
+        """
         try:
             answer = self.connection.query(command)
-            log.debug("instrument answered: %s", answer)
+            log.debug(
+                "writing command to instrument: %s; instrument answered: %s",
+                command,
+                answer,
+            )
             if answer[0] == "?":
                 raise RetryVISAError(
                     f"The instrument did not understand this command: {command}"
@@ -135,15 +84,15 @@ class RetryingVISAAdapter_oinst(RetryingVISAAdapter):
 
     def sanity_handling(self, device_output, command, count=0, *args, **kwargs):
         """match the reply from a device with the specifying regex
-        retry the request in case of mismatch
-        use custom sanity regex incorporating the command message
-
+        retry the request in case of a mismatch
+            use the custom sanity regex incorporating the command message
 
         :param device_output: String ASCII response of the device
         :param command: command used in the initial query
         :returns:   in case it matches: device_output,
                     else: recursively retry self.ask(command)
         """
+
         # log.debug(f"now handling sanity, this is trial nr. {count}")
         # log.debug(f"sanity regex pristine = {self.sanity_regex}, command={command}")
         san_regex = self.sanity_regex.format(command[0])
@@ -153,7 +102,10 @@ class RetryingVISAAdapter_oinst(RetryingVISAAdapter):
         try:
             if not re.match(san_regex, device_output):
                 log.debug(
-                    f"reply '{device_output}' is not sane, trying again, this is trial nr. {count}"
+                    "reply '%s' is not sane for command '%s', trying again, this is trial nr. %s",
+                    device_output,
+                    command,
+                    count,
                 )
                 try:
                     self.read()
@@ -167,7 +119,6 @@ class RetryingVISAAdapter_oinst(RetryingVISAAdapter):
                         raise e_visa
                 return self.ask(command, count=count)
         except TypeError:
-            # log.debug(f"got a TypeError, trying again")
             try:
                 self.read()
             except VisaIOError as e_visa:
@@ -179,10 +130,7 @@ class RetryingVISAAdapter_oinst(RetryingVISAAdapter):
                 else:
                     raise e_visa
             return self.ask(command, count=count)
-        # log.debug("finally, a sane reply!")
         return device_output
 
     def __repr__(self):
-        return (
-            "<RetryingVISAAdapter_oinst(resource='%s')>" % self.connection.resource_name
-        )
+        return "<RetryingVISAAdapter(resource='%s')>" % self.connection.resource_name
