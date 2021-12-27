523856f9a28c67b6de6760584e863800c3b015dd
Michele Sardo <37845722+msmttchr@users.noreply.github.com>
2021-12-27 12:32:01 +0100

    Removed unneeded import

--- pymeasure/instruments/diamondengineering/damsx000.py
+++ pymeasure/instruments/diamondengineering/damsx000.py
@@ -24,7 +24,6 @@
 
 from pymeasure.instruments import Instrument
 from pymeasure.instruments.validators import strict_discrete_set, strict_range
-from pymeasure.adapters import VISAAdapter
 from math import sqrt, sin, cos, radians, degrees, atan
 
 class ZeroPositionNotSet(Exception):
