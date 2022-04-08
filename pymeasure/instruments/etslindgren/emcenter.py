35f5225fa7c4787d57fd896a18cc249ac37ae10b
IanCharboneau <ianchar13oneau@gmail.com>
2022-04-08 17:04:07 -0400

    continued inital developement

--- pymeasure/instruments/etslindgren/emcenter.py
+++ pymeasure/instruments/etslindgren/emcenter.py
@@ -29,7 +29,7 @@ import re
 from datetime import datetime, timezone, timedelta
 import numpy as np
 import pandas as pd
-
+from pymeasure.instruments import Instrument
 from pymeasure.instruments.validators import (
     strict_discrete_set, strict_discrete_range,
     truncated_discrete_set, strict_range
@@ -43,7 +43,7 @@ log.addHandler(logging.NullHandler())
 
 
 
-class EMCenter():
+class EMCenter(Instrument):
     """ Represents ETS-Lindgren EMCenter main frame.
 
     Subclasses implement the functionalities of the different modules:
@@ -54,23 +54,40 @@ class EMCenter():
 
     
     """
+    id = Instrument.measurement(
+         '*IDN?',
+         """ Returns the identification string of the EMCenter.""",
+    )
+    status = Instrument.measurement(
+            'STATUS?',  
+            """ Returns the status byte of the EMCenter.""",
+    )
+    local = Instrument.control(
+            'LOCAL',
+            """ Sets the EMCenter to local mode.""",
+    )
+    reboot = Instrument.control(
+            'REBOOT SYSTEM',
+            """ Reboots the EMCenter.""",
+    )
 
-    def __init__(self, device_name='', name='EMCenter'):
-
+    def __init__(self, resource_name, name='EMCenter', **kwargs):
+        kwargs.setdefault('timeout', 2000)
+        kwargs.setdefault('write_termination', '\n')
+        super().__init__(
+            resource_name,
+            "test",
+            **kwargs
+        )
    
 
     class EMCenterModule():
-        def __init__(self, acquire_instr, reset,
-                     instr_identifier, vb_name=''):
+        def __init__(self, 
+                     instr_identifier, ):
             """Initialize instrument 
             """
-            # Parameters & Handle of VirtualBench Instance
-            self._vb_handle = acquire_instr.__self__
-            self._device_name = self._vb_handle.device_name
-            self.name = (vb_name + " " + instr_identifier.upper()).strip()
-            log.info("Initializing %s." % self.name)
-            self._instrument_handle = acquire_instr(self._device_name, reset)
-            self.isShutdown = False
+            
+           
 
 
     class PositionerCard(EMCenterModule):
@@ -78,7 +95,7 @@ class EMCenter():
         Allows for controll and feedback of the positioner card.
         """
 
-        def __init__(self, virtualbench, lines, reset, vb_name=''):
+        def __init__(self, ):
         
 
    
