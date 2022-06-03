bbf0bddf8f972960c506e9ebbbc7e038b4731b8b
Bastian Leykauf <leykauf@physik.hu-berlin.de>
2022-06-03 10:47:30 +0200

    Revert "add optonal instrument tests"
    This reverts commit c15d706985ffa7a0c3bf7f571c1cc822cc83b2c9.

--- tests/instruments/rohdeschwarz/test_hmp.py
+++ tests/instruments/rohdeschwarz/test_hmp.py
@@ -22,14 +22,12 @@
 # THE SOFTWARE.
 #
 
-from time import sleep
-
 import pytest
-from pymeasure.instruments.rohdeschwarz.hmp import HMP4040, process_sequence
+from pymeasure.instruments.rohdeschwarz.hmp import process_sequence
 
 
 def test_process_sequence():
-    """Test `process_sequence` function."""
+    "Test `process_sequence` function."
 
     # Sequence must contain multiple of 3 values.
     with pytest.raises(ValueError):
@@ -45,78 +43,3 @@ def test_process_sequence():
     # Test with a valid sequence.
     sequence = [1.0, 2.0, 3.0, 4.0, 5.0, 6.0]
     assert process_sequence(sequence) == "1.0,2.0,3.0,4.0,5.0,6.0"
-
-
-# The following tests need a real instrument. ---------------------------------
-
-@pytest.fixture(scope="session")
-def hmp4040(pytestconfig):
-    """
-    Return a HMP4040 instrument. The resource name can be specified via the
-    --resource-name command line option. Make sure --instrument-present is also
-    set in order to run the tests.
-    """
-    resource_name = pytestconfig.getoption("resource_name")
-    hmp4040 = HMP4040(resource_name)
-    hmp4040.reset()
-    return hmp4040
-
-
-@pytest.mark.needs_instrument
-def test_beep(hmp4040):
-    """Test emission of a beep from the instrument."""
-    hmp4040.beep()
-    hmp4040.reset()
-
-
-@pytest.mark.needs_instrument
-def test_voltage_limits(hmp4040):
-    """Test minimum and maximum voltages."""
-    hmp4040.voltage_to_min()
-    assert hmp4040.voltage == hmp4040.min_voltage
-    hmp4040.voltage_to_max()
-    assert hmp4040.voltage == hmp4040.max_voltage
-    hmp4040.reset()
-
-
-@pytest.mark.needs_instrument
-def test_voltage_stepping(hmp4040):
-    """Test stepping voltages up and down."""
-    hmp4040.voltage = 0.0
-    hmp4040.step_voltage_up()
-    assert hmp4040.voltage == hmp4040.voltage_step
-    hmp4040.step_voltage_down()
-    assert hmp4040.voltage == 0.0
-    hmp4040.reset()
-
-
-@pytest.mark.needs_instrument
-def test_channel_state(hmp4040):
-    """Test activation and deactivation of channels."""
-    hmp4040.selected_channel = 1
-    assert hmp4040.selected_channel == 1
-
-    hmp4040.set_channel_state(2, True)
-    # check that the selected channel is reset to 2.
-    assert hmp4040.selected_channel == 1
-    hmp4040.reset()
-
-
-@pytest.mark.needs_instrument
-def test_sequence(hmp4040):
-    """Test sequence execution."""
-    # NOTE: There are actually no assertations in this test. But it is possible
-    # to check the sequence execution by looking at display of the instrument.
-    hmp4040.selected_channel == 1
-    hmp4040.voltage = 0.0
-    hmp4040.sequence = [1.0, 1.0, 1.0, 2.0, 2.0, 1.0]
-    hmp4040.repetitions = 0
-    hmp4040.transfer_sequence(1)
-    hmp4040.output_enabled = True
-    hmp4040.selected_channel_active = True
-    hmp4040.start_sequence(1)
-    sleep(4)
-    hmp4040.stop_sequence(1)
-    hmp4040.output_enabled = False
-    hmp4040.clear_sequence(1)
-    hmp4040.reset()
