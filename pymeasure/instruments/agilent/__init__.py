53ac0d62f5d184083b4549d8a2d85d2f17d55b29
Ian <ianchar13oneau@gmail.com>
2022-06-28 21:03:13 -0400

    create instrument

--- pymeasure/instruments/agilent/__init__.py
+++ pymeasure/instruments/agilent/__init__.py
@@ -33,3 +33,5 @@ from .agilent33220A import Agilent33220A
 from .agilent33500 import Agilent33500
 from .agilent33521A import Agilent33521A
 from .agilentB1500 import AgilentB1500
+from .agilentE4407B import AgilentE4407B
+
