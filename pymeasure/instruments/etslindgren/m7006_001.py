61ed874a69408d668b089d325eaf8d96bb4ac429
IanCharboneau <ianchar13oneau@gmail.com>
2022-06-29 17:43:04 -0400

    implement basic functionality

--- pymeasure/instruments/etslindgren/m7006_001.py
+++ pymeasure/instruments/etslindgren/m7006_001.py
@@ -27,6 +27,7 @@
 import logging
 import re
 from datetime import datetime, timezone, timedelta
+from turtle import position
 import numpy as np
 import pandas as pd
 from pymeasure.instruments import Instrument
@@ -59,7 +60,105 @@ class M7006_001(Instrument):
         self._slot = slot
         self._device = device
 
-    acceleration = Instrument.control(
-        f'{self._slot}{self._device}:ACCEL',
+  
+    @property
+    def acceleration(self):
+        return self.adapter.ask(f'{self._slot}lf._device}ACC?')
     
-cla
+    @acceleration.setter
+    def acceleration(self, value):
+        value = strict_range(value, 0.1, 30.0)
+        return self.adapter.ask(f'{self._slot}{self._device}ACC {value}')
+
+    @property
+    def speed(self):
+        return self.adapter.ask(f'{self._slot}{self._device}SPEED?')
+    
+    @speed.setter
+    def speed(self, value):
+        value = strict_range(value, 0.0, 100.0)
+        return self.adapter.write(f'{self._slot}{self._device}SPEED {value}')
+    
+    @property
+    def aux_output(self, number=1):
+        number = strict_discrete_set(number, 1, 2)
+        return self.adapter.write(f'{self._slot}{self._device}AUX{number}?')
+    
+    @aux_output.setter
+    def aux_output(self, value, number=1):
+        number = strict_discrete_set(number, 1, 2)
+        value = strict_discrete_set(value, 'ON', 'OFF')
+        return self.adapter.write(f'{self._slot}{self._device}AUX{number} {value}')
+
+    def counter_clockwise(self):
+        return self.adapter.write(f'{self._slot}{self._device}CC')
+    
+    def clockwise(self):
+        return self.adapter.write(f'{self._slot}{self._device}CW')
+
+    def down(self):
+        return self.adapter.write(f'{self._slot}{self._device}DN')
+    
+    def up(self):
+        return self.adapter.write(f'{self._slot}{self._device}UP')
+
+    def stop(self):
+        return self.adapter.write(f'{self._slot}{self._device}ST')
+    
+    @property
+    def polarity(self):
+        
+        result = self.adapter.write(f'{self._slot}{self._device}P?')
+        if result== 1:
+            return 'H'
+        elif  result == 0:
+            return 'V'
+        else:
+            raise ValueError('Polarity not recognized')
+    
+    
+
+    
+    @polarity.setter
+    def polarity(self, value):
+        value = strict_discrete_set(value, 'H', 'V')
+        value = map()
+        return self.adapter.write(f'{self._slot}{self._device}P{value}')
+
+
+    
+    @property
+    def position(self):
+        """ Returns the current position of the device. """
+        return self.adapter.ask(f'{self._slot}{self._device}CP?')
+    
+    @position.setter
+    def position(self, value):
+        """ Sets the current position of the device. does not move the device. """
+        value = strict_range(value, -999.9, 999.9)
+        return self.adapter.write(f'{self._slot}{self._device}CP {value}')
+    
+    def direction(self):
+        return self.adapter.write(f'{self._slot}{self._device}DIR?')
+
+    def seek_position(self, position):
+        """ Moves the device to the specified position. by the shortest distance. """
+        position = strict_range(position, -999.9, 999.9)
+        return self.adapter.write(f'{self._slot}{self._device}SK {position}')
+    
+    def seek_negative_postion(self, position):
+        """ Moves the device to the specified position, in the down or counter clockwise direction. """
+        position = strict_range(position, -999.9, 999.9)
+        return self.adapter.write(f'{self._slot}{self._device}SKN {position}')
+    
+    def seek_positive_postion(self, position):
+        """ Moves the device to the specified position, in the up or clockwise direction. """
+        position = strict_range(position, -999.9, 999.9)
+        return self.adapter.write(f'{self._slot}{self._device}SKP {position}')
+    
+    def seek_relative_postion_rela(self, position):
+        """ Moves the device to the specified position relative to the current position. """
+        position = strict_range(position, -999.9, 999.9)
+        return self.adapter.write(f'{self._slot}{self._device}SKR {position}')
+
+
