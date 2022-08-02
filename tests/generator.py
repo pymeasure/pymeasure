82ae3e0eb1f0fa46ebcb8d8963039f4c1d69ec21
Benedikt Moneke <67148916+bmoneke@users.noreply.github.com>
2022-08-02 19:04:05 +0200

    Test generator (including tests) added.

---
+++ tests/generator.py
@@ -0,0 +1,201 @@
+#
+# This file is part of the PyMeasure package.
+#
+# Copyright (c) 2013-2022 PyMeasure Developers
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
+
+import io
+import logging
+import re
+
+from pymeasure.adapters import VISAAdapter
+
+log = logging.getLogger(__name__)
+log.addHandler(logging.NullHandler())
+
+
+def write_test(file, name, cls_name, comm_pairs, tests):
+    """Write a test"""
+    instr = ""
+    for test in tests:
+        if "instr" in test:
+            instr = " as instr"
+            break
+    file.writelines(
+        ["\n", "\n", f"def test_{name}():\n",
+         "    with expected_protocol(\n",
+         f"            {cls_name},\n",
+         f"            {comm_pairs}\n".replace("), (", "),\n             ("),
+         f"            ){instr}:\n"])
+    if tests == ["pass"]:
+        tests = ["pass  # Verify the expected communication."]
+    file.writelines(f"        {test}\n" for test in tests)
+
+
+def parse_binary_string(text):
+    """Parse a bytes string in `text` and return the bytes object."""
+    return b''.join([bytes(text, 'ascii') or bytes([int(binary[-2:], 16)])
+                     for binary, text in re.findall(r"(\\x[0-9a-fA-F]{2,2})|(.+?)",
+                                                    text[2:-1], re.DOTALL)])
+
+
+def parse_stream(stream):
+    """
+    Parse the data stream.
+
+    It is expected, that a message is always written in one write, while
+    reading may extend over several reads, e.g. reading bytes.
+    """
+    comm = []
+    lines = stream.readlines()
+    print(lines)
+    write = None
+    read = None
+    for line in lines:
+        if line.startswith(b"WRITE:"):
+            # Store the last comm_pair unless there is none.
+            if write is not None or read is not None:
+                comm.append((write, read))
+                read = None
+            write = line[6:-1]#parse_binary_string(line[6:-1])
+        else:
+            if read is not None:
+                read += line[5:-1]#parse_binary_string(line[5:-1])
+            else:
+                read = line[5:-1]#parse_binary_string(line[5:-1])
+    if read is not None or write is not None:
+        comm.append((write, read))
+    return comm
+
+
+class ByteFormatter(logging.Formatter):
+    @staticmethod
+    def make_bytes(value):
+        if isinstance(value, (bytes, bytearray)):
+            return value
+        if isinstance(value, str):
+            return value.encode()
+
+    def format(self, record):
+        return b"".join((record.msg.replace(r"%s", "").encode(),
+                          *[self.make_bytes(arg) for arg in record.args]))
+
+
+class ByteStreamHandler(logging.StreamHandler):
+
+    terminator = b"\n"
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.formatter = ByteFormatter()
+
+
+class Generator:
+    """
+    Generates tests from the communication with an instrument.
+
+    Example usage:
+    g = Generator("test_tc038.py")
+    g.instantiate(TC038, "COM5", 'hcp')
+    g.test_property("information")
+    g.test_property("monitored_value")
+    g.test_property_setter("setpoint", 20)
+    g.test_property("setpoint")
+    """
+
+    def __init__(self, filepath="tests.py"):
+        """Initialize the generator writing the testfile to `filepath`."""
+        if isinstance(filepath, io.StringIO):
+            self._file = filepath
+        else:
+            self._file = open(filepath, "w")
+        self._stream = io.BytesIO()
+        self._index = 0
+        self._incomm = []  # Initializiation comm_pairs
+
+    def __del__(self):
+        self._file.close()
+
+    def parse_stream(self):
+        """Parse the stream not yet read."""
+        self._stream.seek(self._index)
+        comm = parse_stream(self._stream)
+        self._index = self._stream.tell()
+        return self._incomm + comm
+
+    def instantiate(self, instrument_class, adapter, manufacturer, **kwargs):
+        """
+        Instantiate the instrument with the `adapter` and `kwargs`.
+
+        This method also writes the necessary import header for the tests.
+
+        The argument `manufacturer` is the module from which to import the
+        instrument, e.g. 'hcp' if instrument_class is 'pymeasure.hcp.tc038'.
+        """
+        self._class = instrument_class.__name__
+        log.info(f"Instantiate {self._class}.")
+        self._file.write(
+            "from pymeasure.test import expected_protocol\n"
+            f"from pymeasure.instruments.{manufacturer} import {self._class}\n")
+        if isinstance(adapter, (int, str)):
+            try:
+                adapter = VISAAdapter(adapter, **kwargs)
+            except ImportError:
+                raise Exception("Invalid Adapter provided for Instrument since"
+                                " PyVISA is not present")
+        adapter.log.addHandler(ByteStreamHandler(self._stream))
+        adapter.log.setLevel(logging.DEBUG)
+        self.instr = instrument_class(adapter, **kwargs)
+        comm = self.parse_stream()
+        self._incomm = comm
+        write_test(self._file, "init", self._class, comm, ['pass'])
+
+    def test_property(self, property):
+        """Test getting the instrument's `property`."""
+        log.info(f"Test property {property} getter.")
+        value = getattr(self.instr, property)
+        comm = self.parse_stream()
+        if isinstance(value, str):
+            value = f"\'{value}\'"
+        write_test(self._file, property, self._class, comm,
+                   [f"assert instr.{property} == {value}"])
+        return value
+
+    def test_property_setter(self, property, value):
+        """Test setting the instrument's `property` to `value`."""
+        log.info(f"Test property {property} setter.")
+        setattr(self.instr, property, value)
+        comm = self.parse_stream()
+        if isinstance(value, str):
+            value = f"\'{value}\'"
+        write_test(self._file, f"{property}_setter", self._class, comm,
+                   [f"instr.{property} = {value}"])
+
+    def test_method(self, method, *args, **kwargs):
+        """Test the instrument's `method` with `args` and `kwargs`."""
+        log.info(f"Test method {method}.")
+        value = getattr(self.instr, method)(*args, **kwargs)
+        comm = self.parse_stream()
+        if isinstance(value, str):
+            value = f"\'{value}\'"
+        write_test(self._file, method, self._class, comm,
+                   [f"assert instr.{method}({f'*{args}, ' if args else ''}"
+                    f"{f'**{kwargs}' if kwargs else ''}) == {value}"])
---
+++ tests/test_generator.py
@@ -0,0 +1,261 @@
+#
+# This file is part of the PyMeasure package.
+#
+# Copyright (c) 2013-2022 PyMeasure Developers
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
+
+import io
+import logging
+import pytest
+
+from pymeasure.adapters import ProtocolAdapter
+from pymeasure.instruments.hcp import TC038, TC038D
+
+from generator import write_test, parse_binary_string, parse_stream, Generator, ByteStreamHandler
+
+
+class Test_write_test:
+    @pytest.fixture
+    def file(self):
+        s = io.StringIO()
+        yield s
+        s.close()
+
+    def test_write(self, file):
+        write_test(file, "init", "Super", [(b"sent", b"received")], ["pass"])
+        assert file.getvalue() == """
+
+def test_init():
+    with expected_protocol(
+            Super,
+            [(b'sent', b'received')]
+            ):
+        pass  # Verify the expected communication.
+"""
+
+    def test_write_multiple_comm_pairs(self, file):
+        write_test(file, "init", "Super", [(b"sent", b"received"),
+                                           (b"sent2", b'rec2')], ["pass"])
+        assert file.getvalue() == """
+
+def test_init():
+    with expected_protocol(
+            Super,
+            [(b'sent', b'received'),
+             (b'sent2', b'rec2')]
+            ):
+        pass  # Verify the expected communication.
+"""
+
+    def test_write_multiple_tests(self, file):
+        write_test(file, "init", "Super", [(b"sent", b"received")],
+                   ["instr.xy = 5", "assert instr.xy == 5"])
+        assert file.getvalue() == """
+
+def test_init():
+    with expected_protocol(
+            Super,
+            [(b'sent', b'received')]
+            ) as instr:
+        instr.xy = 5
+        assert instr.xy == 5
+"""
+
+    def test_write_bytes(self, file):
+        write_test(file, "init", "Super",
+                   [(b'\x0201010WRS01D0002\x03', b'\x020101OK\x03')],
+                   ["del instr"])
+        assert file.getvalue() == r"""
+
+def test_init():
+    with expected_protocol(
+            Super,
+            [(b'\x0201010WRS01D0002\x03', b'\x020101OK\x03')]
+            ) as instr:
+        del instr
+"""
+
+
+class Test_parse_bytes:
+    @pytest.mark.parametrize(
+        "text, bytedata", (
+            ("b'ascii\tonly\ntsuper xy'", b'ascii\tonly\ntsuper xy'),
+            ("b'abc\ts\\x15dfa\\x00'", b'abc\ts\x15dfa\x00'),
+            (r'b"\x01\x10\x01\x0A\x00\x04\x08\x00"',
+             b"\x01\x10\x01\x0A\x00\x04\x08\x00"),
+        ))
+    def test_parsing(self, text, bytedata):
+        assert parse_binary_string(text) == bytedata
+
+
+@pytest.mark.skip("moved to bytes")
+class Test_parse_stream2:
+    @pytest.mark.parametrize(
+        "text, comms", (
+            ("WRITE:abc\n", [(b"abc", None)]),
+            ("READ:def\n", [(None, b"def")]),
+            ("READ:a\nREAD:bc\n", [(None, b"abc")]),
+            ("WRITE:abc\nREAD:def\n", [(b"abc", b"def")]),
+            ("WRITE:abc\nREAD:d\nREAD:ef\n", [(b"abc", b"def")]),
+            ("WRITE:abc\nREAD:def\nWRITE:ghi\nREAD:jkl\n",
+             [(b"abc", b"def"), (b"ghi", b"jkl")]),
+            ("WRITE:abc\nWRITE:def\n", [(b"abc", None), (b"def", None)]),
+         ))
+    def test_parsing(self, text, comms):
+        with io.StringIO(text) as buf:
+            assert parse_stream(buf) == comms
+
+
+@pytest.mark.skip("changet to all bytes")
+class Test_parse_stream3:
+    @pytest.mark.parametrize(
+        "text, comms", (
+            ("WRITE:b'abc'\n", [(b"abc", None)]),
+            ("READ:b'def'\n", [(None, b"def")]),
+            ("READ:b'a'\nREAD:b'bc'\n", [(None, b"abc")]),
+            ("WRITE:b'abc'\nREAD:b'def'\n", [(b"abc", b"def")]),
+            ("WRITE:b'abc'\nREAD:b'd'\nREAD:b'ef'\n", [(b"abc", b"def")]),
+            ("WRITE:b'abc'\nREAD:b'def'\nWRITE:b'ghi'\nREAD:b'jkl'\n",
+             [(b"abc", b"def"), (b"ghi", b"jkl")]),
+            ("WRITE:b'abc'\nWRITE:b'def'\n", [(b"abc", None), (b"def", None)]),
+            ("WRITE:b'\\x03ab\\x04'\nREAD:b'super\\x05'\n",
+             [(b'\x03ab\x04', b'super\x05')])
+         ))
+    def test_parsing(self, text, comms):
+        with io.StringIO(text) as buf:
+            assert parse_stream(buf) == comms
+
+
+class Test_parse_stream:
+    @pytest.mark.parametrize(
+        "text, comms", (
+            (b"WRITE:abc\n", [(b"abc", None)]),
+            (b"READ:def\n", [(None, b"def")]),
+            (b"READ:a\nREAD:bc\n", [(None, b"abc")]),
+            (b"WRITE:abc\nREAD:def\n", [(b"abc", b"def")]),
+            (b"WRITE:abc\nREAD:d\nREAD:ef\n", [(b"abc", b"def")]),
+            (b"WRITE:abc\nREAD:def\nWRITE:ghi\nREAD:jkl\n",
+             [(b"abc", b"def"), (b"ghi", b"jkl")]),
+            (b"WRITE:abc\nWRITE:def\n", [(b"abc", None), (b"def", None)]),
+            (b"WRITE:\x03ab\x04\nREAD:super\x05\n",
+             [(b'\x03ab\x04', b'super\x05')])
+         ))
+    def test_parsing(self, text, comms):
+        with io.BytesIO(text) as buf:
+            assert parse_stream(buf) == comms
+
+
+class Test_generator:
+    @pytest.fixture
+    def bare_generator(self):
+        s = io.StringIO()
+        yield Generator(s)
+        s.close()
+
+    @pytest.fixture
+    def generator(self, bare_generator):
+        adapter = ProtocolAdapter(
+            [(b"\x0201010WRS01D0002\x03", b"\x020101OK\x03")])
+        adapter.log.addHandler(ByteStreamHandler(bare_generator._stream))
+        adapter.log.setLevel(logging.DEBUG)
+        bare_generator.instr = TC038(adapter)
+        bare_generator._class = "TC038"
+        return bare_generator
+
+    def test_instantiate(self, bare_generator):
+        generator = bare_generator
+        a = ProtocolAdapter([(b"\x0201010WRS01D0002\x03", b"\x020101OK\x03")])
+        generator.instantiate(TC038, a, "hcp")
+        assert generator._file.getvalue() == r"""from pymeasure.test import expected_protocol
+from pymeasure.instruments.hcp import TC038
+
+
+def test_init():
+    with expected_protocol(
+            TC038,
+            [(b'\x0201010WRS01D0002\x03', b'\x020101OK\x03')]
+            ):
+        pass  # Verify the expected communication.
+"""
+
+    def test_property(self, generator):
+        generator.instr.adapter.comm_pairs.extend(
+            [(b"\x0201010WRDD0002,01\x03", b"\x020101OK00C8\x03")])
+        assert generator.test_property("temperature") == 20
+        assert generator._file.getvalue() == r"""
+
+def test_temperature():
+    with expected_protocol(
+            TC038,
+            [(b'\x0201010WRS01D0002\x03', b'\x020101OK\x03'),
+             (b'\x0201010WRDD0002,01\x03', b'\x020101OK00C8\x03')]
+            ) as instr:
+        assert instr.temperature == 20.0
+"""
+
+    def test_property_setter(self, generator):
+        generator.instr.adapter.comm_pairs.extend(
+            [(b"\x0201010WWRD0120,01,00C8\x03", b"\x020101OK\x03")])
+        generator.test_property_setter("setpoint", 20)
+        assert generator._file.getvalue() == r"""
+
+def test_setpoint_setter():
+    with expected_protocol(
+            TC038,
+            [(b'\x0201010WRS01D0002\x03', b'\x020101OK\x03'),
+             (b'\x0201010WWRD0120,01,00C8\x03', b'\x020101OK\x03')]
+            ) as instr:
+        instr.setpoint = 20
+"""
+
+    def test_method(self, generator):
+        generator.instr.adapter.comm_pairs.extend(
+            [(b'\x0201010WRS01D0002\x03', b'\x020101OK\x03')])
+        generator.test_method("set_monitored_quantity", "temperature")
+        assert generator._file.getvalue() == r"""
+
+def test_set_monitored_quantity():
+    with expected_protocol(
+            TC038,
+            [(b'\x0201010WRS01D0002\x03', b'\x020101OK\x03'),
+             (b'\x0201010WRS01D0002\x03', b'\x020101OK\x03')]
+            ) as instr:
+        assert instr.set_monitored_quantity(*('temperature',), ) == None
+"""
+
+    def test_bytes(self, bare_generator):
+        generator = bare_generator
+        a = ProtocolAdapter([(b"\x01\x03\x00\x00\x00\x02\xC4\x0B",
+                              b"\x01\x03\x04\x00\x00\x03\xE8\xFA\x8D")])
+        a.log.addHandler(ByteStreamHandler(bare_generator._stream))
+        a.log.setLevel(logging.DEBUG)
+        bare_generator.instr = TC038D(a)
+        bare_generator._class = "TC038D"
+        assert generator.test_property("temperature") == 100
+        assert generator._file.getvalue() == r"""
+
+def test_temperature():
+    with expected_protocol(
+            TC038D,
+            [(b'\x01\x03\x00\x00\x00\x02\xc4\x0b', b'\x01\x03\x04\x00\x00\x03\xe8\xfa\x8d')]
+            ) as instr:
+        assert instr.temperature == 100.0
+"""
