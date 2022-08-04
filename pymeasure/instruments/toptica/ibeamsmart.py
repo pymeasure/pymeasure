fc9713e899f042231319e0d15242f7fe4738f2bf
Benedikt Moneke <67148916+bmoneke@users.noreply.github.com>
2022-08-04 19:46:06 +0200

    Toptica IBeamSmart moved from its own adapter to pyvisa.

--- pymeasure/instruments/toptica/ibeamsmart.py
+++ pymeasure/instruments/toptica/ibeamsmart.py
@@ -22,9 +22,18 @@
 # THE SOFTWARE.
 #
 
+import logging
+import re
+import time
+
+from pyvisa.errors import VisaIOError
+
 from pymeasure.instruments import Instrument
-from pymeasure.instruments.toptica.adapters import TopticaAdapter
 from pymeasure.instruments.validators import strict_discrete_set, strict_range
+
+
+log = logging.getLogger(__name__)
+log.addHandler(logging.NullHandler())
 
 
 class IBeamSmart(Instrument):
@@ -34,14 +43,95 @@ class IBeamSmart(Instrument):
     :param baud_rate: communication speed, defaults to 115200
     :param kwargs: Any valid key-word argument for VISAAdapter
     """
-    def __init__(self, adapter, baud_rate=115200, **kwargs):
-        if not isinstance(adapter, str):
-            raise TypeError("'adapter' must be a pyvisa resource name string")
+    _reg_value = re.compile(r"\w+\s+=\s+(\w+)")
+
+    def __init__(self, adapter, name="Toptica IBeam Smart laser diode", **kwargs):
+        kwargs.setdefault('read_termination', '\r\n')
+        kwargs.setdefault('write_termination', '\r\n')
         super().__init__(
-            TopticaAdapter(adapter, baud_rate, **kwargs),
-            "toptica IBeam Smart laser diode",
+            adapter,
+            name,
             includeSCPI=False,
+            asrl={'baud_rate': 115200},
+            **kwargs
         )
+        # configure communication mode
+        self.write('echo off')
+        self.write('prom off')
+        time.sleep(0.04)
+        # clear the initial messages from the controller
+        try:
+            self.adapter.flush_read_buffer()
+        except AttributeError:
+            log.warning("Adapter does not have 'flush_read_buffer' method.")
+        self.ask('talk usual')
+
+    def read(self):
+        """Read a reply of the instrument and exctract the values.
+
+        Reads a reply of the instrument which consists of at least two
+        lines. The initial ones are the reply to the command while the last one
+        should be '[OK]' which acknowledges that the device is ready to receive
+        more commands.
+
+        Note: '[OK]' is always returned as last message even in case of an
+        invalid command, where a message indicating the error is returned
+        before the '[OK]'
+
+        Value extraction: extract <value> from 'name = <value> [unit]'.
+        If <value> can not be identified the orignal string is returned.
+
+        :returns: string containing the ASCII response of the instrument.
+        """
+        reply = super().read()  # read back the LF+CR which is always sent back
+        if reply != "":
+            raise ValueError(
+                "TopticaAdapter.read(1): Error after command "
+                f"'{self.lastcommand}' with message '{reply}'")
+        msg = []
+        try:
+            while True:
+                line = super().read()
+                if line == '[OK]':
+                    break
+                msg.append(line)
+        except VisaIOError:
+            reply = '\n'.join(msg)
+            try:
+                self.adapter.flush_read_buffer()
+            except AttributeError:
+                log.warning("Adapter does not have 'flush_read_buffer' method.")
+            raise ValueError(
+                "TopticaAdapter.read(2): Error after command "
+                f"'{self.lastcommand}' with message '{reply}'")
+        reply = '\n'.join(msg)
+        r = self._reg_value.search(reply)
+        if r:
+            return r.groups()[0]
+        else:
+            return reply
+
+    def write(self, command, check_ack=True):
+        """ Writes a command to the instrument. Also reads back a LF+CR which
+        is always sent back.
+
+        :param command: command string to be sent to the instrument
+        :param check_ack: flag to decide if also an acknowledgement from the
+          device is expected. This is the case for set commands.
+        """
+        self.lastcommand = command
+        super().write(command)
+        # The following lines are used in order to avoid the need of a
+        # complicated Instrument where every property would need to use
+        # check_set/get_errors and many Adapter functions would need to be
+        # reimplemented in the Instrument. See discussion in PR #352
+        if check_ack:
+            reply = self.read()
+            if reply != "":
+                # if any message is returned here this indicates some misuse
+                raise ValueError(
+                    f"TopticaAdapter.write: Error after command '{command}'"
+                    f"with message '{reply}'")
 
     version = Instrument.measurement(
            "ver", """ Firmware version number """,
