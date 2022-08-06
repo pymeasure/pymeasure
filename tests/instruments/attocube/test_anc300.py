335eaa3fd1dc77639e36cb19361fe0511378ae6d
Benedikt Moneke <67148916+bmoneke@users.noreply.github.com>
2022-08-06 09:50:14 +0200

    Attocube test made to work.

--- tests/instruments/attocube/test_anc300.py
+++ tests/instruments/attocube/test_anc300.py
@@ -25,11 +25,87 @@
 import pytest
 
 from pymeasure.test import expected_protocol
+from pymeasure.instruments.attocube.adapters import AttocubeConsoleAdapter
 
 from pymeasure.instruments.attocube import ANC300Controller
+from pymeasure.instruments.attocube import anc300
 
 
-@pytest.mark.skip("Adapter rework not yet done.")
+class Mock_Adapter(AttocubeConsoleAdapter):
+    """Mocking the real adapter using the ProtocolAdapter as connection."""
+    def __init__(self, host, port, passwd, **kwargs):
+        self.read_termination = '\r\n'
+        self.write_termination = self.read_termination
+        kwargs.setdefault('preprocess_reply', self.extract_value)
+        self.preprocess_reply = kwargs['preprocess_reply']
+        self.connection = host  # take the ProtocolAdapter as connection
+        self.connection.close = self.close
+        self.query_delay = 0
+        self.connection.read()  # clear messages sent upon opening the connection
+        # send password and check authorization
+        self.write(passwd, check_ack=False)
+        ret = self.connection.read()
+        authmsg = ret.split(self.read_termination)[1]
+        if authmsg != 'Authorization success':
+            raise Exception(f"Attocube authorization failed '{authmsg}'")
+        # switch console echo off
+        _ = self.ask('echo off')  # TODO
+
+    def close(self):
+        pass
+
+    def check_acknowledgement(self, reply, msg=""):
+        """ checks the last reply of the instrument to be 'OK', otherwise a
+        ValueError is raised.
+
+        :param reply: last reply string of the instrument
+        :param msg: optional message for the eventual error
+        """
+        if reply != 'OK':
+            if msg == "":  # clear buffer
+                msg = reply
+                self.connection.read()
+            raise ValueError("AttocubeConsoleAdapter: Error after command "
+                             f"{self.lastcommand} with message {msg}")
+
+    def read(self):
+        """ Reads a reply of the instrument which consists of two or more
+        lines. The first ones are the reply to the command while the last one
+        is 'OK' or 'ERROR' to indicate any problem. In case the reply is not OK
+        a ValueError is raised.
+
+        :returns: String ASCII response of the instrument.
+        """
+        raw = self.connection.read().strip(self.read_termination)
+        # one would want to use self.read_termination as 'sep' below, but this
+        # is not possible because of a firmware bug resulting in inconsistent
+        # line endings
+        ret, ack = raw.rsplit(sep='\n', maxsplit=1)
+        ret = ret.strip('\r')  # strip possible CR char
+        self.check_acknowledgement(ack, ret)
+        return ret
+
+    def write(self, command, check_ack=True):
+        """ Writes a command to the instrument
+
+        :param command: command string to be sent to the instrument
+        :param check_ack: boolean flag to decide if the acknowledgement is read
+            back from the instrument. This should be True for set pure commands
+            and False otherwise.
+        """
+        self.lastcommand = command
+        self.connection.write(command + self.write_termination)
+        if check_ack:
+            reply = self.connection.read()
+            msg = reply.strip(self.read_termination)
+            self.check_acknowledgement(msg)
+
+
+@pytest.fixture(autouse=True)
+def modding(monkeypatch):
+    monkeypatch.setattr(anc300, "AttocubeConsoleAdapter", Mock_Adapter)
+
+
 def test_stepu():
     """Test a setting."""
     with expected_protocol(
@@ -44,7 +120,6 @@ def test_stepu():
         instr.a.stepu = 15
 
 
-@pytest.mark.skip("Adapter rework not yet done.")
 def test_voltage():
     """Test a measurement."""
     with expected_protocol(
