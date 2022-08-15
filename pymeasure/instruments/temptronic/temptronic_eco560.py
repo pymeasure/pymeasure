461c5d8a5022f553184f54603f0fded563d15f81
dschmeer <dschmeer@cryptoquantique.com>
2022-08-15 15:57:08 +0100

    fix documentation CI failure

--- pymeasure/instruments/temptronic/temptronic_eco560.py
+++ pymeasure/instruments/temptronic/temptronic_eco560.py
@@ -99,7 +99,7 @@ class ECO560(ATSBase):
     )
 
     copy_active_setup_file = None
-    ## Not Implemented in ECO-560
+    # Not Implemented in ECO-560
 
     def __init__(self, adapter, **kwargs):
         kwargs.setdefault('timeout', 3000)
