8d2248b90873ea2317f1f37156b4d0601784254c
Bastian Leykauf <leykauf@physik.hu-berlin.de>
2022-10-04 17:37:38 +0200

    prevent false positive of C901 if formatting with black

--- pymeasure/instruments/instrument.py
+++ pymeasure/instruments/instrument.py
@@ -275,19 +275,21 @@ class Instrument:
         return self.adapter.binary_values(command, header_bytes, dtype)
 
     @staticmethod
-    def control(get_command,  # noqa: C901 accept that this is a complex method
-                set_command,
-                docs,
-                validator=lambda v, vs: v,
-                values=(),
-                map_values=False,
-                get_process=lambda v: v,
-                set_process=lambda v: v,
-                command_process=lambda c: c,
-                check_set_errors=False,
-                check_get_errors=False,
-                dynamic=False,
-                **kwargs):
+    def control(  # noqa: C901 accept that this is a complex method
+        get_command,
+            set_command,
+            docs,
+            validator=lambda v, vs: v,
+            values=(),
+            map_values=False,
+            get_process=lambda v: v,
+            set_process=lambda v: v,
+            command_process=lambda c: c,
+            check_set_errors=False,
+            check_get_errors=False,
+            dynamic=False,
+            **kwargs
+    ):
         """Returns a property for the class based on the supplied
         commands. This property may be set and read from the
         instrument. See also :meth:`measurement` and :meth:`setting`.
