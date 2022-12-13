2554228b9dd24cc6cd32888a1c62d87f0a6765d3
Scott Candey <scandey@users.noreply.github.com>
2022-12-13 09:30:40 -0800

    Adding Keysight E36312A power supply (#785)
    * Initial instrument class for Keysight E36312A
    
    * Class file for keysight E36312A
    
    * Add my name to the Authors list
    
    * Initial test script
    
    * Fix initial E36312A test, adding responses to protocol test
    
    * Another fix for the E36312A test
    
    * Cleaned up flake errors in E36312A initial files
    
    * Fix typo in E36312A test
    
    * Typo in E36312A voltage_setpoint
    
    * Add comments about setter/getter to E36312A test script.
    
    * Add test for E36312A output enabled
    
    * Add Keysight E36312A to docs
    
    * Make E36312A validators dynamic and set channel specific values in init. Added test to match, but commented out for now.
    
    * Fixed E36312A spacing for flake
    
    * Fix E36312A title overline for docs
    
    * Fixed some formatting in E36312A tests
    
    * Test KeysightE36312A modified current limit validator
    
    Co-authored-by: Scott Candey <scottcandey@berkeley.edu>

--- pymeasure/instruments/keysight/__init__.py
+++ pymeasure/instruments/keysight/__init__.py
@@ -1,3 +1,4 @@
 from .keysightDSOX1102G import KeysightDSOX1102G
 from .keysightN5767A import KeysightN5767A
 from .keysightN7776C import KeysightN7776C
+from .keysightE36312A import KeysightE36312A
