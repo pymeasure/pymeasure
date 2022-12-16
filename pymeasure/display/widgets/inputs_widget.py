80f08691e94fa6ffebfe2084c4d4fb9e1afb3bdf
Casper Schippers <Casperschippers@hotmail.com>
2022-12-16 12:08:03 +0100

    replace stateChanged with toggled for getting checkbox value in input-groups

--- pymeasure/display/widgets/inputs_widget.py
+++ pymeasure/display/widgets/inputs_widget.py
@@ -107,7 +107,7 @@ class InputsWidget(QtWidgets.QWidget):
 
                 if isinstance(getattr(self, group_name), BooleanInput):
                     # Adjust the boolean condition to a condition suitable for a checkbox
-                    condition = QtCore.Qt.CheckState.Checked if condition else QtCore.Qt.CheckState.Unchecked  # noqa: E501
+                    condition = bool(condition)
 
                 if group_name not in groups:
                     groups[group_name] = []
@@ -118,8 +118,8 @@ class InputsWidget(QtWidgets.QWidget):
             toggle = partial(self.toggle_group, group_name=group_name, group=group)
             group_el = getattr(self, group_name)
             if isinstance(group_el, BooleanInput):
-                group_el.stateChanged.connect(toggle)
-                toggle(group_el.checkState())
+                group_el.toggled.connect(toggle)
+                toggle(group_el.isChecked())
             elif isinstance(group_el, StringInput):
                 group_el.textChanged.connect(toggle)
                 toggle(group_el.text())
