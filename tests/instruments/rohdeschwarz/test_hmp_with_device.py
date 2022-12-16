7e1af7191d85b487ec5274a939775e2898f738d9
Bastian Leykauf <leykauf@physik.hu-berlin.de>
2022-12-16 13:36:41 +0100

    use separate fixture for resetting

--- tests/instruments/rohdeschwarz/test_hmp_with_device.py
+++ tests/instruments/rohdeschwarz/test_hmp_with_device.py
@@ -31,64 +31,63 @@ from pymeasure.instruments.rohdeschwarz.hmp import HMP4040
 
 @pytest.fixture(scope="module")
 def hmp4040(connected_device_address):
-    """
-    Return a HMP4040 instrument.
-    """
+    """Return a HMP4040 instrument."""
     hmp4040 = HMP4040(connected_device_address)
+    return hmp4040
+
+
+@pytest.fixture
+def resetted_hmp4040(hmp4040):
+    """Return a HMP4040 instrument with resetted state."""
     hmp4040.reset()
     return hmp4040
 
 
-def test_beep(hmp4040):
+def test_beep(resetted_hmp4040):
     """Test emission of a beep from the instrument."""
-    hmp4040.beep()
-    hmp4040.reset()
+    resetted_hmp4040.beep()
 
 
-def test_voltage_limits(hmp4040):
+def test_voltage_limits(resetted_hmp4040):
     """Test minimum and maximum voltages."""
-    hmp4040.voltage_to_min()
-    assert hmp4040.voltage == hmp4040.min_voltage
-    hmp4040.voltage_to_max()
-    assert hmp4040.voltage == hmp4040.max_voltage
-    hmp4040.reset()
+    resetted_hmp4040.voltage_to_min()
+    assert resetted_hmp4040.voltage == resetted_hmp4040.min_voltage
+    resetted_hmp4040.voltage_to_max()
+    assert resetted_hmp4040.voltage == resetted_hmp4040.max_voltage
 
 
-def test_voltage_stepping(hmp4040):
+def test_voltage_stepping(resetted_hmp4040):
     """Test stepping voltages up and down."""
-    hmp4040.voltage = 0.0
-    hmp4040.step_voltage_up()
-    assert hmp4040.voltage == hmp4040.voltage_step
-    hmp4040.step_voltage_down()
-    assert hmp4040.voltage == 0.0
-    hmp4040.reset()
+    resetted_hmp4040.voltage = 0.0
+    resetted_hmp4040.step_voltage_up()
+    assert resetted_hmp4040.voltage == resetted_hmp4040.voltage_step
+    resetted_hmp4040.step_voltage_down()
+    assert resetted_hmp4040.voltage == 0.0
 
 
-def test_channel_state(hmp4040):
+def test_channel_state(resetted_hmp4040):
     """Test activation and deactivation of channels."""
-    hmp4040.selected_channel = 1
-    assert hmp4040.selected_channel == 1
+    resetted_hmp4040.selected_channel = 1
+    assert resetted_hmp4040.selected_channel == 1
 
-    hmp4040.set_channel_state(2, True)
+    resetted_hmp4040.set_channel_state(2, True)
     # check that the selected channel is reset to 2.
-    assert hmp4040.selected_channel == 1
-    hmp4040.reset()
+    assert resetted_hmp4040.selected_channel == 1
 
 
-def test_sequence(hmp4040):
+def test_sequence(resetted_hmp4040):
     """Test sequence execution."""
-    # NOTE: There are actually no assertions in this test. But it is possible
-    # to check the sequence execution by looking at display of the instrument.
-    hmp4040.selected_channel == 1
-    hmp4040.voltage = 0.0
-    hmp4040.sequence = [1.0, 1.0, 1.0, 2.0, 2.0, 1.0]
-    hmp4040.repetitions = 0
-    hmp4040.transfer_sequence(1)
-    hmp4040.output_enabled = True
-    hmp4040.selected_channel_active = True
-    hmp4040.start_sequence(1)
+    # NOTE: There are actually no assertions in this test. But it is possible to check the sequence
+    # execution by looking at display of the instrument.
+    resetted_hmp4040.selected_channel == 1
+    resetted_hmp4040.voltage = 0.0
+    resetted_hmp4040.sequence = [1.0, 1.0, 1.0, 2.0, 2.0, 1.0]
+    resetted_hmp4040.repetitions = 0
+    resetted_hmp4040.transfer_sequence(1)
+    resetted_hmp4040.output_enabled = True
+    resetted_hmp4040.selected_channel_active = True
+    resetted_hmp4040.start_sequence(1)
     sleep(4)
-    hmp4040.stop_sequence(1)
-    hmp4040.output_enabled = False
-    hmp4040.clear_sequence(1)
-    hmp4040.reset()
+    resetted_hmp4040.stop_sequence(1)
+    resetted_hmp4040.output_enabled = False
+    resetted_hmp4040.clear_sequence(1)
