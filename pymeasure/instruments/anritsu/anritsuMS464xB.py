db089e2db6fc30f4ed6974e3f91cec9f7de1d1dd
Ashok Bruno <ashokbruno@gmail.com>
2022-12-29 11:41:05 -0500

    add additional functionality

--- pymeasure/instruments/anritsu/anritsuMS464xB.py
+++ pymeasure/instruments/anritsu/anritsuMS464xB.py
@@ -21,129 +21,298 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 #
-
+from enum import Enum
 from pymeasure.instruments import Instrument, Channel
-from pymeasure.instruments.validators import strict_range, strict_discrete_range
+from pymeasure.instruments.validators import (
+    strict_range,
+    strict_discrete_set,
+)
+
+
+class CustomEnum(Enum):
+    """Provides additional methods to IntEnum:
+
+    * Conversion to string automatically replaces '_' with ' ' in names
+      and converts to title case
+
+    * get classmethod to get enum reference with name or integer
+
+    .. automethod:: __str__
+    """
+
+    def __str__(self):
+        """Gives title case string of enum value."""
+        return str(self.value).replace("_", " ")
+        # str() conversion just because of pylint bug
+
+    @classmethod
+    def get(cls, input_value):
+        """Gives Enum member by specifying name or value.
+
+        Args:
+            input_value (str|int): Enum name or value
+
+        Returns:
+            _type_: enum member
+        """
+
+        if isinstance(input_value, int):
+            return cls(input_value)
+        else:
+            return cls[input_value.upper()]
 
 
 class VNAChannel(Channel):
-    """ Implementation of a Anritsu MS464xB channel. """
+    """Implementation of a Anritsu MS464xB channel."""
 
-    _SWEEP_TYPES = ["LIN", "LOG", "FSEGM", "ISEGM", "POW", "MFGC"]
+    _SWEEP_TYPES = {"LIN", "LOG", "FSEGM", "ISEGM", "POW", "MFGC"}
 
     center_frequency = Instrument.control(
-        ":SENS<ch>:FREQ:CENT?", ":SENS<ch>:FREQ:CENT %d",
-        """Center Frequency in Hz""",
+        ":FREQ:CENT?",
+        ":FREQ:CENT %d",
+        """Center Frequency in Hz.""",
         validator=strict_range,
-        values=[30000, 8000000000]
+        values=[30000, 8000000000],
     )
 
     cw_frequency = Instrument.control(
-        ":SENS<ch>:FREQ:CW?", ":SENS<ch>:FREQ:CW %d",
-        """Center Frequency in Hz""",
+        ":FREQ:CW?",
+        ":FREQ:CW %d",
+        """CQ Frequency in Hz.""",
         validator=strict_range,
-        values=[300000, 8000000000]
+        values=[30000, 8000000000],
     )
 
     span_frequency = Instrument.control(
-        ":SENS<ch>:FREQ:SPAN?", ":SENS<ch>:FREQ:SPAN %d",
+        ":FREQ:SPAN?",
+        ":FREQ:SPAN %d",
         """Span Frequency in Hz""",
         validator=strict_range,
-        values=[1, 8000000000]
+        values=[1, 8000000000],
     )
 
     start_frequency = Instrument.control(
-        ":SENS<ch>:FREQ:STAR?", ":SENS<ch>:FREQ:STAR %d",
-        """ Starting frequency in Hz """,
+        ":FREQ:STAR?",
+        ":FREQ:STAR %d",
+        """ Starting frequency in Hz.""",
         validator=strict_range,
-        values=[1, 8000000000]
+        values=[1, 8000000000],
     )
 
     stop_frequency = Instrument.control(
-        ":SENS<ch>:FREQ:STOP?", ":SENS<ch>:FREQ:STOP %d",
-        """ Stop frequency in Hz """,
+        ":FREQ:STOP?",
+        ":FREQ:STOP %d",
+        """ Stop frequency in Hz.""",
         validator=strict_range,
-        values=[1, 8000000000]
+        values=[1, 8000000000],
     )
 
     sweep_type = Instrument.control(
-        ":SENS<ch>:SWE:TYP?", ":SENS<ch>:SWE:TYP %s",
-        """ Sweep Type. Valid options are LIN|LOG|FSEGM|ISEGM|POW|MFGC""",
-        validator=strict_discrete_range,
-        values=_SWEEP_TYPES
+        ":SWE:TYP?",
+        ":SWE:TYP %s",
+        """ Sweep Type. Valid options are LIN|LOG|FSEGM|ISEGM|POW|MFGC.""",
+        validator=strict_discrete_set,
+        values=_SWEEP_TYPES,
     )
 
     sweep_mode = Instrument.control(
-        ":SENS<ch>:SA:MODE?", ":SENS<ch>:SA:MODE %s",
-        """ Sweep mode. Valid options are VNA|CLAS """,
-        validator=strict_discrete_range,
-        values=["VNA", "CLAS"]
+        ":SA:MODE?",
+        ":SA:MODE %s",
+        """ Sweep mode. Valid options are VNA|CLAS.""",
+        validator=strict_discrete_set,
+        values=["VNA", "CLAS"],
     )
 
-    sweep_point = Instrument.control(
-        ":SENS<ch>:SWE:POI?", ":SENS<ch>:SWE:POI %d",
-        """ Sweep point""",
-        validator=strict_range,
-        values=[2, 100000]
+    sweep_points = Instrument.control(
+        ":SWE:POI?", ":SWE:POI %d", """ Sweep point.""", validator=strict_range, values=[2, 100000]
     )
 
     num_averages = Instrument.control(
-        "SENS<ch>:AVER:COUN?", "SENS<ch>:AVER:COUN %d",
-        """ Sets the number of averages""",
+        ":AVER:COUN?",
+        ":AVER:COUN %d",
+        """ Sets the number of averages.""",
         validator=strict_range,
-        values=[1, 20000]
+        values=[1, 20000],
     )
 
     bandwidth = Instrument.control(
-        "SENS<ch>:BWID?", "SENS<ch>:BWID %d",
-        """ Sets the IF bandwidth""",
+        ":BWID?",
+        ":BWID %d",
+        """ Sets the IF bandwidth.""",
         validator=strict_range,
-        values=[1, 1000000]
+        values=[1, 1000000],
     )
 
-    def __init__(self, instrument, number):
-        self.instrument = instrument
-        self.number = number
-
-    def values(self, command, **kwargs):
-        return self.instrument.values(command.replace("<ch>", str(self.number)),
-                                      **kwargs)
-
-    def write(self, command):
-        self.instrument.write(command.replace("<ch>", str(self.number)))
+    def insert_id(self, command):
+        return f":SENS{self.id}{command}"
 
 
 class AnritsuMS464xB(Instrument):
-    """ Represents the Anritsu MS474xB VNA. Implements controls to change the analysis
-        range and to retreve the data for the trace.
+    """Represents the Anritsu MS474xB VNA. Implements controls to change the analysis
+    range and to retreve the data for the trace.
     """
-    channels = Instrument.ChannelCreator(
-        VNAChannel, (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15))
 
-    def __init__(self, adapter, **kwargs):
-        super().__init__(
-            adapter,
-            "Anritsu MS464xB",
-            **kwargs
-        )
+    channels = Instrument.ChannelCreator(
+        VNAChannel, (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16)
+    )
+
+    class BinaryData_Byte_Format(CustomEnum):
+        MSB_FIRST = "NORM"
+        LSB_FIRST = "SWAP"
+
+    class DataFileParameter(CustomEnum):
+        """Parameter format in a data file"""
+
+        LINEAR_AND_PHASE = "LINPH"
+        LOG_AND_PHASE = "LOGPH"
+        REAL_AND_IMAGINARY_NUMBERS = "REIM"
+
+    class BinaryDataFormat(CustomEnum):
+        """Binary Data file format."""
+
+        ASC = "ASC"
+        REAL = "REAL"
+        REAL32 = "REAL32"
+
+    class DataFileFrequency(CustomEnum):
+        """Binary Data file frequency units."""
+
+        HZ = "HZ"
+        KHZ = "KHZ"
+        MHZ = "MHZ"
+        GHZ = "GHZ"
+
+    def __init__(self, adapter, name="Anritsu MS464xB", **kwargs):
+        super().__init__(adapter, name, **kwargs)
 
     def load_data_file(self, filename: str):
+        """Loads a data file in VNA HDD.
+
+        Args:
+            filename (str): full filename including path
+        """
         self.write(f":MMEM:LOAD '{filename}'")
 
     def delete_data_file(self, filename: str):
+        """Deletes a file on the VNA HDD.
+
+        Args:
+            filename (str): full filename including path
+        """
         self.write(f":MMEM:DEL '{filename}'")
 
     def copy_data_file(self, from_filename: str, to_filename: str):
+        """Copies a file on the VNA HDD.
+
+        Args:
+            from_filename (str): full filename including path
+            to_filename (str): full filename including path
+        """
         self.write(f":MMEM:COPY '{from_filename}', '{to_filename}'")
 
     def load_data_file_to_memory(self, filename: str):
+        """Loads a data file to a memory trace.
+
+        Args:
+            filename (str): full filename including path
+        """
         self.write(f":MMEM:LOAD:MDATA '{filename}'")
 
     def create_directory(self, dir_name: str):
+        """Create directory on the VNA HDD.
+
+        Args:
+            dir_name (str): full filename including path
+        """
         self.write(f":MMEM:MDIR '{dir_name}'")
 
     def delete_directory(self, dir_name: str):
+        """Deletes a directory on the VNA HDD.
+
+        Args:
+            dir_name (str): full filename including path
+        """
         self.write(f":MMEM:RDIR '{dir_name}'")
 
     def store_image(self, filename: str):
+        """Captures a screenshot to the file specified.
+
+        Args:
+            filename (str): full filename including path
+        """
         self.write(f":MMEM:STOR:IMAG '{filename}'")
+
+    binary_data_byte_format = Instrument.control(
+        ":FORM:BORD?",
+        ":FORM:BORD %s",
+        """ IO Data format. NORM|SWAP = MSB First|LSB First.""",
+        validator=strict_discrete_set,
+        values=BinaryData_Byte_Format,
+    )
+
+    binary_data_format = Instrument.control(
+        ":FORM:DATA?",
+        ":FORM:DATA %s",
+        """ IO Data format. ASC|REAL|REAL32.""",
+        validator=strict_discrete_set,
+        values=BinaryDataFormat,
+    )
+
+    datafile_header = Instrument.control(
+        ":FORM:DATA:HEAD?",
+        ":FORM:DATA:HEAD %d",
+        """Sets the on/off state of including a heading with data files.""",
+        validator=strict_discrete_set,
+        values={True: 1, False: 0},
+        map_values=True,
+    )
+
+    datafile_frequency = Instrument.control(
+        ":FORM:SNP:FREQ?",
+        ":FORM:SNP:FREQ %s",
+        """ Sets the frequency unit in a SNP data file.
+
+        Use the DataFileFrequency enum as input values.""",
+        validator=strict_discrete_set,
+        values=DataFileFrequency,
+    )
+
+    datafile_parameter = Instrument.control(
+        ":FORM:SNP:PAR?",
+        ":FORM:SNP:PAR %s",
+        """ Sets the parameter format in a SNP data file.
+
+        Use the DataFileParameter enum as input values.""",
+        validator=strict_discrete_set,
+        values=DataFileParameter,
+    )
+
+    def read_datafile(
+        self,
+        channel: int,
+        sweep_points: int,
+        datafile_freq: DataFileFrequency,
+        datafile_par: DataFileParameter,
+        filename: str,
+    ):
+        """Reads a data file from the VNA
+
+        Args:
+            channel (int): Channel Index
+            sweep_points (int): number of sweep point as an integer
+            datafile_freq (DataFileFrequency): Data file frequency based on enum
+            datafile_par (DataFileParameter): Data file parameter based on enum
+            filename (str): full path of the file to be saved
+        """
+        cur_ch = self.channels[channel]  # type: VNAChannel
+        cur_ch.sweep_points = sweep_points
+        self.datafile_frequency = datafile_freq
+        self.datafile_parameter = datafile_par
+        self.write("TRS;WFS;OS2P")
+
+        bytes_to_transfer = int(self.read_bytes(11)[2:11])
+        data = self.read_bytes(bytes_to_transfer)
+        with open(filename, "w") as textfile:
+            data_list = data.split(b"\r\n")
+            for s in data_list:
+                textfile.write(str(s)[2 : len(s)] + "\n")  # noqa
--- tests/instruments/anritsu/test_anritsuMS464xB.py
+++ tests/instruments/anritsu/test_anritsuMS464xB.py
@@ -29,16 +29,142 @@ from pymeasure.instruments.anritsu import AnritsuMS464xB
 
 def test_init():
     with expected_protocol(
-            AnritsuMS464xB,
-            [],
-            ):
+        AnritsuMS464xB,
+        [],
+    ):
         pass  # Verify the expected communication.
 
 
 def test_center_freq():
     with expected_protocol(
-            AnritsuMS464xB,
-            [(b":SENS1:FREQ:CENT 35000", None), (b":SENS1:FREQ:CENT?", 35000)],
-            ) as instr:
-        instr.channels[1].center_frequency = 35000
-        assert instr.channels[1].center_frequency == 35000
+        AnritsuMS464xB,
+        [(b":SENS1:FREQ:CENT 35000", None), (b":SENS1:FREQ:CENT?", 35000)],
+    ) as instr:
+        instr.ch_1.center_frequency = 35000
+        assert instr.ch_1.center_frequency == 35000
+
+
+def test_cw_freq():
+    with expected_protocol(
+        AnritsuMS464xB,
+        [(b":SENS1:FREQ:CW 35000", None), (b":SENS1:FREQ:CW?", 35000)],
+    ) as instr:
+        instr.ch_1.cw_frequency = 35000
+        assert instr.ch_1.cw_frequency == 35000
+
+
+def test_span_frequency():
+    with expected_protocol(
+        AnritsuMS464xB,
+        [(b":SENS1:FREQ:SPAN 35000", None), (b":SENS1:FREQ:SPAN?", 35000)],
+    ) as instr:
+        instr.ch_1.span_frequency = 35000
+        assert instr.ch_1.span_frequency == 35000
+
+
+def test_start_frequency():
+    with expected_protocol(
+        AnritsuMS464xB,
+        [(b":SENS1:FREQ:STAR 35000", None), (b":SENS1:FREQ:STAR?", 35000)],
+    ) as instr:
+        instr.ch_1.start_frequency = 35000
+        assert instr.ch_1.start_frequency == 35000
+
+
+def test_stop_frequency():
+    with expected_protocol(
+        AnritsuMS464xB,
+        [(b":SENS1:FREQ:STOP 35000", None), (b":SENS1:FREQ:STOP?", 35000)],
+    ) as instr:
+        instr.ch_1.stop_frequency = 35000
+        assert instr.ch_1.stop_frequency == 35000
+
+
+def test_sweep_type():
+    with expected_protocol(
+        AnritsuMS464xB,
+        [(b":SENS1:SWE:TYP LOG", None), (b":SENS1:SWE:TYP?", "LOG")],
+    ) as instr:
+        instr.ch_1.sweep_type = "LOG"
+        assert instr.ch_1.sweep_type == "LOG"
+
+
+def test_sweep_mode():
+    with expected_protocol(
+        AnritsuMS464xB,
+        [(b":SENS1:SA:MODE VNA", None), (b":SENS1:SA:MODE?", "VNA")],
+    ) as instr:
+        instr.ch_1.sweep_mode = "VNA"
+        assert instr.ch_1.sweep_mode == "VNA"
+
+
+def test_sweep_point():
+    with expected_protocol(
+        AnritsuMS464xB,
+        [(b":SENS1:SWE:POI 6535", None), (b":SENS1:SWE:POI?", 6535)],
+    ) as instr:
+        instr.ch_1.sweep_points = 6535
+        assert instr.ch_1.sweep_points == 6535
+
+
+def test_num_averages():
+    with expected_protocol(
+        AnritsuMS464xB,
+        [(b":SENS1:AVER:COUN 6535", None), (b":SENS1:AVER:COUN?", 6535)],
+    ) as instr:
+        instr.ch_1.num_averages = 6535
+        assert instr.ch_1.num_averages == 6535
+
+
+def test_bandwidth():
+    with expected_protocol(
+        AnritsuMS464xB,
+        [(b":SENS1:BWID 65535", None), (b":SENS1:BWID?", 65535)],
+    ) as instr:
+        instr.ch_1.bandwidth = 65535
+        assert instr.ch_1.bandwidth == 65535
+
+
+def test_binary_data_byte_format():
+    with expected_protocol(
+        AnritsuMS464xB,
+        [(b":FORM:BORD NORM", None), (b":FORM:BORD?", "NORM")],
+    ) as instr:
+        instr.binary_data_byte_format = AnritsuMS464xB.BinaryData_Byte_Format.MSB_FIRST
+        assert instr.binary_data_byte_format == str(AnritsuMS464xB.BinaryData_Byte_Format.MSB_FIRST)
+
+
+def test_binary_format():
+    with expected_protocol(
+        AnritsuMS464xB,
+        [(b":FORM:DATA REAL", None), (b":FORM:DATA?", "REAL")],
+    ) as instr:
+        instr.binary_data_format = AnritsuMS464xB.BinaryDataFormat.REAL
+        assert instr.binary_data_format == str(AnritsuMS464xB.BinaryDataFormat.REAL)
+
+
+def test_datafile_header():
+    with expected_protocol(
+        AnritsuMS464xB,
+        [(b":FORM:DATA:HEAD 1", None), (b":FORM:DATA:HEAD?", 1)],
+    ) as instr:
+        instr.datafile_header = True
+        assert instr.datafile_header is True
+
+
+def test_datafile_frequency():
+    with expected_protocol(
+        AnritsuMS464xB,
+        [(b":FORM:SNP:FREQ KHZ", None), (b":FORM:SNP:FREQ?", "KHZ")],
+    ) as instr:
+        instr.datafile_frequency = AnritsuMS464xB.DataFileFrequency.KHZ
+        assert instr.datafile_frequency == str(AnritsuMS464xB.DataFileFrequency.KHZ)
+
+
+def test_datafile_parameter():
+    with expected_protocol(
+        AnritsuMS464xB,
+        [(b":FORM:SNP:PAR LOGPH", None), (b":FORM:SNP:PAR?", "LOGPH")],
+    ) as instr:
+        instr.datafile_parameter = AnritsuMS464xB.DataFileParameter.LOG_AND_PHASE
+        assert instr.datafile_parameter == str(AnritsuMS464xB.DataFileParameter.LOG_AND_PHASE)
