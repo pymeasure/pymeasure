308c37f6f1fc013c9a189f2782b17c97aa196995
mcdo0486 <mcdo0486@umn.edu>
2023-01-03 17:21:25 -0600

    rm dock_menu

--- pymeasure/display/widgets/dock_widget.py
+++ pymeasure/display/widgets/dock_widget.py
@@ -100,13 +100,6 @@ class DockWidget(TabWidget, QtWidgets.QWidget):
                 menu.addAction(self.save_dock_action())
                 menu.exec(self.mapToGlobal(position))
 
-    def dock_menu(self, position):
-        # if child widget is PlotWidget or DockLabel, create context menu
-        if isinstance(self.childAt(position), (PlotWidget, DockLabel)):
-            menu = QtWidgets.QMenu(self)
-            menu.addAction(self.save_dock_action())
-            menu.exec(self.mapToGlobal(position))
-
     def _setup_ui(self):
         for i in range(self.num_plots):
             # Set the default label for current dock from x_axis_labels and y_axis_labels
