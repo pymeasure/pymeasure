d2c31a27768674e2e1a0ad4c1ca329923c4e836e
Michele Sardo <msmttchr@gmail.com>
2023-04-16 11:17:20 +0200

    Added tutorial documentation
    Also changed float display from f to g format (allow display of floating numbers of
    any size)

--- examples/Basic/gui_table.py
+++ examples/Basic/gui_table.py
@@ -1,3 +1,27 @@
+#
+# This file is part of the PyMeasure package.
+#
+# Copyright (c) 2013-2023 PyMeasure Developers
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
+
 """
 This example demonstrates how to make a graphical interface, and uses
 a random number generator to simulate data so that it does not require
@@ -71,7 +95,7 @@ class MainWindow(ManagedWindowBase):
             displays=['iterations', 'delay', 'seed'],
             widget_list=widget_list,
         )
-        logging.getLogger().addHandler(widget_list[1].handler)  # needs to be in Qt context?
+        logging.getLogger().addHandler(widget_list[1].handler)
         log.setLevel(self.log_level)
         log.info("ManagedWindow connected to logging")
         self.setWindowTitle('GUI Example')
