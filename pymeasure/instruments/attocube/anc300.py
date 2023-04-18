93a2c8cb4de8eaa3578f2627e43756d4828b2d58
Benedikt Moneke <67148916+bmoneke@users.noreply.github.com>
2023-04-18 12:11:01 +0200

    A few instruments improved.

--- pymeasure/instruments/attocube/anc300.py
+++ pymeasure/instruments/attocube/anc300.py
@@ -189,7 +189,7 @@ class Axis:
         else:
             pass  # do not set stepu/d to 0 since it triggers a continous move
         # wait for the move to finish
-        self.controller.wait_for(abs(steps)/self.frequency)
+        self.controller.wait_for(abs(steps) / self.frequency)
         # ask if movement finished
         self.ask('stepw')
         if gnd:
@@ -231,11 +231,11 @@ class ANC300Controller(Instrument):
     :param kwargs: Any valid key-word argument for VISAAdapter
     """
     version = Instrument.measurement(
-        "ver", """ Version number and instrument identification """
+        "ver", """ Get the version number and instrument identification. """
     )
 
     controllerBoardVersion = Instrument.measurement(
-        "getcser", """ Serial number of the controller board """
+        "getcser", """ Get the serial number of the controller board. """
     )
 
     _reg_value = re.compile(r"\w+\s+=\s+([\w\.]+)")
