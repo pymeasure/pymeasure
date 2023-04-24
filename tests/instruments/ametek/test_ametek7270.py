f00a892e7713b7521d800b3e96824b8b6d1695c4
Sébastien Weber <seba.weber@gmail.com>
2023-04-24 15:52:22 +0200

    added author and test file

---
+++ tests/instruments/ametek/test_ametek7270.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created the 24/04/2023
+
+@author: Sebastien Weber
+"""
+
+
+import pytest
+
+from pymeasure.test import expected_protocol
+
+from pymeasure.instruments.ametek.ametek7270 import Ametek7270
+
+
+
