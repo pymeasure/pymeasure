1ab88dfd0178a928895c626c9342f48fcfbca93d
mcdo0486 <mcdo0486@umn.edu>
2023-05-12 09:44:08 -0500

    Replace union operator with unpacking

--- pymeasure/display/widgets/plot_frame.py
+++ pymeasure/display/widgets/plot_frame.py
@@ -70,7 +70,7 @@ class PlotFrame(QtWidgets.QFrame):
 
         self.plot = self.plot_widget.getPlotItem()
 
-        style = dict(self.LABEL_STYLE | {'justify': 'right'})
+        style = dict({'justify': 'right'}, **self.LABEL_STYLE)
         if "font-size" in style:  # LabelItem wants the size as 'size' rather than 'font-size'
             style["size"] = style.pop("font-size")
         self.coordinates = pg.LabelItem("", parent=self.plot, **style)
