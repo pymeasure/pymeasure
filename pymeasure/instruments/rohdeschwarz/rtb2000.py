a57c633b03f79c17d7f0fc72d311f6b80f2a1d41
neuschs <sebastian.neusch@gmail.com>
2023-06-09 22:12:34 +0200

    * added passive probe commands * horizontal settings and vertical settings

--- pymeasure/instruments/rohdeschwarz/rtb2000.py
+++ pymeasure/instruments/rohdeschwarz/rtb2000.py
@@ -26,8 +26,6 @@ import logging
 
 from enum import Enum
 
-from numpy import arange
-
 from pymeasure.instruments.validators import strict_discrete_set, strict_range
 from pymeasure.instruments import Instrument, Channel
 
@@ -112,56 +110,119 @@ class ThresholdHysteresis(StrEnum):
     Large = "LARG"
 
 
+class DecimationMode(StrEnum):
+    """Enumeration to represent the decimation modes per channel"""
+
+    #: Input data is acquired with a sample rate which is aligned to the time base (horizontal
+    # scale) and the record length.
+    Sample = "SAMP"
+
+    #: Peak Detect: the minimum and the maximum of n samples in a sample interval are recorded as
+    # waveform points.
+    PeakDetect = "PDET"
+
+    #: High resolution: The average of n sample points is recorded as waveform point.
+    HighResolution = "HRES"
+
+
+class AcquisitionMode(StrEnum):
+    """Enumeration to represent the acquisition modes"""
+
+    #: The acquisitions are displayed as they are done.
+    Refresh = "REF"
+
+    #: The acquisitions are averaged.
+    Average = "AVER"
+
+    #: The envelope of a repetitive signal is shown, representing the borders in which the signal
+    # occurs.
+    Envelope = "ENV"
+
+
+class ArithmeticMethod(StrEnum):
+    """Enumeration to represent the acquisition modes"""
+
+    #: The data of the current acquisition is recorded according to the decimation settings.
+    Off = "OFF"
+
+    #: Detects the minimum and maximum values in an sample interval over a number of acquisitions.
+    Envelope = "ENV"
+
+    #: Calculates the average from the data of the current acquisition and a number of
+    # acquisitions before. The number of used acquisitions is set with
+    # :attr:`RTB200X.acquisition_average_count`
+    Average = "AVER"
+
+
+class InterpolationMode(StrEnum):
+    """Enumeration to represent the interpolation modes"""
+
+    #: Linear interpolation between two adjacent sample points.
+    Linear = "LIN"
+
+    #: Interpolation by means of a sin(x)/x curve.
+    SinX = "SINX"
+
+    #: Sample & hold causes a histogram-like interpolation.
+    SampleHold = "SMHD"
+
+
 class RTB200XAnalogChannel(Channel):
     """
     One RTB2000 series oscilloscope channel
     """
 
+    # vertical settings
     state = Channel.control(
         "CHANnel{ch}:STATe?", "CHANnel{ch}:STATe %s",
         """
-        Control the channel signal. 
+        Control the channel signal.
         Switch it either on or off.
         """,
         validator=strict_discrete_set,
         map_values=True,
         values={True: "ON", False: "OFF"},
-        cast=str
+        cast=str,
+        check_set_errors=True,
     )
 
-    scale = Channel.control(
+    vertical_scale = Channel.control(
         "CHANnel{ch}:SCALe?", "CHANnel{ch}:SCALe %g",
         """
         Control the vertical scale for this channel.
         """,
         validator=strict_range,
-        values=[1e-3, 10]
+        values=[1e-3, 10],
+        check_set_errors=True,
     )
 
-    range = Channel.control(
+    vertical_range = Channel.control(
         "CHANnel{ch}:RANGe?", "CHANnel{ch}:RANGe %g",
         """
         Control the voltage range across the all vertical divisions of the diagram. Use the command
         alternatively instead :attr:`scale`
         """,
         validator=strict_range,
-        values=[8e-3, 80]
+        values=[8e-3, 80],
+        check_set_errors=True,
     )
 
-    position = Channel.control(
+    vertical_position = Channel.control(
         "CHANnel{ch}:POSition?", "CHANnel{ch}:POSition %g",
         """
         Control the vertical position of the waveform in divisions.
         """,
         validator=strict_range,
-        values=[-5, 5]
+        values=[-5, 5],
+        check_set_errors=True,
     )
 
-    offset = Channel.control(
+    vertical_offset = Channel.control(
         "CHANnel{ch}:OFFSet?", "CHANnel{ch}:OFFSet %g",
         """
         Control the offset voltage, which is subtracted to correct an offset-affected signal.
-        """
+        """,
+        check_set_errors=True,
     )
 
     coupling = Channel.control(
@@ -170,7 +231,8 @@ class RTB200XAnalogChannel(Channel):
         Control the connection of the indicated channel signal - coupling and termination.
         """,
         validator=strict_discrete_set,
-        values=[e for e in Coupling]
+        values=[e for e in Coupling],
+        check_set_errors=True,
     )
 
     bandwidth = Channel.control(
@@ -179,7 +241,8 @@ class RTB200XAnalogChannel(Channel):
         Control the bandwidth limit for the indicated channel.
         """,
         validator=strict_discrete_set,
-        values=[e for e in Bandwidth]
+        values=[e for e in Bandwidth],
+        check_set_errors=True,
     )
 
     polarity_inversion_active = Channel.control(
@@ -193,31 +256,34 @@ class RTB200XAnalogChannel(Channel):
         validator=strict_discrete_set,
         map_values=True,
         values={True: "INV", False: "NORM"},
-        cast=str
+        cast=str,
+        check_set_errors=True,
     )
 
     skew = Channel.control(
         "CHANnel{ch}:SKEW?", "CHANnel{ch}:SKEW %g",
         """
         Control a delay for the selected channel.
-        
+
         Deskew compensates delay differences between channels caused by the different
         length of cables, probes, and other sources. Correct deskew values are important for
         accurate triggering.
-        """
+        """,
+        check_set_errors=True,
     )
 
     zero_offset = Channel.control(
         "CHANnel{ch}:ZOFFset?", "CHANnel{ch}:ZOFFset %g",
         """
         Control the zero offset.
-        
+
         Differences in DUT and oscilloscope ground levels may cause larger zero errors affecting
         the waveform. If the DUT is ground-referenced, the "Zero Offset" corrects the zero
         error and sets the probe to the zero level.
         You can assess the zero error by measuring the mean value of a signal that should
         return zero.
-        """
+        """,
+        check_set_errors=True,
     )
 
     waveform_color = Channel.control(
@@ -226,6 +292,7 @@ class RTB200XAnalogChannel(Channel):
         Control the color scale for the waveform color. Each scale comprises a set of colors,
         where each color represents a certain frequency of occurrence.
         """,
+        check_set_errors=True,
         validator=strict_discrete_set,
         values=[e for e in WaveformColor]
     )
@@ -233,10 +300,10 @@ class RTB200XAnalogChannel(Channel):
     threshold = Channel.control(
         "CHANnel{ch}:THReshold?", "CHANnel{ch}:THReshold %g",
         """
-        Control the Threshold value for digitization of analog signals. 
-    
-        If the signal value is higher than the threshold, the signal state is high (1 or true for 
-        the Boolean logic). Otherwise, the signal state is considered low (0 or false) if the 
+        Control the Threshold value for digitization of analog signals.
+
+        If the signal value is higher than the threshold, the signal state is high (1 or true for
+        the Boolean logic). Otherwise, the signal state is considered low (0 or false) if the
         signal value is below the threshold.
 
         Often used values are:
@@ -244,7 +311,8 @@ class RTB200XAnalogChannel(Channel):
         - ECL: -1.3 V
         - CMOS: 2.5 V
         Default unit: V
-        """
+        """,
+        check_set_errors=True
     )
 
     def find_threshold(self):
@@ -256,18 +324,96 @@ class RTB200XAnalogChannel(Channel):
     threshold_hysteresis = Channel.control(
         "CHANnel{ch}:THReshold:HYSTeresis?", "CHANnel{ch}:THReshold:HYSTeresis %s",
         """
-       Defines the size of the hysteresis to avoid the change of signal states due to noise.
+        Control the size of the hysteresis to avoid the change of signal states due to noise.
         """,
+        check_set_errors=True,
         validator=strict_discrete_set,
         values=[e for e in ThresholdHysteresis]
     )
 
-    label = Channel.setting(
-        "CHANnel{ch}:LABel %s",
+    label = Channel.control(
+        "CHANnel{ch}:LABel?", "CHANnel{ch}:LABel %s",
         """
         Set a name for the selected channel.
-        """
+
+        Maximum 8 characters more will be cut.
+        """,
+        check_set_errors=True,
+        get_process=lambda v: v[1:-1],
+        set_process=lambda v: "\"" + v + "\""
     )
+
+    label_active = Channel.control(
+        "CHANnel{ch}:LABel:STATe?", "CHANnel{ch}:LABel:STATe %s",
+        """
+        Control if the channel label will be shown or not
+        """,
+        validator=strict_discrete_set,
+        map_values=True,
+        values={True: "ON", False: "OFF"},
+        cast=str,
+        check_set_errors=True,
+    )
+
+    # passive probes
+    probe_unit = Channel.control(
+        "PROBe{ch}:SETup:ATTenuation:UNIT?", "PROBe{ch}:SETup:ATTenuation:UNIT %s",
+        """
+        Control the unit that the probe can measure.
+
+        Either 'V' or 'A',
+        """,
+        validator=strict_discrete_set,
+        values=['V', 'A'],
+        check_set_errors=True
+    )
+
+    probe_attenuation = Channel.control(
+        "PROBe{ch}:SETup:ATTenuation:MANual?", "PROBe{ch}:SETup:ATTenuation:MANual %g",
+        """
+        Control the attenuation of the probe.
+
+        Range: 0.0001 to 10e6
+        """,
+        validator=strict_range,
+        values=[1e-4, 10e6],
+        check_set_errors=True
+    )
+
+    probe_gain = Channel.control(
+        "PROBe{ch}:SETup:GAIN:MANual?", "PROBe{ch}:SETup:GAIN:MANual %g",
+        """
+        Control the gain of the probe or signal chain.
+
+        Range: 0.0001 to 10e6
+        """,
+        validator=strict_range,
+        values=[1e-4, 10e3],
+        check_set_errors=True
+    )
+
+    decimation_mode = Channel.control(
+        "CHANnel{ch}:TYPE?", "CHANnel{ch}:TYPE %s",
+        """
+        Control the method to reduce the data stream of the ADC to a stream of waveform points
+        with lower sample rate.
+        """,
+        check_set_errors=True,
+        validator=strict_discrete_set,
+        values=[e for e in DecimationMode]
+    )
+
+    arithmetics = Channel.control(
+        "CHANnel{ch}:ARIThmetics?", "CHANnel{ch}:ARIThmetics %s",
+        """
+        Control the method to build the resulting waveform from several consecutive acquisitions
+        of the signal.
+        """,
+        check_set_errors=True,
+        validator=strict_discrete_set,
+        values=[e for e in ArithmeticMethod]
+    )
+
 
 class RTB200X(Instrument):
     """
@@ -293,11 +439,12 @@ class RTB200X(Instrument):
 
     acquisition_state = Instrument.control(
         "ACQuire:STATe?", "ACQuire:STATe %s",
-    """
+        """
         Control the acquisition state of the instrument
         """,
         validator=strict_discrete_set,
-        values=[e for e in AcquisitionState]
+        values=[e for e in AcquisitionState],
+        check_set_errors=True,
     )
 
     def activate_analog_channels(self):
@@ -312,8 +459,222 @@ class RTB200X(Instrument):
         """
         self.write("CHANNEL1:AOFF")
 
+    # horizontal settings
+    horizontal_scale = Instrument.control(
+        "TIMebase:SCALe?", "TIMebase:SCALe %g",
+        """
+        Control the horizontal scale for all channel and math waveforms.
+        """,
+        validator=strict_range,
+        values=[1e-9, 50],
+        check_set_errors=True,
+    )
+
+    horizontal_position = Instrument.control(
+        "TIMebase:POSition?", "TIMebase:POSition %g",
+        """
+        Control the trigger position, the time distance from the trigger point to the reference
+        point (trigger offset). The trigger point is the zero point of the diagram. Changing the
+        horizontal position, you can move the trigger, even outside the screen.
+        """,
+        check_set_errors=True,
+    )
+
+    timebase_reference = Instrument.control(
+        "TIMebase:REFerence?", "TIMebase:REFerence %g",
+        """
+        Control the time reference point in the diagram. The reference point is the rescaling
+        center of the time scale on the screen. If you modify the time scale, the reference point
+        remains fixed on the screen, and the scale is stretched or compressed to both sides of
+        the reference point.
+        The reference point defines which part of the waveform is shown. By default, the reference
+        point is displayed in the center of the window, and you can move it to the left or
+        right.
+        """,
+        validator=strict_discrete_set,
+        values=[8.33, 50, 91.67],
+        check_set_errors=True,
+    )
+
+    acquisition_time = Instrument.control(
+        "TIMebase:ACQTime?", "TIMebase:ACQTime %g",
+        """
+        Control the time of one acquisition, that is the time across the 12 divisions of the diagram
+        """,
+        validator=strict_range,
+        values=[250e-12, 500],
+        check_set_errors=True,
+    )
+
+    horizontal_divisions = Instrument.measurement(
+        "TIMebase:DIVisions?",
+        """
+        Get the number of horizontal divisions on the screen.
+        """
+    )
+
+    # TIMebase:RATime not implemented
+
+    # acquisition settings
+    acquisition_points_automatic = Instrument.control(
+        "ACQuire:POINts:AUTomatic?", "ACQuire:POINts:AUTomatic %s",
+        """
+        Control the automatic record length. The instrument sets a value that fits to the
+        selected timebase. If you set a specific value with :attr:`acquisition_points`, the
+        automatic assignment of a record length is turned off.
+
+        .. code-block:: python
+            instr.acquisition_points_automatic = True
+            instr.horizontal_scale = 1e-9
+            print(instr.acquisition_points_automatic)
+            # 10000
+            instr.horizontal_scale = 5e-3
+            print(instr.acquisition_points_automatic)
+            # 20000000
+        """,
+        validator=strict_discrete_set,
+        map_values=True,
+        values={True: "ON", False: "OFF"},
+        cast=str,
+        check_set_errors=True,
+    )
+
+    acquisition_points = Instrument.control(
+        "ACQuire:POINts:VALue?", "ACQuire:POINts:VALue %g",
+        """
+        Control the record length value, the number of recorded waveform points in a segment.
+        The command turns :attr:`acquisition_points_automatic` false.
+        If :attr:`acquisition_points_automatic` is true, the query acquisition_points returns
+        the automatically set record length.
+        Each predefined record length corresponds to a maximum number of history segments,
+        which are stored in the instrument's memory. If option R&S RTB-K15 is installed,
+        you can display the history segments.
+
+        If the entered value differs from the predefined values, the instrument sets the closest
+        value.
+
+        Available record length values are:
+        - 10 kSa (13107 history segments)
+        - 20 kSa (13107 history segments)
+        - 50 kSa (3276 history segments)
+        - 100 kSa (2621 history segments)
+        - 200 kSa (1456 history segments)
+        - 500 kSa (319 history segments)
+        - 1 MSa (319 history segments)
+        - 2 MSa (159 history segments)
+        - 5 MSa (40 history segments)
+        - 10 MSa (32 history segments)
+        - 20 MSa (16 history segments)
+        """,
+        validator=strict_discrete_set,
+        values=[10e3, 20e3, 50e3, 100e3, 200e3, 500e3, 1e6, 2e6, 5e6, 10e6, 20e6],
+        check_set_errors=True,
+    )
+
+    acquisition_mode = Instrument.control(
+        "ACQuire:TYPE?", "ACQuire:TYPE %s",
+        """
+        Control the type of the aquisition mode.
+        """,
+        validator=strict_discrete_set,
+        values=[e for e in AcquisitionMode],
+        check_set_errors=True,
+    )
+
+    acquisition_count = Instrument.control(
+        "ACQuire:NSINgle:COUNt?", "ACQuire:NSINgle:COUNt %g",
+        """
+        Control the number of waveforms acquired with :attr:`acquisition_state`
+        """,
+        check_set_errors=True,
+    )
+
+    acquisition_average_count = Instrument.control(
+        "ACQuire:AVERage:COUNt?", "ACQuire:AVERage:COUNt %g",
+        """
+        Control the number of waveforms used to calculate the average waveform. The higher the
+        number, the better the noise is reduced.
+        """,
+        validator=strict_range,
+        values=[2, 100000],
+        check_set_errors=True,
+    )
+
+    def reset_averaging(self):
+        """
+        Delete the waveform and restarts the average calculation.
+        """
+        self.write("ACQuire:AVERage:RESet")
+
+    average_complete = Instrument.measurement(
+        "ACQuire:AVERage:COMPlete?",
+        """
+        Get the state of averaging.
+        """,
+        map_values=True,
+        values={True: "1", False: "0"},
+        cast=str,
+    )
+
+    roll_automatic_enabled = Instrument.control(
+        "TIMebase:ROLL:AUTomatic?", "TIMebase:ROLL:AUTomatic %s",
+        """
+        Control the automatic roll mode. The instrument switches to roll mode if the timebase
+        is equal or slower than the roll mode limit defined with TODO TIMebase:ROLL:MTIMe.
+        """,
+        validator=strict_discrete_set,
+        map_values=True,
+        values={True: "ON", False: "OFF"},
+        cast=str,
+        check_set_errors=True,
+    )
+
+    roll_minimum_time = Instrument.control(
+        "TIMebase:ROLL:MTIMe?", "TIMebase:ROLL:MTIMe %g",
+        """
+        Control the value at which the roll mode gets automatically activated,
+        if :attr:`roll_automatic_enabled` is active.
+        Default unit: s/div
+        """,
+        check_set_errors=True,
+    )
+
+    interpolation_mode = Instrument.control(
+        "ACQuire:INTerpolate?", "ACQuire:INTerpolate %s",
+        """
+        Control the interpolation mode.
+        """,
+        validator=strict_discrete_set,
+        values=[e for e in InterpolationMode],
+        check_set_errors=True,
+    )
+
+    adc_sample_rate = Instrument.measurement(
+        "ACQuire:POINts:ARATe?",
+        """
+        Get the sample rate of the ADC, that is the number of points that are sampled by
+        the ADC in one second.
+        Default unit: Hz
+        """
+    )
+
+    sample_rate = Instrument.measurement(
+        "ACQuire:SRATe?",
+        """
+        Get the sample rate, that is the number of recorded waveform samples per second.
+        Default unit: Sa/s
+        """
+    )
+
 
 class RTB2004(RTB200X):
+    def __init__(self, adapter, name="Rohde & Schwarz RTB2004", **kwargs):
+        super().__init__(
+            adapter,
+            name,
+            **kwargs,
+        )
+
     analog = Instrument.ChannelCreator(
         RTB200XAnalogChannel,
         (1, 2, 3, 4),
@@ -321,6 +682,14 @@ class RTB2004(RTB200X):
 
 
 class RTB2002(RTB200X):
+
+    def __init__(self, adapter, name="Rohde & Schwarz RTB2002", **kwargs):
+        super().__init__(
+            adapter,
+            name,
+            **kwargs,
+        )
+
     analog = Instrument.ChannelCreator(
         RTB200XAnalogChannel,
         (1, 2),
--- tests/instruments/rohdeschwarz/test_rtb2000.py
+++ tests/instruments/rohdeschwarz/test_rtb2000.py
@@ -26,24 +26,33 @@ import pytest
 from pymeasure.test import expected_protocol
 
 from pymeasure.instruments.rohdeschwarz.rtb2000 import RTB200X, RTB2004, Coupling, Bandwidth, \
-    WaveformColor, ThresholdHysteresis
+    WaveformColor, ThresholdHysteresis, DecimationMode, AcquisitionMode, ArithmeticMethod, \
+    InterpolationMode
 
 from pymeasure.instruments.rohdeschwarz.rtb2000 import AcquisitionState
+
+CHECK_ERROR = ("SYST:ERR?", "0,\"No error\"")
 
 
 class TestRTB200X:
 
-    @pytest.mark.parametrize("enum", [e for e in AcquisitionState])
-    def test_acquisition_state(self, enum):
+    @pytest.mark.parametrize("enum,attr,command", [
+        (AcquisitionState, "acquisition_state", "ACQuire:STATe"),
+        (AcquisitionMode, "acquisition_mode", "ACQuire:TYPE"),
+        (InterpolationMode, "interpolation_mode", "ACQuire:INTerpolate")
+    ])
+    def test_enumeration_attributes(self, enum, attr, command):
+        value = [e for e in enum][0]
         with expected_protocol(
                 RTB200X,
                 [
-                    ("ACQuire:STATe %s" % enum, None),
-                    ("ACQuire:STATe?", enum)
+                    ("%s %s" % (command, value), None),
+                    CHECK_ERROR,
+                    ("%s?" % command, value)
                 ],
         ) as instr:
-            instr.acquisition_state = enum
-            assert instr.acquisition_state == enum
+            setattr(instr, attr, value)
+            assert getattr(instr, attr) == value
 
     def test_autoscale(self):
         with expected_protocol(
@@ -54,37 +63,108 @@ class TestRTB200X:
         ) as instr:
             instr.autoscale()
 
+    @pytest.mark.parametrize("attr,value,command", [
+        ("horizontal_scale", 2, "TIMebase:SCALe"),
+        ("horizontal_position", 2, "TIMebase:POSition"),
+        ("timebase_reference", 50, "TIMebase:REFerence"),
+        ("acquisition_time", 1, "TIMebase:ACQTime"),
+        ("acquisition_points", 10000, "ACQuire:POINts:VALue"),
+        ("acquisition_count", 10, "ACQuire:NSINgle:COUNt"),
+        ("acquisition_average_count", 10, "ACQuire:AVERage:COUNt"),
+        ("roll_minimum_time", 10, "TIMebase:ROLL:MTIMe")
+    ])
+    def test_value_control(self, attr, value, command):
+        with expected_protocol(
+                RTB200X,
+                [
+                    ("%s %s" % (command, str(value)), None),
+                    CHECK_ERROR,
+                    ("%s?" % command, value)
+                ],
+        ) as instr:
+            setattr(instr, attr, value)
+            assert getattr(instr, attr) == value
+
+    @pytest.mark.parametrize("attr,value,command", [
+        ("horizontal_divisions", 12, "TIMebase:DIVisions"),
+        ("adc_sample_rate", 10000000, "ACQuire:POINts:ARATe"),
+        ("sample_rate", 1000000, "ACQuire:SRATe")
+    ])
+    def test_value_measure(self, attr, value, command):
+        with expected_protocol(
+                RTB200X,
+                [
+                    ("%s?" % command, value)
+                ],
+        ) as instr:
+            assert getattr(instr, attr) == value
+
+    @pytest.mark.parametrize("attr, command", [
+        ("acquisition_points_automatic", "ACQuire:POINts:AUTomatic"),
+        ("roll_automatic_enabled", "TIMebase:ROLL:AUTomatic")
+    ])
+    @pytest.mark.parametrize("on_off,state", [("ON", True), ("OFF", False)])
+    def test_on_off(self, on_off, state, attr, command):
+        with expected_protocol(
+                RTB200X,
+                [
+                    ("%s %s" % (command, on_off), None),
+                    CHECK_ERROR,
+                    ("%s?" % command, on_off)
+                ],
+        ) as instr:
+            setattr(instr, attr, state)
+            assert getattr(instr, attr) == state
+
+    def test_average_complete(self):
+        with expected_protocol(
+                RTB200X,
+                [
+                    ("ACQuire:AVERage:COMPlete?", "1")
+                ],
+        ) as instr:
+            assert instr.average_complete is True
+
 
 class TestRTB200XAnalogChannel:
     DUT = RTB2004
 
+    @pytest.mark.parametrize("attr, command", [
+        ("state", "STATe"),
+        ("label_active", "LABel:STATe")
+    ])
     @pytest.mark.parametrize("on_off,state", [("ON", True), ("OFF", False)])
-    def test_on_off(self, on_off, state):
+    def test_on_off(self, on_off, state, attr, command):
         with expected_protocol(
                 self.DUT,
                 [
-                    ("CHANnel1:STATe %s" % on_off, None),
-                    ("CHANnel1:STATe?", on_off)
+                    ("CHANnel1:%s %s" % (command, on_off), None),
+                    CHECK_ERROR,
+                    ("CHANnel1:%s?" % command, on_off)
                 ],
         ) as instr:
-            instr.ch1.state = state
-            assert instr.ch1.state == state
+            setattr(instr.ch1, attr, state)
+            assert getattr(instr.ch1, attr) == state
 
     @pytest.mark.parametrize("attr,value,command", [
-        ("scale", 2.0, "SCALe"),
-        ("range", 2.0, "RANGe"),
-        ("position", 5, "POSition"),
-        ("offset", 2, "OFFSet"),
-        ("skew", 2, "SKEW"),
-        ("zero_offset", 2, "ZOFFset"),
-        ("threshold", 2, "THReshold")
+        ("vertical_scale", 2, "CHANnel1:SCALe"),
+        ("vertical_range", 2, "CHANnel1:RANGe"),
+        ("vertical_position", 5, "CHANnel1:POSition"),
+        ("vertical_offset", 2, "CHANnel1:OFFSet"),
+        ("skew", 2, "CHANnel1:SKEW"),
+        ("zero_offset", 2, "CHANnel1:ZOFFset"),
+        ("threshold", 2, "CHANnel1:THReshold"),
+        ("probe_unit", "V", "PROBe1:SETup:ATTenuation:UNIT"),
+        ("probe_attenuation", 10, "PROBe1:SETup:ATTenuation:MANual"),
+        ("probe_gain", 10, "PROBe1:SETup:GAIN:MANual")
     ])
     def test_value_control(self, attr, value, command):
         with expected_protocol(
                 self.DUT,
                 [
-                    ("CHANnel1:%s %g" % (command, value), None),
-                    ("CHANnel1:%s?" % command, value)
+                    ("%s %s" % (command, str(value)), None),
+                    CHECK_ERROR,
+                    ("%s?" % command, value)
                 ],
         ) as instr:
             setattr(instr.ch1, attr, value)
@@ -94,7 +174,9 @@ class TestRTB200XAnalogChannel:
         (Coupling, "coupling", "COUPling"),
         (Bandwidth, "bandwidth", "BANDwidth"),
         (WaveformColor, "waveform_color", "WCOLor"),
-        (ThresholdHysteresis, "threshold_hysteresis", "THReshold:HYSTeresis")
+        (ThresholdHysteresis, "threshold_hysteresis", "THReshold:HYSTeresis"),
+        (DecimationMode, "decimation_mode", "TYPE"),
+        (ArithmeticMethod, "arithmetics", "ARIThmetics")
     ])
     def test_enumeration_attributes(self, enum, attr, command):
         value = [e for e in enum][0]
@@ -102,6 +184,7 @@ class TestRTB200XAnalogChannel:
                 self.DUT,
                 [
                     ("CHANnel1:%s %s" % (command, value), None),
+                    CHECK_ERROR,
                     ("CHANnel1:%s?" % command, value)
                 ],
         ) as instr:
@@ -114,6 +197,7 @@ class TestRTB200XAnalogChannel:
                 self.DUT,
                 [
                     ("CHANnel1:POLarity %s" % on_off, None),
+                    CHECK_ERROR,
                     ("CHANnel1:POLarity?", on_off)
                 ],
         ) as instr:
@@ -133,10 +217,12 @@ class TestRTB200XAnalogChannel:
         with expected_protocol(
                 self.DUT,
                 [
-                    ("CHANnel1:LABel ABCDEFGH", None)
+                    ("CHANnel1:LABel \"ABCDEFGH\"", None),
+                    CHECK_ERROR
                 ],
         ) as instr:
             instr.ch1.label = "ABCDEFGH"
+
 
 class TestGeneral:
     @pytest.mark.parametrize("channel", ["ch1", "ch2", "ch3", "ch4"])
