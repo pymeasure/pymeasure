27ab793f7b8bf0f04bb20f89fb6fecba93f5e8f6
amiravan <138904398+amiravan@users.noreply.github.com>
2023-08-21 11:37:13 -0700

    Temptronic3010 Support

--- pymeasure/instruments/temptronic/__init__.py
+++ pymeasure/instruments/temptronic/__init__.py
@@ -22,40 +22,4 @@
 # THE SOFTWARE.
 #
 
-from ..errors import RangeError, RangeException
-from .instrument import Instrument
-from .resources import list_resources
-from .validators import discreteTruncate
-
-from . import advantest
-from . import agilent
-from . import ametek
-from . import ami
-from . import anaheimautomation
-from . import anapico
-from . import andeenhagerling
-from . import anritsu
-from . import attocube
-from . import danfysik
-from . import deltaelektronika
-from . import fluke
-from . import fwbell
-from . import heidenhain
-from . import hp
-from . import keithley
-from . import keysight
-from . import lakeshore
-from . import newport
-from . import ni
-from . import oxfordinstruments
-from . import parker
-from . import picotech
-from . import razorbill
-from . import rohdeschwarz
-from . import signalrecovery
-from . import srs
-from . import tektronix
-from . import temptronic
-from . import thorlabs
-from . import toptica
-from . import yokogawa
+from .temptronic3010 import Temptronic3010
---
+++ pymeasure/instruments/temptronic/temptronic3010.py
@@ -0,0 +1,33 @@
+import logging
+log = logging.getLogger(__name__)
+log.addHandler(logging.NullHandler())
+from time import sleep
+
+from pymeasure.adapters import VISAAdapter
+
+from pymeasure.instruments import Instrument, RangeException
+from pymeasure.instruments.validators import truncated_discrete_set, truncated_range, \
+    modular_range_bidirectional, strict_discrete_set,strict_range
+
+class Temptronic3010(Instrument):
+    """Represents Temptronic3010 Temperature Controller"""
+
+    MAX_TEMP = 200
+    val = input() #input from GUI
+    temperature_setpoint = Instrument.control("SS", f"Rs%g",
+                                    """Set the temperature setpoint. from 20.0 to 200.0 to TMAX""")
+    # returns current temp setpoint, and set temp setpoint
+
+  
+
+    temperature_actual = Instrument.measurement("ST", "Current temperature in C")
+    
+    def __init__(self, adapter, **kwargs):
+        super(Temptronic3010, self).__init__(
+            adapter, "Temptronic heater controller", **kwargs)
+
+        
+ # should search by GPIB::9::INSTR
+
+
+ 
