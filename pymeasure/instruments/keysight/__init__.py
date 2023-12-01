b38d04ded49077764d57ac1954d52b48fbd14654
OptimisticBeliever <pinto_armindo@yahoo.fr>
2023-12-01 15:15:18 +0100

    rename the KeysightE3631A.py to keysightE3631A.py

--- pymeasure/instruments/keysight/__init__.py
+++ pymeasure/instruments/keysight/__init__.py
@@ -26,4 +26,4 @@ from .keysightDSOX1102G import KeysightDSOX1102G
 from .keysightN5767A import KeysightN5767A
 from .keysightN7776C import KeysightN7776C
 from .keysightE36312A import KeysightE36312A
-from .KeysightE3631A import KeysightE3631A
+from .keysightE3631A import KeysightE3631A
