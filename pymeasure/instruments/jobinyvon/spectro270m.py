131bbeccaf15b0f0567c57be4389957906c5ead6
Daniel-Mael <m_leblan@insa-toulouse.fr>
2023-12-13 16:57:34 +0100

    Update spectro270m.py
    We have added the function for controlling the motor depending on the wavelength, and controlling that the motor is not busy.

--- pymeasure/instruments/jobinyvon/spectro270m.py
+++ pymeasure/instruments/jobinyvon/spectro270m.py
@@ -20,21 +20,24 @@ class JY270M(Instrument):
             **kwargs)
     )
 
+    """Backlash in number of motor steps"""
+    backlash = 320
+
+
     motor_init = Instrument.setting(
         b'A',
         """This command is used to initialize the monochromator, only needs to be called once""",
     )
 
-    """Backlash in number of motor steps"""
-    backlash = 320
+    motor_busy_check= Instrument.measurement(
+        "E\r".encode('utf-8'),
+        """it asks the motor if it's busy. If we receive "oq" it's busy and "oz" if it's not """,
+    )
 
-    """32 motor steps / nm"""
-    steps_nm = 32
-
-
-    def write(self, command):
-        """This function allows for easier writing of the write command."""
-        Instrument.write(self, command)
+    motor_position = Instrument.measurement(
+        b'H0\r',
+        """This property asks the instrument for its current position.""",
+    )
 
     def read_int(self):
         """This function allows to read input and transform it into integer format."""
@@ -46,7 +49,7 @@ class JY270M(Instrument):
         Instrument.write(self, b'A')
 
     def motor_busy_check(self):
-        self.write(b'E')
+        Instrument.write(self, b'E\r')
         ans = self.adapter.readall()
         char = ans.decode('utf-8')
         if char == "oz":
@@ -65,24 +68,29 @@ class JY270M(Instrument):
                 ser.write(b' ')
                 time.sleep(0.5)
                 rep = ser.readall()
+                print(1)
         while chr(rep[0]) != 'F':
             if chr(rep[0]) == "*":
                 ser.write(bytes([0xF7]))
+                print(2)
                 ser.write(b' ')
                 rep = ser.readall()
             elif chr(rep[0]) == "B":
                 ser.write(b'O2000\0')
+                print(3)
                 time.sleep(0.5)
                 ser.write(b' ')
                 rep = ser.readall()
             elif rep[0] == 27:
                 ser.write(bytes([0xF8]))
+                print(4)
                 time.sleep(0.5)
                 ser.write(b' ')
                 rep = ser.readall()
                 if chr(rep[0]) != "F":
                     ser.write(bytes([0xF8]))
                     ser.write(bytes([0xDE]))
+                    print(5)
                     time.sleep(0.5)
                     ser.write(b' ')
                     rep = ser.readall()
@@ -91,29 +99,41 @@ class JY270M(Instrument):
                 time.sleep(0.5)
                 rep = ser.readall()
 
+    def motor_move_relative(self, motor_step):
+        """we check if the motor is busy before sending the movement command."""
+        while not self.motor_busy_check():
+            pass
+        command = "F0," + str(motor_step) + "\r"
+        Instrument.write(self, command.encode('utf-8'))
+        while not self.motor_busy_check():
+            pass
+
+    def readall(self):
+        ser.readall()
 
     def motor_control(self, lamda):
-        self.write(b'H0\r')
-        position = self.adapter.read_all()
+        """32 motor steps / nm"""
+        steps_nm = 32
+        Instrument.write(self, b'H0\r')
+        position = self.read_int()
         """Wavelength is transformed into motor steps."""
-        motor_step = lamda*steps_nm
-        while motor_step != position:
-            if self.motor_busy_check():
-                if motor_step > position:
-                    s = ("%s%f\r'", "b'F0,", motor_step - position)
-                    print(s)
-                    ser.write(s)
-
-                if motor_step < position:
-                    s = ("%s%f\r'", "b'F0,", position - motor_step)
-                    ser.write(s)
-                    print(s)
-            else:
-                time.sleep(1)
-
+        motor_final_step = lamda*steps_nm
+        time.sleep(1)
+        while abs(motor_final_step - position) > 1:
+            self.motor_move_relative(motor_final_step-position)
+            Instrument.write(self, b'H0\r')
+            position = self.read_int()
+        print("The final motor position is: " + str(position))
+        print("The final wavelength is: " + str(position/32))
 
 spectro=JY270M(adapter=ser)
 
-spectro.motor_control(20000)
+"""spectro.write(b'H0\r')
+print(spectro.read_int())"""
 
+spectro.motor_control(900.5)
+
+"""spectro.write(b'A')"""
+
+"""spectro.motor_move_relative(-10000)"""
 """control("H0\r", f"G0,{-1000}\r"""
