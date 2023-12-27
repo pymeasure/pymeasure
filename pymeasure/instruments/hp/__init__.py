27e491b3378a562d76b743d22dd2bf4aebe7bcf8
Sionwage <None>
2023-12-26 18:46:03 -0600

    Sorted imports alphabetically

--- pymeasure/instruments/hp/__init__.py
+++ pymeasure/instruments/hp/__init__.py
@@ -22,17 +22,17 @@
 # THE SOFTWARE.
 #
 
-from .hp33120A import HP33120A
-from .hp34401A import HP34401A
-from .hp3478A import HP3478A
 from .hp3437A import HP3437A
+from .hp3478A import HP3478A
 from .hp8116a import HP8116A
-from .hp8657b import HP8657B
 from .hp856Xx import HP8560A
 from .hp856Xx import HP8561B
+from .hp8657b import HP8657B
+from .hp8753e import HP8753E
 from .hp11713a import HP11713A
+from .hp33120A import HP33120A
+from .hp34401A import HP34401A
 from .hpsystempsu import HP6632A
 from .hpsystempsu import HP6633A
 from .hpsystempsu import HP6634A
 from .hplegacyinstrument import HPLegacyInstrument
-from .hp8753e import HP8753E
