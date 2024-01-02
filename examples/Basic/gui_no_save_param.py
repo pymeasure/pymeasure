03010359c3168534a5bca551a47f8b2f9bc7b2f4
Per-Olof Svensson <poje@plid.se>
2024-01-02 02:10:16 +0100

    Example of using parameters that are not saved.

--- examples/Basic/gui_no_save_param.py
+++ examples/Basic/gui_no_save_param.py
@@ -36,8 +36,10 @@ python gui.py
 import sys
 import random
 from time import sleep
+from PyQt5.QtCore import QLocale
+from pyqtgraph.Qt import QtCore
 
-from pymeasure.experiment import Procedure, IntegerParameter, Parameter, FloatParameter
+from pymeasure.experiment import Procedure, IntegerParameter, Parameter, FloatParameter, ListParameter, unique_filename, Metadata, Results
 from pymeasure.display.Qt import QtWidgets
 from pymeasure.display.windows import ManagedWindow
 
@@ -48,15 +50,26 @@ log.addHandler(logging.NullHandler())
 
 class TestProcedure(Procedure):
 
+    p_dutId = Parameter('DUT ID', default='NA', save=False)
     iterations = IntegerParameter('Loop Iterations', default=100)
-    delay = FloatParameter('Delay Time', units='s', default=0.2)
+    delay = FloatParameter('Delay Time', units='s', default=0.02)
     seed = Parameter('Random Seed', default='12345')
+    param_set = ListParameter('Paramters to set', choices=['None','Group 1','Group 2'], default='None', save=False)
+    # Parameter group 1
+    param_1_1 = IntegerParameter('Paramter 1.1', default=1, group_by={'param_set' : 'Group 1'})
+    # Parameter group 2
+    param_2_1 = IntegerParameter('Paramter 2.1', default=2, group_by={'param_set' : 'Group 2'})
 
+    # Metadata
+    m_dutId = Metadata('DUT_ID', default='')
+
+    # a list defining the order and appearance of columns in our data file
     DATA_COLUMNS = ['Iteration', 'Random Number']
 
     def startup(self):
         log.info("Setting up random number generator")
         random.seed(self.seed)
+        self.m_dutId = self.p_dutId
 
     def execute(self):
         log.info("Starting to generate numbers")
@@ -82,16 +95,32 @@ class MainWindow(ManagedWindow):
     def __init__(self):
         super().__init__(
             procedure_class=TestProcedure,
-            inputs=['iterations', 'delay', 'seed'],
-            displays=['iterations', 'delay', 'seed'],
+            inputs=['p_dutId', 'iterations', 'delay', 'seed',
+                    'param_set', 'param_1_1', 'param_2_1'],
+            displays=['p_dutId', 'iterations', 'delay', 'seed'],
             x_axis='Iteration',
-            y_axis='Random Number'
+            y_axis='Random Number',
+            enable_file_input=False
         )
         self.setWindowTitle('GUI Example')
 
+    def queue(self):
+        procedure = self.make_procedure()
+        filename = unique_filename('Result', prefix='rnd_{DUT ID}_{Random Seed}_',procedure=procedure)
+        procedure.data_filename = filename
+        results = Results(procedure, filename)
+        experiment = self.new_experiment(results)
+        self.manager.queue(experiment)
+
 
 if __name__ == "__main__":
+    # Fix for axis scaling (2 lines) https://github.com/pyqtgraph/pyqtgraph/issues/756
+    QtWidgets.QApplication.setHighDpiScaleFactorRoundingPolicy(QtCore.Qt.HighDpiScaleFactorRoundingPolicy.PassThrough) 
+    QtCore.QCoreApplication.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling, True)
     app = QtWidgets.QApplication(sys.argv)
+    # Fix to handle numeric fields, decimal dot is used https://github.com/pymeasure/pymeasure/issues/602
+    QLocale.setDefault(QLocale(QLocale.English, QLocale.UnitedStates)) 
+
     window = MainWindow()
     window.show()
     sys.exit(app.exec())
