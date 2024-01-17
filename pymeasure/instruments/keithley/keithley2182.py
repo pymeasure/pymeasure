a5d2ca791f02310b603806b1bbfed51633beb534
Connor Carr <connor.g.carr@gmail.com>
2024-01-17 17:32:35 -0600

    Implemented Keithley 2182 channels.

--- pymeasure/instruments/keithley/keithley2182.py
+++ pymeasure/instruments/keithley/keithley2182.py
@@ -25,8 +25,8 @@
 import logging
 import time
 
-from pymeasure.instruments import Instrument, RangeException
-from pymeasure.instruments.validators import truncated_range, strict_discrete_set
+from pymeasure.instruments import Instrument, Channel
+from pymeasure.instruments.validators import truncated_range, strict_discrete_set, strict_range
 
 from .buffer import KeithleyBuffer
 
@@ -35,8 +35,118 @@ log = logging.getLogger(__name__)
 log.addHandler(logging.NullHandler())
 
 
+class Keithley2182Channel(Channel):
+    """Implementation of a Keithley 2182 channel.
+
+    Channel 1 is the fundamental measurement channel, while channel 2 provides
+    sense measurements. Channel 2 inputs are referenced to Channel 1 LO.
+    Possible configurations are
+
+    Voltage     (Channel 1)
+    Temperature (Channel 1)
+    Voltage     (Channel 1) and Voltage     (Channel 2)
+    Voltage     (Channel 1) and Temperature (Channel 2)
+    """
+
+    def __init__(self, instrument, id_):
+        """Set max voltage depending on channel."""
+        if id_==1:
+            self.voltage_range_values = [0, 120]
+            self.voltage_offset_values = [-120, 120]
+        else:
+            self.voltage_range_values = [0, 12]
+            self.voltage_offset_values = [-12, 12]
+        super().__init__(instrument, id_)
+
+    voltage_range = Channel.control(
+        ":SENS:VOLT:CHAN{ch}:RANG?", ":SENS:VOLT:CHAN{ch}:RANG %g",
+        """ A floating point property that controls the positive full-scale 
+        measurement voltage range in Volts, which can take values from 0 to
+        120 V (Ch. 1) or 12 V (Ch. 2). Auto-range is automatically disabled
+        when this property is set. """,
+        validator=truncated_range,
+        values=[0, 120],
+        dynamic=True,
+    )
+    voltage_range_auto = Channel.control(
+        ":SENS:VOLT:CHAN{ch}:RANG:AUTO?", ":SENS:VOLT:CHAN{ch}:RANG:AUTO %d",
+        """ A boolean property that controls whether auto voltage ranging is
+        enabled. Valid values are True and False.""",
+        validator=strict_discrete_set,
+        values={True: 1, False: 0},
+        map_values=True,
+    )
+    voltage_offset = Channel.control(
+        ":SENS:VOLT:CHAN{ch}:REF?", ":SENS:VOLT:CHAN{ch}:REF %g",
+        """ A floating point property that controls the relative offset for
+        measuring voltage. Displayed value = actual value - offset value.
+        Valid ranges are -120 V to +120 V for Ch. 1, and -12 V to +12 V for
+        Ch. 2.""",
+        validator=strict_range,
+        values=[-120, 120],
+        dynamic=True,
+    )
+    temperature_offset = Channel.control(
+        ":SENS:TEMP:CHAN{ch}:REF?", ":SENS:TEMP:CHAN{ch}:REF %g",
+        """ A floating point property that controls the relative offset for
+        measuring temperature. Displayed value = actual value - offset value.
+        Valid ranges are -273 C to 1800 C.""",
+        validator=strict_range,
+        values=[-273, 1800],
+    )
+    voltage_offset_state = Channel.control(
+        ":SENS:VOLT:CHAN{ch}:REF:STAT?", ":SENS:VOLT:CHAN{ch}:REF:STAT %s",
+        """ A string property that controls whether voltage is measured as a
+        relative or absolute value. Enabled by default for Ch. 2 voltage, which
+        is measured relative to Ch. 1 voltage.""",
+        validator=strict_discrete_set,
+        values=['OFF', 'ON'],
+    )
+    temperature_offset_state = Channel.control(
+        ":SENS:TEMP:CHAN{ch}:REF:STAT?", ":SENS:TEMP:CHAN{ch}:REF:STAT %s",
+        """ A string property that controls whether temperature is measured as
+        a relative or absolute value. Disabled by default.""",
+        validator=strict_discrete_set,
+        values=['OFF', 'ON'],
+    )
+
+    def measure_voltage(self, auto_range=True, nplc=1):
+        """ Configures and acquires voltage measurement.
+        
+        :param auto_range: Enables auto_range if True, else uses set voltage range
+        :param nplc: Number of power line cycles (NPLC) from 0.01 to 50/60
+        """
+        log.info("%s is measuring voltage.", self.name)
+        self.write(":SENS:CHAN {ch};"
+                   ":SENS:FUNC 'VOLT';"
+                   f":SENS:VOLT:NPLC {nplc};")
+        if auto_range:
+            self.write(":SENS:VOLT:RANG:AUTO 1;")
+        self.write(":READ?")
+        self.check_errors()
+
+    def measure_temperature(self, nplc=1):
+        """ Configures and acquires temperature measurement.
+
+        :param channel: Temperature measurement channel
+        :param nplc: Number of power line cycles (NPLC) from 0.01 to 50/60
+        """
+        log.info("%s is measuring temperature.", self.name)
+        self.write(":SENS:CHAN {ch};"
+                   ":SENS:FUNC 'TEMP';"
+                   f":SENS:CURR:NPLC {nplc};")
+        self.write(":READ?")
+        self.check_errors()
+
+    def acquire_reference(self, func='VOLT'):
+        """Acquire a measurement and store it as the relative offset value.
+        
+        :param func: 'VOLT' or 'TEMP'
+        """
+        self.write(f":SENS:{func}:""CHAN{ch}:REF:ACQ")
+
 class Keithley2182(KeithleyBuffer, Instrument):
-    """ Represents the Keithley 2182 Nanovoltmeter and provides a
+    """Represents the Keithley 2182 Nanovoltmeter and provides a
     high-level interface for interacting with the instrument.
 
     .. code-block:: python
@@ -49,10 +159,13 @@ class Keithley2182(KeithleyBuffer, Instrument):
         keithley.channel_function = 'voltage'   # Configures active channel for voltage measurement
         print(keithley.voltage)                 # Prints the voltage in volts
         
-        keithley.measure_voltage(channel=1)     # Measure voltage on channel 1
-        keithley.measure_temp(channel=2)        # Measure temperature on channel 2
+        keithley.ch_1.measure_voltage()         # Measure voltage on channel 1
+        keithley.ch_2.measure_temp()            # Measure temperature on channel 2
 
     """
+
+    ch_1 = Instrument.ChannelCreator(Keithley2182Channel, 1)
+    ch_2 = Instrument.ChannelCreator(Keithley2182Channel, 2)
 
     #################
     # Configuration #
@@ -62,6 +175,7 @@ class Keithley2182(KeithleyBuffer, Instrument):
         ":SYST:AZER:STAT?", ":SYST:AZER:STAT %s",
         """ A property that controls the auto zero option. Valid values are
         True (enabled) and False (disabled). """,
+        validator=strict_discrete_set,
         values={True: 1, False: 0},
         map_values=True,
     )
@@ -74,6 +188,7 @@ class Keithley2182(KeithleyBuffer, Instrument):
         ":DISP:ENAB?", ":DISP:ENAB %d",
         """ A boolean property that controls whether or not the display of the
         voltmeter is enabled. Valid values are True and False. """,
+        validator=strict_discrete_set,
         values={True: 1, False: 0},
         map_values=True,
     )
@@ -93,7 +208,6 @@ class Keithley2182(KeithleyBuffer, Instrument):
         values={'voltage': 'VOLT', 'temperature': 'TEMP'},
         map_values=True
     )
-    
 
     ###############
     # Voltage (V) #
@@ -104,15 +218,6 @@ class Keithley2182(KeithleyBuffer, Instrument):
         """ Reads the voltage in Volts, if active channel is configured for
         this reading. """
     )
-    voltage_range = Instrument.control(
-        ":SENS:VOLT:RANG?", ":SENS:VOLT:RANG %g",
-        """ A floating point property that controls the positive full-scale 
-        measurement voltage range in Volts, which can take values from 0 to
-        120 V (Ch. 1) or 12 V (Ch. 2). Auto-range is automatically disabled
-        when this property is set. """,
-        validator=truncated_range,
-        values=[0, 120]
-    )
     voltage_nplc = Instrument.control(
         ":SENS:VOLT:NPLC?", ":SENS:VOLT:NPLC %g",
         """ A floating point property that controls the number of power line
@@ -121,7 +226,8 @@ class Keithley2182(KeithleyBuffer, Instrument):
         depending on the line frequency. 0.1, 1, and 10 are Fast, Medium, and
         Slow respectively. """,
         validator=truncated_range,
-        values=[0.01, 60]
+        values=[0.01, 60],
+        dynamic=True,
     )
 
     ###################
@@ -140,13 +246,6 @@ class Keithley2182(KeithleyBuffer, Instrument):
         validator=strict_discrete_set,
         values=['B','E','J','K','N','R','S','T']
     )
-    temperature_reference = Instrument.control(
-        ":SENS:TEMP:REF?", ":SENS:TEMP:REF %g",
-        """ A floating point property that controls the relative temperature 
-        offset value in Celsius, which can take values from -273 to 1800 C. """,
-        validator=truncated_range,
-        values=[-273, 1800]
-    )
     temperature_nplc = Instrument.control(
         ":SENS:TEMP:NPLC?", ":SENS:TEMP:NPLC %g",
         """ A floating point property that controls the number of power line
@@ -155,8 +254,27 @@ class Keithley2182(KeithleyBuffer, Instrument):
         depending on the line frequency. 0.1, 1, and 10 are Fast, Medium, and
         Slow respectively. """,
         validator=truncated_range,
-        values=[0.01, 60]
+        values=[0.01, 60],
+        dynamic=True,
     )
+    temperature_reference_junction = Instrument.control(
+        ":SENS:TEMP:RJUN:RSEL?", ":SENS:TEMP:RJUN:RSEL %s",
+        """ A property that controls the type of thermocouple reference
+        junction. Default is internal.""",
+        validator=strict_discrete_set,
+        values=['SIM','INT'],
+    )
+    temperature_simulated_reference = Instrument.control(
+        ":SENS:TEMP:RJUN:SIM?", ":SENS:TEMP:RJUN:SIM %g",
+        """ A property that controls the value of the simulated thermocouple
+        reference junction in Celsius. Default is 23 C.""",
+        validator=truncated_range,
+        values=[0, 60],
+    )
+    internal_temperature = Instrument.measurement(
+        ":SENS:TEMP:RTEM?",
+        """ Query the internal temperature in Celsius."""
+        )
 
     ##############
     # Statistics #
@@ -201,7 +319,7 @@ class Keithley2182(KeithleyBuffer, Instrument):
         cast=int
     )
     trigger_delay = Instrument.control(
-        ":TRIG:SEQ:DEL?", ":TRIG:SEQ:DEL %g",
+        ":TRIG:DEL?", ":TRIG:DEL %g",
         """ A floating point property that controls the trigger delay
         in seconds, which can take values from 0 to 999999.999 s. """,
         validator=truncated_range,
@@ -216,39 +334,17 @@ class Keithley2182(KeithleyBuffer, Instrument):
         super().__init__(
             adapter, name, **kwargs
         )
+        self.temperature_nplc_values = [0.01, 60]
+        self.voltage_nplc_values = [0.01, 60]
 
-    def measure_voltage(self, channel=1, nplc=1, voltage=120.0, auto_range=True):
-        """ Configures and acquires voltage measurement.
-        
-        :param channel: Voltage measurement channel
-        :param nplc: Number of power line cycles (NPLC) from 0.01 to 50/60
-        :param voltage: Upper limit of measurement range in Volts, from 0 to 120 V
-        :param auto_range: Enables auto_range if True, else uses the set voltage
-        """
-        log.info("%s is measuring voltage." % self.name)
-        self.write(":SENS:CHAN %d;"
-                   ":SENS:FUNC 'VOLT';"
-                   ":SENS:VOLT:NPLC %f;" % (channel, nplc))
-        if auto_range:
-            self.write(":SENS:VOLT:RANG:AUTO 1;")
+    def auto_line_frequency(self):
+        """Set appropriate limits for NPLC voltage and temperature readings."""
+        if self.line_frequency==50:
+            self.temperature_nplc_values = [0.01, 50]
+            self.voltage_nplc_values = [0.01, 50]
         else:
-            self.voltage_range = voltage
-        self.write(":READ?")
-        self.check_errors()
-
-    def measure_temperature(self, channel=2, nplc=1, TC='S'):
-        """ Configures and acquires temperature measurement.
-
-        :param channel: Temperature measurement channel
-        :param nplc: Number of power line cycles (NPLC) from 0.01 to 50/60
-        :param TC: Thermocouple type
-        """
-        log.info("%s is measuring temperature." % self.name)
-        self.write(":SENS:CHAN %d;"
-                   ":SENS:FUNC 'TEMP';"
-                   ":SENS:CURR:NPLC %f;" % (channel, nplc))
-        self.write(":READ?")
-        self.check_errors()
+            self.temperature_nplc_values = [0.01, 60]
+            self.voltage_nplc_values = [0.01, 60]
 
     def beep(self, frequency, duration):
         """ Sounds a system beep.
@@ -287,7 +383,7 @@ class Keithley2182(KeithleyBuffer, Instrument):
         code, message = self.error
         while code != 0:
             t = time.time()
-            log.info("Keithley 2182 reported error: %d, %s" % (code, message))
+            log.info("Keithley 2182 reported error: %d, %s", code, message)
             code, message = self.error
             if (time.time() - t) > 10:
                 log.warning("Timed out for Keithley 2182 error retrieval.")
@@ -306,70 +402,21 @@ class Keithley2182(KeithleyBuffer, Instrument):
         """ Configures measurements to be taken with the internal
         trigger at the maximum sampling rate.
         """
-        self.write(":ARM:SOUR IMM;:TRIG:SOUR IMM;")
+        self.write(":TRIG:SOUR IMM;")
 
     def trigger_on_bus(self):
         """ Configures the trigger to detect events based on the bus
         trigger, which can be activated by :meth:`~.trigger`.
         """
-        self.write(":ARM:COUN 1;:ARM:SOUR BUS;:TRIG:SOUR BUS;")
-
-    def set_trigger_counts(self, arm, trigger):
-        """ Sets the number of counts for both the sweeps (arm) and the
-        points in those sweeps (trigger), where the total number of
-        points can not exceed 2500
-        """
-        if arm * trigger > 2500 or arm * trigger < 0:
-            raise RangeException("Keithley 2182 has a combined maximum "
-                                 "of 2500 counts")
-        if arm < trigger:
-            self.write(":ARM:COUN %d;:TRIG:COUN %d" % (arm, trigger))
-        else:
-            self.write(":TRIG:COUN %d;:ARM:COUN %d" % (trigger, arm))
+        self.write(":TRIG:SOUR BUS")
 
     def sample_continuously(self):
         """ Causes the instrument to continuously read samples
         and turns off any buffer or output triggering
         """
         self.disable_buffer()
-        self.disable_output_trigger()
         self.trigger_immediately()
 
-    def set_timed_arm(self, interval):
-        """ Sets up the measurement to be taken with the internal
-        trigger at a variable sampling rate defined by the interval
-        in seconds between sampling points
-        """
-        if interval > 99999.99 or interval < 0.001:
-            raise RangeException("Keithley 2182 can only be time"
-                                 " triggered between 1 mS and 1 Ms")
-        self.write(":ARM:SOUR TIM;:ARM:TIM %.3f" % interval)
-
-    def trigger_on_external(self, line=1):
-        """ Configures the measurement trigger to be taken from a
-        specific line of an external trigger
-
-        :param line: A trigger line from 1 to 4
-        """
-        cmd = ":ARM:SOUR TLIN;:TRIG:SOUR TLIN;"
-        cmd += ":ARM:ILIN %d;:TRIG:ILIN %d;" % (line, line)
-        self.write(cmd)
-
-    def output_trigger_on_external(self, line=1, after='DEL'):
-        """ Configures the output trigger on the specified trigger link
-        line number, with the option of supplying the part of the
-        measurement after which the trigger should be generated
-        (default to delay, which is right before the measurement)
-
-        :param line: A trigger line from 1 to 4
-        :param after: An event string that determines when to trigger
-        """
-        self.write(":TRIG:OUTP %s;:TRIG:OLIN %d;" % (after, line))
-
-    def disable_output_trigger(self):
-        """ Disables the output trigger for the Trigger layer
-        """
-        self.write(":TRIG:OUTP NONE")
-
+    @property
     def status(self):
         return self.ask("status:queue?;")
