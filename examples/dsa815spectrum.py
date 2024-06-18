a9e4bb19a84e6bfa8ad8d230e1aec8a8f645ed26
adeshazer123 <adeshazer@wisc.edu>
2024-06-18 10:05:14 +0900

    brushed up code. Added comments. Added an array of frequency and amplitude to be read as a Dataframe array

--- examples/dsa815spectrum.py
+++ examples/dsa815spectrum.py
@@ -19,17 +19,28 @@ from pymeasure.experiment import IntegerParameter, FloatParameter
 
 #adapter ="USB0::0x1AB1::0x0960::DSA8A154202508::INSTR"
 adapter = DSA815("USB0::0x1AB1::0x0960::DSA8A154202508::INSTR")
-adapter.write = ("SENSe:FREQuency:STARt 6")
-adapter.write = ("SENSe:FREQuency:CENTer 5e6") 
-adapter.write = ("SENSe:FREQuency:STOP 10e6")
-adapter.write = ("SENSe:SWEep:TIME 0.01")
-adapter.write = ("SENSe:SWEep:POINts 3001")
+
+#Set the parameters for the spectrum analyzer
+
+
+
 
 class DSA815Spectrum(Procedure):
-    #def __init__(self,address, **kwargs):
-        #super().__init__(**kwargs)
-        #self.address = address
-    
+    def __init__(self, adapter, name="RIGOL DSA815 Spectrum Analyzer", **kwargs):
+        super().__init__(
+            adapter,
+            name,
+            **kwargs
+        )
+    start_freq = adapter.write("SENSe:FREQuency:STARt 6e6")
+    center_freq = adapter.write("SENSe:FREQuency:CENTer 5e6")
+    stop_freq = adapter.write("`SENSe:FREQuency:STOP 10e6")
+    sweep_time = adapter.write("SENSe:SWEep:TIME 1")
+    data_points = adapter.write("SENSe:SWEep:POINts 3001")
+    adapter.write("INITiate:CONTinuous OFF")
+
+    adapter.write("INITiate:IMMediate")
+    adapter.write("TRACe:DATA TRACE1")
 
     #start_freq = FloatParameter('Start Frequency', units = 'Hz', default =0)
     #center_freq = FloatParameter('Center Frequency', units = 'Hz', default = 5e6)
@@ -66,7 +77,7 @@ if __name__ == "__main__":
     data_filename = "test_spectrum.csv"
     log.info("Getting spectrum results with a data file: %s" % data_filename)
     results = Results(procedure, data_filename)
-   #plotter = Plotter(results)
+    plotter = Plotter(results)
 
     log.info("Constructing the measurement")
     worker = Worker(results)
--- pymeasure/instruments/rigol/rigoldsa815.py
+++ pymeasure/instruments/rigol/rigoldsa815.py
@@ -1,48 +1,25 @@
 # Author: Amelie Deshazer 
 # Date: 6/6/2024
 # Address: USB0::0x1AB1::0x0960::DSA8A154202508::INSTR 
-# Purpose: Properties and methods used to define start, stop, and center frequency to be controlled. Sweep time and continuous sweep can be controlled and set by their parameters. 
-# Citations: 
-
-import logging
-log = logging.getLogger(__name__)
-log.addHandler(logging.NullHandler())
-
+# Purpose:Comunicate with the Rigol DSA815 Spectrum Analyzer and read out the start, center, and stop frequency. 
+#         Also, set and read the sweep time and turn on or off the continuous sweep. For measurement to work, 
+#         continuous sweep must be off.  
 
 import numpy as np
 import pandas as pd
-from io import StringIO
-from time import sleep
-from pymeasure.log import console_log
-from pymeasure.experiment import Procedure, Results, Worker 
-from pymeasure.experiment import IntegerParameter
 from pymeasure.instruments import Instrument
-from pymeasure.instruments.validators import (truncated_discrete_set, truncated_range, strict_discrete_set)
-
-dsa815 = "USB0::0x1AB1::0x0960::DSA8A154202508::INSTR"
-
-#log = logging.getLogger(__name__)
-#log.addHandler(logging.NullHandler())
+from pymeasure.instruments.validators import truncated_range
 
 class DSA815(Instrument):
-   # """Class for controlling RIGOL DSA815 spectrum analyzer"""
-    #def __init__(self, adapter, name="RIGOL DSA815", **kwargs):
-     #   super().__init__(adapter, name,includeSCPI=True,
-          #  **kwargs)
-    def __init__(self, adapter, name="RIGOL DSA815 Spectrum Analyzer", **kwargs):
-        super().__init__(
-            adapter,
-            name,
-            **kwargs
-        )
+ 
     start_frequency = Instrument.control(
         ":SENSe:FREQuency:STARt?", 
         ":SENSe:FREQuency:STARt %e",
-    """Communicates with DSA815 Machine and reads out the Start Frequency""",
+    """Reads and sets start frequency""",
     validator = truncated_range, 
-    values = [0,7.5e9] #units in Hz. Possible unit conversion? Use set_process 
+    values = [0,7.5e9] 
     )
-
+ 
     center_frequency = Instrument.control(
         ":SENSe:FREQuency:CENTer?",
         ":SENSe:FREQuency:CENTer %e",
@@ -50,7 +27,7 @@ class DSA815(Instrument):
     validator = truncated_range,
     values = [0, 7.5e9]                                       
     )
-
+ 
     stop_frequency = Instrument.control(
         ":SENSe:FREQuency:STOP?",
         ":SENSe:FREQuency:STOP %e",
@@ -58,98 +35,65 @@ class DSA815(Instrument):
     validator = truncated_range,
     values = [0,1.5e9]
     )
-    #Need to set this to 601
+
     frequency_points = Instrument.control(
           ":SENSe:SWEep:POINts?", ":SENSe:SWEep:POINts %d",
           """Sets the amount of sweep data points""",
           validator = truncated_range,
-          values = [101,3001],
-        #   cast = int,
-        #   low = 101,
-        #   high = 3001
+          values = [101,3001]
     )
-
+ 
     sweep_time = Instrument.control(
         ":SENSe:SWEEP:TIME?",
         ":SENSe:SWEEP:TIME %e",
-        """Reads and sets the sweep time. This function measures the start 
-        and stop length to acquire frequencies""",
+        """Reads and sets the sweep time.""",
     validator = truncated_range, 
-    values = [2e-5,7500] #converted into seconds                  
+    values = [2e-5,7500]                 
     )
+
     #Turns on (1) or off(0) continuous sweep
     continuous_sweep = Instrument.control(
         ":INITiate:CONTinuous?",
         ":INITiate:CONTinuous %d",
-        """Reads and sets continuous sweep""",
-    )
+        """Reads and sets continuous sweep on (1) or off (0)""",
+    ) 
+
+    def __init__(self, adapter, name="RIGOL DSA815 Spectrum Analyzer", **kwargs):
+        super().__init__(
+            adapter,
+            name,
+            **kwargs
+        ) 
+ 
+    def frequencies(self, freq_points = 601):
+        """ Returns an array of frequency points based on the 
+        start and stop frequency"""
+        return np.linspace(
+              self.start_frequency, 
+              self.stop_frequency,
+              freq_points, 
+              dtype = np.float64
+         )
     
     def trace(self):
-            try:
-                print("please work") 
-                self.write("INITiate:IMMediate")
-                print("Hello world")
-                self.write("TRACe:DATA TRACE1")
-                print("OH IT WORKED!")
-                data = self.ask(":TRACE? TRACE1")[12:]
-                print("YES IT IS WORKING")
-                return [float(x) for x in data.split(",")]
-            except Exception as e:
-                raise Exception("Error in trace function: " + str(e))
+        """Returns an array of `amplitude` data from the trace."""
+        self.write("INITiate:IMMediate")
+        self.write("TRACe:DATA TRACE1")
+        data = self.ask(":TRACE? TRACE1")[12:]
+        amplitude = [float(x) for x in data.split(",")]
+        return amplitude
     
+    def trace_df(self):
+       """ Returns a pandas DataFrame containing the frequency
+        and peak data for a particular trace, based on the
+        trace number (1, 2, or 3).
+        """
+       trace1 = self.trace()
+       return pd.DataFrame({
+            'Frequency (GHz)': self.frequencies(len(trace1)) * 1e-9,
+            'Amplitude (dBm)': trace1
+            })
     
-    
-    #def frequencies(self):
-    # def trace(self):
-    #         self.write(":INITiate:IMMediate")
-    #         self.write(":TRACe:DATA TRACE1")
-    #         data =self.query(':TRACE? TRACE1')[12:] # Strip off 10 characters of header
-    #         return data.split(",")
-       
-        # data = np.loadtxt(
-        #     StringIO(self.ask(":TRACe:DATA? TRACE1")),
-        #     delimiter=',',
-        #     dtype=np.float64
-        # )
-        # print(data)
-        # return data
-    
-
-
-    # measure_data = Instrument.control(
-    #     "INITiate:IMMediate", ":READ",
-    #     """Sets up the measurement"""
-    # )
-    # data_format = Instrument.control(
-    #     ":FORMat:TRACe:DATA?",
-    #     """Reads the data format"""
-    # )
-    # def trace(self):
-    #     """ Returns a numpy array of the data for a particular trace
-    #     based on the trace number (1, 2, or 3).
-    #     """
-    #     self.write("TRACe:DATA TRACE%d") #sets up the measurement
-    # def frequencies(self):
-    #     """ Returns a numpy array of frequencies in Hz that
-    #     correspond to the current settings of the instrument.
-    #     """
-    #     return np.linspace(
-    #         self.start_frequency(),
-    #         self.stop_frequency(),
-    #         self.frequency_points(),
-    #         dtype=np.float64
-    #     )
-
-    #def trace_df(self, number=1):
-       # """ Returns a pandas DataFrame containing the frequency
-        #and peak data for a particular trace, based on the
-        #trace number (1, 2, or 3).
-        #"""
-        #return pd.DataFrame({
-            #'Frequency (GHz)': self.frequencies * 1e-9,
-            #'Peak (dB)': self.trace(number)
-      #  })
-               
 
 
 
