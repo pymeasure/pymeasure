3e0cad9f42192cf2e491d7f80722564e7cba877d
David Sun <54460365+Aphelion82@users.noreply.github.com>
2024-06-27 09:26:36 -0500

    completely remove all `*LANG SCPI` calls

--- tests/instruments/keithley/test_keithleyDAQ6510.py
+++ tests/instruments/keithley/test_keithleyDAQ6510.py
@@ -26,14 +26,6 @@ from pymeasure.test import expected_protocol
 from pymeasure.instruments.keithley import KeithleyDAQ6510
 
 
-def test_init():
-    with expected_protocol(
-            KeithleyDAQ6510,
-            [(b'*LANG SCPI', None)],
-    ):
-        pass  # Verify the expected communication.
-
-
 def test_current_range_set():
     with expected_protocol(
             KeithleyDAQ6510,
