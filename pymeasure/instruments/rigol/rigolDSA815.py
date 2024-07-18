25bab708c6ee744d30ad04b80dc03edee5853b9b
nano713 <50872819+nano713@users.noreply.github.com>
2024-07-18 11:01:43 +0900

    Update the rigol driver with Amelie's change

--- pymeasure/instruments/rigol/rigolDSA815.py
+++ pymeasure/instruments/rigol/rigolDSA815.py
@@ -1,118 +1,95 @@
-#
-# This file is part of the PyMeasure package.
-#
-# Copyright (c) 2013-2024 PyMeasure Developers
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-# THE SOFTWARE.
-#
+# Author: Amelie Deshazer 
+# Date: 6/6/2024
+# Address: USB0::0x1AB1::0x0960::DSA8A154202508::INSTR 
+# Purpose:Comunicate with the Rigol DSA815 Spectrum Analyzer and read out the start, center, and stop frequency. 
+#         Also, set and read the sweep time and turn on or off the continuous sweep. For measurement to work, 
+#         continuous sweep must be off.  
 
+import numpy as np
+import pandas as pd
 from pymeasure.instruments import Instrument
 from pymeasure.instruments.validators import truncated_range
 
-from io import StringIO
-import numpy as np
-import pandas as pd
-
-
-class RigolDSA815(Instrument):
-    """ Represents the RigolDSA815 Spectrum Analyzer
-    and provides a high-level interface for taking scans of
-    high-frequency spectrums
-    """
-
+class DSA815(Instrument):
+ 
     start_frequency = Instrument.control(
-        ":SENS:FREQ:STAR?;", ":SENS:FREQ:STAR %e Hz;",
-        """ A floating point property that represents the start frequency
-        in Hz. This property can be set.
-        """
+        ":SENSe:FREQuency:STARt?", 
+        ":SENSe:FREQuency:STARt %e",
+    """Reads and sets start frequency""",
+    validator = truncated_range, 
+    values = [0,7.5e9] 
     )
-    stop_frequency = Instrument.control(
-        ":SENS:FREQ:STOP?;", ":SENS:FREQ:STOP %e Hz;",
-        """ A floating point property that represents the stop frequency
-        in Hz. This property can be set.
-        """
-    )
-    frequency_points = Instrument.control(
-        ":SENSe:SWEEp:POINts?;", ":SENSe:SWEEp:POINts %d;",
-        """ An integer property that represents the number of frequency
-        points in the sweep. This property can take values from 101 to 3001.
-        """,
-        validator=truncated_range,
-        values=[101, 3001],
-        cast=int
-    )
-    frequency_step = Instrument.control(
-        ":SENS:FREQ:CENT:STEP:INCR?;", ":SENS:FREQ:CENT:STEP:INCR %g Hz;",
-        """ A floating point property that represents the frequency step
-        in Hz. This property can be set.
-        """
-    )
+ 
     center_frequency = Instrument.control(
-        ":SENS:FREQ:CENT?;", ":SENS:FREQ:CENT %e Hz;",
-        """ A floating point property that represents the center frequency
-        in Hz. This property can be set.
-        """
+        ":SENSe:FREQuency:CENTer?",
+        ":SENSe:FREQuency:CENTer %e",
+        """Reads and sets center frequency""",
+    validator = truncated_range,
+    values = [0, 7.5e9]                                       
     )
-    sweep_time = Instrument.control(
-        ":SENS:SWE:TIME?;", ":SENS:SWE:TIME %.2e;",
-        """ A floating point property that represents the sweep time
-        in seconds. This property can be set.
-        """
+ 
+    stop_frequency = Instrument.control(
+        ":SENSe:FREQuency:STOP?",
+        ":SENSe:FREQuency:STOP %e",
+        """Reads and sets the stop frequency""",
+    validator = truncated_range,
+    values = [0,1.5e9]
     )
 
-    def __init__(self, adapter, name="Rigol DSA815 Spectrum Analyzer", **kwargs):
+    frequency_points = Instrument.control(
+          ":SENSe:SWEep:POINts?", ":SENSe:SWEep:POINts %d",
+          """Sets the amount of sweep data points""",
+          validator = truncated_range,
+          values = [101,3001]
+    )
+ 
+    sweep_time = Instrument.control(
+        ":SENSe:SWEEP:TIME?",
+        ":SENSe:SWEEP:TIME %e",
+        """Reads and sets the sweep time.""",
+    validator = truncated_range, 
+    values = [2e-5,7500]                 
+    )
+
+    #Turns on (1) or off(0) continuous sweep
+    continuous_sweep = Instrument.control(
+        ":INITiate:CONTinuous?",
+        ":INITiate:CONTinuous %d",
+        """Reads and sets continuous sweep on (1) or off (0)""",
+    ) 
+
+    def __init__(self, adapter, name="RIGOL DSA815 Spectrum Analyzer", **kwargs):
         super().__init__(
             adapter,
             name,
             **kwargs
-        )
-
-    @property
-    def frequencies(self):
-        """ Returns a numpy array of frequencies in Hz that
-        correspond to the current settings of the instrument.
-        """
+        ) 
+ 
+    def frequencies(self, freq_points = 601):
+        """ Returns an array of frequency points based on the 
+        start and stop frequency"""
         return np.linspace(
-            self.start_frequency,
-            self.stop_frequency,
-            self.frequency_points,
-            dtype=np.float64
-        )
-
-    def trace(self, number=1):
-        """ Returns a numpy array of the data for a particular trace
-        based on the trace number (1, 2, or 3).
-        """
-        self.write(":FORMat:TRACe:DATA ASCII;")
-        data = np.loadtxt(
-            StringIO(self.ask(":TRACE:DATA? TRACE%d;" % number)),
-            delimiter=',',
-            dtype=np.float64
-        )
-        return data
-
-    def trace_df(self, number=1):
-        """ Returns a pandas DataFrame containing the frequency
+              self.start_frequency, 
+              self.stop_frequency,
+              freq_points, 
+              dtype = np.float64
+         )
+    
+    def trace(self):
+        """Returns an array of `amplitude` data from the trace."""
+        self.write("INITiate:IMMediate")
+        self.write("TRACe:DATA TRACE1")
+        data = self.ask(":TRACE? TRACE1")[12:]
+        amplitude = [float(x) for x in data.split(",")]
+        return amplitude
+    
+    def trace_df(self):
+       """ Returns a pandas DataFrame containing the frequency
         and peak data for a particular trace, based on the
         trace number (1, 2, or 3).
         """
-        return pd.DataFrame({
-            'Frequency (GHz)': self.frequencies * 1e-9,
-            'Peak (dB)': self.trace(number)
-        })
+       trace1 = self.trace()
+       return pd.DataFrame({
+            'Frequency (GHz)': self.frequencies(len(trace1)) * 1e-9,
+            'Amplitude (dBm)': trace1
+            })
