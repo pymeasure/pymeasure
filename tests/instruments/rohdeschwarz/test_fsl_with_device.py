c43e3aa1212cd29b25ce870e5e9df9e60976c9f4
jnnskls <jkleineschoenepauck@gmail.com>
2024-09-30 17:07:00 +0200

    adjust test for fsl with device

--- tests/instruments/rohdeschwarz/test_fsl_with_device.py
+++ tests/instruments/rohdeschwarz/test_fsl_with_device.py
@@ -1,31 +1,34 @@
-# from time import sleep
-
 import pytest
 from pymeasure.instruments.rohdeschwarz.fsseries import FSL
 
+connected_device_address = "TCPIP::000.00.00.000::INSTR"
+
 pytest.skip("Only works with connected hardware", allow_module_level=True)
-RESOURCE = "TCPIP::141.20.43.231::INSTR"
+
+
+@pytest.fixture(scope="module")
+def fsl(connected_device_address):
+    instr = FSL(connected_device_address)
+    return instr
 
 
 @pytest.fixture
-def fsl_default_state():
+def fsl_default_state(fsl):
     """
     Return an FSL instrument in its default state.
     """
 
-    fsl = FSL(RESOURCE)
     fsl.reset()
     return fsl
 
 
 @pytest.fixture
-def fsl_with_channels():
+def fsl_with_channels(fsl):
     """Return an FSL instrument with 3 channels = Spectrum:SANALYZER, Test
     Spectrum: SANALYZER and
     Test Phase Noise: PNOISE
 
     """
-    fsl = FSL(RESOURCE)
     fsl.reset()
     fsl.create_channel("SANALYZER", "Test Spectrum")
     fsl.create_channel("PNOISE", "Test Phase Noise")
@@ -33,11 +36,10 @@ def fsl_with_channels():
     return fsl
 
 
-def test_create_channels(fsl_default_state):
+def test_create_channels(fsl):
     """Create two channels one to get spectrum and the other to get phase
     noise."""
 
-    fsl = fsl_default_state
     fsl.create_channel("SANALYZER", "Test Spectrum")
     fsl.create_channel("PNOISE", "Test Phase Noise")
 
@@ -48,32 +50,28 @@ def test_create_channels(fsl_default_state):
     }
 
 
-def test_activating_channel(fsl_with_channels):
+def test_activating_channel(fsl):
 
     """Activate the "Test Spectrum" channel and check that the
     channel_type is "SANALYZER" for it."""
-
-    fsl = fsl_with_channels
 
     fsl.activate_channel = "Test Spectrum"
     assert fsl.available_channels.get(fsl.active_channel) == "SANALYZER"
     fsl.reset()
 
 
-def test_renaming_channel(fsl_with_channels):
+def test_renaming_channel(fsl):
     """Rename the "Test Phase Noise" channel to "Test pnoise" """
-    fsl = fsl_with_channels
 
     fsl.rename_channel("Test Phase Noise", "Test pnoise")
     assert fsl.active_channel == "Test pnoise"
     fsl.reset()
 
 
-def test_deleting_channels(fsl_with_channels):
+def test_deleting_channels(fsl):
     """Delete the channel "Spectrum" and check the available
     channels"""
 
-    fsl = fsl_with_channels
     fsl.delete_channel("Spectrum")
     assert fsl.available_channels == {
         "Test Spectrum": "SANALYZER",
