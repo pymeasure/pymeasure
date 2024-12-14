c3225d7dfcb11f75cd766653d2c983d5a41c6e91
TEM01* <Pearce.thomas42@gmail.com>
2024-12-14 10:45:51 -0800

    Using SCPIMixin, instead of unknown. Cleaned up old copy paste. Added Output triggers

--- pymeasure/instruments/hp/hp66312A.py
+++ pymeasure/instruments/hp/hp66312A.py
@@ -22,41 +22,29 @@
 # THE SOFTWARE.
 #
 #work in progress by TEM01STAR
-from pymeasure.instruments import Instrument, SCPIUnknownMixin
+from pymeasure.instruments import Instrument, SCPIMixin
 from pymeasure.instruments.validators import strict_discrete_set
 
-class HP66312A(SCPIUnknownMixin, Instrument):
-    """ Represents the HP / Agilent / Keysight 66312AA Multimeter and
+class HP66312A(SCPIMixin, Instrument):
+    """ Represents the HP / Agilent / Keysight 66312AA Dynamic Measurement DC Source and
     provides a high-level interface for interacting with the instrument.
 
     .. code-block:: python
-#garbage below, reminder to make example code
-        dmm = HP66312A("GPIB::1")
-        dmm.function_ = "DCV"
-        print(dmm.reading)  # -> Single float reading
+    
+        source = HP66312A("GPIB::1")
+        source.voltage_setpoint = 5
+        source.current_limit = 0.2
+        source.output_enabled = 1
+        print(source.current) #-> Measure current draw of load
 
-        dmm.nplc = 0.02
-        dmm.autozero_enabled = False
-        dmm.trigger_count = 100
-        dmm.trigger_delay = "MIN"
-        print(dmm.reading)  # -> Array of 100 very fast readings
 
     """
-    FUNCTIONS = {"DCV": "VOLT", "DCV_RATIO": "VOLT:RAT",
-                 "ACV": "VOLT:AC", "DCI": "CURR", "ACI": "CURR:AC",
-                 "R2W": "RES", "R4W": "FRES", "FREQ": "FREQ",
-                 "PERIOD": "PER", "CONTINUITY": "CONT", "DIODE": "DIOD"}
-
-    FUNCTIONS_WITH_RANGE = {
-        "DCV": "VOLT", "ACV": "VOLT:AC", "DCI": "CURR",
-        "ACI": "CURR:AC", "R2W": "RES", "R4W": "FRES",
-        "FREQ": "FREQ", "PERIOD": "PER"}
 
     BOOL_MAPPINGS = {True: 1, False: 0}
 
     # Below: stop_bits: 20 comes from
     # https://pyvisa.readthedocs.io/en/latest/api/constants.html#pyvisa.constants.StopBits
-    def __init__(self, adapter, name="HP 34401A", **kwargs):
+    def __init__(self, adapter, name="HP 66312A", **kwargs):
         super().__init__(
             adapter,
             name,
@@ -107,111 +95,67 @@ class HP66312A(SCPIUnknownMixin, Instrument):
         map_values=True,
     )
     
-    # #Measurement commands WIP
-    # current = Instrument.control(
-        # "MEAS:CURR?",
-    # )
+    # #Measurement commands
+    current = Instrument.measurement(
+        "MEAS:CURR?",
+        """Measures and returns the DC output current.""",
+    )
     
-    # current_rms = Instrument.control(
-        # "MEAS:CURR:ACDC?",
-    # )
+    current_rms = Instrument.measurement(
+        "MEAS:CURR:ACDC?",
+        """Measures and returns the AC+DC RMS output current.""",
+    )
     
-    # current_min = Instrument.control(
-        # "MEAS:CURR:MIN?",
-    # )
+    current_min = Instrument.measurement(
+        "MEAS:CURR:MIN?",
+        """Returns the minimum DC output current from a sample.""",
+    )
 
-    # current_max = Instrument.control(
-        # "MEAS:CURR:MAX?",
-    # )    
+    current_max = Instrument.measurement(
+        "MEAS:CURR:MAX?",
+        """Returns the maximum DC output current from a sample.""",
+    )    
     
-    # voltage = Instrument.control(
-        # "MEAS:VOLT?",
-    # )
+    voltage = Instrument.measurement(
+        "MEAS:VOLT?",
+        """Measures and returns the dc output voltage.""",
+    )
 
-    # voltage_rms = Instrument.control(
-        # "MEAS:VOLT:ACDC?",
-    # )
+    voltage_rms = Instrument.measurement(
+        "MEAS:VOLT:ACDC?",
+        """Measures and returns the ac+dc RMs output voltage.""",
+    )
 
-    # voltage_min = Instrument.control(
-        # "MEAS:VOLT:MIN?",
-    # )
+    voltage_min = Instrument.measurement(
+        "MEAS:VOLT:MIN?",
+        """Returns the low voltage level from a sample.""",
+    )
     
-    # voltage_max = Instrument.control(
-        # "MEAS:VOLT:MAX?",
-    # )
+    voltage_max = Instrument.measurement(
+        "MEAS:VOLT:MAX?",
+        """Returns the high voltage level from a sample.""",
+    )
 
-    # Trigger related commands WIP
-    def init_trigger(self):
+    # Output Trigger related commands
+    def init_single_output_trigger(self):
         """Set the state of the triggering system to "wait-for-trigger".
 
-        Measurements will begin when the specified trigger conditions
-        are satisfied after this command is received."""
-        self.write("INIT"),
+        The source will change the output levels to voltage_trigger_level and current_trigger_level when it recieves a trigger command.
+        It will return to Idle after a single trigger"""
+        self.write("INIT:NAME TRAN"),
+        
+    def enable_cont_output_trigger(self):
+        """Set the state of the triggering system to "wait-for-trigger".
 
-    reading = Instrument.measurement(
-        "READ?",
-        """Take a measurement of the currently selected function.
+        The source will change the output levels to voltage_trigger_level and current_trigger_level when it recieves a trigger command.
+        It will return to Idle after a single trigger"""
+        self.write("INIT:CONT:NAME TRAN, ON"),
 
-        Reading this property is equivalent to calling `init_trigger()`,
-        waiting for completion and fetching the reading(s).""",
+    trigger_output = Instrument.setting(
+        "*TRG",
+        """Send a single trigger to the instrument""",
     )
-
-    trigger_source = Instrument.control(
-        "TRIG:SOUR?", "TRIG:SOUR %s",
-        """Control the trigger source.
-
-        Valid values: "IMM", "BUS", "EXT"
-        The multimeter will accept a software (bus) trigger,
-        an immediate internal trigger (this is the default source),
-        or a hardware trigger from the rear-panel
-        Ext Trig (external trigger) terminal.""",
-        validator=strict_discrete_set,
-        values=["IMM", "BUS", "EXT"],
-    )
-
-    trigger_delay = Instrument.control(
-        "TRIG:DEL?", "TRIG:DEL %s",
-        """Control the trigger delay in seconds.
-
-        Valid values (incl. floats): 0 to 3600 seconds, "MIN", "MAX".""",
-    )
-
-    trigger_auto_delay_enabled = Instrument.control(
-        "TRIG:DEL:AUTO?", "TRIG:DEL:AUTO %s",
-        """Control the automatic trigger delay state.
-
-        If enabled, the delay is determined by function, range, integration time,
-        and ac filter setting. Selecting a specific trigger delay value
-        automatically turns off the automatic trigger delay.""",
-        validator=strict_discrete_set,
-        values=BOOL_MAPPINGS,
-        map_values=True,
-    )
-
-    sample_count = Instrument.control(
-        "SAMP:COUN?", "SAMP:COUN %s",
-        """Controls the number of samples per trigger event.
-
-        Valid values: 1 to 50000, "MIN", "MAX".""",
-    )
-
-    trigger_count = Instrument.control(
-        "TRIG:COUN?", "TRIG:COUN %s",
-        """Control the number of triggers accepted before returning to the "idle" state.
-
-        Valid values: 1 to 50000, "MIN", "MAX", "INF".
-        The INFinite parameter instructs the multimeter to continuously accept triggers
-        (you must send a device clear to return to the "idle" state).""",
-    )
-
-    stored_reading = Instrument.measurement(
-        "FETC?",
-        """Measure the reading(s) currently stored in the multimeter's internal memory.
-
-        Reading this property will NOT initialize a trigger.
-        If you need that, use the `reading` property instead.""",
-    )
-
+   
     # Display related commands
     display_enabled = Instrument.control(
         "DISP?", "DISP %s",
@@ -230,7 +174,7 @@ class HP66312A(SCPIUnknownMixin, Instrument):
 
     displayed_text = Instrument.control(
         "DISP:TEXT?", "DISP:TEXT \"%s\"",
-        """Control the text displayed on the multimeter's display.
+        """Control the text displayed on the source's display.
 
         The text can be up to 12 characters long;
         any additional characters are truncated my the multimeter.""",
@@ -261,12 +205,12 @@ class HP66312A(SCPIUnknownMixin, Instrument):
 
     scpi_version = Instrument.measurement(
         "SYST:VERS?",
-        """The SCPI version of the multimeter.""",
+        """The SCPI version of the source.""",
     )
 
     self_test_result = Instrument.measurement(
         "*TST?",
-        """Initiate a self-test of the multimeter and return the result.
+        """Initiate a self-test of the source and return the result.
 
         Be sure to set an appropriate connection timeout,
         otherwise the command will fail.""",
@@ -274,15 +218,4 @@ class HP66312A(SCPIUnknownMixin, Instrument):
 
     def write(self, command):
         """Write a command to the instrument."""
-        if "{function_prefix_for_range}" in command:
-            command = command.replace("{function_prefix_for_range}",
-                                      self._get_function_prefix_for_range())
-        elif "{function}" in command:
-            command = command.replace("{function}", HP34401A.FUNCTIONS[self.function_])
         super().write(command)
-
-    def _get_function_prefix_for_range(self):
-        function_prefix = HP34401A.FUNCTIONS_WITH_RANGE[self.function_]
-        if function_prefix in ["FREQ", "PER"]:
-            function_prefix += ":VOLT"
-        return function_prefix
