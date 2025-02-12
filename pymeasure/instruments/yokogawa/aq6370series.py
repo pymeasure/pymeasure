45553b907a590a71eaf788bbbe26a35733d259c2
fredgillard <fred.gillard@lumentum.com>
2025-02-12 11:18:49 +0000

    Created AQ6370E class (sensitivity_level is only valid for AQ6370E) and clarified units for sensitivity_level

--- pymeasure/instruments/yokogawa/aq6370series.py
+++ pymeasure/instruments/yokogawa/aq6370series.py
@@ -118,13 +118,6 @@ class AQ6370Series(SCPIMixin, Instrument):
         values={"NHLD": 0, "NAUT": 1, "MID": 2, "HIGH1": 3, "HIGH2": 4, "HIGH3": 5, "NORM": 6},
     )
 
-    sensitivity_level = Instrument.control(
-        ":SENSe:SENSe:LEVel?",
-        ":SENSe:SENSe:LEVel %g",
-        """Control the sweep sensitivity by specifying the sensitivity level you want to measure at.
-        The sensitivity closest to that level, and the sweep speed are automatically selected.""",
-    )
-
     # Wavelength settings (all assuming wavelength mode, not frequency mode) -----------------------
 
     wavelength_center = Instrument.control(
@@ -240,6 +233,29 @@ class AQ6370Series(SCPIMixin, Instrument):
 
 
 # subclasses of specific instruments ---------------------------------------------------------------
+
+
+class AQ6370E(AQ6370Series):
+    """Represents Yokogawa AQ6370E optical spectrum analyzer."""
+
+    sweep_speed = Instrument.control(
+        ":SENSe:SWEep:SPEed?",
+        ":SENSe:SWEep:SPEed %d",
+        "Control the sweep speed (str '1x' or '2x' for double speed).",
+        validator=strict_discrete_set,
+        map_values=True,
+        values={"1x": 0, "2x": 1},
+    )
+
+    sensitivity_level = Instrument.control(
+        ":SENSe:SENSe:LEVel?",
+        ":SENSe:SENSe:LEVel %g",
+        """Control the sweep sensitivity by specifying the sensitivity level you want to measure at,
+        in dBm. The sensitivity closest to that level, and the sweep speed are automatically
+        selected.""",
+    )
+
+    pass
 
 
 class AQ6370D(AQ6370Series):
