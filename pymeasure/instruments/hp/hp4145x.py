5c1298aeb38d468f15c62b0df942a2b669d4627c
neuschs <sebastian.neusch@homenet-seb.de>
2025-02-23 18:30:43 +0100

    * added features and documentation as well as tests

--- pymeasure/instruments/hp/hp4145x.py
+++ pymeasure/instruments/hp/hp4145x.py
@@ -69,6 +69,7 @@ def mode_assert_lambda(parent, mode):
 
     return func
 
+
 class Status(IntEnum):
     DATA_READY = 0b00000001
     SYNTAX_ERROR = 0b00000010
@@ -82,7 +83,23 @@ class Status(IntEnum):
     SMU_SHUT_DOWN = 0b10001000
     POWER_FAILURE = 0b10100000
 
+
 class VS(Channel):
+    _voltage_name = "VS"
+
+    _channel_functions = {"VAR1": 1, "VAR2": 2, "CONST": 3, "VAR1'": 4}
+    _channel_function = 3
+
+    _disabled = False
+
+    manual_flush = False
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.voltage_set_process = mode_assert_lambda(self.parent, "USER_MODE")
+
+        self._voltage_name = f"VS{self.id}"
+
     voltage = Channel.setting(
         "DS {ch}, %f",
         """
@@ -94,17 +111,83 @@ class VS(Channel):
         """,
         validator=strict_range,
         values=[0, 20],
-        # set_process=lambda v: mode_assert_lambda(v, super(), "USER_MODE")
+        check_set_errors=True
     )
+
+    @property
+    def voltage_name(self):
+        """
+        Control the voltage name for `SYSTEM_MODE`'s channel definition.
+
+        Defaults to VS[1..2]. Must be unique otherwise the instrument returns a 'PROGRAM_ERROR'.
+
+        Returns not the actual state of the instrument but the state of the object.
+        """
+        return self._voltage_name
+
+    @property
+    def disabled(self):
+        return self._disabled
+
+    @disabled.setter
+    def disabled(self, value):
+        if value != True:
+            raise ValueError("Channel can only be disabled, it gets enabled implicitly with the other functions")
+
+        self._disabled = True
+        self.write("DE VS {ch}")
+
+    @voltage_name.setter
+    def voltage_name(self, value):
+        if len(value.strip()) > 6 or len(value.strip()) < 1:
+            raise ValueError("Name must be less than six and at least one characters")
+
+        self._voltage_name = value.upper()
+        self._disabled = False
+        if not self.manual_flush:
+            self.write("DE VS %d, '%s', %d" % (self.id, self._voltage_name, self._channel_function))
+            self.check_errors()
+
+    @property
+    def channel_function(self):
+        """
+        Control the source function, values "VAR1", "VAR2", "CONST" or "VAR1'" are allowed.
+
+        Implicitly writes/flushs :attr:`voltage_name`.
+        Returns not the actual state of the instrument but the state of the object.
+        """
+        for k, v in self._channel_functions.items():
+            if v == self._channel_function:
+                return k
+
+    @channel_function.setter
+    def channel_function(self, value):
+        self._channel_function = self._channel_functions[strict_discrete_set(value, self._channel_functions)]
+        self._disabled = False
+        if not self.manual_flush:
+            self.write("DE VS %d, '%s', %d" % (self.id, self._voltage_name, self._channel_function))
+            self.check_errors()
+
+    def flush_channel_definition(self):
+        """
+        Flushes the channel definition manually in case the :attr:`manual_flush` option is set.
+        """
+        self.write("DE VS %d, '%s'" % (self.id, self._voltage_name))
+        self.check_errors()
 
 
 class VM(Channel):
+    _voltage_name = "VM"
+    _disabled = False
 
-    def insert_id(self, command):
-        normal_id = self.id
-        self.id += 4
-        super().insert_id(command)
-        self.id = normal_id
+    manual_flush = False
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.voltage_get_process = lambda v: mode_assert_lambda(self.parent, "USER_MODE")(
+            check_errors_user_mode_value(v))
+
+        self.voltage_get_command = f"TV {self.id + 4}"
 
     voltage = Channel.measurement(
         "TV {ch}",
@@ -115,9 +198,51 @@ class VM(Channel):
             Only works with mode == 'USER_MODE' of instrument. In 'SYSTEM_MODE' no
             direct access is allowed.
         """,
-        # get_process=lambda v: mode_assert_lambda(v, super(), "USER_MODE")
-        get_process=check_errors_user_mode_value
+        check_get_errors=True,
+        dynamic=True
     )
+
+    @property
+    def disabled(self):
+        return self._disabled
+
+    @disabled.setter
+    def disabled(self, value):
+        if value != True:
+            raise ValueError("Channel can only be disabled, it gets enabled implicitly with the other functions")
+
+        self._disabled = True
+        self.write("DE VM {ch}")
+
+    @property
+    def voltage_name(self):
+        """
+        Control the voltage name for `SYSTEM_MODE`'s channel definition.
+
+        Defaults to VM[1..2]. Must be unique otherwise the instrument returns a 'PROGRAM_ERROR'.
+
+        Returns not the actual state of the instrument but the state of the object.
+        """
+        return self._voltage_name
+
+    @voltage_name.setter
+    def voltage_name(self, value):
+        if len(value.strip()) > 6 or len(value.strip()) < 1:
+            raise ValueError("Name must be less than six and at least one characters")
+
+        self._voltage_name = value.upper()
+        self._disabled = False
+
+        if not self.manual_flush:
+            self.write("DE VM %d, '%s'" % (self.id, self._voltage_name))
+            self.check_errors()
+
+    def flush_channel_definition(self):
+        """
+        Flushes the channel definition manually in case the :attr:`manual_flush` option is set.
+        """
+        self.write("DE VM %d, '%s'" % (self.id, self._voltage_name))
+        self.check_errors()
 
 
 class SMU(Channel):
@@ -132,11 +257,34 @@ class SMU(Channel):
     _voltage_max = 100
     _voltage_compliance = 0.0
 
+    _voltage_name = ''
+    _current_name = ''
+
+    _channel_function = 3
+    _channel_functions = {"VAR1": 1, "VAR2": 2, "CONST": 3, "VAR1'": 4}
+
+    _channel_mode = 3
+    _channel_modes = {'V': 1, 'I': 2, 'COM': 3}
+
+    _disabled = False
+
+    manual_flush = False
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.voltage_set_process = mode_assert_lambda(self.parent, "USER_MODE")
         self.voltage_get_process = lambda v: mode_assert_lambda(self.parent, "USER_MODE")(
             check_errors_user_mode_value(v))
+        self.current_set_process = mode_assert_lambda(self.parent, "USER_MODE")
+        self.current_get_process = lambda v: mode_assert_lambda(self.parent, "USER_MODE")(
+            check_errors_user_mode_value(v))
+
+        self._voltage_name = f"V{self.id}"
+        self._current_name = f"I{self.id}"
+
+        self.channel_mode_set_command = (
+                "DE CH {ch}, '%s', '%s', %d, %d" %
+                (self._voltage_name, self._current_name, self._channel_mode, self._channel_function))
 
     """
     A SMU channel of the HP4145x Instrument
@@ -149,14 +297,20 @@ class SMU(Channel):
         Automatically puts the SMU into force voltage mode once set.
         
         .. warning::
+
             Only works with mode == 'USER_MODE' of instrument. In 'SYSTEM_MODE' no
             direct access is allowed.
+            
+        .. code-block:: python
+
+            hp4145a.SMU2.current_compliance = 10e-3
+            hp4145a.SMU2.voltage = 10
+            
         """,
         validator=strict_range,
         values=[0, 100 if _voltage_range == 0 else _voltage_range],
         dynamic=True,
-        set_process=None,
-        get_process=None,
+        check_set_errors=True
     )
 
     current = Channel.control(
@@ -166,19 +320,40 @@ class SMU(Channel):
         Automatically puts the SMU into force current mode once set.
 
         .. warning::
+
             Only works with mode == 'USER_MODE' of instrument. In 'SYSTEM_MODE' no
             direct access is allowed.
+            
+        .. code-block:: python
+
+            hp4145a.SMU2.voltage_compliance = 10
+            hp4145a.SMU2.current = 10e-3
         """,
         validator=strict_range,
         values=[0, 100 if _voltage_range == 0 else _voltage_range],
         dynamic=True,
-        # set_process=lambda v: modeAssertLambda(v, super(HP4145x), "USER_MODE"),
-        # get_process=lambda v: modeAssertLambda(v, super(HP4145x), "USER_MODE")
-        get_process=check_errors_user_mode_value
+        check_set_errors=True
     )
 
     @property
+    def disabled(self):
+        return self._disabled
+
+    @disabled.setter
+    def disabled(self, value):
+        if value != True:
+            raise ValueError("Channel can only be disabled, it gets enabled implicitly with the other functions")
+
+        self._disabled = True
+        self.write("DE CH {ch}")
+
+    @property
     def voltage_range(self):
+        """
+        Controls the voltage range used by :attr:`voltage`. Only applicable in `USER_MODE`.
+
+        Returns not the actual state of the instrument but the state of the object.
+        """
         for k, v in self._voltage_ranges.items():
             if v == self._voltage_range:
                 return k
@@ -190,6 +365,11 @@ class SMU(Channel):
 
     @property
     def current_range(self):
+        """
+        Controls the current range used by :attr:`current`. Only applicable in `USER_MODE`.
+
+        Returns not the actual state of the instrument but the state of the object.
+        """
         for k, v in self._current_ranges.items():
             if v == self._current_range:
                 return k
@@ -201,6 +381,11 @@ class SMU(Channel):
 
     @property
     def current_compliance(self):
+        """
+        Controls the current compliance of the channel.
+
+        Returns not the actual state of the instrument but the state of the object.
+        """
         return self._current_compliance
 
     @current_compliance.setter
@@ -210,12 +395,130 @@ class SMU(Channel):
 
     @property
     def voltage_compliance(self):
+        """
+        Controls the voltage compliance of the channel.
+
+        Returns not the actual state of the instrument but the state of the object.
+        """
         return self._voltage_compliance
 
     @voltage_compliance.setter
     def voltage_compliance(self, value):
         self._voltage_compliance = strict_range(value, [0, self._voltage_max])
         self.current_set_command = "DI {ch}, %d, %s, %f" % (self._current_range, "%f", self._voltage_compliance)
+
+    @property
+    def voltage_name(self):
+        """
+        Control the voltage name for `SYSTEM_MODE`'s channel definition.
+
+        Defaults to V[1..4]. Must be unqiue otherwise the instrument returns a 'PROGRAM_ERROR'.
+        Only takes affect with a setting of :attr:`channel_mode`.
+
+        Returns not the actual state of the instrument but the state of the object.
+        """
+        return self._voltage_name
+
+    @voltage_name.setter
+    def voltage_name(self, value):
+        if len(value.strip()) > 6 or len(value.strip()) < 1:
+            raise ValueError("Name must be less than six and at least one characters")
+
+        self._voltage_name = value.upper()
+        self._disabled = False
+
+        if not self.manual_flush:
+            self.write(self.insert_id("DE CH {ch}, '%s', '%s', %d, %d" %
+                                      (self._voltage_name, self._current_name, self._channel_mode,
+                                       self._channel_function)))
+            self.check_errors()
+
+    @property
+    def current_name(self):
+        """
+        Control the current name for `SYSTEM_MODE`'s channel definition.
+
+        Defaults to I[1..4]. Must be unqiue otherwise the instrument returns a 'PROGRAM_ERROR'.
+        Only takes affect with a setting of :attr:`channel_mode`.
+
+        Returns not the actual state of the instrument but the state of the object.
+        """
+        return self._current_name
+
+    @current_name.setter
+    def current_name(self, value):
+        if len(value.strip()) > 6 or len(value.strip()) < 1:
+            raise ValueError("Name must be less than six and at least one characters")
+
+        self._current_name = value
+        self._disabled = False
+
+        if not self.manual_flush:
+            self.write(self.insert_id("DE CH {ch}, '%s', '%s', %d, %d" %
+                                      (self._voltage_name, self._current_name, self._channel_mode,
+                                       self._channel_function)))
+            self.check_errors()
+
+    @property
+    def channel_function(self):
+        """
+        Control the source function, values "VAR1", "VAR2", "CONST" or "VAR1'" are allowed.
+
+        Implicitly writes/flushes :attr:`channel_mode`, :attr:`current_name` and :attr:`voltage_name`.
+        Returns not the actual state of the instrument but the state of the object.
+        """
+        for k, v in self._channel_functions.items():
+            if v == self._channel_function:
+                return k
+
+    @channel_function.setter
+    def channel_function(self, value):
+        self._channel_function = self._channel_functions[strict_discrete_set(value, self._channel_functions)]
+
+        self._disabled = False
+        if not self.manual_flush:
+            self.write(self.insert_id("DE CH {ch}, '%s', '%s', %d, %d" %
+                                      (self._voltage_name, self._current_name, self._channel_mode,
+                                       self._channel_function)))
+            self.check_errors()
+
+    @property
+    def channel_mode(self):
+        """
+        Set the source mode of the channel.
+
+        Implicitly writes/flushes :attr:`channel_function`, :attr:`current_name` and :attr:`voltage_name`.
+
+        .. code-block:: python
+
+            hp4145a.SMU2.voltage_name = "VCE2"
+            hp4145a.SMU2.current_name = "ICE2"
+            hp4145a.SMU2.channel_function = "VAR2"
+            hp4145a.SMU2.channel_mode = "V"
+        """
+
+        for k, v in self._channel_modes.items():
+            if v == self._channel_mode:
+                return k
+
+    @channel_mode.setter
+    def channel_mode(self, value):
+        self._channel_mode = self._channel_modes[strict_discrete_set(value, self._channel_modes)]
+
+        self._disabled = False
+        if not self.manual_flush:
+            self.write(self.insert_id("DE CH {ch}, '%s', '%s', %d, %d" %
+                                      (self._voltage_name, self._current_name, self._channel_mode,
+                                       self._channel_function)))
+            self.check_errors()
+
+    def flush_channel_definition(self):
+        """
+        Flushes the channel definition manually in case the :attr:`manual_flush` option is set.
+        """
+        self.write(self.insert_id("DE CH {ch}, '%s', '%s', %d, %d" %
+                                  (self._voltage_name, self._current_name, self._channel_mode, self._channel_function)))
+        self.check_errors()
 
 
 class HP4145x(Instrument):
@@ -226,11 +529,14 @@ class HP4145x(Instrument):
     VS1: VS = Instrument.ChannelCreator(VS, 1)
     VS2: VS = Instrument.ChannelCreator(VS, 2)
     VM1: VM = Instrument.ChannelCreator(VM, 1)
-    VM2: VM = Instrument.ChannelCreator(VM, 1)
+    VM2: VM = Instrument.ChannelCreator(VM, 2)
 
     _mode = "SYSTEM_MODE"
 
+    _manual_flush = False
+
     def __init__(self, adapter, name="Hewlett-Packard HP4145x", **kwargs):
+        kwargs.setdefault('timeout', 100000)
         super().__init__(
             adapter,
             name,
@@ -239,7 +545,35 @@ class HP4145x(Instrument):
             **kwargs,
         )
 
-        # self.mode = self._mode
+    def flush_channel_definition(self):
+        """
+        Flushes the channel definitions of all sub channels. Only required with :attr:`manual_flush` True.
+        """
+        self.SMU1.flush_channel_definition()
+        self.SMU2.flush_channel_definition()
+        self.SMU3.flush_channel_definition()
+        self.SMU4.flush_channel_definition()
+        self.VM1.flush_channel_definition()
+        self.VM2.flush_channel_definition()
+        self.VS1.flush_channel_definition()
+        self.VS2.flush_channel_definition()
+
+    @property
+    def manual_flush(self):
+        return self._manual_flush
+
+    @manual_flush.setter
+    def manual_flush(self, value):
+        self.SMU1.manual_flush = value
+        self.SMU2.manual_flush = value
+        self.SMU3.manual_flush = value
+        self.SMU4.manual_flush = value
+        self.VM1.manual_flush = value
+        self.VM2.manual_flush = value
+        self.VS1.manual_flush = value
+        self.VS2.manual_flush = value
+
+        self._manual_flush = value
 
     @property
     def mode(self):
@@ -255,7 +589,8 @@ class HP4145x(Instrument):
 
     @property
     def status(self):
-        return self.adapter.connection.read_stb()
+        stb = self.adapter.connection.read_stb()
+        return stb if type(stb) is int else 0
 
     def reset(self):
         self.adapter.connection.clear()
@@ -267,17 +602,70 @@ class HP4145x(Instrument):
     def check_errors(self):
         errors = []
         error_status = [Status.EMERGENCY, Status.SYNTAX_ERROR, Status.BUSY, Status.ILLEGAL_PROGRAM]
-        while True:
-            status = self.status
+        emergency_status = [Status.POWER_FAILURE, Status.FIXTURE_LID_OPEN, Status.SMU_SHUT_DOWN]
+        while (errors[0].value & Status.EMERGENCY.value) is False if len(errors) > 0 else True:
+            # mask out RQS (always set with other bits)
+            status = self.status & ~Status.RQS
             if any(es & status for es in error_status):
                 for es in error_status:
-                    if status & es:
-                        log.error(f"HP4145x: {es}")
+                    if status & es and es == Status.EMERGENCY:
+                        log.error(f"HP4145x: {Status(status).value} - {Status(status).name}")
+                        errors.append(Status(status))
+                        break
+                    else:
+                        log.error(f"HP4145x: {es.value} - {es.name}")
                         errors.append(es)
             else:
                 break
         return errors
 
-    # integration_time = Instrument.setting(
-    #     ""
-    # )
+    integration_time = Instrument.setting(
+        "IT%d",
+        """
+        Set the integration time. 'SHORT' no integration at all, 'MEDIUM' 16 samples per measurement and 'LONG'
+        256 samples per measurement.
+        """,
+        map_values=True,
+        values={"SHORT": 1, "MEDIUM": 2, "LONG": 3},
+        validator=strict_discrete_set,
+        check_set_errors=True
+    )
+
+    auto_calibrate = Instrument.setting(
+        "CA%d",
+        """
+        Set the instrument to automatically calibrate internally every 15minutes.
+        """,
+        values=[True, False],
+        validator=strict_discrete_set,
+        check_set_errors=True
+    )
+
+    data_ready_srq = Instrument.setting(
+        "DR%d",
+        """
+        Sets the data ready enablement of the bit of the status byte.
+        """,
+        values=[True, False],
+        validator=strict_discrete_set,
+        check_set_errors=True
+    )
+
+    def self_test(self):
+        """
+        Execute the self test and return pass/fail.
+        """
+        self.write("SF")
+        status = self.status
+        return bool((not (status & Status.SELF_TEST_FAIL)) and (status & Status.END_STATUS))
+
+    def get_trace(self, name):
+        """
+        Returns the list of values for the given variable name. Name is the same as defined in the channel definition.
+        """
+        self.write(f"DO '%s'" % name)
+
+        result = self.read().replace("\r", "").replace("\n", "")
+        removed_status = filter(None, result.replace("N", "").replace("C", "").split(","))
+
+        return [float(x) for x in removed_status]
--- tests/instruments/hp/test_hp4145x.py
+++ tests/instruments/hp/test_hp4145x.py
@@ -28,14 +28,128 @@ from pymeasure.test import expected_protocol
 from pymeasure.instruments.hp import HP4145x
 
 
-def test_calibrate():
+@pytest.mark.parametrize("channel", range(1, 5))
+def test_voltage_and_current_compliance(channel):
     with expected_protocol(
             HP4145x,
             [
                 ("US", None),
-                ("DV 1, 0, %f, %f" % (99.9, 0.00), None)
-             ],
+                ("DV %s, 0, %f, %f" % (channel, 99.9, 10e-3), None)
+            ],
     ) as instr:
         instr.mode = 'USER_MODE'
-        instr.SMU1.voltage = 99.9
+        getattr(instr, f"SMU{channel}").current_compliance = 10e-3
+        getattr(instr, f"SMU{channel}").voltage = 99.9
+
+
+@pytest.mark.parametrize("channel", range(1, 5))
+def test_current_and_voltage_compliance(channel):
+    with expected_protocol(
+            HP4145x,
+            [
+                ("US", None),
+                ("DI %s, 0, %f, %f" % (channel, 10e-3, 10), None)
+            ],
+    ) as instr:
+        instr.mode = 'USER_MODE'
+        getattr(instr, f"SMU{channel}").voltage_compliance = 10
+        getattr(instr, f"SMU{channel}").current = 10e-3
+
+
+@pytest.mark.parametrize("integ_time, value", [("SHORT", 1), ("MEDIUM", 2), ("LONG", 3)])
+def test_integration_time(integ_time, value):
+    with expected_protocol(
+            HP4145x,
+            [
+                ("IT%d" % (value), None)
+            ],
+    ) as instr:
+        instr.integration_time = integ_time
+
+
+@pytest.mark.parametrize("value", [True, False])
+def test_auto_calibrate(value):
+    with expected_protocol(
+            HP4145x,
+            [
+                ("CA%d" % value, None)
+            ],
+    ) as instr:
+        instr.auto_calibrate = value
+
+
+@pytest.mark.parametrize("source_function, source_function_id", [("VAR1", 1), ("VAR2", 2), ("CONST", 3), ("VAR1'", 4)])
+@pytest.mark.parametrize("source_mode, source_mode_id", [("V", 1), ("I", 2), ("COM", 3)])
+@pytest.mark.parametrize("channel", range(1, 5))
+def test_channel_mode_and_channel_function(channel, source_mode, source_mode_id, source_function, source_function_id):
+    with expected_protocol(
+            HP4145x,
+            [
+                ("DE CH %d, '%s', '%s', %d, %d" % (channel, "VBE", f"I{channel}", 3, 3), None),
+                ("DE CH %d, '%s', '%s', %d, %d" % (channel, "VBE", "IB", 3, 3), None),
+                ("DE CH %d, '%s', '%s', %d, %d" % (channel, "VBE", "IB", 3, source_function_id), None),
+                ("DE CH %d, '%s', '%s', %d, %d" % (channel, "VBE", "IB", source_mode_id, source_function_id), None)
+            ],
+    ) as instr:
+        getattr(instr, f"SMU{channel}").voltage_name = "VBE"
+        getattr(instr, f"SMU{channel}").current_name = "IB"
+        getattr(instr, f"SMU{channel}").channel_function = source_function
+        getattr(instr, f"SMU{channel}").channel_mode = source_mode
+
+
+@pytest.mark.parametrize("source_function, source_function_id", [("VAR1", 1), ("VAR2", 2), ("CONST", 3), ("VAR1'", 4)])
+@pytest.mark.parametrize("channel", range(1, 3))
+def test_channel_function_vs(channel, source_function, source_function_id):
+    with expected_protocol(
+            HP4145x,
+            [
+                ("DE VS %d, '%s', %d" % (channel, "VBE", 3), None),
+                ("DE VS %d, '%s', %d" % (channel, "VBE", source_function_id), None),
+            ],
+    ) as instr:
+        getattr(instr, f"VS{channel}").voltage_name = "VBE"
+        getattr(instr, f"VS{channel}").channel_function = source_function
+
+
+@pytest.mark.parametrize("channel", range(1, 3))
+def test_channel_function_vm(channel):
+    with expected_protocol(
+            HP4145x,
+            [
+                ("DE VM %d, '%s'" % (channel, "VBE"), None)
+            ],
+    ) as instr:
+        getattr(instr, f"VM{channel}").voltage_name = "VBE"
+
+
+@pytest.mark.parametrize("channel", range(1, 5))
+def test_smu_disabled(channel):
+    with expected_protocol(
+            HP4145x,
+            [
+                ("DE CH %d" % (channel), None)
+            ],
+    ) as instr:
+        getattr(instr, f"SMU{channel}").disabled = True
+
+
+@pytest.mark.parametrize("channel", range(1, 3))
+def test_vm_disabled(channel):
+    with expected_protocol(
+            HP4145x,
+            [
+                ("DE VM %d" % (channel), None)
+            ],
+    ) as instr:
+        getattr(instr, f"VM{channel}").disabled = True
+
+@pytest.mark.parametrize("channel", range(1, 3))
+def test_vs_disabled(channel):
+    with expected_protocol(
+            HP4145x,
+            [
+                ("DE VS %d" % (channel), None)
+            ],
+    ) as instr:
+        getattr(instr, f"VS{channel}").disabled = True
 
