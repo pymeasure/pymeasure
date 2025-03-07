09a92963684548ace2d553f43b6e01896204fd58
Sergey Ilyev <ilev.sv@phystech.edu>
2025-03-07 15:13:51 +0300

    Modify commands and ranges for inherited properties

--- pymeasure/instruments/keysight/keysight81160A.py
+++ pymeasure/instruments/keysight/keysight81160A.py
@@ -26,11 +26,117 @@ from pymeasure.instruments import Instrument
 from pymeasure.instruments.agilent import Agilent33500
 from pymeasure.instruments.agilent.agilent33500 import Agilent33500Channel
 
+WF_SHAPES = ["SIN", "SQU", "RAMP", "PULS", "NOIS", "DC", "USER"]
+FREQUENCY_RANGE = [1e-6, 5e8]
+AMPLITUDE_RANGE = [0.05, 5]
+OFFSET_RANGE = [-4.995, 4.995]
+VOLTAGE_HIGH_RANGE = [-4.95, 5.0]
+VOLTAGE_LOW_RANGE = [-5, 4.95]
+SQUARE_DUTYCYCLE_RANGE = [0.0001515, 99.9998485]
+PULSE_PERIOD_RANGE = [2e-9, 1e6]
+PULSE_WIDTH_RANGE = [1.5e-9, 1e6]
+PULSE_DUTYCYCLE_RANGE = [1.5e-1, 99.99]
+PULSE_TRANSITION_RANGE = [1e-9, 1e3]
+BURST_PERIOD = [3.03e-9, 1e6]
+BURST_NCYCLES = [2, 2.147483647e9]
+LIMIT_HIGH_RANGE = [-9.99, 10]
+LIMIT_LOW_RANGE = [-10, 9.99]
+STATES = ["ON", "OFF"]
+IMPEDANCE_RANGE = [3e-1, 1e6]
+
 
 class Keysight81160AChannel(Agilent33500Channel):
-    pass
+    shape_values = WF_SHAPES
+    shape_get_command = ":FUNC{ch}?"
+    shape_set_command = ":FUNC{ch} %s"
+
+    frequency_values = FREQUENCY_RANGE
+    frequency_get_command = ":FREQ{ch}?"
+    frequency_set_command = ":FREQ{ch} %f"
+
+    amplitude_values = AMPLITUDE_RANGE
+    amplitude_get_command = ":VOLT{ch}?"
+    amplitude_set_command = ":VOLT{ch} %f"
+
+    amplitude_unit_get_command = ":VOLT{ch}:UNIT?"
+    amplitude_unit_set_command = ":VOLT{ch}:UNIT %s"
+
+    offset_values = OFFSET_RANGE
+    offset_get_command = ":VOLT{ch}:OFFS?"
+    offset_set_command = ":VOLT{ch}:OFFS %f"
+
+    voltage_high_values = VOLTAGE_HIGH_RANGE
+    voltage_high_get_command = ":VOLT{ch}:HIGH?"
+    voltage_high_set_command = ":VOLT{ch}:HIGH %f"
+
+    voltage_low_values = VOLTAGE_LOW_RANGE
+    voltage_low_get_command = ":VOLT{ch}:LOW?"
+    voltage_low_set_command = ":VOLT{ch}:LOW %f"
+
+    square_dutycycle_values = SQUARE_DUTYCYCLE_RANGE
+    square_dutycycle_set_command = ":FUNC{ch}:SQU:DCYC %f"
+    square_dutycycle_get_command = ":FUNC{ch}:SQU:DCYC?"
+
+    ramp_symmetry_set_command = ":FUNC{ch}:RAMP:SYMM %f"
+    ramp_symmetry_get_command = ":FUNC{ch}:RAMP:SYMM?"
+
+    pulse_period_values = PULSE_PERIOD_RANGE
+    pulse_period_set_command = ":PULS:PER{ch} %e"
+    pulse_period_get_command = ":PULS:PER{ch}?"
+
+    pulse_hold_set_command = ":PULS:HOLD{ch} %s"
+    pulse_hold_get_command = ":PULS:HOLD{ch}?"
+
+    pulse_width_values = PULSE_WIDTH_RANGE
+    pulse_width_set_command = ":PULS:WIDT{ch} %e"
+    pulse_width_get_command = ":PULS:WIDT{ch}?"
+
+    pulse_dutycycle_values = PULSE_DUTYCYCLE_RANGE
+    pulse_dutycycle_set_command = ":PULS:DCYC{ch} %f"
+    pulse_dutycycle_get_command = ":PULS:DCYC{ch}?"
+
+    pulse_transition_values = PULSE_TRANSITION_RANGE
+    pulse_transition_set_command = ":FUNC{ch}:PULS:TRAN %e"
+    pulse_transition_get_command = ":FUNC{ch}:PULS:TRAN?"
+
+    output_load_values = IMPEDANCE_RANGE
+    output_load_set_command = ":OUTP{ch}:LOAD %e"
+    output_load_get_command = ":OUTP{ch}:LOAD?"
+
+    burst_state_values = STATES
+    burst_state_set_command = ":BURS{ch}:STAT %s"
+    burst_state_get_command = ":BURS{ch}:STAT?"
+
+    burst_mode_set_command = ":BURS{ch}:MODE %s"
+    burst_mode_get_command = ":BURS{ch}:MODE?"
+
+    burst_period_values = BURST_PERIOD
+    burst_period_set_command = ":BURS{ch}:INT:PER %e"
+    burst_period_get_command = ":BURS{ch}:INT:PER?"
+
+    burst_ncycles_values = BURST_NCYCLES
+    burst_ncycles_set_command = ":BURS{ch}:NCYC %d"
+    burst_ncycles_get_command = ":BURS{ch}:NCYC?"
 
 
 class Keysight81160A(Agilent33500):
     ch_1 = Instrument.ChannelCreator(Keysight81160AChannel, 1)
     ch_2 = Instrument.ChannelCreator(Keysight81160AChannel, 2)
+
+    shape_values = WF_SHAPES
+    frequency_values = FREQUENCY_RANGE
+    amplitude_values = AMPLITUDE_RANGE
+    offset_values = OFFSET_RANGE
+    voltage_high_values = VOLTAGE_HIGH_RANGE
+    voltage_low_values = VOLTAGE_LOW_RANGE
+    square_dutycycle_values = SQUARE_DUTYCYCLE_RANGE
+    pulse_period_values = PULSE_PERIOD_RANGE
+    pulse_period_get_command = ":PULS:PER?"
+    pulse_period_set_command = ":PULS:PER %e"
+    pulse_width_values = PULSE_WIDTH_RANGE
+    pulse_dutycycle_values = PULSE_DUTYCYCLE_RANGE
+    pulse_transition_values = PULSE_TRANSITION_RANGE
+    pulse_transition_set_command = ":FUNC:PULS:TRAN %e"
+    burst_period_values = BURST_PERIOD
+    burst_ncycles_values = BURST_NCYCLES
+    arb_file_get_command = ":FUNC:USER?"
