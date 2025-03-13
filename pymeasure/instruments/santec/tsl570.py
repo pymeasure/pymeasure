679aac37563d56922ab2e7d5b0a99931cde32353
fredgillard <fred.gillard@lumentum.com>
2025-03-13 11:49:30 +0000

    Expansion of tests, bug fixing,  amd changing to values_kwargs to avoid FutureWarning

--- pymeasure/instruments/santec/tsl570.py
+++ pymeasure/instruments/santec/tsl570.py
@@ -73,7 +73,7 @@ class TSL570(SCPIMixin, Instrument):
         ":POWer?",
         ":Power %e",
         """Control the output optical power, units defined by power_unit.""",
-        check_instr_errors=True,
+        values_kwargs={"check_instr_errors": True},
     )
 
     power_reading = Instrument.measurement(
@@ -99,28 +99,28 @@ class TSL570(SCPIMixin, Instrument):
         ":WAVelength?",
         ":WAVelength %e",
         """Control the output wavelength, in m.""",
-        check_instr_errors=True,
+        values_kwargs={"check_instr_errors": True},
     )
 
     wavelength_start = Instrument.control(
         ":WAVelength:SWEep:STARt?",
         ":WAVelength:SWEep:STARt %e",
         """Control the sweep start wavelength, in m.""",
-        check_instr_errors=True,
+        values_kwargs={"check_instr_errors": True},
     )
 
     wavelength_stop = Instrument.control(
         ":WAVelength:SWEep:STOP?",
         ":WAVelength:SWEep:STOP %e",
         """Control the sweep stop wavelength, in m.""",
-        check_instr_errors=True,
+        values_kwargs={"check_instr_errors": True},
     )
 
     wavelength_step = Instrument.control(
         ":WAVelength:SWEep:STEP?",
         ":WAVelength:SWEep:STEP %e",
         """Control the sweep step wavelength when in step sweep mode, in m.""",
-        check_instr_errors=True,
+        values_kwargs={"check_instr_errors": True},
     )
 
     # --- Optical frequency control ---
@@ -141,28 +141,28 @@ class TSL570(SCPIMixin, Instrument):
         ":FREQuency?",
         ":FREQuency %e",
         """Control the output frequency, in m.""",
-        check_instr_errors=True,
+        values_kwargs={"check_instr_errors": True},
     )
 
     frequency_start = Instrument.control(
         ":FREQuency:SWEep:STARt?",
         ":FREQuency:SWEep:STARt %e",
         """Control the sweep start frequency, in m.""",
-        check_instr_errors=True,
+        values_kwargs={"check_instr_errors": True},
     )
 
     frequency_stop = Instrument.control(
         ":FREQuency:SWEep:STOP?",
         ":FREQuency:SWEep:STOP %e",
         """Control the sweep stop frequency, in m.""",
-        check_instr_errors=True,
+        values_kwargs={"check_instr_errors": True},
     )
 
     frequency_step = Instrument.control(
         ":FREQuency:SWEep:STEP?",
         ":FREQuency:SWEep:STEP %e",
         """Control the sweep step frequency when in step sweep mode, in m.""",
-        check_instr_errors=True,
+        values_kwargs={"check_instr_errors": True},
     )
 
     # --- Sweep settings ---
@@ -199,7 +199,7 @@ class TSL570(SCPIMixin, Instrument):
         validator=strict_discrete_set,
         values={
             "Stepped One-way": 0,
-            "Continuous One-way:": 1,
+            "Continuous One-way": 1,
             "Stepped Two-way": 2,
             "Continuous Two-way": 3,
         },
--- tests/instruments/santec/test_tsl570.py
+++ tests/instruments/santec/test_tsl570.py
@@ -9,7 +9,7 @@ class TSL570Test(TSL570):
         pass
 
 
-# Patch the TSL570Test class methods to ignore the 'check_instr_errors' keyword.
+# Patch the TSL570Test class methods to ignore only the 'check_instr_errors' keyword.
 def _patched_write(self, command, **kwargs):
     kwargs.pop("check_instr_errors", None)
     return super(TSL570Test, self).write(command, **kwargs)
@@ -28,14 +28,70 @@ init_sequence = [
     (b":SYSTem:COMMunicate:CODe 1", None),
 ]
 
-
-def test_init():
-    # This test simply verifies that instantiation sends the init command.
-    with expected_protocol(TSL570Test, init_sequence):
-        pass
+# ========================= Optical Power Control Tests =========================
 
 
-# --- Wavelength Tests ---
+def test_output_enabled_setter():
+    protocol = init_sequence + [(b":POWer:STATe 1", None)]
+    with expected_protocol(TSL570Test, protocol) as inst:
+        inst.output_enabled = True
+
+
+def test_output_enabled_getter():
+    protocol = init_sequence + [(b":POWer:STATe?", b"1\n")]
+    with expected_protocol(TSL570Test, protocol) as inst:
+        value = inst.output_enabled
+        assert value is True
+
+
+def test_power_unit_setter():
+    protocol = init_sequence + [(b":POWer:UNIT 1", None)]
+    with expected_protocol(TSL570Test, protocol) as inst:
+        inst.power_unit = "mW"
+
+
+def test_power_unit_getter():
+    protocol = init_sequence + [(b":POWer:UNIT?", b"0\n")]
+    with expected_protocol(TSL570Test, protocol) as inst:
+        value = inst.power_unit
+        assert value == "dBm"
+
+
+def test_power_setpoint_setter():
+    protocol = init_sequence + [(b":Power 5.000000e+00", None)]
+    with expected_protocol(TSL570Test, protocol) as inst:
+        inst.power_setpoint = 5.0
+
+
+def test_power_setpoint_getter():
+    protocol = init_sequence + [(b":POWer?", b"5.000000e+00\n")]
+    with expected_protocol(TSL570Test, protocol) as inst:
+        value = inst.power_setpoint
+        assert abs(value - 5.0) < 1e-12
+
+
+def test_power_reading():
+    protocol = init_sequence + [(b":POWer:ACTual?", b"3.500000e+00\n")]
+    with expected_protocol(TSL570Test, protocol) as inst:
+        value = inst.power_reading
+        assert abs(value - 3.5) < 1e-12
+
+
+# ========================= Wavelength Control Tests =========================
+
+
+def test_wavelength_min():
+    protocol = init_sequence + [(b":WAVelength:SWEep:RANGe:MINimum?", b"1.000000e-06\n")]
+    with expected_protocol(TSL570Test, protocol) as inst:
+        value = inst.wavelength_min
+        assert abs(value - 1e-06) < 1e-12
+
+
+def test_wavelength_max():
+    protocol = init_sequence + [(b":WAVelength:SWEep:RANGe:MAXimum?", b"2.000000e-06\n")]
+    with expected_protocol(TSL570Test, protocol) as inst:
+        value = inst.wavelength_max
+        assert abs(value - 2e-06) < 1e-12
 
 
 def test_wavelength_setter():
@@ -49,3 +105,249 @@ def test_wavelength_getter():
     with expected_protocol(TSL570Test, protocol) as inst:
         value = inst.wavelength
         assert abs(value - 1.55e-06) < 1e-12
+
+
+def test_wavelength_start_setter():
+    protocol = init_sequence + [(b":WAVelength:SWEep:STARt 1.500000e-06", None)]
+    with expected_protocol(TSL570Test, protocol) as inst:
+        inst.wavelength_start = 1.5e-06
+
+
+def test_wavelength_start_getter():
+    protocol = init_sequence + [(b":WAVelength:SWEep:STARt?", b"1.500000e-06\n")]
+    with expected_protocol(TSL570Test, protocol) as inst:
+        value = inst.wavelength_start
+        assert abs(value - 1.5e-06) < 1e-12
+
+
+def test_wavelength_stop_setter():
+    protocol = init_sequence + [(b":WAVelength:SWEep:STOP 1.600000e-06", None)]
+    with expected_protocol(TSL570Test, protocol) as inst:
+        inst.wavelength_stop = 1.6e-06
+
+
+def test_wavelength_stop_getter():
+    protocol = init_sequence + [(b":WAVelength:SWEep:STOP?", b"1.600000e-06\n")]
+    with expected_protocol(TSL570Test, protocol) as inst:
+        value = inst.wavelength_stop
+        assert abs(value - 1.6e-06) < 1e-12
+
+
+def test_wavelength_step_setter():
+    protocol = init_sequence + [(b":WAVelength:SWEep:STEP 1.000000e-09", None)]
+    with expected_protocol(TSL570Test, protocol) as inst:
+        inst.wavelength_step = 1e-09
+
+
+def test_wavelength_step_getter():
+    protocol = init_sequence + [(b":WAVelength:SWEep:STEP?", b"1.000000e-09\n")]
+    with expected_protocol(TSL570Test, protocol) as inst:
+        value = inst.wavelength_step
+        assert abs(value - 1e-09) < 1e-12
+
+
+# ========================= Frequency Control Tests =========================
+
+
+def test_frequency_min():
+    protocol = init_sequence + [(b":FREQuency:SWEep:RANGe:MINimum?", b"1.000000e+14\n")]
+    with expected_protocol(TSL570Test, protocol) as inst:
+        value = inst.frequency_min
+        assert abs(value - 1e14) < 1e-6
+
+
+def test_frequency_max():
+    protocol = init_sequence + [(b":FREQuency:SWEep:RANGe:MAXimum?", b"2.000000e+14\n")]
+    with expected_protocol(TSL570Test, protocol) as inst:
+        value = inst.frequency_max
+        assert abs(value - 2e14) < 1e-6
+
+
+def test_frequency_setter():
+    protocol = init_sequence + [(b":FREQuency 2.000000e+14", None)]
+    with expected_protocol(TSL570Test, protocol) as inst:
+        inst.frequency = 2e14
+
+
+def test_frequency_getter():
+    protocol = init_sequence + [(b":FREQuency?", b"2.000000e+14\n")]
+    with expected_protocol(TSL570Test, protocol) as inst:
+        value = inst.frequency
+        assert abs(value - 2e14) < 1e-6
+
+
+def test_frequency_start_setter():
+    protocol = init_sequence + [(b":FREQuency:SWEep:STARt 1.900000e+14", None)]
+    with expected_protocol(TSL570Test, protocol) as inst:
+        inst.frequency_start = 1.9e14
+
+
+def test_frequency_start_getter():
+    protocol = init_sequence + [(b":FREQuency:SWEep:STARt?", b"1.900000e+14\n")]
+    with expected_protocol(TSL570Test, protocol) as inst:
+        value = inst.frequency_start
+        assert abs(value - 1.9e14) < 1e-6
+
+
+def test_frequency_stop_setter():
+    protocol = init_sequence + [(b":FREQuency:SWEep:STOP 2.100000e+14", None)]
+    with expected_protocol(TSL570Test, protocol) as inst:
+        inst.frequency_stop = 2.1e14
+
+
+def test_frequency_stop_getter():
+    protocol = init_sequence + [(b":FREQuency:SWEep:STOP?", b"2.100000e+14\n")]
+    with expected_protocol(TSL570Test, protocol) as inst:
+        value = inst.frequency_stop
+        assert abs(value - 2.1e14) < 1e-6
+
+
+def test_frequency_step_setter():
+    protocol = init_sequence + [(b":FREQuency:SWEep:STEP 1.000000e+12", None)]
+    with expected_protocol(TSL570Test, protocol) as inst:
+        inst.frequency_step = 1e12
+
+
+def test_frequency_step_getter():
+    protocol = init_sequence + [(b":FREQuency:SWEep:STEP?", b"1.000000e+12\n")]
+    with expected_protocol(TSL570Test, protocol) as inst:
+        value = inst.frequency_step
+        assert abs(value - 1e12) < 1e-6
+
+
+# ========================= Sweep Settings Tests =========================
+
+
+def test_start_sweep():
+    protocol = init_sequence + [(b":WAVelength:SWEep 1", None)]
+    with expected_protocol(TSL570Test, protocol) as inst:
+        assert inst.start_sweep() is None
+
+
+def test_start_repeat():
+    protocol = init_sequence + [(b":WAVelength:SWEep:REPeat", None)]
+    with expected_protocol(TSL570Test, protocol) as inst:
+        assert inst.start_repeat() is None
+
+
+def test_stop_sweep():
+    protocol = init_sequence + [(b":WAVelength:SWEep 0", None)]
+    with expected_protocol(TSL570Test, protocol) as inst:
+        assert inst.stop_sweep() is None
+
+
+def test_sweep_staus():
+    protocol = init_sequence + [(b"WAVelength:SWEep?", b"0\n")]
+    with expected_protocol(TSL570Test, protocol) as inst:
+        value = inst.sweep_staus
+        assert value == "Stopped"
+
+
+def test_sweep_mode_setter():
+    protocol = init_sequence + [(b":WAVelength:SWEep:MODe 1", None)]
+    with expected_protocol(TSL570Test, protocol) as inst:
+        inst.sweep_mode = "Continuous One-way"
+
+
+def test_sweep_mode_getter():
+    protocol = init_sequence + [(b":WAVelength:SWEep:MODe?", b"1\n")]
+    with expected_protocol(TSL570Test, protocol) as inst:
+        value = inst.sweep_mode
+        assert value == "Continuous One-way"
+
+
+def test_sweep_pattern_setter():
+    # Assume the initial sweep_mode is "Stepped Two-way" (mapped from 2).
+    # Expect command to change sweep_mode to "Continuous Two-way" (mapped from 3).
+    protocol = init_sequence + [
+        (b":WAVelength:SWEep:MODe?", b"2\n"),
+        (b":WAVelength:SWEep:MODe 3", None),
+    ]
+    with expected_protocol(TSL570Test, protocol) as inst:
+        inst.sweep_pattern = "Continuous"
+
+
+def test_sweep_pattern_getter():
+    protocol = init_sequence + [(b":WAVelength:SWEep:MODe?", b"1\n")]
+    with expected_protocol(TSL570Test, protocol) as inst:
+        # Assuming the sweep_mode getter returns "Continuous One-way" (mapped from 1),
+        # then sweep_pattern should be "Continuous".
+        assert inst.sweep_pattern == "Continuous"
+
+
+def test_sweep_routing_setter():
+    # Assume the initial sweep_mode is "Stepped Two-way" (mapped from 2).
+    # Expect command to change sweep_mode to "Stepped One-way" (mapped from 0).
+    protocol = init_sequence + [
+        (b":WAVelength:SWEep:MODe?", b"2\n"),
+        (b":WAVelength:SWEep:MODe 0", None),
+    ]
+    with expected_protocol(TSL570Test, protocol) as inst:
+        inst.sweep_routing = "One-way"
+
+
+def test_sweep_routing_getter():
+    protocol = init_sequence + [(b":WAVelength:SWEep:MODe?", b"1\n")]
+    with expected_protocol(TSL570Test, protocol) as inst:
+        # Assuming the sweep_mode getter returns "Continuous One-way" (mapped from 1),
+        # then sweep_routing should be "Continuous".
+        assert inst.sweep_routing == "One-way"
+
+
+def test_sweep_speed_setter():
+    protocol = init_sequence + [(b":WAVelength:SWEep:SPEed 10", None)]
+    with expected_protocol(TSL570Test, protocol) as inst:
+        inst.sweep_speed = 10
+
+
+def test_sweep_speed_getter():
+    protocol = init_sequence + [(b":WAVelength:SWEep:SPEed?", b"10\n")]
+    with expected_protocol(TSL570Test, protocol) as inst:
+        value = inst.sweep_speed
+        assert value == 10
+
+
+def test_sweep_dwell_setter():
+    protocol = init_sequence + [(b":WAVelength:SWEep:DWELl 0.5", None)]
+    with expected_protocol(TSL570Test, protocol) as inst:
+        inst.sweep_dwell = 0.5
+
+
+def test_sweep_dwell_getter():
+    protocol = init_sequence + [(b":WAVelength:SWEep:DWELl?", b"0.5\n")]
+    with expected_protocol(TSL570Test, protocol) as inst:
+        value = inst.sweep_dwell
+        assert abs(value - 0.5) < 1e-12
+
+
+def test_sweep_delay_setter():
+    protocol = init_sequence + [(b":WAVelength:SWEep:DELay 5", None)]
+    with expected_protocol(TSL570Test, protocol) as inst:
+        inst.sweep_delay = 5
+
+
+def test_sweep_delay_getter():
+    protocol = init_sequence + [(b":WAVelength:SWEep:DELay?", b"1.0\n")]
+    with expected_protocol(TSL570Test, protocol) as inst:
+        value = inst.sweep_delay
+        assert abs(value - 1.0) < 1e-12
+
+
+def test_sweep_cycles_setter():
+    protocol = init_sequence + [(b":WAVelength:SWEep:CYCLes 5", None)]
+    with expected_protocol(TSL570Test, protocol) as inst:
+        inst.sweep_cycles = 5
+
+
+def test_sweep_cycles_getter():
+    protocol = init_sequence + [(b":WAVelength:SWEep:CYCLes?", b"5\n")]
+    with expected_protocol(TSL570Test, protocol) as inst:
+        value = inst.sweep_cycles
+        assert value == 5
+
+
+def test_sweep_count():
+    protocol = init_sequence + [(b":WAVelength:SWEep:COUNt?", b"3\n")]
+    with expected_protocol(TSL570Test, protocol) as inst:
+        value = inst.sweep_count
+        assert value == 3
