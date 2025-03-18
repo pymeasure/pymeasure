9e38ca0aee4fd71bab6e6642a71cf72b03d1df9f
Falk3579 <202940408+Falk3579@users.noreply.github.com>
2025-03-18 15:06:11 +0100

    Create test_agilentB2980_with_device.py
    Preparations for test with instrument

---
+++ tests/instruments/agilent/test_agilentB2980_with_device.py
@@ -0,0 +1,39 @@
+#
+# This file is part of the PyMeasure package.
+#
+# Copyright (c) 2013-2025 PyMeasure Developers
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
+
+# Tested using SCPI over USB. call signature:
+# $ pytest test_agilentB2980_with_device.py --device-address ''
+#
+
+
+import pytest
+from pymeasure.instruments.agilent.agilentB2980 import AgilentB2980
+
+
+@pytest.mark.parametrize("freq", [40, 140E6])
+def test_set_start_freq(freq):
+    """ Test Agilent B2980 xyz setter """
+    with expected_protocol(AgilentB2980, [(f"STAR {freq:.0f} HZ", None), ],) as inst:
+        inst.start_frequency = freq
+
