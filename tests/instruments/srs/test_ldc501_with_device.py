b5c2809a194e14870d04fb3d3c204281ad2eabf5
fredgillard <fred.gillard@lumentum.com>
2025-04-16 15:37:04 +0100

    Refactored with_device test

--- tests/instruments/srs/test_ldc501_with_device.py
+++ tests/instruments/srs/test_ldc501_with_device.py
@@ -3,76 +3,86 @@ import pytest
 from pymeasure.errors import Error
 from pymeasure.instruments.srs import LDC500Series
 
+##################################
+# LDC501 device address goes here:
+connected_device_address = "GPIB0::12::INSTR"
+##################################
 
-class TestLDC501:
 
-    ##################################
-    # LDC501 device address goes here:
-    RESOURCE = "GPIB0::12::INSTR"
-    ##################################
+@pytest.fixture(scope="module")
+def ldc501(connected_device_address):
+    instr = LDC500Series(connected_device_address)
+    instr.reset()
+    instr.check_errors()
+    instr.ld_current_range = "LOW"
+    return instr
 
-    INSTR = LDC500Series(RESOURCE)
 
-    INSTR.reset()
-    INSTR.check_errors()
-    INSTR.ld_current_range = "LOW"
+# --- ld current ---
 
-    @pytest.fixture
-    def instr(self):
-        return self.INSTR
 
-    # --- ld current ---
+def test_ld_current_limit_no_error(ldc501):
+    ldc501.ld_current_limit = 100
+    assert ldc501.ld_current_limit == 100
 
-    def test_ld_current_limit_no_error(self, instr):
-        instr.ld_current_limit = 100
-        assert instr.ld_current_limit == 100
 
-    def test_ld_current_limit_out_range(self, instr):
-        with pytest.raises(Error):
-            instr.ld_current_limit = 300
+def test_ld_current_limit_out_range(ldc501):
+    with pytest.raises(Error):
+        ldc501.ld_current_limit = 300
 
-    def test_ld_current_setpoint_no_error(self, instr):
-        instr.ld_current_limit = 100
-        instr.ld_current_setpoint = 25
-        assert instr.ld_current_setpoint == 25
 
-    def test_ld_current_setpoint_out_range(self, instr):
-        instr.ld_current_limit = 100
-        with pytest.raises(Error):
-            instr.ld_current_setpoint = 101
+def test_ld_current_setpoint_no_error(ldc501):
+    ldc501.ld_current_limit = 100
+    ldc501.ld_current_setpoint = 25
+    assert ldc501.ld_current_setpoint == 25
 
-    # --- tec current ---
 
-    def test_tec_current_setpoint_no_error(self, instr):
-        instr.tec_current_limit = 1
-        instr.tec_current_setpoint = 0.5
-        assert instr.tec_current_setpoint == 0.5
+def test_ld_current_setpoint_out_range(ldc501):
+    ldc501.ld_current_limit = 100
+    with pytest.raises(Error):
+        ldc501.ld_current_setpoint = 101
 
-    def test_tec_current_setpoint_out_range(self, instr):
-        instr.tec_current_limit = 1
-        with pytest.raises(Error):
-            instr.tec_current_setpoint = 1.1
 
-    # --- tec temperature ---
+# --- tec current ---
 
-    def test_tec_temperature_limits_no_error(self, instr):
-        instr.tec_temperature_limits = (25, 75)
-        assert instr.tec_temperature_limits == (25, 75)
 
-    def test_tec_temperature_low_limit_out_range(self, instr):
-        with pytest.raises(Error):
-            instr.tec_temperature_low_limit = -300
+def test_tec_current_setpoint_no_error(ldc501):
+    ldc501.tec_current_limit = 1
+    ldc501.tec_current_setpoint = 0.5
+    assert ldc501.tec_current_setpoint == 0.5
 
-    def test_tec_temperature_high_limit_out_range(self, instr):
-        with pytest.raises(Error):
-            instr.tec_temperature_high_limit = 300
 
-    def test_tec_temperature_setpoint_no_error(self, instr):
-        instr.tec_temperature_limits = (25, 75)
-        instr.tec_temperature_setpoint = 55
-        assert instr.tec_temperature_setpoint == 55
+def test_tec_current_setpoint_out_range(ldc501):
+    ldc501.tec_current_limit = 1
+    with pytest.raises(Error):
+        ldc501.tec_current_setpoint = 1.1
 
-    def test_tec_temperature_setpoint_out_range(self, instr):
-        instr.tec_temperature_limits = (25, 75)
-        with pytest.raises(Error):
-            instr.tec_temperature_setpoint = 100
+
+# --- tec temperature ---
+
+
+def test_tec_temperature_limits_no_error(ldc501):
+    ldc501.tec_temperature_limits = (25, 75)
+    assert ldc501.tec_temperature_limits == (25, 75)
+
+
+def test_tec_temperature_low_limit_out_range(ldc501):
+    with pytest.raises(Error):
+        ldc501.tec_temperature_low_limit = -300
+
+
+def test_tec_temperature_high_limit_out_range(ldc501):
+    with pytest.raises(Error):
+        ldc501.tec_temperature_high_limit = 300
+
+
+def test_tec_temperature_setpoint_no_error(ldc501):
+    ldc501.tec_temperature_limits = (25, 75)
+    ldc501.tec_temperature_setpoint = 55
+    assert ldc501.tec_temperature_setpoint == 55
+
+
+def test_tec_temperature_setpoint_out_range(ldc501):
+    ldc501.tec_temperature_limits = (25, 75)
+    with pytest.raises(Error):
+        ldc501.tec_temperature_setpoint = 100
