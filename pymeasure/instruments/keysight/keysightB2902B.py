f61511821aed1a9060277a767fcf5d5e070606af
Carsten Langrock <carsten.langrock@keysight.com>
2025-04-25 17:19:03 -0700

    Added two-channel version of SMU, B2902B.

---
+++ pymeasure/instruments/keysight/keysightB2902B.py
@@ -0,0 +1,48 @@
+#
+# This file is part of the PyMeasure package.
+#
+# Copyright (c) 2013-2025 PyMeasure Developers
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
+
+import logging
+from pymeasure.instruments import Instrument, SCPIMixin, Channel
+from pymeasure.instruments.validators import strict_discrete_set, strict_range
+from .keysightB2901B import KeysightB2901B, SMUChannel
+
+log = logging.getLogger(__name__)
+log.addHandler(logging.NullHandler())
+
+
+class KeysightB2902B(KeysightB2901B):
+    """
+    This represents the Keysight B2902B Source-Measure Unit interface.
+
+    .. code-block:: python
+
+        smu = KeysightB2902B(address)
+
+    """
+
+    def __init__(self, adapter, name="Keysight B2902B Source-Measure Unit", **kwargs):
+        super().__init__(
+            adapter, name, **kwargs)
+
+    channel_2 = Instrument.ChannelCreator(SMUChannel, 2)
