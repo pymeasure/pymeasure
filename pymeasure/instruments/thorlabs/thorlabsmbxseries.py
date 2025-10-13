f20bad6d87f164867f596b2044c67c400cbfa0c6
fredgillard <fred.gillard@lumentum.com>
2025-10-13 11:49:50 +0100

    Maintainer change requests

--- pymeasure/instruments/thorlabs/thorlabsmbxseries.py
+++ pymeasure/instruments/thorlabs/thorlabsmbxseries.py
@@ -24,11 +24,7 @@
 
 from enum import IntEnum
 
-import random
-import colorsys
-from time import time, sleep
-
-from pymeasure.instruments import Instrument, SCPIMixin
+from pymeasure.instruments import Channel, Instrument, SCPIMixin
 from pymeasure.instruments.validators import (
     strict_range,
     strict_discrete_range,
@@ -60,6 +56,173 @@ class RgbPowerMode(IntEnum):
     WHITE = 2
 
 
+class ThorlabsMBXSeriesMZM(Channel):
+    """ThorlabsMBXSeries channel for control of the Mach-Zender Modulator (MZM)."""
+
+    def calibrate(self):
+        """Run a MZM bias calibration."""
+        self.ask("MZM:RESET")
+
+    is_calibrating = Instrument.measurement(
+        "MZM:CALIBRATING?",
+        """Get whether the MZM is currently being calibrated (bool).""",
+        cast=bool,
+    )
+
+    is_stable = Instrument.measurement(
+        "MZM:SETPOINT?",
+        """Get whether the MZM is stable and at the setpoint (bool).""",
+        cast=bool,
+    )
+
+    mode = Instrument.control(
+        "MZM:MODE?",
+        "MZM:MODE: %d",
+        """Control the MZM bias mode (:class:`MzmMode` enum).""",
+    )
+
+    ratio_setpoint = Instrument.control(
+        "MZM:HOLD:RATIO?",
+        "MZM:HOLD:RATIO: %d",
+        """Control the input to output power ratio setpoint
+        when `mzm_mode` is set to `MzmMode.AUTOPOWERPOS` or `MzmMode.AUTOPOWERNEG`
+        (float strictly in range 2.5 to 100).""",
+        validator=strict_range,
+        values=(2.5, 100),
+        get_process=lambda v: v / 100,
+        set_process=lambda v: round(v * 100),
+    )
+
+    voltage_setpoint = Instrument.control(
+        "MZM:HOLD:VOLTAGE?",
+        "MZM:HOLD:VOLTAGE: %d",
+        """Control the MZM voltage setpoint, in V, when `mzm_mode` is set to MzmMode.MANUAL
+        (float strictly in range -10 to 10).""",
+        validator=strict_range,
+        values=(-10, 10),
+        get_process=lambda v: v / 1000,
+        set_process=lambda v: round(v * 1000),
+    )
+
+    voltage = Instrument.measurement(
+        "MZM:VOLTAGE?",
+        """Measure the current MZM bias voltage, in V (float).""",
+    )
+
+    power = Instrument.measurement(
+        "MZM:TAP:MW?",
+        """Measure the MZM output optical power, in mW (float).""",
+    )
+
+
+class ThorlabsMBXSeriesVOA(Channel):
+    """ThorlabsMBXSeries channel for control of the Variable Optical Attenuator (VOA)."""
+
+    enabled = Instrument.control(
+        "VOA:POWER?",
+        "VOA:POWER: %d",
+        """Control whether the VOA is enabled (bool).""",
+        validator=strict_discrete_set,
+        values={True: 1, False: 0},
+        map_values=True,
+    )
+
+    is_stable = Instrument.measurement(
+        "VOA:SETPOINT?",
+        """Get whether the VOA attenuation is within 0.1dB of the setpoint (bool).""",
+        cast=bool,
+    )
+
+    @property
+    def mode(self):
+        """Control the VOA mode (:class:`VoaMode` enum)."""
+        return MzmMode(int(self.ask("VOA:MODE?")))
+
+    @mode.setter
+    def mode(self, v):
+        # Ask as "VOA:MODE: {0 or 1}" returns "1" on receipt of command
+        self.ask(f"VOA:MODE: {v.value}")
+
+    attenuation_setpoint = Instrument.control(
+        "VOA:ATTEN?",
+        "VOA:ATTEN: %g",
+        """Control the VOA optical attenuation setpoint, in dB
+        (float strictly in range 0.5 to 20)""",
+        validator=strict_range,
+        values=(0.5, 20),
+    )
+
+    attenuation = Instrument.measurement(
+        "VOA:MEASURED?",
+        """Measure the VOA optical attenuation, in dB (float).""",
+    )
+
+    attenuation_error = Instrument.measurement(
+        "VOA:ERROR?",
+        """Measure the difference between the setpoint and the measured value
+        for the VOA optical attenuation, in dB (float).""",
+    )
+
+    power_setpoint = Instrument.control(
+        "VOA:OUTPUT:MW?",
+        "VOA:OUTPUT:MW: %g",
+        """Control the VOA output power setpoint, in mW (float strictly in range 0.01 to 100).""",
+        validator=strict_range,
+        values=(0.01, 100),
+    )
+
+    power = Instrument.measurement(
+        "VOA:TAP:MW?",
+        """Measure the VOA output optical power, in mW (float).""",
+    )
+
+
+class ThorlabsMBXSeriesRGB(Channel):
+    """ThorlabsMBXSeries channel for control of the asethetic RGB under-chassis lighting."""
+
+    mode = Instrument.control(
+        "RGB:POWER:?",
+        "RGB:POWER: %d",
+        """Control the under-chassis LED accent lighting mode (:class:`RgbPowerMode` enum).""",
+    )
+
+    red = Instrument.control(
+        "RGB:RED?",
+        "RGB:RED: %d",
+        """Control the brightness of the red under-chassis accent lighting LEDs
+        (int, strictly in range 0 to 100).""",
+        validator=lambda v, vs: strict_discrete_range(v, vs, 1),
+        values=(0, 100),
+    )
+
+    green = Instrument.control(
+        "RGB:GREEN?",
+        "RGB:GREEN: %d",
+        """Control the brightness of the green under-chassis accent lighting LEDs
+        (int, strictly in range 0 to 100).""",
+        validator=lambda v, vs: strict_discrete_range(v, vs, 1),
+        values=(0, 100),
+    )
+
+    blue = Instrument.control(
+        "RGB:BLUE?",
+        "RGB:BLUE: %d",
+        """Control the brightness of the blue under-chassis accent lighting LEDs
+        (int, strictly in range 0 to 100).""",
+        validator=lambda v, vs: strict_discrete_range(v, vs, 1),
+        values=(0, 100),
+    )
+
+    white = Instrument.control(
+        "RGB:BLUE?",
+        "RGB:BLUE: %d",
+        """Control the brightness of the white under-chassis accent lighting
+        (int, strictly in range 0 to 100).""",
+        validator=lambda v, vs: strict_discrete_range(v, vs, 1),
+        values=(0, 100),
+    )
+
+
 class ThorlabsMBXSeries(SCPIMixin, Instrument):
     """Represents Thorlabs MBX Series Modulator Bias Controllers.
 
@@ -74,199 +237,11 @@ class ThorlabsMBXSeries(SCPIMixin, Instrument):
 
     def __init__(self, adapter, name="ThorlabsMBXSeries modulator bias controller", **kwargs):
 
-        kwargs.setdefault("baud_rate", 115200)
         kwargs.setdefault("timeout", 1000)
-        kwargs.setdefault("write_termination", "\r")
-        kwargs.setdefault("read_termination", "\r")
-        super().__init__(adapter, name, **kwargs)
+        super().__init__(
+            adapter, name, baud_rate=115200, write_termination="\r", read_termination="\r"
+        )
 
-    # === MACH-ZENDER INTERFEROMETER (MZM) ===
-
-    def calibrate_mzm(self):
-        """Run a MZM bias calibration."""
-        self.ask("MZM:RESET")
-
-    mzm_calibrating = Instrument.measurement(
-        "MZM:CALIBRATING?",
-        """Get whether the MZM is currently being calibrated (bool).""",
-        cast=bool,
-    )
-
-    mzm_stable = Instrument.measurement(
-        "MZM:SETPOINT?",
-        """Get whether the MZM is stable and at the setpoint (bool).""",
-        cast=bool,
-    )
-
-    mzm_mode = Instrument.control(
-        "MZM:MODE?",
-        "MZM:MODE: %d",
-        """Control the MZM bias mode (MzmMode enum).""",
-        get_process=lambda v: MzmMode(v),
-        set_process=lambda v: v.value,
-    )
-
-    mzm_ratio_setpoint = Instrument.control(
-        "MZM:HOLD:RATIO?",
-        "MZM:HOLD:RATIO: %d",
-        """Control the input to output power ratio setpoint
-        when `mzm_mode` is set to `MzmMode.AUTOPOWERPOS` or `MzmMode.AUTOPOWERNEG`
-        (float strictly in range 2.5 to 100).""",
-        validator=strict_range,
-        values=(2.5, 100),
-        get_process=lambda v: v / 100,
-        set_process=lambda v: round(v * 100),
-    )
-
-    mzm_voltage_setpoint = Instrument.control(
-        "MZM:HOLD:VOLTAGE?",
-        "MZM:HOLD:VOLTAGE: %d",
-        """Control the MZM voltage setpoint, in V, when `mzm_mode` is set to MzmMode.MANUAL
-        (float strictly in range -10 to 10).""",
-        validator=strict_range,
-        values=(-10, 10),
-        get_process=lambda v: v / 1000,
-        set_process=lambda v: round(v * 1000),
-    )
-
-    mzm_voltage = Instrument.measurement(
-        "MZM:VOLTAGE?",
-        """Measure the current MZM bias voltage, in V (float).""",
-    )
-
-    mzm_power = Instrument.measurement(
-        "MZM:TAP:MW?",
-        """Measure the MZM output optical power, in mW (float).""",
-    )
-
-    # === VARIABLE OPTICAL ATTENUATOR (VOA) ===
-
-    voa_enabled = Instrument.control(
-        "VOA:POWER?",
-        "VOA:POWER: %d",
-        """Control whether the VOA is enabled (bool).""",
-        validator=strict_discrete_set,
-        values={True: 1, False: 0},
-        map_values=True,
-    )
-
-    voa_stable = Instrument.measurement(
-        "VOA:SETPOINT?",
-        """Get whether the VOA attenuation is withing 0.1dB of the setpoint (bool).""",
-        cast=bool,
-    )
-
-    # Handled differently as "VOA:MODE: {0 or 1}" returns "1" on receipt of command
-
-    @property
-    def voa_mode(self):
-        """Control the VOA mode (VoaMode enum)."""
-        return MzmMode(int(self.ask("VOA:MODE?")))
-
-    @voa_mode.setter
-    def voa_mode(self, v):
-        self.ask(f"VOA:MODE: {v.value}")
-
-    voa_attenuation_setpoint = Instrument.control(
-        "VOA:ATTEN?",
-        "VOA:ATTEN: %g",
-        """Control the VOA optical attenuation setpoint, in dB
-        (float strictly in range 0.5 to 20)""",
-        validator=strict_range,
-        values=(0.5, 20),
-    )
-
-    voa_attenuation = Instrument.measurement(
-        "VOA:MEASURED?",
-        """Measure the VOA optical attenuation, in dB (float).""",
-    )
-
-    voa_attenuation_error = Instrument.measurement(
-        "VOA:ERROR?",
-        """Measure the difference between the setpoint and the measured value
-        for the VOA optical attenuation, in dB (float).""",
-    )
-
-    voa_power_setpoint = Instrument.control(
-        "VOA:OUTPUT:MW?",
-        "VOA:OUTPUT:MW: %g",
-        """Control the VOA output power setpoint, in mW.""",
-        validator=strict_range,
-        values=(0.01, 100),
-    )
-
-    voa_power = Instrument.measurement(
-        "VOA:TAP:MW?",
-        """Measure the VOA output optical power, in mW (float).""",
-    )
-
-    # === RGB CONTROL ===
-    # This section is excluded from coverage because it is purely aesthetic
-
-    rgb_power = Instrument.control(
-        "RGB:POWER:?",
-        "RGB:POWER: %d",
-        """Control the under-chassis LED accent lighting mode (RgbPowerMode enum).""",
-        get_process=lambda v: RgbPowerMode(v),
-        set_process=lambda v: v.value,
-    )  # pragma: no cover
-
-    rgb_red = Instrument.control(
-        "RGB:RED?",
-        "RGB:RED: %d",
-        """Control the brightness of the red under-chassis accent lighting LEDs
-        (int, strictly in range 0 to 100).""",
-        validator=lambda v, vs: strict_discrete_range(v, vs, 1),
-        values=(0, 100),
-    )  # pragma: no cover
-
-    rgb_green = Instrument.control(
-        "RGB:GREEN?",
-        "RGB:GREEN: %d",
-        """Control the brightness of the green under-chassis accent lighting LEDs
-        (int, strictly in range 0 to 100).""",
-        validator=lambda v, vs: strict_discrete_range(v, vs, 1),
-        values=(0, 100),
-    )  # pragma: no cover
-
-    rgb_blue = Instrument.control(
-        "RGB:BLUE?",
-        "RGB:BLUE: %d",
-        """Control the brightness of the blue under-chassis accent lighting LEDs
-        (int, strictly in range 0 to 100).""",
-        validator=lambda v, vs: strict_discrete_range(v, vs, 1),
-        values=(0, 100),
-    )  # pragma: no cover
-
-    rgb_white = Instrument.control(
-        "RGB:BLUE?",
-        "RGB:BLUE: %d",
-        """Control the brightness of the white under-chassis accent lighting
-        (int, strictly in range 0 to 100).""",
-        validator=lambda v, vs: strict_discrete_range(v, vs, 1),
-        values=(0, 100),
-    )  # pragma: no cover
-
-    def disco(self, duration=10, pause=0.5):  # pragma: no cover
-        self.rgb_power = RgbPowerMode.RGB
-
-        t0 = time()
-        while time() < t0 + duration:
-            r, g, b = colorsys.hsv_to_rgb(random.random(), 1, 1)
-            self.rgb_red, self.rgb_green, self.rgb_blue = [round(x * 100) for x in (r, g, b)]
-            sleep(pause)
-
-        self.rgb_power = RgbPowerMode.OFF
-
-    def rainbow(self, duration=10, pause=0):  # pragma: no cover
-        self.rgb_power = RgbPowerMode.RGB
-
-        t0 = time()
-        h = 0
-        while time() < t0 + duration:
-            r, g, b = colorsys.hsv_to_rgb(h % 600 / 600, 1, 1)
-            self.rgb_red, self.rgb_green, self.rgb_blue = [round(x * 100) for x in (r, g, b)]
-            h += 1
-            sleep(pause)
-
-        self.rgb_power = RgbPowerMode.OFF
+    mzm = Instrument.ChannelCreator(ThorlabsMBXSeriesMZM, "")
+    voa = Instrument.ChannelCreator(ThorlabsMBXSeriesVOA, "")
+    rgb = Instrument.ChannelCreator(ThorlabsMBXSeriesRGB, "")
--- tests/instruments/thorlabs/test_thorlabsmbxseries.py
+++ tests/instruments/thorlabs/test_thorlabsmbxseries.py
@@ -9,136 +9,136 @@ from pymeasure.instruments.thorlabs.thorlabsmbxseries import (
 def test_calibrate_mzm():
     protocol = [("MZM:RESET", b"1")]
     with expected_protocol(ThorlabsMBXSeries, protocol) as inst:
-        inst.calibrate_mzm()
+        inst.mzm.calibrate()
 
 
-def test_mzm_calibrating():
+def test_mzm_is_calibrating():
     protocol = [("MZM:CALIBRATING?", b"0")]
     with expected_protocol(ThorlabsMBXSeries, protocol) as inst:
-        assert inst.mzm_calibrating is False
+        assert inst.mzm.is_calibrating is False
 
 
-def test_mzm_stable():
+def test_mzm_is_stable():
     protocol = [("MZM:SETPOINT?", b"1")]
     with expected_protocol(ThorlabsMBXSeries, protocol) as inst:
-        assert inst.mzm_stable is True
+        assert inst.mzm.is_stable is True
 
 
 def test_mzm_mode_setter():
     protocol = [("MZM:MODE: 3", None)]
     with expected_protocol(ThorlabsMBXSeries, protocol) as inst:
-        inst.mzm_mode = MzmMode.AUTOQUADPOS
+        inst.mzm.mode = MzmMode.AUTOQUADPOS
 
 
 def test_mzm_mode_getter():
     protocol = [("MZM:MODE?", b"5")]
     with expected_protocol(ThorlabsMBXSeries, protocol) as inst:
-        assert inst.mzm_mode == MzmMode.HOLDQUADPOS
+        assert inst.mzm.mode == MzmMode.HOLDQUADPOS
 
 
 def test_mzm_ratio_setpoint_setter():
     protocol = [("MZM:HOLD:RATIO: 500", None)]
     with expected_protocol(ThorlabsMBXSeries, protocol) as inst:
-        inst.mzm_ratio_setpoint = 5.004
+        inst.mzm.ratio_setpoint = 5.004
 
 
 def test_mzm_ratio_setpoint_getter():
     protocol = [("MZM:HOLD:RATIO?", b"500")]
     with expected_protocol(ThorlabsMBXSeries, protocol) as inst:
-        assert inst.mzm_ratio_setpoint == 5.0
+        assert inst.mzm.ratio_setpoint == 5.0
 
 
 def test_mzm_voltage_setpoint_setter():
     protocol = [("MZM:HOLD:VOLTAGE: 5000", None)]
     with expected_protocol(ThorlabsMBXSeries, protocol) as inst:
-        inst.mzm_voltage_setpoint = 5.0004
+        inst.mzm.voltage_setpoint = 5.0004
 
 
 def test_mzm_voltage_setpoint_getter():
     protocol = [("MZM:HOLD:VOLTAGE?", b"5000")]
     with expected_protocol(ThorlabsMBXSeries, protocol) as inst:
-        assert inst.mzm_voltage_setpoint == 5
+        assert inst.mzm.voltage_setpoint == 5
 
 
 def test_mzm_voltage():
     protocol = [("MZM:VOLTAGE?", b"5.42")]
     with expected_protocol(ThorlabsMBXSeries, protocol) as inst:
-        assert inst.mzm_voltage == 5.42
+        assert inst.mzm.voltage == 5.42
 
 
 def test_mzm_power():
     protocol = [("MZM:TAP:MW?", b"1.23")]
     with expected_protocol(ThorlabsMBXSeries, protocol) as inst:
-        assert inst.mzm_power == 1.23
+        assert inst.mzm.power == 1.23
 
 
 def test_voa_enabled_setter():
     protocol = [("VOA:POWER: 1", None)]
     with expected_protocol(ThorlabsMBXSeries, protocol) as inst:
-        inst.voa_enabled = True
+        inst.voa.enabled = True
 
 
 def test_voa_enabled_getter():
     protocol = [("VOA:POWER?", b"0")]
     with expected_protocol(ThorlabsMBXSeries, protocol) as inst:
-        assert inst.voa_enabled is False
+        assert inst.voa.enabled is False
 
 
-def test_voa_stable():
+def test_voa_is_stable():
     protocol = [("VOA:SETPOINT?", b"0")]
     with expected_protocol(ThorlabsMBXSeries, protocol) as inst:
-        assert inst.voa_stable is False
+        assert inst.voa.is_stable is False
 
 
 def test_voa_mode_setter():
     protocol = [("VOA:MODE: 0", b"1")]
     with expected_protocol(ThorlabsMBXSeries, protocol) as inst:
-        inst.voa_mode = VoaMode.ATTENUATION
+        inst.voa.mode = VoaMode.ATTENUATION
 
 
 def test_voa_mode_getter():
     protocol = [("VOA:MODE?", b"1")]
     with expected_protocol(ThorlabsMBXSeries, protocol) as inst:
-        assert inst.voa_mode == VoaMode.POWER
+        assert inst.voa.mode == VoaMode.POWER
 
 
 def test_voa_attenuation_setpoint_setter():
     protocol = [("VOA:ATTEN: 5", None)]
     with expected_protocol(ThorlabsMBXSeries, protocol) as inst:
-        inst.voa_attenuation_setpoint = 5
+        inst.voa.attenuation_setpoint = 5
 
 
 def test_voa_attenuation_setpoint_getter():
     protocol = [("VOA:ATTEN?", b"5")]
     with expected_protocol(ThorlabsMBXSeries, protocol) as inst:
-        assert inst.voa_attenuation_setpoint == 5
+        assert inst.voa.attenuation_setpoint == 5
 
 
 def test_voa_attenuation():
     protocol = [("VOA:MEASURED?", b"5")]
     with expected_protocol(ThorlabsMBXSeries, protocol) as inst:
-        assert inst.voa_attenuation == 5
+        assert inst.voa.attenuation == 5
 
 
 def test_voa_attenuation_error():
     protocol = [("VOA:ERROR?", b"0.1")]
     with expected_protocol(ThorlabsMBXSeries, protocol) as inst:
-        assert inst.voa_attenuation_error == 0.1
+        assert inst.voa.attenuation_error == 0.1
 
 
 def test_voa_power_setpoint_setter():
     protocol = [("VOA:OUTPUT:MW: 5", None)]
     with expected_protocol(ThorlabsMBXSeries, protocol) as inst:
-        inst.voa_power_setpoint = 5
+        inst.voa.power_setpoint = 5
 
 
 def test_voa_power_setpoint_getter():
     protocol = [("VOA:OUTPUT:MW?", b"5")]
     with expected_protocol(ThorlabsMBXSeries, protocol) as inst:
-        assert inst.voa_power_setpoint == 5
+        assert inst.voa.power_setpoint == 5
 
 
 def test_voa_power():
     protocol = [("VOA:TAP:MW?", b"5")]
     with expected_protocol(ThorlabsMBXSeries, protocol) as inst:
-        assert inst.voa_power == 5
+        assert inst.voa.power == 5
