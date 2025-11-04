355bbba5d527f747a2abe7d7904ee293ff90228f
Falk3579 <202940408+Falk3579@users.noreply.github.com>
2025-11-04 14:47:23 +0100

    Update parameters.py

--- pymeasure/experiment/parameters.py
+++ pymeasure/experiment/parameters.py
@@ -328,7 +328,7 @@ class VectorParameter(Parameter):
                                  " denoted by square brackets if initializing"
                                  " by string.")
             raw_list = value[1:-1].split(",")
-        elif isinstance(value, (list, tuple)):
+        elif isinstance(value, (list, tuple, np.ndarray)):
             raw_list = value
         else:
             raise ValueError("VectorParameter given undesired value of "
