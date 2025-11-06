55c7ab846e1fe335fed962e04fa50ea58af9501f
Nathan Isaac <nat111@hotmail.nl>
2025-11-06 17:37:21 +0100

    added granville+phillips file

---
+++ pymeasure/instruments/granville_phillips/GP350.py
@@ -0,0 +1,73 @@
+# This file is part of the PyMeasure package.
+#
+# Copyright (c) 2013-2025 PyMeasure Developers
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+ 
+from pymeasure.instruments import Instrument, Channel
+
+
+class GP_PressureChannel(Channel):
+    """
+    Channel of the GP350UHV vacuum sensor for pressure measurement.
+
+    Each channel corresponds to a filament or display output of the sensor.
+    """
+
+    pressure = Instrument.measurement(
+        "#RD{ch}",
+        """Measure the current pressure in mbar (float).""",
+        preprocess_reply=lambda reply: reply.lstrip("* ").strip(),
+        get_process=lambda r: float("nan") if float(r) >= 9.90e9 else float(r),
+    )
+
+
+class GP350UHV(Instrument):
+    """
+    Representation of the GP350UHV Vacuum Sensor.
+
+    Provides access to multiple pressure channels corresponding to filaments
+    and display outputs. Each channel can be queried for its current pressure
+    in mbar.
+    
+    Parameters
+        ----------
+        adapter : Adapter
+            Communication adapter (e.g., SerialAdapter, VISAAdapter).
+        name : str, optional
+            Name of the instrument, default is "GP350UHV Vacuum Sensor".
+        **kwargs
+            Additional keyword arguments passed to the Instrument base class.
+    
+    """
+
+    def __init__(self, adapter, name="GP350UHV Vacuum Sensor", **kwargs):
+        super().__init__(adapter, name, includeSCPI=False, **kwargs)
+
+    # Channels 1 and 2 correspond to the ionization filaments,
+    # while A and B represent the display lines on the GP350 gauge.
+    
+    filament_1 = Instrument.ChannelCreator(GP_PressureChannel, "1")
+    
+    filament_2 = Instrument.ChannelCreator(GP_PressureChannel, "2")
+    
+    display_A = Instrument.ChannelCreator(GP_PressureChannel, "A")
+    
+    display_B = Instrument.ChannelCreator(GP_PressureChannel, "B")
+
