b33eba1c46aa21762bd954e33a82d8533b9c5265
WiktorJJ <jankowski@insa-toulouse.fr>
2025-11-28 17:40:30 +0100

    ajout de set_type
    En plus de ajouter les isinstance (checking for strings) pour les check_type

--- pymeasure/instruments/smaract/utils.py
+++ pymeasure/instruments/smaract/utils.py
@@ -1,14 +1,20 @@
 from typing import Union
 
 from pint import Quantity as Q_
+from pint.errors import DimensionalityError
 
-def set_type(reponse_raw : str, index : str, unit: str = 'Hz' ):
-    return
+def set_type(response_raw : str, index : int, unit: str = 'Hz' ):
+    return Q_(response_raw[index:], unit)
 
 """il va falloir le genraliser avec tous les types de donnés"""
-def check_type(input: Union[int, Q_], unit='Hz'):
+def check_type(input: Union[str, int, Q_], unit='Hz'):
+    if isinstance(input, str):
+        input = Q_(input)
     if isinstance(input, Q_):
-        return input.to(unit).magnitude
+        try:
+            return input.to(unit).magnitude
+        except DimensionalityError:
+            raise TypeError('Invalid input for the instrument')
     elif isinstance(input, int):
         return input
     else:
