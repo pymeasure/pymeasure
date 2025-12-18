e3797f11667dfdb79acea0383ea53b903cc27caa
S. C. Chiu <jerry31205@gmail.com>
2025-12-18 15:13:14 +0800

    Add test for enable_filter

--- tests/instruments/keithley/test_keithley2000.py
+++ tests/instruments/keithley/test_keithley2000.py
@@ -77,3 +77,13 @@ def test_measure_voltage():
          ],
     ) as inst:
         inst.measure_voltage(max_voltage=300, ac=True)
+
+def test_enable_filter():
+    with expected_protocol(
+        Keithley2000,
+        [(":SENS:VOLT:AC:AVER:STAT 1", None),
+         (":SENS:VOLT:AC:AVER:TCON repeat", None),
+         (":SENS:VOLT:AC:AVER:COUN 10", None),
+         ],
+    ) as inst:
+        inst.enable_filter(mode='voltage ac', type='repeat', count=10)
