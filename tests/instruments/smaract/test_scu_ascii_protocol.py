e2ec3adb5200b10fe22307cd3d99eaba2e689721
WiktorJJ <jankowski@insa-toulouse.fr>
2026-03-27 14:36:57 +0100

    cleaning files

--- tests/instruments/smaract/test_scu_ascii_protocol.py
+++ tests/instruments/smaract/test_scu_ascii_protocol.py
@@ -1,61 +1,188 @@
-from pymeasure.generator import Generator
+import pytest
+
+from pymeasure.test import expected_protocol
 from pymeasure.instruments.smaract.scu_ascii import SmarActSCULinear
 from pint import Quantity as Q_
 
-# Create generator
-gen = Generator()
 
-# Instantiate instrument THROUGH generator
-inst = gen.instantiate(
-    SmarActSCULinear,
-    "ASRL3::INSTR",   # your device address
-    "smaract",
-    adapter_kwargs={"baud_rate": 9600}
-)
+CHANNELS = ['0']
 
-# -----------------------
-# Instrument-level tests
-# -----------------------
-inst.model
-inst.serial_nb
 
-# -----------------------
-# Channel-level tests
-# -----------------------
-ch = inst.channel0
+def test_init():
+    with expected_protocol(
+            SmarActSCULinear,
+            [],
+    ):
+        pass  # Verify the expected communication.
 
-# Configuration
-ch.frequency_max = Q_(500, "Hz")
-ch.frequency_max
+@pytest.mark.parametrize("channel", CHANNELS)
+def test_channel_frequency_max_setter(channel):
+    with expected_protocol(
+            SmarActSCULinear,
+            [(f':SCLF{channel}F500'.encode(), None)],
+    ) as inst:
+        inst.channels[channel].frequency_max = Q_(500, 'Hz')
 
-ch.safe_direction = "up"
-ch.safe_direction
 
-ch.safe_direction = "down"
-ch.safe_direction
+def test_channel0_frequency_max_getter():
+    with expected_protocol(
+            SmarActSCULinear,
+            [(b':GCLF0', b':CLF0F500')],
+    ) as inst:
+        assert inst.channel0.frequency_max.magnitude == 500
 
-# Position / status
-ch.get_position()
-ch.get_positioner_type()
 
-# Motion commands
-ch.move_abs(Q_(100, "um"))
-ch.move_rel(Q_(50, "um"))
+@pytest.mark.parametrize("comm_pairs, value", (
+    ([(b':SSD0D1', None)],
+     'up'),
+    ([(b':SSD0D0', None)],
+     'down'),
+))
+def test_channel0_safe_direction_setter(comm_pairs, value):
+    with expected_protocol(
+            SmarActSCULinear,
+            comm_pairs,
+    ) as inst:
+        inst.channel0.safe_direction = value
 
-ch.move_steps_up(100)
-ch.move_steps_down(100)
 
-ch.move_to_ref()
-ch.move_to_end_up()
-ch.move_to_end_down()
+@pytest.mark.parametrize("comm_pairs, value", (
+    ([(b':GSD0', b':SD0D1')],
+     'up'),
+    ([(b':GSD0', b':SD0D0')],
+     'down'),
+))
+def test_channel0_safe_direction_getter(comm_pairs, value):
+    with expected_protocol(
+            SmarActSCULinear,
+            comm_pairs,
+    ) as inst:
+        assert inst.channel0.safe_direction == value
 
-ch.stop()
 
-# Calibration / sensor
-ch.check_sensor_present()
-ch.calibrate_sensor()
+def test_model_getter():
+    with expected_protocol(
+            SmarActSCULinear,
+            [(b':I', b':ISmarAct CU-1D')],
+    ) as inst:
+        assert inst.model == 'SmarAct CU-1D'
 
-# -----------------------
-# Write test file
-# -----------------------
-gen.write_file("generated_scu_protocol_tests.py")
+
+def test_serial_nb_getter():
+    with expected_protocol(
+            SmarActSCULinear,
+            [(b':GID', b':ID722998302')],
+    ) as inst:
+        assert inst.serial_nb == '722998302'
+
+
+def test_ask():
+    with expected_protocol(
+            SmarActSCULinear,
+            [(b':M0', b':M0S')],
+    ) as inst:
+        assert inst.ask(*(':M0',), ) == ':M0S'
+
+
+def test_channel0_calibrate_sensor():
+    with expected_protocol(
+            SmarActSCULinear,
+            [(b':CS0', None),
+             (b':M0', b':M0C')],
+    ) as inst:
+        assert inst.channel0.calibrate_sensor() == ':M0C'
+
+
+def test_channel0_check_sensor_present():
+    with expected_protocol(
+            SmarActSCULinear,
+            [(b':GSP0', b':SP0P')],
+    ) as inst:
+        assert inst.channel0.check_sensor_present() is True
+
+
+def test_channel0_get_position():
+    with expected_protocol(
+            SmarActSCULinear,
+            [(b':GP0', b':P0P-0.5')],
+    ) as inst:
+        assert inst.channel0.get_position() == Q_(-0.5, 'um')
+
+
+def test_channel0_get_positioner_type():
+    with expected_protocol(
+            SmarActSCULinear,
+            [(b':GST0', b':ST0T21')],
+    ) as inst:
+        assert inst.channel0.get_positioner_type() == ':ST0T21'
+
+
+def test_channel0_move_abs():
+    with expected_protocol(
+            SmarActSCULinear,
+            [(b':MPA0P100', None)],
+    ) as inst:
+        inst.channel0.move_abs((Q_(100, 'um')))
+
+
+def test_channel0_move_rel():
+    with expected_protocol(
+            SmarActSCULinear,
+            [(b':MPR0P50', None)],
+    ) as inst:
+        inst.channel0.move_rel((Q_(50, 'um')))
+
+
+def test_channel0_move_steps_down():
+    with expected_protocol(
+            SmarActSCULinear,
+            [(b':D0F260A300S100', None)],
+    ) as inst:
+        assert inst.channel0.move_steps_down(*(100,), ) is None
+
+
+def test_channel0_move_steps_up():
+    with expected_protocol(
+            SmarActSCULinear,
+            [(b':U0F260A300S100', None)],
+    ) as inst:
+        assert inst.channel0.move_steps_up(*(100,), ) is None
+
+
+def test_channel0_move_to_end_down():
+    with expected_protocol(
+            SmarActSCULinear,
+            [(b':MES0DD', None)],
+    ) as inst:
+        assert inst.channel0.move_to_end_down() is None
+
+
+def test_channel0_move_to_end_up():
+    with expected_protocol(
+            SmarActSCULinear,
+            [(b':MES0DU', None)],
+    ) as inst:
+        assert inst.channel0.move_to_end_up() is None
+
+
+def test_channel0_move_to_ref():
+    with expected_protocol(
+            SmarActSCULinear,
+            [(b':MTR0H0Z1', None)],
+    ) as inst:
+        assert inst.channel0.move_to_ref() is None
+
+
+def test_channel0_stop():
+    with expected_protocol(
+            SmarActSCULinear,
+            [(b':S0', None)],
+    ) as inst:
+        assert inst.channel0.stop() is None
+
+def test_reset():
+    with expected_protocol(
+            SmarActSCULinear,
+        [(b':R', None)],
+    ) as inst:
+        assert inst.reset() is None
--- tests/instruments/smaract/test_scu_ascii_with_device.py
+++ tests/instruments/smaract/test_scu_ascii_with_device.py
@@ -26,259 +26,137 @@ def smaractascii(connected_device_address: str = "ASRL3::INSTR"):
     # ensure the device is in a defined state, e.g. by resetting it.
     return instr
 
+class TestSCUIdentificate:
 
+    @pytest.mark.parametrize("channel", CHANNELS)
+    def test_model(self, smaractascii, channel):
+        model = smaractascii.model
+        assert isinstance(model, str)
+        assert len(model) > 0
 
-@pytest.mark.parametrize("channel,issensor", zip(CHANNELS, SENSOR))
-def test_sensor(smaractascii, channel, issensor):
-    """Simple test to verify if it has a sensor."""
-    assert smaractascii.channels[channel].check_sensor_present() is issensor
+    @pytest.mark.parametrize("channel", CHANNELS)
+    def test_serial_number(self, smaractascii, channel):
+        serial = smaractascii.serial_nb
+        assert isinstance(serial, str)
+        assert len(serial) > 0
 
-@pytest.mark.parametrize("channel", CHANNELS)
-@pytest.mark.parametrize("test_value, expected_outcome", [
-    (9600, 9600),
-    (1500, pyvisa.VisaIOError),
-])
-def test_baudrate(smaractascii, test_value, expected_outcome, channel):
-    if expected_outcome == pyvisa.VisaIOError:
-        with pytest.raises(pyvisa.VisaIOError):
-            smaractascii.set_baudrate(test_value)
-    else:
-        smaractascii.set_baudrate(test_value)
+    @pytest.mark.parametrize("channel,issensor", zip(CHANNELS, SENSOR))
+    def test_sensor(self, smaractascii, channel, issensor):
+        assert smaractascii.channels[channel].check_sensor_present() is issensor
 
+    @pytest.mark.parametrize("channel,issensor", zip(CHANNELS, SENSOR))
+    def test_calibration(self,smaractascii,channel,issensor):
+        status = smaractascii.channels[channel].calibrate_sensor()
+        assert status == ':M0C'
 
-""" This tests the frequency and amplitude values for a given instrument, assuming that the frequency 
-    and amplitude will be similar on multiple channels if there exist more than one channel"""
+    @pytest.mark.parametrize("channel", CHANNELS)
+    def test_set_zero(self,smaractascii, channel):
+        pos = smaractascii.channels[channel].get_position()
+        smaractascii.channels[channel].set_zero_pos()
+        assert smaractascii.channels[channel].set_zero_pos() != pos
 
-@pytest.mark.parametrize(" test_value, expected_outcome", [
-    # --- FREQUENCY TESTS
-    (Q_(500, 'Hz'), Q_(500, 'Hz')),  # Valid
-    ( Q_(0.5, 'Hz'), ValueError),  # Invalid: Too low
-    ( Q_(20000, 'Hz'), ValueError)]) # Invalid: Too high
+class TestSCUConfiguration:
 
-def test_freq(smaractascii, test_value, expected_outcome):
-    # We expect a ValueError (Negative Testing)
-    if expected_outcome == ValueError:
+    def test_frequency(self, smaractascii):
+        smaractascii.frequency = Q_(500, "Hz")
+        assert smaractascii.frequency == Q_(500, "Hz")
+
+    def test_amplitude(self, smaractascii):
+        smaractascii.amplitude = Q_(300, "dV")
+        assert smaractascii.amplitude == Q_(300, "dV")
+
+    def test_invalid_frequency(self, smaractascii):
         with pytest.raises(ValueError):
-            smaractascii.frequency = test_value
-    else:
-        smaractascii.frequency = test_value
-        assert smaractascii.frequency == expected_outcome
+            smaractascii.frequency = Q_(20000, "Hz")
 
-@pytest.mark.parametrize(" test_value, expected_outcome", [
-    # --- AMPLITUDE TESTS
-    ( Q_(500, 'dV'), Q_(500, 'dV')),  # Valid
-    ( Q_(100, 'dV'), ValueError),  # Invalid: Too low
-    (Q_(1300, 'dV'), ValueError),  # Invalid: Too high
-])
-def test_ampli(smaractascii, test_value, expected_outcome):
-    # We expect a ValueError (Negative Testing)
-    if expected_outcome == ValueError:
+    def test_invalid_amplitude(self, smaractascii):
         with pytest.raises(ValueError):
-                smaractascii.amplitude = test_value
+            smaractascii.amplitude = Q_(50, "dV")
 
-    # We expect it to succeed
-    else:
-        smaractascii.amplitude = test_value
-        assert smaractascii.amplitude == expected_outcome
+class TestSCUChannel:
 
-@pytest.mark.parametrize("test_steps, expected_outcome", [
-    (1000, 1000),  # Valid: Normal steps
-    (0, ValueError),  # Invalid: Too low
-    (40000, ValueError),  # Invalid: Too high
-])
-def test_check_steps_method(smaractascii, test_steps, expected_outcome):
-    """
-    Parametrized test specifically for the check_steps method.
-    """
-    #  We expect an error
-    if expected_outcome == ValueError:
-        with pytest.raises(ValueError):
-            smaractascii.check_steps(test_steps)
+    @pytest.mark.parametrize("channel", CHANNELS)
+    def test_safe_direction(self, smaractascii,channel):
+        smaractascii.channels[channel].safe_direction = 'up'
+        assert smaractascii.channels[channel].safe_direction == 'up'
 
-    #  We expect a valid return value
-    else:
-        result = smaractascii.check_steps(test_steps)
-        assert result == expected_outcome
-
-@pytest.mark.parametrize("channel", CHANNELS)
-def test_get_position(smaractascii, channel):
-    pos = smaractascii.channels[channel].get_position()
-
-    # we verify we do recive a(Quantity)
-    assert isinstance(pos, Q_)
-    # we verify its unity is um
-    assert str(pos.units) == 'micrometer'
-
-@pytest.mark.parametrize("channel", CHANNELS)
-def test_move_to_ref(smaractascii, channel):
-    smaractascii.channels[channel].move_to_ref()
-
-@pytest.mark.parametrize("channel", CHANNELS)
-def test_move_abs(smaractascii,channel):
-    target_pos = Q_(500, 'um')
-    smaractascii.channels[channel].move_abs(target_pos)
-
-@pytest.mark.parametrize("channel", CHANNELS)
-def test_move_rel(smaractascii, channel):
-    target_pos = Q_(500, 'um')
-    smaractascii.channels[channel].move_rel(target_pos)
-
-@pytest.mark.parametrize("channel", CHANNELS)
-def test_read_status(smaractascii, channel):
-    status=smaractascii.channels[channel].ask(f":M{channel}")
-    assert status.startswith(f":M{channel}")
+        smaractascii.channels[channel].safe_direction = 'down'
+        assert smaractascii.channels[channel].safe_direction == 'down'
 
 
-@pytest.mark.parametrize("channel", CHANNELS)
-def test_move_steps_down(smaractascii,channel):
-    smaractascii.channels[channel].move_steps_down(1000, 500, 500)
+    @pytest.mark.parametrize("channel", CHANNELS)
+    def test_frequency_max(self, smaractascii, channel):
+        smaractascii.channels[channel].max_frequency = Q_(500, 'Hz')
+        assert smaractascii.channels[channel].max_frequency == Q_(500, 'Hz')
 
-@pytest.mark.parametrize("channel", CHANNELS)
-def test_move_steps_up(smaractascii,channel):
-    smaractascii.channels[channel].move_steps_up(1000,500,500)
+    @pytest.mark.parametrize("channel", CHANNELS)
+    def test_read_status(self,smaractascii, channel):
+        status = smaractascii.channels[channel].ask(f":M{channel}")
+        assert status.startswith(f":M{channel}")
 
-@pytest.mark.parametrize("channel", CHANNELS)
-def test_move_to_end_up(smaractascii,channel):
-    smaractascii.channels[channel].move_to_end_up()
+    @pytest.mark.parametrize("channel", CHANNELS)
+    def test_get_positioner_type(self, smaractascii,channel):
+        t = smaractascii.channels[channel].get_positioner_type()
+        assert t.startswith(f":ST{channel}T")
 
-@pytest.mark.parametrize("channel", CHANNELS)
-def test_move_to_end_down(smaractascii,channel):
-    smaractascii.channels[channel].move_to_end_down()
+class TestSCUMotion:
 
-@pytest.mark.parametrize("channel", CHANNELS)
-def test_stop(smaractascii, channel):
-    smaractascii.channels[channel].stop()
+    @pytest.mark.parametrize("channel", CHANNELS)
+    def test_get_position(self, smaractascii, channel):
+        pos = smaractascii.channels[channel].get_position()
 
+        # we verify we do recive a(Quantity)
+        assert isinstance(pos, Q_)
+        # we verify its unity is um
+        assert str(pos.units) == 'micrometer'
 
-def test_ref_and_move_absolute_sequence(smaractascii,channel):
-    """
-    Integration test validating the absolute movement sequence:
-    1. Go to ref.
-    2. Verify the position is read as 0 um.
-    3. Move absolute to 500 um.
-    4. Verify the final position is 500 um.
-    """
-    smaractascii.channels[channel].move_to_ref()
-    status = smaractascii.ask(f":M{channel}")
-    while status == f":M{channel}R":
-        time.sleep(1.0)
-        status = smaractascii.ask(f":M{channel}")
-    initial_pos = smaractascii.channel0.get_position()
-    assert initial_pos.magnitude == pytest.approx(0.0, abs=5), \
-        f"Expected 0 um after zeroing, got {initial_pos}"
-    target_pos = Q_(500, 'um')
-    smaractascii.channel0.move_abs(target_pos)
-    status = smaractascii.ask(f":M{channel}")
-    while status == f":M{channel}T":
-        time.sleep(1.0)
-        status = smaractascii.ask(f":M{channel}")
-
-    final_pos = smaractascii.channel0.get_position()
-    assert final_pos.magnitude== pytest.approx(500.0, abs=2), \
-        f"Expected 500 um, got {final_pos}"
-
-@pytest.mark.parametrize("channel", CHANNELS)
-def test_zero_and_move_rel_sequence(smaractascii,channel):
-        """
-        Integration test validating the movement sequence:
-        0. Get to ref.
-        1. Verify the position is read as 0 um.
-        2. Move rel to 500 .
-        3. Verify the final position is 500 um.
-        """
+    @pytest.mark.parametrize("channel", CHANNELS)
+    def test_move_to_ref(self,smaractascii, channel):
         smaractascii.channels[channel].move_to_ref()
-        status = smaractascii.ask(f":M{channel}")
-        while status == f":M{channel}R":
-            time.sleep(1.0)
-            status = smaractascii.ask(f":M{channel}")
-        initial_pos = smaractascii.channels[channel].get_position()
-        assert initial_pos.magnitude == pytest.approx(0.0, abs=0.5), \
-            f"Expected 0 um after zeroing, got {initial_pos}"
+
+    @pytest.mark.parametrize("channel", CHANNELS)
+    def test_move_abs(self,smaractascii, channel):
+        target_pos = Q_(500, 'um')
+        smaractascii.channels[channel].move_abs(target_pos)
+
+    @pytest.mark.parametrize("channel", CHANNELS)
+    def test_move_rel(slef,smaractascii, channel):
         target_pos = Q_(500, 'um')
         smaractascii.channels[channel].move_rel(target_pos)
-        status = smaractascii.ask(f":M{channel}")
-        while status == f":M{channel}T":
-            time.sleep(1.0)
-            status = smaractascii.ask(f":M{channel}")
-        final_pos = smaractascii.channels[channel].get_position()
-        assert final_pos.magnitude== pytest.approx(500.0, abs=1.0), \
-            f"Expected 500 um, got {final_pos}"
 
-@pytest.mark.parametrize("channel", CHANNELS)
-def test_move_step_sequence(smaractascii,channel):
-    """
-    Integration test validating the movement sequence:
-    0.Reset to Reference to ensure safe range of motion
-    1. Set the current position as zero (reference).
-    2. Verify the position is read as 0 um.
-    3. Move absolute to 500 um.
-    4. Verify the final position is 500 um.
+    @pytest.mark.parametrize("channel", CHANNELS)
+    def test_move_steps_down(self,smaractascii, channel):
+        smaractascii.channels[channel].move_steps_down(1000, 500, 500)
 
-    IMPORTANT : The hardware of the SmarAct SCU will be limiting the
-                precision of the movement sequence with steps. The main
-                factor that will play a role is the amplitude (dV), which
-                above ~200dV will not be as precise (above 2µm of imprecision)
+    @pytest.mark.parametrize("channel", CHANNELS)
+    def test_move_steps_up(self,smaractascii, channel):
+        smaractascii.channels[channel].move_steps_up(1000, 500, 500)
 
-    """
-    smaractascii.channels[channel].move_to_ref()
-    status = smaractascii.ask(f":M{channel}")
-    while status == f":M{channel}R":
-        time.sleep(1.0)
-        status = smaractascii.ask(f":M{channel}")
+    @pytest.mark.parametrize("channel", CHANNELS)
+    def test_move_to_end_up(self,smaractascii, channel):
+        smaractascii.channels[channel].move_to_end_up()
 
-    initial_pos=smaractascii.channels[channel].get_position()
-    assert initial_pos.magnitude == pytest.approx(0.0, abs=1.0), \
-        f"Expected 0 um after zeroing, got {initial_pos}"
+    @pytest.mark.parametrize("channel", CHANNELS)
+    def test_move_to_end_down(self,smaractascii, channel):
+        smaractascii.channels[channel].move_to_end_down()
 
-    # 2. Define Step Parameters locally
-    steps_to_move = 100
-    freq = Q_(1000, 'Hz')
-    amp = Q_(200, 'dV')  # Max amplitude
-    smaractascii.channels[channel].move_steps_up(steps_to_move, freq, amp)
-    time.sleep(2.0)
-    smaractascii.channels[channel].move_steps_down(steps_to_move,freq, amp)
+    @pytest.mark.parametrize("channel", CHANNELS)
+    def test_stop(self,smaractascii, channel):
+        smaractascii.channels[channel].stop()
 
-    time.sleep(2.0)
+###BAUDRATE TEST###
+    @pytest.mark.parametrize("channel", CHANNELS)
+    @pytest.mark.parametrize("test_value, expected_outcome", [
+        (9600, 9600),
+        (1500, pyvisa.VisaIOError),
+    ])
+    def test_baudrate(self, smaractascii, test_value, expected_outcome, channel):
+        if expected_outcome == pyvisa.VisaIOError:
+            with pytest.raises(pyvisa.VisaIOError):
+                smaractascii.set_baudrate(test_value)
+        else:
+            smaractascii.set_baudrate(test_value)
 
-    final_pos = smaractascii.channels[channel].get_position()
-    assert final_pos.magnitude == pytest.approx(0.0, abs=2.0), \
-        f"Expected 0 um, got {final_pos}"
 
-"""DANGEROUS ELEMENT WHICH IS UNSTABLE AND MAY CAUSE HARM TO THE INSTRUMENT"""
-@pytest.mark.parametrize("channel", CHANNELS)
-def test_stop_function(smaractascii, channel):
-    """Test the Emergency Stop functionality."""
-     # 1. Go to 0
-    smaractascii.channels[channel].move_to_ref()
-    time.sleep(1.0)
-
-     # 2. Set slow speed to give us time to stop
-    smaractascii.channels[channel].frequency_max = Q_(500, 'Hz')
-
-         # 3. Order a long move (5000 um)
-    smaractascii.channels[channel].move_abs(Q_(5000, 'um'))
-
-     # 4. Wait briefly then STOP
-    time.sleep(1)
-    smaractascii.channels[channel].stop()
-
-    final_pos = smaractascii.channels[channel].get_position()
-    print(f"Stopped at {final_pos}")
-    assert final_pos.magnitude < 4000.0
-
-     # Cleanup: Restore speed
-    smaractascii.channels[channel].frequency_max = Q_(1000, 'Hz')
-
-@pytest.mark.parametrize("channel", CHANNELS)
-def test_move_to_end(smaractascii,channel):
-    smaractascii.channels[channel].move_to_end_up()
-    status = smaractascii.ask(f":M{channel}")
-    while status==f"M{channel}T" :
-        time.sleep(1.0)
-        status = smaractascii.ask(f":M{channel}")
-
-    smaractascii.channels[channel].move_to_end_down()
-
-#pytest test_scu_ascii_with_device.py --device-address "ASRL3::INSTR" -s
 
 
