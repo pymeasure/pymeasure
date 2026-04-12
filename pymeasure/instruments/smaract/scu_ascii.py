fc769de74671017d028aa839dd71101bfca20870
WiktorJJ <jankowski@insa-toulouse.fr>
2026-04-12 18:37:32 +0200

    fixing the code and increasing the percentage of tested code (codecov)

--- pymeasure/instruments/smaract/scu_ascii.py
+++ pymeasure/instruments/smaract/scu_ascii.py
@@ -392,21 +392,3 @@ class SmarActSCUAngular(SmarActSCU_ASCII):
     channel0 = Instrument.ChannelCreator(SCUChannelAngular, "0")
     channel1 = Instrument.ChannelCreator(SCUChannelAngular, "1")
     channel2 = Instrument.ChannelCreator(SCUChannelAngular, "2")
-
-
-# if __name__ == "__main__":
-#     inst = SmarActSCULinear('ASRL3::INSTR')
-#     #inst.baudrate = 9600
-#     pass
-#
-#     #import pyvisa
-#     # rm = pyvisa.ResourceManager()
-#     #ressources = rm.list_resources()
-#     #print(ressources)
-#     #
-#     # inst = rm.open_resource(ressources[0])
-#     # inst.write_termination = '\n'
-#     # inst.read_termination = '\n'
-#     # pass
-#     #
-#     inst.close()
