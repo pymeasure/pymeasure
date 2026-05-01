a11dabe02b3d165f6d8b1b029ef9d920de39e09d
fredgillard <fred.gillard@lumentum.com>
2026-05-01 15:57:16 +0100

    Introduced the IntEnum SensorTypes to represent the four types of sensor, and refactored sensor checking in the rest of the file to make use of it. Added new measurements. Switched wavelength validation from truncated to strict. Added deprecation notices for flags and wavelength_settable, in favour of sensor_flags and is_wavelength_settable for consistency

--- pymeasure/instruments/thorlabs/thorlabspm100usb.py
+++ pymeasure/instruments/thorlabs/thorlabspm100usb.py
@@ -23,12 +23,21 @@
 #
 
 import logging
+from enum import IntEnum
+from warnings import warn
 
 from pymeasure.instruments import Instrument, SCPIUnknownMixin
-from pymeasure.instruments.validators import truncated_range
+from pymeasure.instruments.validators import strict_range
 
 log = logging.getLogger(__name__)
 log.addHandler(logging.NullHandler())
+
+
+class SensorTypes(IntEnum):
+    PHOTODIODE = 1
+    THERMOPILE = 2
+    PYROELECTRIC = 3
+    FOUR_QUADRANT_THERMOPILE = 4
 
 
 class ThorlabsPM100USB(SCPIUnknownMixin, Instrument):
@@ -36,15 +45,11 @@ class ThorlabsPM100USB(SCPIUnknownMixin, Instrument):
 
     def __init__(self, adapter, name="ThorlabsPM100USB powermeter", **kwargs):
         super().__init__(adapter, name, **kwargs)
-        self._set_flags()
+        self.configure_sensor()
 
-    wavelength_min = Instrument.measurement(
-        "SENS:CORR:WAV? MIN", "Measure minimum wavelength, in nm"
-    )
+    wavelength_min = Instrument.measurement("SENS:CORR:WAV? MIN", "Get minimum wavelength, in nm")
 
-    wavelength_max = Instrument.measurement(
-        "SENS:CORR:WAV? MAX", "Measure maximum wavelength, in nm"
-    )
+    wavelength_max = Instrument.measurement("SENS:CORR:WAV? MAX", "Get maximum wavelength, in nm")
 
     @property
     def wavelength(self):
@@ -55,25 +60,53 @@ class ThorlabsPM100USB(SCPIUnknownMixin, Instrument):
     @wavelength.setter
     def wavelength(self, value):
         """Wavelength in nm."""
-        if self.wavelength_settable:
+        if self.is_wavelength_settable:
             # Store min and max wavelength to only request them once.
             if not hasattr(self, "_wavelength_min"):
                 self._wavelength_min = self.wavelength_min
             if not hasattr(self, "_wavelength_max"):
                 self._wavelength_max = self.wavelength_max
 
-            value = truncated_range(value, [self._wavelength_min, self._wavelength_max])
+            value = strict_range(value, [self._wavelength_min, self._wavelength_max])
             self.write(f"SENSE:CORR:WAV {value}")
         else:
             raise AttributeError(f"{self.sensor_name} does not allow setting the wavelength.")
 
     @property
+    def is_power_sensor(self):
+        """Get whether the sensor can measure power,
+        i.e. is a photodiode, thermopile, or 4-quadrant thermopile sensor, bool."""
+        return self.sensor_type in {
+            SensorTypes.PHOTODIODE,
+            SensorTypes.THERMOPILE,
+            SensorTypes.FOUR_QUADRANT_THERMOPILE,
+        }
+
+    @property
     def power(self):
-        """Measure the power in W."""
+        """Measure the power in W.
+        Only supported for photodiode, thermopile, and 4-quadrant thermopile sensors,
+        raises `AttributeError` otherwise."""
         if self.is_power_sensor:
             return self.values("MEAS:POW?")[0]
         else:
             raise AttributeError(f"{self.sensor_name} is not a power sensor.")
+
+    @property
+    def power_density(self):
+        """Measure the power density in W/cm^2.
+        Only supported for photodiode, thermopile, and 4-quadrant thermopile sensors,
+        raises `AttributeError` otherwise."""
+        if self.is_power_sensor:
+            return self.values("MEAS:PDEN?")[0]
+        else:
+            raise AttributeError(f"{self.sensor_name} is not a power sensor.")
+
+    @property
+    def is_energy_sensor(self):
+        """Get whether the sensor can measure energy, i.e. is a pyroelectric sensor, bool.
+        Only supported for Pyroelectric sensors, raises `AttributeError` otherwise."""
+        return self.sensor_type in {SensorTypes.PYROELECTRIC}
 
     @property
     def energy(self):
@@ -83,8 +116,62 @@ class ThorlabsPM100USB(SCPIUnknownMixin, Instrument):
         else:
             raise AttributeError(f"{self.sensor_name} is not an energy sensor.")
 
-    def _set_flags(self):
-        """Get sensor info and write flags."""
+    @property
+    def energy_density(self):
+        """Measure the energy density in J/cm^2.
+        Only supported for pyroelectric sensors, raises `AttributeError` otherwise."""
+        if self.is_energy_sensor:
+            return self.values("MEAS:EDEN?")[0]
+        else:
+            raise AttributeError(f"{self.sensor_name} is not an energy sensor.")
+
+    @property
+    def is_current_sensor(self):
+        """Get whether the sensor can measure current, i.e. is a photodiode sensor, bool."""
+        return self.sensor_type in {SensorTypes.PHOTODIODE}
+
+    @property
+    def current(self):
+        """Measure the current in A.
+        Only supported for photodiode sensors, raises `AttributeError` otherwise."""
+        if self.is_current_sensor:
+            return self.values("MEAS:CURR?")[0]
+        else:
+            raise AttributeError(f"{self.sensor_name} is not a current sensor.")
+
+    @property
+    def is_voltage_sensor(self):
+        """Get whether the sensor can measure voltage,
+        i.e. is a pyroelectric, thermopile, or 4-quadrant thermopile sensor, bool."""
+        return self.sensor_type in {
+            SensorTypes.PYROELECTRIC,
+            SensorTypes.THERMOPILE,
+            SensorTypes.FOUR_QUADRANT_THERMOPILE,
+        }
+
+    @property
+    def voltage(self):
+        """Measure the voltage in A.
+        Only supported for photodiode sensors, raises `AttributeError` otherwise."""
+        if self.is_voltage_sensor:
+            return self.values("MEAS:VOLT?")[0]
+        else:
+            raise AttributeError(f"{self.sensor_name} is not a voltage sensor.")
+
+    @property
+    def temperature(self):
+        """Measure the temperature in degC.
+        Only supported for certain sensors, raises `AttributeError` otherwise."""
+        if self.is_temperature_sensor:
+            return self.values("MEAS:TEMP?")[0]
+        else:
+            raise AttributeError(f"{self.sensor_name} is not a temperature sensor.")
+
+    frequency = Instrument.measurement("MEAS:FREQ?", """Measure the modulation frequency, in Hz.""")
+
+    def configure_sensor(self):
+        """Get sensor info and configure the `ThorlabsPM100USB` class for the sensor.
+        Call whenever the sensor is changed."""
         response = self.values("SYST:SENSOR:IDN?")
         if response[0] == "no sensor":
             raise OSError("No sensor connected.")
@@ -95,23 +182,44 @@ class ThorlabsPM100USB(SCPIUnknownMixin, Instrument):
         self.sensor_subtype = int(response[4])
         self.sensor_flags = int(response[5])
 
-        self.is_power_sensor = self.sensor_type in {1}
-        self.is_energy_sensor = self.sensor_type in {2, 3}
-
         self.is_flags_new_format = bool(self.sensor_flags & 1 << 31)
 
-        # TODO: Add reference to documentation
+        # Interpretation of the flags differs between the PM100D and the PM100D2/PM100D3.
+        # For more information please see the documentation.
+        # PM100D: page 49 of https://media.thorlabs.com/globalassets/items/p/pm/pm1/pm100d/17654-d02.pdf?v=0116021333  # noqa
+        # PM100D2/PM100D3: SYST:SENS#:IDN? section of https://github.com/Thorlabs/Light_Analysis_Examples/tree/main/Python/Thorlabs%20PMxxx%20Power%20Meters/SCPI/commandDocu  # noqa
         if self.is_flags_new_format:
             self.is_wavelength_settable = bool(self.sensor_flags & 1 << 2)
+            self.is_temperature_sensor = bool(self.sensor_flags & 1 << 12)
         else:
             self.is_wavelength_settable = bool(self.sensor_flags & 1 << 5)
+            self.is_temperature_sensor = bool(self.sensor_flags & 1 << 8)
 
+    # For Maintaner: Is it necessary to deprecate properties that weren't exposed in the public API?
     @property
     def flags(self):
-        # TODO: Deprecate
+        """Get the sensor flags, int.
+
+        .. deprecated:: 0.16
+           Instead use `sensor_flags`
+        """
+        warn(
+            """Deprecated to use `ThorlabsPM100USB.flags`.
+            Instead use `ThorlabsPM100USB.sensor_flags`.""",
+            FutureWarning,
+        )
         return self.sensor_flags
 
     @property
     def wavelength_settable(self):
-        # TODO: Deprecate
+        """Get whether the wavelength is settable, bool.
+
+        .. deprecated:: 0.16
+           Instead use `is_wavelength_settable`
+        """
+        warn(
+            """Deprecated to use `ThorlabsPM100USB.wavelength_settable`.
+            Instead use `ThorlabsPM100USB.is_wavelength_settable`.""",
+            FutureWarning,
+        )
         return self.is_wavelength_settable
--- tests/instruments/thorlabs/test_thorlabspm100usb.py
+++ tests/instruments/thorlabs/test_thorlabspm100usb.py
@@ -20,23 +20,106 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
-#
+# clear
 
 from pymeasure.test import expected_protocol
-from pymeasure.instruments.thorlabs.thorlabspm100usb import ThorlabsPM100USB
+from pymeasure.instruments.thorlabs.thorlabspm100usb import ThorlabsPM100USB, SensorTypes
 
-PM100D_INIT_COMMS = ("SYST:SENSOR:IDN?", "S130C,210921324,21-SEP-2021,1,3,33")
-PM100D2_INIT_COMMS = ("SYST:SENSOR:IDN?", '"S130C","210921324","21-SEP-2021",1,3,2147484069')
+PM100D_S130C_INIT_COMMS = ("SYST:SENSOR:IDN?", "S130C,210921324,21-SEP-2021,1,3,33")
+PM100D2_S130C_INIT_COMMS = ("SYST:SENSOR:IDN?", '"S130C","210921324","21-SEP-2021",1,3,2147484069')
 
 
-def test_init():
-    for protocol in [PM100D_INIT_COMMS, PM100D2_INIT_COMMS]:
+def test_configure_sensor():
+    for protocol in [PM100D_S130C_INIT_COMMS, PM100D2_S130C_INIT_COMMS]:
         with expected_protocol(ThorlabsPM100USB, [protocol]) as inst:
             assert inst.sensor_name == "S130C"
             assert inst.sensor_sn == "210921324"
             assert inst.sensor_cal_msg == "21-SEP-2021"
             assert inst.sensor_type == 1
             assert inst.sensor_subtype == 3
-            assert inst.flags
+            assert type(inst.sensor_flags) is int
             assert inst.is_power_sensor
+            assert inst.is_current_sensor
             assert not inst.is_energy_sensor
+            assert not inst.is_voltage_sensor
+            assert not inst.is_temperature_sensor
+
+
+def test_wavelength_setter():
+    with expected_protocol(
+        ThorlabsPM100USB,
+        [
+            PM100D2_S130C_INIT_COMMS,
+            ("SENS:CORR:WAV? MIN", 500),
+            ("SENS:CORR:WAV? MAX", 2000),
+            ("SENSE:CORR:WAV 1500", None),
+        ],
+    ) as inst:
+        inst.wavelength = 1500
+
+
+def test_wavelength_getter():
+    with expected_protocol(
+        ThorlabsPM100USB, [PM100D2_S130C_INIT_COMMS, ("SENSE:CORR:WAV?", 1500)]
+    ) as inst:
+        assert inst.wavelength == 1500
+
+
+def test_power_getter():
+    with expected_protocol(
+        ThorlabsPM100USB, [PM100D2_S130C_INIT_COMMS, ("MEAS:POW?", 1e-3)]
+    ) as inst:
+        assert inst.power == 1e-3
+
+
+def test_power_density_getter():
+    with expected_protocol(
+        ThorlabsPM100USB, [PM100D2_S130C_INIT_COMMS, ("MEAS:PDEN?", 1e-3)]
+    ) as inst:
+        assert inst.power_density == 1e-3
+
+
+def test_energy_getter():
+    with expected_protocol(
+        ThorlabsPM100USB, [PM100D2_S130C_INIT_COMMS, ("MEAS:ENER?", 1e-3)]
+    ) as inst:
+        inst.sensor_type = SensorTypes.PYROELECTRIC  # Override for test
+        assert inst.energy == 1e-3
+
+
+def test_energy_density_getter():
+    with expected_protocol(
+        ThorlabsPM100USB, [PM100D2_S130C_INIT_COMMS, ("MEAS:EDEN?", 1e-3)]
+    ) as inst:
+        inst.sensor_type = SensorTypes.PYROELECTRIC  # Override for test
+        assert inst.energy_density == 1e-3
+
+
+def test_current_getter():
+    with expected_protocol(
+        ThorlabsPM100USB, [PM100D2_S130C_INIT_COMMS, ("MEAS:CURR?", 1e-3)]
+    ) as inst:
+        assert inst.current == 1e-3
+
+
+def test_voltage_getter():
+    with expected_protocol(
+        ThorlabsPM100USB, [PM100D2_S130C_INIT_COMMS, ("MEAS:VOLT?", 1e-3)]
+    ) as inst:
+        inst.sensor_type = SensorTypes.PYROELECTRIC  # Override for test
+        assert inst.voltage == 1e-3
+
+
+def test_temperature_getter():
+    with expected_protocol(
+        ThorlabsPM100USB, [PM100D2_S130C_INIT_COMMS, ("MEAS:TEMP?", 1e-3)]
+    ) as inst:
+        inst.is_temperature_sensor = True  # Override for test
+        assert inst.temperature == 1e-3
+
+
+def test_frequency_getter():
+    with expected_protocol(
+        ThorlabsPM100USB, [PM100D2_S130C_INIT_COMMS, ("MEAS:FREQ?", 100)]
+    ) as inst:
+        assert inst.frequency == 100
