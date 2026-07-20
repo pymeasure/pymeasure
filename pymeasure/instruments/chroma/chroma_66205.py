424b0166c3ef6a55072df4af9352575b93bd59d9
Sam <sjgallagher2@gmail.com>
2026-07-20 09:45:18 -0400

    Update formatting for flake8

--- pymeasure/instruments/chroma/chroma_66205.py
+++ pymeasure/instruments/chroma/chroma_66205.py
@@ -28,7 +28,7 @@ import time
 
 from pymeasure.instruments import Instrument
 from pymeasure.instruments.generic_types import SCPIMixin
-from pymeasure.instruments.validators import strict_discrete_set,truncated_range
+from pymeasure.instruments.validators import strict_discrete_set, truncated_range
 
 
 class Chroma66205(SCPIMixin, Instrument):
@@ -39,53 +39,74 @@ class Chroma66205(SCPIMixin, Instrument):
     with programming information in English is available.
     """
 
-    class Measure(str,Enum):
-        VOLTAGE="V"
-        DC_VOLTAGE="VDC"
-        MEAN_VOLTAGE="VMEAN"
-        CURRENT="I"
-        DC_CURRENT="IDC"
-        REAL_POWER="W"
-        APPARENT_POWER="VA"
-        PEAK_POS_VOLTAGE="VPK+"
-        PEAK_POS_CURRENT="IPK+"
-        PEAK_NEG_VOLTAGE="VPK-"
-        PEAK_NEG_CURRENT="IPK-"
-        POWER_FACTOR="PF"
-        PHASE="DEG"
-        THD_VOLTAGE="THDV"
-        THD_CURRENT="THDI"
-        INRUSH_CURRENT="IS"
-        REACTIVE_POWER="VAR"
-        VOLTAGE_CREST_FACTOR="CFV"
-        CURRENT_CREST_FACTOR="CFI"
-        CHARGE_AH="AH"
-        ENERGY_WH="WH"
-        VOLTAGE_FREQUENCY="VHZ"
-        CURRENT_FREQUENCY="IHZ"
+    class Measure(str, Enum):
+        VOLTAGE = "V"
+        DC_VOLTAGE = "VDC"
+        MEAN_VOLTAGE = "VMEAN"
+        CURRENT = "I"
+        DC_CURRENT = "IDC"
+        REAL_POWER = "W"
+        APPARENT_POWER = "VA"
+        PEAK_POS_VOLTAGE = "VPK+"
+        PEAK_POS_CURRENT = "IPK+"
+        PEAK_NEG_VOLTAGE = "VPK-"
+        PEAK_NEG_CURRENT = "IPK-"
+        POWER_FACTOR = "PF"
+        PHASE = "DEG"
+        THD_VOLTAGE = "THDV"
+        THD_CURRENT = "THDI"
+        INRUSH_CURRENT = "IS"
+        REACTIVE_POWER = "VAR"
+        VOLTAGE_CREST_FACTOR = "CFV"
+        CURRENT_CREST_FACTOR = "CFI"
+        CHARGE_AH = "AH"
+        ENERGY_WH = "WH"
+        VOLTAGE_FREQUENCY = "VHZ"
+        CURRENT_FREQUENCY = "IHZ"
 
-    CHANNEL_A_DISPLAY_OPTS = [Measure.VOLTAGE,Measure.CURRENT,Measure.REAL_POWER,
-                              Measure.APPARENT_POWER,Measure.PEAK_POS_VOLTAGE,
-                              Measure.PEAK_NEG_VOLTAGE,Measure.PEAK_POS_CURRENT,
-                              Measure.PEAK_NEG_CURRENT]
-    CHANNEL_B_DISPLAY_OPTS = [Measure.VOLTAGE,Measure.CURRENT,Measure.REAL_POWER,
-                              Measure.POWER_FACTOR,Measure.PHASE,Measure.THD_VOLTAGE,
-                              Measure.THD_CURRENT,Measure.INRUSH_CURRENT]
-    CHANNEL_C_DISPLAY_OPTS = [Measure.VOLTAGE,Measure.CURRENT,Measure.REAL_POWER,
-                              Measure.REACTIVE_POWER,Measure.VOLTAGE_CREST_FACTOR,
-                              Measure.CURRENT_CREST_FACTOR,Measure.CHARGE_AH,
-                              Measure.ENERGY_WH]
-    CHANNEL_D_DISPLAY_OPTS = [Measure.VOLTAGE,Measure.CURRENT,Measure.REAL_POWER,
-                              Measure.POWER_FACTOR,Measure.VOLTAGE_FREQUENCY,
-                              Measure.CURRENT_FREQUENCY,Measure.THD_VOLTAGE,
-                              Measure.THD_CURRENT]
+    CHANNEL_A_DISPLAY_OPTS = [
+        Measure.VOLTAGE,
+        Measure.CURRENT,
+        Measure.REAL_POWER,
+        Measure.APPARENT_POWER,
+        Measure.PEAK_POS_VOLTAGE,
+        Measure.PEAK_NEG_VOLTAGE,
+        Measure.PEAK_POS_CURRENT,
+        Measure.PEAK_NEG_CURRENT,
+    ]
+    CHANNEL_B_DISPLAY_OPTS = [
+        Measure.VOLTAGE,
+        Measure.CURRENT,
+        Measure.REAL_POWER,
+        Measure.POWER_FACTOR,
+        Measure.PHASE,
+        Measure.THD_VOLTAGE,
+        Measure.THD_CURRENT,
+        Measure.INRUSH_CURRENT,
+    ]
+    CHANNEL_C_DISPLAY_OPTS = [
+        Measure.VOLTAGE,
+        Measure.CURRENT,
+        Measure.REAL_POWER,
+        Measure.REACTIVE_POWER,
+        Measure.VOLTAGE_CREST_FACTOR,
+        Measure.CURRENT_CREST_FACTOR,
+        Measure.CHARGE_AH,
+        Measure.ENERGY_WH,
+    ]
+    CHANNEL_D_DISPLAY_OPTS = [
+        Measure.VOLTAGE,
+        Measure.CURRENT,
+        Measure.REAL_POWER,
+        Measure.POWER_FACTOR,
+        Measure.VOLTAGE_FREQUENCY,
+        Measure.CURRENT_FREQUENCY,
+        Measure.THD_VOLTAGE,
+        Measure.THD_CURRENT,
+    ]
 
     def __init__(self, adapter, name="Chroma 66205", **kwargs):
-        super().__init__(
-            adapter,
-            name,
-            **kwargs
-        )
+        super().__init__(adapter, name, **kwargs)
 
     system_error = Instrument.measurement(
         "SYSTEM:ERROR?",
@@ -99,7 +120,7 @@ class Chroma66205(SCPIMixin, Instrument):
         "CONF:MEAS:MODE %s",
         """Control measurement mode. Can be RMS, DC, or VMEAN.""",
         validator=strict_discrete_set,
-        values=["RMS","DC","VMEAN"],
+        values=["RMS", "DC", "VMEAN"],
         cast=str,
     )
 
@@ -109,7 +130,7 @@ class Chroma66205(SCPIMixin, Instrument):
         """Set the voltage range for measurement. Can be AUTO, V15, V30, V60, V150, V300, or
         V600.""",
         validator=strict_discrete_set,
-        values=["AUTO","V600","V300","V150","V60","V30","V15"],
+        values=["AUTO", "V600", "V300", "V150", "V60", "V30", "V15"],
         cast=str,
     )
 
@@ -120,8 +141,24 @@ class Chroma66205(SCPIMixin, Instrument):
         A02, A005, A002, or A0005 when external shunt is OFF, or AUTO, E015, E01, E005, E0025, or
         E001 when external shunt is ON.""",
         validator=strict_discrete_set,
-        values=["AUTO", "A30", "A20", "A5", "A2", "A05", 'A03', "A02", "A005", "A002", "A0005",
-                "E015","E01","E005","E0025","E001"],
+        values=[
+            "AUTO",
+            "A30",
+            "A20",
+            "A5",
+            "A2",
+            "A05",
+            "A03",
+            "A02",
+            "A005",
+            "A002",
+            "A0005",
+            "E015",
+            "E01",
+            "E005",
+            "E0025",
+            "E001",
+        ],
         cast=str,
     )
 
@@ -131,7 +168,7 @@ class Chroma66205(SCPIMixin, Instrument):
         """Control number of measurements to average in averaging mode. Must be a power of 2
         between 1 and 64 inclusive.""",
         validator=strict_discrete_set,
-        values=[1,2,4,8,16,32,64],
+        values=[1, 2, 4, 8, 16, 32, 64],
     )
 
     update_time = Instrument.control(
@@ -140,7 +177,7 @@ class Chroma66205(SCPIMixin, Instrument):
         """Set the time of measurement in seconds over which the data calculation is to
         be performed. Can be 0.05, 0.1, 0.25, 0.5, 1, 2, 5, or 10.""",
         validator=strict_discrete_set,
-        values=[0.05,0.1,0.25,0.5,1,2,5,10],
+        values=[0.05, 0.1, 0.25, 0.5, 1, 2, 5, 10],
     )
 
     ct_enabled = Instrument.control(
@@ -148,7 +185,7 @@ class Chroma66205(SCPIMixin, Instrument):
         "CONF:INP:CT %s",
         """Control current transformer (CT) function, ON (True) or OFF (False).""",
         validator=strict_discrete_set,
-        values={True:"ON",False:"OFF"},
+        values={True: "ON", False: "OFF"},
         map_values=True,
         cast=str,
     )
@@ -158,7 +195,7 @@ class Chroma66205(SCPIMixin, Instrument):
         "CONF:INP:CT:RAT %f",
         """Set the CT ratio, from 1.0 to 9999.9 with 0.1 resolution.""",
         validator=truncated_range,
-        values=[1,9999.9],
+        values=[1, 9999.9],
         set_process=lambda v: round(v, 1),
     )
 
@@ -167,7 +204,7 @@ class Chroma66205(SCPIMixin, Instrument):
         "CONF:INP:HV %s",
         """Control the HV function ON (True) or OFF (False).""",
         validator=strict_discrete_set,
-        values={True:"ON",False:"OFF"},
+        values={True: "ON", False: "OFF"},
         map_values=True,
         cast=str,
     )
@@ -177,7 +214,7 @@ class Chroma66205(SCPIMixin, Instrument):
         "CONF:INP:SHUN %s",
         """Control the external input shunt resistance ON (True) or OFF (False).""",
         validator=strict_discrete_set,
-        values={True:"ON",False:"OFF"},
+        values={True: "ON", False: "OFF"},
         map_values=True,
         cast=str,
     )
@@ -187,8 +224,8 @@ class Chroma66205(SCPIMixin, Instrument):
         "CONF:INP:SHUNT:RES %f",
         """Set the external input shunt resistance. Can be between 0.0000001 and 99.9999999.""",
         validator=truncated_range,
-        values=[0.0000001,99.9999999],
-        set_process=lambda v:round(v,7)
+        values=[0.0000001, 99.9999999],
+        set_process=lambda v: round(v, 7),
     )
 
     null_measure_current = Instrument.control(
@@ -197,7 +234,7 @@ class Chroma66205(SCPIMixin, Instrument):
         """Control null measurements function for measuring current, enabled (True) or disabled
         (False).""",
         validator=strict_discrete_set,
-        values={True:"ON",False:"OFF"},
+        values={True: "ON", False: "OFF"},
         map_values=True,
         cast=str,
     )
@@ -207,7 +244,7 @@ class Chroma66205(SCPIMixin, Instrument):
         "CONF:HOLD %s",
         """Control the hold function, ON (True) or OFF (False).""",
         validator=strict_discrete_set,
-        values={True:"ON",False:"OFF"},
+        values={True: "ON", False: "OFF"},
         map_values=True,
         cast=str,
     )
@@ -217,7 +254,7 @@ class Chroma66205(SCPIMixin, Instrument):
         "CONF:HOLD:MODE %s",
         """Control the hold function mode. Can be STOP, MAX, or MIN.""",
         validator=strict_discrete_set,
-        values=["STOP","MAX","MIN"],
+        values=["STOP", "MAX", "MIN"],
         cast=str,
     )
 
@@ -226,7 +263,7 @@ class Chroma66205(SCPIMixin, Instrument):
         "CONF:HOLD:TIME %d",
         """Control the time of the hold function in seconds, from 0s to 9999s.""",
         validator=truncated_range,
-        values=[0,9999],
+        values=[0, 9999],
     )
 
     window_time = Instrument.control(
@@ -234,7 +271,7 @@ class Chroma66205(SCPIMixin, Instrument):
         "CONF:MEAS:WINDOW %f",
         """Control window time, from 0.1s to 60.0s in 0.1s increments.""",
         validator=truncated_range,
-        values = [0.1,60.],
+        values=[0.1, 60.0],
         set_process=lambda v: round(v, 1),
     )
 
@@ -243,7 +280,7 @@ class Chroma66205(SCPIMixin, Instrument):
         "CONF:MEAS:WINDOW:UPDATE %s",
         """Set fixed or varied interval according to window time. Can be FIXED or WINDOW.""",
         validator=strict_discrete_set,
-        values=["FIXED","WINDOW"],
+        values=["FIXED", "WINDOW"],
         cast=str,
     )
 
@@ -252,7 +289,7 @@ class Chroma66205(SCPIMixin, Instrument):
         "CONF:INTEGRATE %s",
         """Control meter integration ON or OFF.""",
         validator=strict_discrete_set,
-        values={True:"ON",False:"OFF"},
+        values={True: "ON", False: "OFF"},
         map_values=True,
         cast=str,
     )
@@ -262,7 +299,7 @@ class Chroma66205(SCPIMixin, Instrument):
         "CONF:INTEG:MODE %s",
         """Set the mode of execution in integration mode. Can be NORMAL or CONTINUE.""",
         validator=strict_discrete_set,
-        values=["NORMAL","CONTINUE"],
+        values=["NORMAL", "CONTINUE"],
         cast=str,
     )
 
@@ -271,7 +308,7 @@ class Chroma66205(SCPIMixin, Instrument):
         "CONF:INTEGRATE:TIME %d",
         """Set integration time in seconds, between 0s and 35,999,999s.""",
         validator=truncated_range,
-        values=[0,35999999],
+        values=[0, 35999999],
     )
 
     filter = Instrument.control(
@@ -280,7 +317,7 @@ class Chroma66205(SCPIMixin, Instrument):
         """Set the bandwidth of the digital low pass filter on input signal path.
         Can be OFF, BW500, or BW5500.""",
         validator=strict_discrete_set,
-        values=["OFF","BW500","BW5500"],
+        values=["OFF", "BW500", "BW5500"],
         cast=str,
     )
 
@@ -290,7 +327,7 @@ class Chroma66205(SCPIMixin, Instrument):
         """Control the low pass filter on the path of frequency detect, True (ON) or
         False (OFF).""",
         validator=strict_discrete_set,
-        values={True:"ON",False:"OFF"},
+        values={True: "ON", False: "OFF"},
         map_values=True,
         cast=str,
     )
@@ -300,7 +337,7 @@ class Chroma66205(SCPIMixin, Instrument):
         "CONF:ENERGY:TIME %d",
         """Set energy measurement time in seconds, from 0s to 35,999,999s.""",
         validator=truncated_range,
-        values=[0,35999999]
+        values=[0, 35999999],
     )
 
     # DISPLAY #
@@ -321,10 +358,10 @@ class Chroma66205(SCPIMixin, Instrument):
         +---------+---------------------------------------+
         | D       | V, I, W, PF, VHZ, IHZ, THDV, THDI     |
         +---------+---------------------------------------+
-        """
+        """,
     )
 
-    def configure_display(self,A: Measure, B: Measure, C: Measure, D:Measure):
+    def configure_display(self, A: Measure, B: Measure, C: Measure, D: Measure):
         """Control the measurements shown on the four displays.
 
         +---------+---------------------------------------+
@@ -340,18 +377,26 @@ class Chroma66205(SCPIMixin, Instrument):
         +---------+---------------------------------------+
         """
         if A not in self.CHANNEL_A_DISPLAY_OPTS:
-            raise ValueError(f"Parameter {A} not valid for display A. Must be "
-                             f"one of: {self.CHANNEL_A_DISPLAY_OPTS}")
+            raise ValueError(
+                f"Parameter {A} not valid for display A. Must be "
+                f"one of: {self.CHANNEL_A_DISPLAY_OPTS}"
+            )
         if B not in self.CHANNEL_B_DISPLAY_OPTS:
-            raise ValueError(f"Parameter {B} not valid for display B. Must be "
-                             f"one of: {self.CHANNEL_B_DISPLAY_OPTS}")
+            raise ValueError(
+                f"Parameter {B} not valid for display B. Must be "
+                f"one of: {self.CHANNEL_B_DISPLAY_OPTS}"
+            )
         if C not in self.CHANNEL_C_DISPLAY_OPTS:
-            raise ValueError(f"Parameter {C} not valid for display C. Must be "
-                             f"one of: {self.CHANNEL_C_DISPLAY_OPTS}")
+            raise ValueError(
+                f"Parameter {C} not valid for display C. Must be "
+                f"one of: {self.CHANNEL_C_DISPLAY_OPTS}"
+            )
         if D not in self.CHANNEL_D_DISPLAY_OPTS:
-            raise ValueError(f"Parameter {D} not valid for display D. Must be "
-                             f"one of: {self.CHANNEL_D_DISPLAY_OPTS}")
-        self.show = f'{A.value}, {B.value}, {C.value}, {D.value}'
+            raise ValueError(
+                f"Parameter {D} not valid for display D. Must be "
+                f"one of: {self.CHANNEL_D_DISPLAY_OPTS}"
+            )
+        self.show = f"{A.value}, {B.value}, {C.value}, {D.value}"
 
     # MEASUREMENTS #
     voltage_rms = Instrument.measurement(
@@ -475,7 +520,7 @@ class Chroma66205(SCPIMixin, Instrument):
         """Set which mode will be triggered by trigger command.
         Can be NONE|INTEGRATION|INRUSH|LIMIT.""",
         validator=strict_discrete_set,
-        values=["NONE","INTEGRATION","INRUSH","LIMIT"],
+        values=["NONE", "INTEGRATION", "INRUSH", "LIMIT"],
         cast=str,
     )
 
@@ -492,7 +537,7 @@ class Chroma66205(SCPIMixin, Instrument):
         """Get a measurement."""
         return float(self.ask(f"MEAS? {param.value}").strip())
 
-    def integrate_energy(self, integration_time_s: float=10.):
+    def integrate_energy(self, integration_time_s: float = 10.0):
         # Set up
         self.energy_time = integration_time_s
         self.trigger_mode = "INTEGRATION"
@@ -505,33 +550,33 @@ class Chroma66205(SCPIMixin, Instrument):
         if tr != "RUNNING":
             print(f"Warning: Triggered but trigger is not set to 'RUNNING' (instead got {tr})")
         else:
-            timeout_duration = integration_time_s*1.5  # use 1.5x expected integration time
+            timeout_duration = integration_time_s * 1.5  # use 1.5x expected integration time
             start_time = time.monotonic()
             while self.trigger == "RUNNING":
                 if time.monotonic() - start_time > timeout_duration:
-                    raise TimeoutError(f"Integration was expected to take {integration_time_s} "
-                                       f"secs, but took longer than {timeout_duration} secs.")
+                    raise TimeoutError(
+                        f"Integration was expected to take {integration_time_s} "
+                        f"secs, but took longer than {timeout_duration} secs."
+                    )
         return self.energy_wh
 
-    def capture_waveform(self, param:str='V'):
+    def capture_waveform(self, param: str = "V"):
         """Get waveform as ts,wave for voltage (V) or current (I)."""
-        if param not in ['I','V']:
+        if param not in ["I", "V"]:
             raise ValueError(f"Unexpected parameter {param}. Must be 'V' or 'I'.")
         timeout = 100
-        while self.ask('WAVEFORM:CAPTURE?') != 'OK\n' and timeout > 0:
+        while self.ask("WAVEFORM:CAPTURE?") != "OK\n" and timeout > 0:
             timeout -= 1
-        if timeout  <= 0:
+        if timeout <= 0:
             raise TimeoutError("Waveform capture timeout.")
         print("Receiving waveform. This may take a minute...")
-        bvals = self.binary_values(f'WAVEFORM:DATA? {param}',dtype=np.uint8)
+        bvals = self.binary_values(f"WAVEFORM:DATA? {param}", dtype=np.uint8)
         print("Done.")
-        bvals = bvals[len('#48192'):-1]  # strip
+        bvals = bvals[len("#48192"):-1]  # strip
         wave = bvals.view(np.float32)
 
         # Wave contains exactly two 60Hz cycles, so sample rate is 61440 (=2048 Samples / 2 * 60Hz)
         # This might not be consistent across all settings and configurations?
-        Fs=61.44e3
-        ts = np.arange(0,len(wave)/Fs, 1/Fs)
-        return ts,wave
-
-
+        Fs = 61.44e3
+        ts = np.arange(0, len(wave) / Fs, 1 / Fs)
+        return ts, wave
