d8d0f0e53e3ebfd78bec3ea4e7fb72da129da672
S. C. Chiu <jerry31205@gmail.com>
2026-07-23 23:25:28 +0800

    Add IEEE4882Mixin to __init__.py

--- pymeasure/instruments/__init__.py
+++ pymeasure/instruments/__init__.py
@@ -26,4 +26,4 @@ from .channel import Channel
 from .instrument import AdapterType, Instrument
 from .common_base import cast_or_str, identity, IdType, InstrumentProperty
 from .resources import find_serial_port, list_resources
-from .generic_types import SCPIMixin, SCPIUnknownMixin
+from .generic_types import IEEE4882Mixin, SCPIMixin, SCPIUnknownMixin
