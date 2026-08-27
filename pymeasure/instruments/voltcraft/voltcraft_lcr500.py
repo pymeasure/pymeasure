66b3da5bf3f09275ff91650d5cd345a053cf9310
cckssr <cedric.kessler@me.com>
2026-08-27 14:52:26 +0200

    fix: typing, copyright, docstring style

--- pymeasure/instruments/voltcraft/voltcraft_lcr500.py
+++ pymeasure/instruments/voltcraft/voltcraft_lcr500.py
@@ -22,7 +22,10 @@
 # THE SOFTWARE.
 #
 
+from typing import Literal, cast
+
 from pymeasure.instruments import Instrument, SCPIMixin
+from pymeasure.adapters import Adapter
 from pymeasure.instruments.validators import (
     strict_discrete_set,
 )
@@ -44,6 +47,23 @@ _RESIS_OHM = ["auto", 10, 100, 1_000, 10_000, 100_000]  # Allowed resistance ran
 _LEVELS_MVRMS = [300, 600]  # Allowed levels in mVrms
 _IMPA = ["r", "l", "c", "z", "auto"]  # Allowed main measurement parameters
 _IMPB = ["x", "q", "d", "theta", "esr"]  # Allowed secondary measurement parameters
+
+MeasurementFrequency = Literal[
+    100,
+    120,
+    400,
+    1_000,
+    4_000,
+    10_000,
+    40_000,
+    50_000,
+    75_000,
+    100_000,
+]
+MeasurementRanges = Literal["auto", 10, 100, 1_000, 10_000, 100_000]
+SignalLevels = Literal[300, 600]
+MainParameters = Literal["r", "l", "c", "z", "auto"]
+SecondaryParameters = Literal["x", "q", "d", "theta", "esr"]
 
 
 class LCR500(SCPIMixin, Instrument):
@@ -85,9 +105,16 @@ class LCR500(SCPIMixin, Instrument):
 
         primary, secondary, range_used = lcr.fetch()
         print(f"Capacitance: {primary} F, Q: {secondary}")
+
+    .. warning::
+        This instrument uses different write and read terminators.
+        The write and read terminators are each ``\\n``. A mismatch can lead to
+        communication issues when sending commands, as responses may not be
+        properly terminated or recognized. If a custom adapter is used,
+        ensure that these are set correctly.
     """
 
-    def __init__(self, adapter, name="Voltcraft LCR-500", **kwargs):
+    def __init__(self, adapter: Adapter | str, name: str = "Voltcraft LCR-500", **kwargs):
         super().__init__(adapter, name, write_termination="\n", read_termination="\n", **kwargs)
 
     go_to_local = Instrument.setting(
@@ -95,107 +122,128 @@ class LCR500(SCPIMixin, Instrument):
     )
 
     # -- Frequency Subsystem --
-    frequency = Instrument.control(
-        "FREQ?",
-        "FREQ %d",
-        """Control the measurement frequency in Hz.
+    frequency: MeasurementFrequency = cast(
+        MeasurementFrequency,
+        Instrument.control(
+            "FREQ?",
+            "FREQ %d",
+            """Control the measurement frequency in Hz.
 
-        :type: int
+            :type: int
 
-        Allowed values: 100, 120, 400, 1000, 4000, 10000, 40000, 50000,
-        75000, 100000.
-        """,
-        validator=strict_discrete_set,
-        values=_FREQS_HZ,
+            Allowed values: 100, 120, 400, 1000, 4000, 10000, 40000, 50000,
+            75000, 100000.
+            """,
+            validator=strict_discrete_set,
+            values=_FREQS_HZ,
+        ),
     )
 
     # -- Function Subsystem --
-    main_parameter = Instrument.control(
-        "FUNC:IMPA?",
-        "FUNC:IMPA %s",
-        """Control the main measurement parameter.
+    main_parameter: MainParameters = cast(
+        MainParameters,
+        Instrument.control(
+            "FUNC:IMPA?",
+            "FUNC:IMPA %s",
+            """Control the main measurement parameter.
 
-        :type: str
+            :type: str
 
-        Allowed values:
+            Allowed values:
 
-        * ``'r'``: Resistance
-        * ``'l'``: Inductance
-        * ``'c'``: Capacitance
-        * ``'z'``: Impedance
-        * ``'auto'``: Automatic
-        """,
-        validator=strict_discrete_set,
-        values=_IMPA,
-        get_process=lambda v: v.split("-")[-1],
+            * ``'r'``: Resistance
+            * ``'l'``: Inductance
+            * ``'c'``: Capacitance
+            * ``'z'``: Impedance
+            * ``'auto'``: Automatic
+            """,
+            validator=strict_discrete_set,
+            values=_IMPA,
+            get_process=lambda v: v.split("-")[-1],
+        ),
     )
 
-    secondary_parameter = Instrument.control(
-        "FUNC:IMPB?",
-        "FUNC:IMPB %s",
-        """Control the secondary measurement parameter.
+    secondary_parameter: SecondaryParameters = cast(
+        SecondaryParameters,
+        Instrument.control(
+            "FUNC:IMPB?",
+            "FUNC:IMPB %s",
+            """Control the secondary measurement parameter.
 
-        :type: str
+            :type: str
 
-        Allowed values:
+            Allowed values:
 
-        * ``'x'``: Reactance
-        * ``'q'``: Quality factor
-        * ``'d'``: Dissipation factor
-        * ``'theta'``: Phase angle
-        * ``'esr'``: Equivalent series resistance
-        """,
-        validator=strict_discrete_set,
-        values=_IMPB,
+            * ``'x'``: Reactance
+            * ``'q'``: Quality factor
+            * ``'d'``: Dissipation factor
+            * ``'theta'``: Phase angle
+            * ``'esr'``: Equivalent series resistance
+            """,
+            validator=strict_discrete_set,
+            values=_IMPB,
+        ),
     )
 
-    measurement_range = Instrument.control(
-        "FUNC:RANG?",
-        "FUNC:RANG %s",
-        """Control the measurement range in Ohm.
+    measurement_range: MeasurementRanges = cast(
+        MeasurementRanges,
+        Instrument.control(
+            "FUNC:RANG?",
+            "FUNC:RANG %s",
+            """Control the measurement range in Ohm.
 
-        :type: str or int
+            :type: str or int
 
-        Allowed values: ``'auto'``, 10, 100, 1000, 10000, 100000.
-        """,
-        validator=strict_discrete_set,
-        values=_RESIS_OHM,
+            Allowed values: ``'auto'``, 10, 100, 1000, 10000, 100000.
+            """,
+            validator=strict_discrete_set,
+            values=_RESIS_OHM,
+        ),
     )
 
-    level = Instrument.control(
-        "FUNC:LEV?",
-        "FUNC:LEV %d",
-        """Control the measurement signal level in mVrms.
+    level: SignalLevels = cast(
+        SignalLevels,
+        Instrument.control(
+            "FUNC:LEV?",
+            "FUNC:LEV %d",
+            """Control the measurement signal level in mVrms.
 
-        :type: int
+            :type: int
 
-        Allowed values: 300, 600.
-        """,
-        validator=strict_discrete_set,
-        values=_LEVELS_MVRMS,
+            Allowed values: 300, 600.
+            """,
+            validator=strict_discrete_set,
+            values=_LEVELS_MVRMS,
+        ),
     )
 
-    equivalent_circuit_serial_enabled = Instrument.control(
-        "FUNC:EQUI?",
-        "FUNC:EQUI %s",
-        """Control the equivalent circuit type.
+    equivalent_circuit_serial_enabled: bool = cast(
+        bool,
+        Instrument.control(
+            "FUNC:EQUI?",
+            "FUNC:EQUI %s",
+            """Control the equivalent circuit type.
+            
+            :type: bool
 
-        :type: bool
-
-        * ``True``: Series equivalent circuit
-        * ``False``: Parallel equivalent circuit
-        """,
-        validator=strict_discrete_set,
-        map_values=True,
-        values={True: "ser", False: "pal"},
+            * ``True``: Series equivalent circuit
+            * ``False``: Parallel equivalent circuit
+            """,
+            validator=strict_discrete_set,
+            map_values=True,
+            values={True: "ser", False: "pal"},
+        ),
     )
 
     # -- Fetch Subsystem --
-    fetch = Instrument.measurement(
-        "FETC?",
-        """Get a measurement result from the instrument.
+    fetch: list[float] = cast(
+        list[float],
+        Instrument.measurement(
+            "FETC?",
+            """Get a measurement result from the instrument.
 
-        :returns: A list of ``[primary_value, secondary_value, range_used]``.
-        :rtype: list[float]
-        """,
+            :returns: A list of ``[primary_value, secondary_value, range_used]``.
+            :rtype: list[float]
+            """,
+        ),
     )
